f227bfa4a941317ed188b99527aecff64dab6c7d
oleksiys <oleksiys@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-07-21 09:20:39 +0000

    + remove System.out thanks to MartinG for catching this
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@48139 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/webservices/soap-tcp/src/main/java/org/glassfish/webservices/transport/tcp/WSEndpointLifeCycleListener.java
+++ appserver/webservices/soap-tcp/src/main/java/org/glassfish/webservices/transport/tcp/WSEndpointLifeCycleListener.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,6 +40,9 @@
 
 package org.glassfish.webservices.transport.tcp;
 
+import com.sun.logging.LogDomains;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.glassfish.webservices.monitoring.Endpoint;
 import org.glassfish.webservices.monitoring.EndpointLifecycleListener;
 
@@ -47,12 +50,18 @@ import org.glassfish.webservices.monitoring.EndpointLifecycleListener;
  * @author Alexey Stashok
  */
 public final class WSEndpointLifeCycleListener implements EndpointLifecycleListener {
+    private static final Logger LOGGER = LogDomains.getLogger(
+            WSEndpointLifeCycleListener.class, LogDomains.WEBSERVICES_LOGGER);
+    
+    @Override
     public void endpointAdded(final Endpoint endpoint) {
-        System.out.println("endpointAdded: " + endpoint);
+        LOGGER.log(Level.FINE, "SOAP/TCP endpoint added: {0}", endpoint);
         AppServRegistry.getInstance().registerEndpoint(endpoint);
     }
 
+    @Override
     public void endpointRemoved(final Endpoint endpoint) {
+        LOGGER.log(Level.FINE, "SOAP/TCP endpoint removed: {0}", endpoint);
         AppServRegistry.getInstance().deregisterEndpoint(endpoint);
     }
 }
