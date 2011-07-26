50122d8ad9d5f49d94358bb4edec47fd8a20b2db
swchan2 <swchan2@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-07-26 01:08:03 +0000

    change package name in WebContainerAvailability
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@48304 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/web-glue/src/main/java/org/glassfish/web/config/serverbeans/WebContainerAvailability.java
+++ appserver/web/web-glue/src/main/java/org/glassfish/web/config/serverbeans/WebContainerAvailability.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,8 +38,9 @@
  * holder.
  */
 
-package com.sun.enterprise.config.serverbeans;
+package org.glassfish.web.config.serverbeans;
 
+import com.sun.enterprise.config.serverbeans.AvailabilityServiceExtension;
 import org.jvnet.hk2.config.Attribute;
 import org.jvnet.hk2.config.ConfigBeanProxy;
 import org.jvnet.hk2.component.Injectable;
