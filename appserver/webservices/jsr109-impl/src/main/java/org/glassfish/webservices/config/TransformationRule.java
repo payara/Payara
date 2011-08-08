fdfeef19e96139a84fa8ac3e3117bfe77b5e419d
snajper <snajper@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-08-08 07:27:54 +0000

    Fix licence header, remove unused import.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@48634 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/config/TransformationRule.java
+++ appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/config/TransformationRule.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -46,7 +46,6 @@ import org.jvnet.hk2.config.ConfigBeanProxy;
 import org.jvnet.hk2.component.Injectable;
 
 import java.beans.PropertyVetoException;
-import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
 
