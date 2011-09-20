2e8f68def51bc70c770f16c90b6dff2346900b3d
sm157516 <sm157516@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-09-20 09:43:46 +0000

    Adding create template instructions for otd.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@49741 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/tests/paas/bookstore/ovm_setup.sh
+++ appserver/tests/paas/bookstore/ovm_setup.sh
@@ -51,7 +51,6 @@ asadmin create-template-user --virtualization ovm --userid glassfish --groupid g
 asadmin create-template --files $SCRIPTS_DIR/OVM_EL5U6_X86_PVM_ORACLEDB.tgz --indexes ServiceType=Database,VirtualizationType=OVM ORACLE_DATABASE
 asadmin create-template-user --virtualization ovm --userid oracle --groupid oinstall --template ORACLE_DATABASE oracle
 
-#asadmin create-template --files /space/kshitiz/glassfish/trunk/templates/OVM_JEOS_64_PVM_OTD_LARGE.tgz --indexes ServiceType=LB,VirtualizationType=OVM otd
-#asadmin create-template-user --virtualization ovm --userid 1000 --groupid 1000 --template otd cloud
+asadmin create-template --files $SCRIPTS_DIR/OVM_JEOS_64_PVM_OTD_LARGE.tgz --indexes ServiceType=LB,VirtualizationType=OVM otd
+asadmin create-template-user --virtualization ovm --userid 1000 --groupid 1000 --template otd cloud
 #asadmin create-virtual-cluster --groupnames pool2 --template GLASSFISH_TINY --min 1 demovc
-
