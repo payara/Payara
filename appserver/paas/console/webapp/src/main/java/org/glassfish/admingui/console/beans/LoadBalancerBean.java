d2a86722c3cac3342a377a8b01b4b7c8230a21b6
srinik76 <srinik76@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-09-21 14:14:29 +0000

    Added the logic to retrieve lb values from back end
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@49807 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/paas/console/webapp/src/main/java/org/glassfish/admingui/console/beans/LoadBalancerBean.java
+++ appserver/paas/console/webapp/src/main/java/org/glassfish/admingui/console/beans/LoadBalancerBean.java
@@ -6,25 +6,52 @@
 package org.glassfish.admingui.console.beans;
 
 import javax.faces.bean.*;
-import javax.faces.context.FacesContextFactory;
-import javax.faces.*;
-import javax.faces.context.FacesContext;
+import org.glassfish.admingui.console.rest.RestUtil;
 import java.util.*;
+import org.glassfish.admingui.console.util.GuiUtil;
 
 @ManagedBean(name="loadBalancerBean")
 @ViewScoped
 public class LoadBalancerBean {
 
-    private String httpPort = "80";
-    private String sslEnabled = "Disabled";
-    private String httpsPort = "OFF";
-    
+    private String httpPort;
+    private String sslEnabled;
+    private String httpsPort;
     
     public LoadBalancerBean() {
-        FacesContextFactory factory = (FacesContextFactory)
-            FactoryFinder.getFactory(FactoryFinder.FACES_CONTEXT_FACTORY);
-        Map requestMap =
-                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        httpPort = "";
+        sslEnabled = "";
+        httpsPort = "";
+        String envName = "basic-db";
+        String appName = null;
+        String svcName = null;
+        List<Map> applications = (new EnvironmentBean(envName)).getApplications();
+        if (applications.size() > 0) {
+            appName = (String)applications.get(0).get("appName");
+            svcName = appName + "-lb";
+        }
+        Map attrs = new HashMap();
+        attrs.put("appname", appName);
+        attrs.put("id", svcName);
+        String endpoint = REST_URL+"/applications/_get-service-description";
+        try{
+            Map responseMap = RestUtil.restRequest( endpoint , attrs, "GET" , null, null, false, true);
+            Map extraPropertiesMap = (Map)((Map)responseMap.get("data")).get("extraProperties");
+            Map result;
+            if (extraPropertiesMap != null){
+                result = (Map)extraPropertiesMap.get("list");
+                if (result != null) {
+                    Map configuration = (Map) result.get("configurations");
+                    if (configuration != null) {
+                        httpPort = (String) configuration.get("http-port");
+                        sslEnabled = (String) configuration.get("ssl-enabled");
+                        httpsPort = (String) configuration.get("https-port");
+                    }
+                }
+            }
+        }catch (Exception ex){
+            GuiUtil.getLogger().severe("cannot List Services");
+        }
     }
 
     public String getHttpPort() {
@@ -38,4 +65,6 @@ public class LoadBalancerBean {
     public String getHttpsPort() {
         return httpsPort;
     }
+
+    private final static String REST_URL = "http://localhost:4848/management/domain";
 }
