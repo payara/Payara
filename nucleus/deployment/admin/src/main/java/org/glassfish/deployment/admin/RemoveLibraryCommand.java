030a72565f272e7f1c55b61e409582a9465d7b78
hzhang_jn <hzhang_jn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-10-04 17:09:51 +0000

    Add support for uninstalling user library from the library directory. User could specify the type (common, ext, app) to have the library uninstalled from the targeted library directory ($DOMAIN_ROOT/lib, $DOMAIN_ROOT/lib/ext, DOMAIN_ROOT/lib/applibs respectively). The current implementation only supports uninstalling a single library file at one time (to be symmetric with the add-library command). Also add support for listing the user libraries in the library directory. User could specify the type (common, ext, app) to list the libraries in the targeted library directory ($DOMAIN_ROOT/lib, $DOMAIN_ROOT/lib/ext, DOMAIN_ROOT/lib/applibs respectively). Ran QL, deployment dev tests.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@50097 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/RemoveLibraryCommand.java
@@ -0,0 +1,106 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.deployment.admin;
+
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.CommandLock;
+import org.glassfish.api.admin.ServerEnvironment;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.ExecuteOn;
+import org.glassfish.api.admin.RuntimeType;
+import org.glassfish.config.support.TargetType;
+import org.glassfish.config.support.CommandTarget;
+import org.jvnet.hk2.annotations.Service;
+import org.jvnet.hk2.annotations.Scoped;
+import org.jvnet.hk2.component.PerLookup;
+import org.jvnet.hk2.annotations.Inject;
+import com.sun.enterprise.util.LocalStringManagerImpl;
+import com.sun.enterprise.util.io.FileUtils;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.List;
+import java.io.File;
+
+@Service(name="remove-library")
+@Scoped(PerLookup.class)
+@CommandLock(CommandLock.LockType.NONE)
+@ExecuteOn(value={RuntimeType.DAS})
+public class RemoveLibraryCommand implements AdminCommand {
+
+    @Param(primary=true)
+    String libraryName = null;
+
+    @Param(optional=true, acceptableValues="common, ext, app")
+    String type = "common";
+
+    @Inject
+    ServerEnvironment env;
+
+    final private static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(RemoveLibraryCommand.class);    
+
+    public void execute(AdminCommandContext context) {
+        
+        final ActionReport report = context.getActionReport();
+        final Logger logger = context.getLogger();
+
+        File libDir = env.getLibPath();
+
+        if (type.equals("ext")) {
+            libDir = new File(libDir, "ext");
+        } else if (type.equals("app")) {
+            libDir = new File(libDir, "applibs");
+        }
+
+        // delete the file from the appropriate library directory
+        File libraryFile = new File(libDir, libraryName);
+        if (libraryFile.exists()) {
+            FileUtils.deleteFile(libraryFile);
+        } else {
+            String msg = localStrings.getLocalString("lfnf","Library file not found", libraryFile.getAbsolutePath());
+            logger.log(Level.WARNING, msg);
+            report.setActionExitCode(ActionReport.ExitCode.WARNING);
+            report.setMessage(msg);
+        }
+    }
+}
