1be7b42c7cf2f5ee344937d9ce3438a6b115c53b
anilam <anilam@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-10-31 17:29:58 +0000

    GLASSFISH-17539.   Allows -1 for the following timeout attributes: request-timeout-seconds, timeout-seconds and connection-upload-timeout-millis in  protocol->http tab.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@50594 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/admingui/common/src/main/resources/js/adminjsf.js
+++ appserver/admingui/common/src/main/resources/js/adminjsf.js
@@ -1184,8 +1184,9 @@ function guiValidate(reqMsg, reqInt, reqPort) {
         }
 
         if (styleClass.match("intAllowMinusOne")) {
-            if (component.value =='' || component.value == '-1')
-                return true;
+            if (component.value =='' || component.value == '-1'){
+                continue;
+            }
             if (! checkForIntValue(component.value)) {
                 component.select();
                 component.focus();
@@ -1195,8 +1196,12 @@ function guiValidate(reqMsg, reqInt, reqPort) {
 
         if (styleClass.match("intAllowMinus")) {
             var num = 0;
-            if (component.value =='')  return true;
-            if ((num + component.value) <=0) return true;
+            if (component.value =='') {
+                continue;
+            }
+            if ((num + component.value) <=0) {
+                continue;
+            }
             if (! checkForIntValue(component.value)) {
                 component.select();
                 component.focus();
