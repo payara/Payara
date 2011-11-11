2d98c4dfc879a396f3f694061db70e2ce8019d19
amyroh <amyroh@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-11-11 23:53:27 +0000

    Fix GLASSFISH-17708.  Remove duplicate jars when returning urls and reduce the number of jars processed by TldScanner.
    Ran QL and web devtests.
    
    
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@50805 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/war-util/src/main/java/org/glassfish/web/loader/WebappClassLoader.java
+++ appserver/web/war-util/src/main/java/org/glassfish/web/loader/WebappClassLoader.java
@@ -1562,18 +1562,30 @@ public class WebappClassLoader
 
         try {
 
-            URL[] urls = new URL[length];
+            ArrayList<URL> urls = new ArrayList<URL>();
             for (i = 0; i < length; i++) {
                 if (i < filesLength) {
-                    urls[i] = getURL(files[i]);
+                    urls.add(i, getURL(files[i]));
                 } else if (i < filesLength + jarFilesLength) {
-                    urls[i] = getURL(jarRealFiles[i - filesLength]);
+                    urls.add(i, getURL(jarRealFiles[i - filesLength]));
                 } else {
-                    urls[i] = external[i - filesLength - jarFilesLength];
+                    urls.add(i, external[i - filesLength - jarFilesLength]);
                 }
             }
 
-            repositoryURLs = urls;
+            // Remove duplicate jars. See IT 17708
+            Set<URL> set = new HashSet<URL>();
+            List<URL> newList = new ArrayList<URL>();
+            for (Iterator iter = urls.iterator(); iter.hasNext();) {
+                URL element = (URL)iter.next();
+                if (set.add(element)) {
+                    newList.add(element);
+                }
+            }
+            urls.clear();
+            urls.addAll(newList);
+
+            repositoryURLs = urls.toArray(new URL[urls.size()]);
 
         } catch (MalformedURLException e) {
             repositoryURLs = new URL[0];
