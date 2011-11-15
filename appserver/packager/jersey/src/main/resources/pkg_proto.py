9cf64570c2c28913c2722603c091472436edd2c8
pavel_bucek <pavel_bucek@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2011-11-15 12:24:28 +0000

    jersey 1.11-b01 integration
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@50857 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/packager/jersey/src/main/resources/pkg_proto.py
+++ appserver/packager/jersey/src/main/resources/pkg_proto.py
@@ -46,7 +46,7 @@ conf = imp.load_source("pkg_conf", "../pkg_conf.py")
 
 pkg = {
     "name"          : "jersey",
-    "version"       : "1.10,0-1.12",
+    "version"       : "1.11,0-0.2",
     "attributes"    : { "pkg.summary" : "Jersey Core, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "Jersey core runtime libraries including some 3rd party dependencies. \
