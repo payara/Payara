4c137c0128c5284f3f965db37e8a428c86483e39
sandhya_kripalani <sandhya_kripalani@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-09 10:53:20 +0000

    fixing CopyRights header
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@51964 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/tests/paas/multiple-spe-test/spetest/src/test/java/org/glassfish/tests/paas/spetest/MultipleSPETest.java
+++ appserver/tests/paas/multiple-spe-test/spetest/src/test/java/org/glassfish/tests/paas/spetest/MultipleSPETest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -77,7 +77,7 @@ import java.util.regex.Pattern;
  * @author Sandhya Kripalani
  */
 
-public class BasicJPAPaaSTest {
+public class MultipleSPETest {
 
     @Test
     public void test() throws Exception {
