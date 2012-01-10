a759a7a6d678ff78e9c07e785296b8cd9006b97e
jfialli <jfialli@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-10 15:22:36 +0000

    Integrate Shoal 1.6.17 into BG. Integrate Shoal fixes that went into Glassfish 3.1.2 into trunk.
    Fix following:
    GLASSFISH-18145
    GLASSFISH-17504 
    
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52009 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/packager/resources/pkg_conf.py
+++ appserver/packager/resources/pkg_conf.py
@@ -68,7 +68,7 @@ jsf_version="2.1.3,0-4"
 grizzly_version="2.1.7,0-0"
 metro_version="2.1,0-30"
 javahelp_version="2.0.2,0-1"
-shoal_version="1.6.15,0-0"
+shoal_version="1.6.17,0-0"
 
 #description
 glassfish_description="GlassFish Application Server"
