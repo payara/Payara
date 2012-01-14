145b2db41f69d9a3b3f3663348ec9f3509929ec7
tbeerbower <tbeerbower@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-14 01:34:42 +0000

    remove usage of deprecated Init interface
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52118 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/test/java/com/sun/enterprise/v3/server/AppServerStartupTest.java
+++ nucleus/core/kernel/src/test/java/com/sun/enterprise/v3/server/AppServerStartupTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -643,7 +643,7 @@ public class AppServerStartupTest {
     /**
      * Test {@link SystemTasks} implementation.
      */
-    public static class TestSystemTasks extends SystemTasks {
+    public static class TestSystemTasks extends SystemTasksImpl {
         @Override
         public void writePidFile() {
             // do nothing.
