e222c40624b4bdd40785ab4072af4385fb6e26b7
hzhang_jn <hzhang_jn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-19 18:31:28 +0000

    Issue 17801 (John's test case). Clean up properly for failed versioning deployment. In this test scenario, the versioning deployment failed and the roll back logic was not able to clean up the repository properly. Ran QL, deployment dev tests.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52211 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
+++ nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -329,7 +329,7 @@ public class DeployCommand extends DeployCommandParameters implements AdminComma
             boolean isDirectoryDeployed = true;
             if (!source.isDirectory()) {
                 isDirectoryDeployed = false;
-                expansionDir = new File(domain.getApplicationRoot(), name);
+                expansionDir = new File(domain.getApplicationRoot(), VersioningUtils.getRepositoryName(name));
                 path = expansionDir;
             } else {
                 // test if a version is already directory deployed from this dir
