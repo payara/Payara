ed6e1a14d48a7060963d4099876ebbe9c4665553
cf126330 <cf126330@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-24 12:19:16 +0000

    change to use named service of ManagedBeanManager contract to pass Habitat check of service name.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52264 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/InternalInterceptorBindingImpl.java
+++ appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/InternalInterceptorBindingImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -103,10 +103,8 @@ public class InternalInterceptorBindingImpl  {
 
         // Register interceptor for any managed beans
         // TODO Handle 299-enabled case
-        ManagedBeanManager managedBeanManager = services.forContract(ManagedBeanManager.class).get();
+        ManagedBeanManager managedBeanManager = services.forContract(ManagedBeanManager.class).
+                named("ManagedBeanManagerImpl").get();
         managedBeanManager.registerRuntimeInterceptor(systemInterceptor, webBundle);
-
-
     }
-
 }
