d2eeb0fd233100dbfcd84d496484c59ba5179a4e
oleksiys <oleksiys@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-01-30 16:36:11 +0000

    + introduce get method to fix findbugs warning TCP109Adapter.java:73: URF_UNREAD_FIELD: Unread field: org.glassfish.webservices.transport.tcp.TCP109Adapter.isEJB
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52343 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/webservices/soap-tcp/src/main/java/org/glassfish/webservices/transport/tcp/TCP109Adapter.java
+++ appserver/webservices/soap-tcp/src/main/java/org/glassfish/webservices/transport/tcp/TCP109Adapter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -82,6 +82,10 @@ public abstract class TCP109Adapter extends TCPAdapter {
         } finally {
             postHandle();
         }
+    }
+
+    public boolean isEJB() {
+        return isEJB;
     }
 
     protected abstract void beforeHandle();
