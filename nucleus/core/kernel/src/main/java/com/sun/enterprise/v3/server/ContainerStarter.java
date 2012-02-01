15341a7a899c7cba085584a722165d0245874131
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-02-01 01:19:22 +0000

    Remove commented/unused code
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52364 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/ContainerStarter.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/ContainerStarter.java
@@ -40,26 +40,25 @@
 
 package com.sun.enterprise.v3.server;
 
-import org.glassfish.server.ServerEnvironmentImpl;
-import com.sun.enterprise.module.*;
-import org.glassfish.internal.data.EngineInfo;
-import org.glassfish.internal.data.ContainerRegistry;
+import com.sun.enterprise.module.Module;
 import com.sun.enterprise.util.StringUtils;
 import org.glassfish.api.container.Container;
 import org.glassfish.api.container.Sniffer;
-import org.jvnet.hk2.component.ComponentException;
-import org.jvnet.hk2.component.Habitat;
-import org.jvnet.hk2.component.Inhabitant;
+import org.glassfish.internal.data.ContainerRegistry;
+import org.glassfish.internal.data.EngineInfo;
+import org.glassfish.server.ServerEnvironmentImpl;
 import org.jvnet.hk2.annotations.Inject;
 import org.jvnet.hk2.annotations.Service;
+import org.jvnet.hk2.component.Habitat;
+import org.jvnet.hk2.component.Inhabitant;
 
-import java.io.*;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -71,8 +70,6 @@ import java.util.logging.Logger;
  */
 @Service
 public class ContainerStarter {
-    @Inject
-    ModulesRegistry modulesRegistry;
 
     @Inject
     Habitat habitat;
@@ -107,7 +104,6 @@ public class ContainerStarter {
         }
 
         Module[] modules;
-//        ClassLoader containerClassLoader;
         // I do the container setup first so the code has a chance to set up
         // repositories which would allow access to the connector module.
         try {
@@ -115,11 +111,6 @@ public class ContainerStarter {
             modules = sniffer.setup(containerHome, logger);
             logger.logp(Level.INFO, "ContainerStarter", "startContainer", "Snifer {0} set up following modules: {1}",
                     new Object[]{sniffer, Arrays.toString(modules)});
-//            if (modules!=null && modules.length>0) {
-//                containerClassLoader = setContainerClassLoader(modules);
-//            } else {
-//                containerClassLoader = snifferModule.getClassLoader();
-//            }
         } catch(FileNotFoundException fnf) {
             logger.log(Level.SEVERE, fnf.getMessage());
             return null;
@@ -130,48 +121,11 @@ public class ContainerStarter {
         }
 
         // first the right container from that module.
-//        ClassLoader cl = Thread.currentThread().getContextClassLoader();
         Map<String, EngineInfo> containers = new HashMap<String, EngineInfo>();
         for (String name : sniffer.getContainersNames()) {
-
-//            try {
-//                Inhabitant<? extends Container> provider = habitat.getInhabitant(Container.class, name);
-//                if (provider==null) {
-//                    try {
-//                        Class<? extends Container> containerClass = containerClassLoader.loadClass(name).asSubclass(Container.class);
-//                        if (containerClass!=null) {
-//                            provider = habitat.getInhabitantByType(containerClass);
-//                            if (provider==null) {
-//                                logger.severe("Cannot find the container " + name + " in the habitat, is it annotated with @Service");
-//                                return null;
-//                            }
-//                        }
-//                    } catch (ClassNotFoundException e) {
-//                        logger.log(Level.SEVERE, "Exception while loading container class ", e);
-//                    }
-//
-//                    if (provider==null) {
-//                        logger.severe("Cannot find Container named " + name);
-//                        logger.severe("Cannot start " + sniffer.getModuleType() + " container");
-//                        return null;
-//                    }
-//                }
-//                Thread.currentThread().setContextClassLoader(containerClassLoader);
-//                EngineInfo info = new EngineInfo(provider, sniffer, containerClassLoader);
-//
-//                ContainerRegistry registry = habitat.getComponent(ContainerRegistry.class);
-//                registry.addContainer(name, info);
-//                containers.add(info);
-//            } catch (ComponentException e) {
-//                logger.log(Level.SEVERE, "Cannot create or inject Container", e);
-//                return null;
-//            } finally {
-//                Thread.currentThread().setContextClassLoader(cl);
-//            }
             Inhabitant<? extends Container> provider = habitat.getInhabitant(Container.class, name);
             if (provider == null) {
-                logger.severe("Cannot find Container named " + name);
-                logger.severe("Cannot start " + sniffer.getModuleType() + " container");
+                logger.severe("Cannot find Container named " + name + ", so unable to start " + sniffer.getModuleType() + " container");
                 return null;
             }
             EngineInfo info = new EngineInfo(provider, sniffer, null /* never used */);
@@ -184,83 +138,5 @@ public class ContainerStarter {
         return containers.values();
     }
 
-    /**
-     * Sets the class loader associated with a container, this class loader will be used to
-     * load any classes and resources the container provides. This will also be used to set the context
-     * class loader.
-     *
-     * @param modules array of modules that are forming the container
-     * @return a class loader capable of loading classes from these modules
-     */
-    ClassLoader setContainerClassLoader(Module[] modules) {
-        if (modules==null) {
-            throw new IllegalArgumentException("passed null module list to setContainerClassLoader");
-        }
-        if (modules.length==1) {
-            // this is quite simple, I am going to return that module's class loader
-            return modules[0].getClassLoader();
-        } else {
-            List<ModuleDefinition> defs = new ArrayList<ModuleDefinition>(modules.length);
-            for (Module module : modules) {
-                defs.add(module.getModuleDefinition());
-            }
-            return modulesRegistry.getModulesClassLoader(modules[0].getClassLoader(), defs);
-        }
-    }
 
-
-    public void explodeJar(File source, File destination) throws IOException {
-
-        JarFile jarFile = null;
-        try {
-            jarFile = new JarFile(source);
-            Enumeration<JarEntry> e = jarFile.entries();
-            while (e.hasMoreElements()) {
-                JarEntry entry = e.nextElement();
-                String fileSystemName = entry.getName().replace('/', File.separatorChar);
-                File out = new File(destination, fileSystemName);
-
-                if (entry.isDirectory()) {
-                    if (!out.mkdirs()) {
-                       logger.log(Level.INFO, "Cannot create directory " + out.getAbsolutePath());
-                    }
-                } else {
-                    InputStream is = null;
-                    FileOutputStream fos = null;
-                    try {
-                        if (!out.getParentFile().exists()) {
-                            if (!out.getParentFile().mkdirs()) {
-                                logger.log(Level.SEVERE, "Cannot create directory " + out.getParentFile());
-                            }
-                        }
-                        is = new BufferedInputStream(jarFile.getInputStream(entry));
-                        fos = new FileOutputStream(out);
-                        ReadableByteChannel inChannel = Channels.newChannel(is);
-                        FileChannel outChannel = fos.getChannel();
-                        outChannel.transferFrom(inChannel, 0, entry.getSize());
-                    } finally {
-                        try {
-                            if (is!=null)
-                                is.close();
-                        } catch(IOException ex) {
-                            try {
-                                if (fos != null)
-                                    fos.close();
-                            } catch (IOException ex2) {
-                               // do nothing
-                            }
-                            // throw original exception
-                            throw ex;
-                        }
-                        if (fos!=null)
-                            fos.close();
-                    }
-                }
-            }
-        } finally {
-            if (jarFile != null) {
-                jarFile.close();
-            }
-        }
-    }
 }
