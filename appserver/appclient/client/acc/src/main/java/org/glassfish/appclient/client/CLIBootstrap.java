9506269c3f875d95d9e28359b6892647b7bc2cf6
tjquinn <tjquinn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-02-08 16:20:51 +0000

    Fix for 18332.
    CLIBootstrap prepares a java command line for launching the ACC which includes the command-line arguments to be passed to the app client.  Due to differences in command-line parsing on the various platforms, this error was visible only on Windows systems using cygwin, but the code used System.out.println to convey the java command to use back to the command shell.  On Windows under cygwin the final command-line argument had a trailing \r character which was then passed into the client.  
    
    This change uses System.out.print (instead of println) to suppress the trailing \r on the last argument.
    
    Tests: QL, manual tests on Windows, Windows with cygwin, Mac OS X
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52503 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/appclient/client/acc/src/main/java/org/glassfish/appclient/client/CLIBootstrap.java
+++ appserver/appclient/client/acc/src/main/java/org/glassfish/appclient/client/CLIBootstrap.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -175,8 +175,12 @@ public class CLIBootstrap {
             /*
              * Write the generated java command to System.out.  The calling
              * shell script will execute this command.
+             * 
+             * Using print instead of println seems to work better.  Using
+             * println added a \r to the end of the last command-line argument
+             * on Windows under cygwin.  
              */
-            System.out.println(outputCommandLine);
+            System.out.print(outputCommandLine);
         } catch (Exception ex) {
             ex.printStackTrace();
             System.exit(1);
