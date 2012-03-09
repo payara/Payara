eb7356d944c52a40721e146bc61a529b1a509983
tbeerbower <tbeerbower@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-03-09 15:37:01 +0000

    Fix copyright notice
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52835 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/load-balancer/gf-load-balancer-connector/src/main/java/org/glassfish/loadbalancer/config/LbConfig.java
+++ appserver/load-balancer/gf-load-balancer-connector/src/main/java/org/glassfish/loadbalancer/config/LbConfig.java
@@ -32,7 +32,7 @@
  * Version 2] license."  If you don't indicate a single choice of license, a
  * recipient has the option to distribute your version of this file under
  * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 codeO
+ * its licensees as provided above.  However, if you add GPL Version 2 code
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
