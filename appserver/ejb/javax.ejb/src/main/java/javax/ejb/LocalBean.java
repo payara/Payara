7134a4579c17e89ecb13cab352cb7c58fa4d483f
mvatkina <mvatkina@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-03-13 23:25:52 +0000

    Improve LocalBean javadoc to have a more detailed description
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@52930 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/javax.ejb/src/main/java/javax/ejb/LocalBean.java
+++ appserver/ejb/javax.ejb/src/main/java/javax/ejb/LocalBean.java
@@ -47,6 +47,14 @@ import static java.lang.annotation.RetentionPolicy.*;
 
 /**
  * Designates that a session bean exposes a no-interface view.
+ * <p>
+ * This annotation is required if a session bean exposes any other client views 
+ * (local, remote, no-interface, 2.x Remote Home, 2.x Local Home, Web 
+ * Service) in addition to the no-interface view or its implements clause contains 
+ * an interface other than <code>java.io.Serializable</code>; <code>java.io.Externalizable</code>; 
+ * or any of the interfaces defined by the <code>javax.ejb</code> package.
+ * <p>
+ * This annotation is optional if a session bean exposes only a no-interface view.
  *
  * @since EJB 3.1
  */
