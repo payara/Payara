c1e12066c546a82151e939823c9f842c2dfcc6e4
amyroh <amyroh@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-03-27 17:11:35 +0000

    Fix GLASSFISH-18552 StartServerTask ant task cannot start embedded server with default domain.xml (non-embedded).
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@53178 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ant-tasks/src/main/java/org/glassfish/ant/embedded/tasks/StartServerTask.java
+++ appserver/ant-tasks/src/main/java/org/glassfish/ant/embedded/tasks/StartServerTask.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -53,6 +53,14 @@ public class StartServerTask extends TaskBase {
         this.serverID = serverID;
     }
 
+    /**
+     * Set the default http port
+     *
+     * The default http port will be used only when a custom domain.xml isn't provided
+     * via instanceRoot or configFileURI
+     *
+     * @param port default http port
+     */
     public void setHttpPort(int port) {
         this.port = port;
     }
--- appserver/ant-tasks/src/main/java/org/glassfish/ant/embedded/tasks/Util.java
+++ appserver/ant-tasks/src/main/java/org/glassfish/ant/embedded/tasks/Util.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -86,8 +86,12 @@ public class Util {
             glassfishProperties.setConfigFileURI(configFileURI);
             glassfishProperties.setConfigFileReadOnly(configFileReadOnly);
         }
-        if (httpPort != -1) {
-            glassfishProperties.setPort("http-listener", httpPort);
+
+        if (instanceRoot==null && configFileURI==null) {
+            // only set port if embedded domain.xml is used
+            if (httpPort != -1) {
+                glassfishProperties.setPort("http-listener", httpPort);
+            }
         }
 
         glassfish = glassfishRuntime.newGlassFish(glassfishProperties);
