248c057350c3d4f343b6f824389da5048711404d
liang.x.zhao@oracle.com <liang.x.zhao@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-04-19 08:11:24 +0000

    Fix GLASSFISH-17223.
    Add logic to read cluster system property JMS_PROVIDER_PORT when composing MQ url.
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@53574 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/jms/jms-core/src/main/java/com/sun/enterprise/connectors/jms/util/JmsRaUtil.java
+++ appserver/jms/jms-core/src/main/java/com/sun/enterprise/connectors/jms/util/JmsRaUtil.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -456,6 +456,12 @@ public class JmsRaUtil {
 
         SystemProperty sp = as.getSystemProperty("JMS_PROVIDER_PORT");
         if (sp != null) return sp.getValue();
+
+        Cluster cluster = as.getCluster();
+        if (cluster != null) {
+            sp = cluster.getSystemProperty("JMS_PROVIDER_PORT");
+            if (sp != null) return sp.getValue();
+        }
 
         Config config = as.getConfig();
         if (config != null)
