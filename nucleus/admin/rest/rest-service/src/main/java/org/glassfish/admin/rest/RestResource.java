0ada04f35c020575df283e1909c0fcb83fff4273
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-04-24 15:40:36 +0000

    Add missing file
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@53631 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/RestResource.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.admin.rest;
+
+/**
+ *
+ * @author jdlee
+ */
+public @interface RestResource {
+    
+}
