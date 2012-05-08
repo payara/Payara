7d01ce0fbc31b1834a55f36b4ef1ed2b59cca3d4
jr158900 <jr158900@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-08 05:56:57 +0000

    Adding imports
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@53874 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/tests/paas/external-db-service/generatederbyvm/src/main/java/org/glassfish/tests/paas/externaldbservicetest/generatederbyvm/DeleteDerbyVM.java
+++ appserver/tests/paas/external-db-service/generatederbyvm/src/main/java/org/glassfish/tests/paas/externaldbservicetest/generatederbyvm/DeleteDerbyVM.java
@@ -49,6 +49,7 @@ import org.glassfish.virtualization.runtime.VirtualMachineLifecycle;
 import org.glassfish.virtualization.spi.VirtualCluster;
 import org.glassfish.virtualization.spi.VirtualMachine;
 import javax.inject.Inject;
+import org.jvnet.hk2.annotations.Optional;
 import org.jvnet.hk2.annotations.Scoped;
 import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.component.PerLookup;
