23b1bcfb7c4b93858cec511891408f0446476a39
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-16 13:42:45 +0000

    Added support for primitive types (corresponding Wrappers handled as well)
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54082 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
@@ -47,21 +47,28 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.glassfish.admin.rest.RestExtension;
-import org.glassfish.pfl.objectweb.asm.Type;
 import org.jvnet.hk2.component.Habitat;
 import org.jvnet.hk2.config.Attribute;
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
+import static org.objectweb.asm.Opcodes.ACC_PROTECTED;
 import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
 import static org.objectweb.asm.Opcodes.ACC_SUPER;
 import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.ARETURN;
+import static org.objectweb.asm.Opcodes.DLOAD;
+import static org.objectweb.asm.Opcodes.DRETURN;
 import static org.objectweb.asm.Opcodes.DUP;
+import static org.objectweb.asm.Opcodes.FLOAD;
+import static org.objectweb.asm.Opcodes.FRETURN;
 import static org.objectweb.asm.Opcodes.GETFIELD;
 import static org.objectweb.asm.Opcodes.ILOAD;
 import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
 import static org.objectweb.asm.Opcodes.IRETURN;
+import static org.objectweb.asm.Opcodes.LLOAD;
+import static org.objectweb.asm.Opcodes.LRETURN;
 import static org.objectweb.asm.Opcodes.NEW;
 import static org.objectweb.asm.Opcodes.PUTFIELD;
 import static org.objectweb.asm.Opcodes.RETURN;
@@ -72,20 +79,18 @@ import static org.objectweb.asm.Opcodes.V1_6;
  * @author jdlee
  */
 public class CompositeUtil {
-//    private static final String PACKAGE_NAME = "org.glassfish.admin.rest.model.proxy";
     private static final Map<String, Class<?>> generatedClasses = new HashMap<String, Class<?>>();
 
     public synchronized static <T> T getModel(Class<T> clazz, Class similarClass,
             Class<?>[] interfaces) throws Exception {
-        String className = //PACKAGE_NAME + "." +
-                clazz.getName() + "Impl";
+        String className = clazz.getName() + "Impl";
         if (!alreadyGenerated(className)) {
             // TODO: This will be replace by HK2 code, once the HK2 integration is completed
 //            Class<?>[] interfaces = new Class<?>[]{
 //                clazz,
 //                ClusterExtension.class
 //            };
-            Map<String, Map<String, String>> properties = new HashMap<String, Map<String, String>>();
+            Map<String, Map<String, Object>> properties = new HashMap<String, Map<String, Object>>();
 
             for (Class<?> iface : interfaces) {
                 for (Method method : iface.getMethods()) {
@@ -93,32 +98,29 @@ public class CompositeUtil {
                     final boolean isGetter = name.startsWith("get");
                     if (isGetter || name.startsWith("set")) {
                         name = name.substring(3);
-                        Map<String, String> property = properties.get(name);
+                        Map<String, Object> property = properties.get(name);
                         if (property == null) {
-                            property = new HashMap<String, String>();
+                            property = new HashMap<String, Object>();
                             properties.put(name, property);
                         }
 
-                        String type = "String";
                         Attribute attr = method.getAnnotation(Attribute.class);
                         if (attr != null) {
-                            property.put("type", attr.dataType().getCanonicalName());
                             property.put("defaultValue", attr.defaultValue());
-                        } else {
-                            property.put("type", isGetter ?
-                                method.getReturnType().getCanonicalName() :
-                                method.getParameterTypes()[0].getCanonicalName());
                         }
+                        Class<?> type = isGetter
+                                ? method.getReturnType()
+                                : method.getParameterTypes()[0];
+                        property.put("type", type);
                     }
                 }
             }
             ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
             visitClass(cw, className, interfaces, properties);
 
-//            for (Map.Entry<String, String> entry : properties.entrySet()) {
-             for (Map.Entry<String, Map<String, String>> entry : properties.entrySet()) {
+             for (Map.Entry<String, Map<String, Object>> entry : properties.entrySet()) {
                 String name = entry.getKey();
-                String type = entry.getValue().get("type");
+                Class<?> type = (Class<?>)entry.getValue().get("type");
                 createField(cw, name, type);
                 createGettersAndSetters(cw, clazz, className, name, type);
 
@@ -131,12 +133,10 @@ public class CompositeUtil {
         }
 
         return (T)generatedClasses.get(className).newInstance();
-                //similarClass.forName(className).newInstance();
     }
 
     // TODO: method enum?
-    public static void getResourceExtensions(Habitat habitat, Class<?> baseClass,
-            Object data, String method) {
+    public static void getResourceExtensions(Habitat habitat, Class<?> baseClass, Object data, String method) {
         Collection<RestExtension> extensions = habitat.getAllByContract(RestExtension.class);
 
         for (RestExtension extension : extensions) {
@@ -146,10 +146,15 @@ public class CompositeUtil {
                 }
             }
         }
-        //.get(foo);
     }
 
-    protected static void visitClass(ClassWriter cw, String className, Class<?>[] ifaces, Map<String, Map<String, String>> properties) {
+    protected static String getInternalTypeString(Class<?> type) {
+        return type.isPrimitive()
+                ? getPrimitiveInternalType(type.getName())//Primitive.getPrimitive(type.getName()).getInternalType()
+                : ("L" + getInternalName(type.getName()) + ";");
+    }
+
+    protected static void visitClass(ClassWriter cw, String className, Class<?>[] ifaces, Map<String, Map<String, Object>> properties) {
         String[] ifaceNames = new String[ifaces.length];
         int i = 0;
         for (Class<?> iface : ifaces) {
@@ -170,25 +175,18 @@ public class CompositeUtil {
         av0.visitEnd();
     }
 
-    protected static void createConstructor(ClassWriter cw, String className, Map<String, Map<String, String>> properties) {
+    protected static void createConstructor(ClassWriter cw, String className, Map<String, Map<String, Object>> properties) {
         // Create the ctor
         MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
         mv.visitCode();
         mv.visitVarInsn(ALOAD, 0);
         mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
 
-        for (Map.Entry<String, Map<String, String>> property : properties.entrySet()) {
-            String name = property.getKey();
-            String defaultValue = property.getValue().get("defaultValue");
+        for (Map.Entry<String, Map<String, Object>> property : properties.entrySet()) {
+            String fieldName = property.getKey();
+            String defaultValue = (String)property.getValue().get("defaultValue");
             if (defaultValue != null && !defaultValue.isEmpty()) {
-                final String type = getInternalName(property.getValue().get("type"));
-                mv.visitVarInsn(ALOAD, 0);
-                mv.visitTypeInsn(NEW, type);
-                mv.visitInsn(DUP);
-                mv.visitLdcInsn(defaultValue);
-                mv.visitMethodInsn(INVOKESPECIAL, type, "<init>", "(Ljava/lang/String;)V");
-                mv.visitFieldInsn(PUTFIELD, getInternalName(className), name, "L" + type + ";");
-
+                setDefaultValue(mv, className, fieldName, (Class<?>)property.getValue().get("type"), defaultValue);
             }
         }
 
@@ -197,38 +195,158 @@ public class CompositeUtil {
         mv.visitEnd();
     }
 
+    static enum Primitive {
+        DOUBLE ("D", DRETURN, DLOAD),
+        FLOAT  ("F", FRETURN, FLOAD),
+        LONG   ("J", LRETURN, LLOAD),
+        SHORT  ("S", IRETURN, ILOAD),
+        INT    ("I", IRETURN, ILOAD),
+//        CHAR   ("C", IRETURN, ILOAD),
+        BYTE   ("B", IRETURN, ILOAD),
+        BOOLEAN("Z", IRETURN, ILOAD);
+        
+        private final int returnOpcode;
+        private final int setOpcode;
+        private final String internalType;
+        
+        Primitive(String type, int returnOpcode, int setOpcode) {
+            this.internalType = type;
+            this.returnOpcode = returnOpcode;
+            this.setOpcode = setOpcode;
+        }
+        public int getReturnOpcode() {
+            return returnOpcode;
+        }
+        public int getSetOpCode() {
+            return setOpcode;
+        }
+        public String getInternalType() {
+            return internalType;
+        }
+        static Primitive getPrimitive (String type) {
+            if ("S".equals(type) || "short".equals(type)) {
+                return SHORT;
+            } else if ("J".equals(type) || "long".equals(type)) {
+                return LONG;
+            } else if ("I".equals(type) || "int".equals(type)) {
+                return INT;
+            } else if ("F".equals(type) || "float".equals(type)) {
+                return FLOAT;
+            } else if ("D".equals(type) || "double".equals(type)) {
+                return DOUBLE;
+//            } else if ("C".equals(type) || "char".equals(type)) {
+//                return CHAR;
+            } else if ("B".equals(type) || "byte".equals(type)) {
+                return BYTE;
+            } else if ("Z".equals(type) || "boolean".equals(type)) {
+                return BOOLEAN;
+            } else {
+                throw new RuntimeException ("Unknown primitive type: " + type);
+            }
+        }
+    };
+
+    protected static void setDefaultValue(MethodVisitor mv, String className, String fieldName, Class<?> fieldClass, String defaultValue) {
+        final String type = getInternalTypeString(fieldClass);
+        Object value = defaultValue;
+
+        if (fieldClass.isPrimitive()) {
+            switch (Primitive.getPrimitive(type)) {
+                case SHORT: value = Short.valueOf(defaultValue); break;
+                case LONG: value = Long.valueOf(defaultValue); break;
+                case INT: value = Integer.valueOf(defaultValue); break;
+                case FLOAT: value = Float.valueOf(defaultValue); break;
+                case DOUBLE: value = Double.valueOf(defaultValue); break;
+//                case CHAR: value = Character.valueOf(defaultValue.charAt(0)); break;
+                case BYTE: value = Byte.valueOf(defaultValue); break;
+                case BOOLEAN: value = Boolean.valueOf(defaultValue); break;
+            }
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitLdcInsn(value);
+            System.out.println("CTOR: Using " + type + " for PUTFIELD " + fieldName);
+            mv.visitFieldInsn(PUTFIELD, getInternalName(className), fieldName, type);
+        } else {
+            if (!fieldClass.equals(String.class)) {
+                mv.visitVarInsn(ALOAD, 0);
+                final String internalName = getInternalName(fieldClass.getName());
+                mv.visitTypeInsn(NEW, internalName);
+                mv.visitInsn(DUP);
+                mv.visitLdcInsn(defaultValue);
+                mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;)V");
+                mv.visitFieldInsn(PUTFIELD, getInternalName(className), fieldName, type);
+            } else {
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitLdcInsn(value);
+                mv.visitFieldInsn(PUTFIELD, getInternalName(className), fieldName, type);
+            }
+        }
+
+
+    }
+
     /**
      * Add the field to the class
      */
-    protected static void createField(ClassWriter cw, String name, String type) {
+    protected static void createField(ClassWriter cw, String name, Class<?> type) {
         // TODO: Add support for primitives
-        FieldVisitor fv = cw.visitField(ACC_PUBLIC, name, "L" + getInternalName(type) + ";", null, null);
+        String internalType = getInternalTypeString(type);
+        FieldVisitor fv = cw.visitField(ACC_PROTECTED, name, internalType, null, null);
         fv.visitAnnotation("Ljavax/xml/bind/annotation/XmlAttribute;", true).visitEnd();
         fv.visitEnd();
     }
 
-    protected static void createGettersAndSetters(ClassWriter cw, Class c, String className, String name, String type) {
-        // TODO: Add support for primitives
-        type = getInternalName(type);
+    protected static void createGettersAndSetters(ClassWriter cw, Class c, String className, String name, Class<?> type) {
+        String internalType = getInternalTypeString(type);
         className = getInternalName(className);
 
         // Create the getter
-        MethodVisitor getterVistor = cw.visitMethod(ACC_PUBLIC, "get" + name, "()L" + type + ";", null, null);
+        MethodVisitor getterVistor = cw.visitMethod(ACC_PUBLIC, "get" + name, "()" + internalType, null, null);
         getterVistor.visitCode();
         getterVistor.visitVarInsn(ALOAD, 0);
-        getterVistor.visitFieldInsn(GETFIELD, className, name, "L" + type + ";");
-        getterVistor.visitInsn(Type.getType(c).getOpcode(IRETURN));
+        getterVistor.visitFieldInsn(GETFIELD, className, name, internalType);
+        int opcode = //type.isPrimitive() ?
+                //Primitive.getPrimitive(internalType).getReturnOpcode() :
+                ARETURN;
+        if (type.isPrimitive()) {
+            switch (Primitive.getPrimitive(internalType)) {
+                case DOUBLE : opcode = DRETURN; break;
+                case FLOAT : opcode = FRETURN; break;
+                case LONG : opcode = LRETURN; break;
+                case BOOLEAN:
+                case BYTE:
+//                case CHAR:
+                case SHORT:
+                case INT: opcode = IRETURN; break;
+            }
+        }
+        getterVistor.visitInsn(opcode);
         getterVistor.visitMaxs(0, 0);
         getterVistor.visitEnd();
 
         // Create the setter
-        MethodVisitor setterVisitor = cw.visitMethod(ACC_PUBLIC, "set" + name, "(L" + type + ";)V", null, null);
+        MethodVisitor setterVisitor = cw.visitMethod(ACC_PUBLIC, "set" + name, "(" + internalType + ")V", null, null);
+        opcode = //type.isPrimitive() ?
+                //Primitive.getPrimitive(internalType).getSetOpCode() :
+                ALOAD;
+        if (type.isPrimitive()) {
+            switch (Primitive.getPrimitive(internalType)) {
+                case DOUBLE : opcode = DLOAD; break;
+                case FLOAT : opcode = FLOAD; break;
+                case LONG : opcode = LLOAD; break;
+                case BOOLEAN:
+                case BYTE:
+//                case CHAR:
+                case SHORT:
+                case INT: opcode = ILOAD; break;
+            }
+        }
         setterVisitor.visitCode();
         setterVisitor.visitVarInsn(ALOAD, 0);
-        setterVisitor.visitVarInsn(Type.getType(c).getOpcode(ILOAD), 1);
-        setterVisitor.visitFieldInsn(PUTFIELD, className, name, "L" + type + ";");
+        setterVisitor.visitVarInsn(opcode, 1);
+        System.out.println("SETTER: Using " + internalType + " for PUTFIELD " + name);
+        setterVisitor.visitFieldInsn(PUTFIELD, className, name, internalType);
         setterVisitor.visitInsn(RETURN);
-        setterVisitor.visitMaxs(0, 0);
+        setterVisitor.visitMaxs(0,0);
         setterVisitor.visitEnd();
     }
 
@@ -238,6 +356,28 @@ public class CompositeUtil {
 
     protected static String getInternalName(String className) {
         return className.replace(".", "/");
+    }
+
+    protected static String getPrimitiveInternalType(String type) {
+        if (type.equals("int")) {
+            return "I";
+        } else if (type.equals("float")) {
+            return "F";
+        } else if (type.equals("double")) {
+            return "D";
+        } else if (type.equals("short")) {
+            return "S";
+        } else if (type.equals("long")) {
+            return "J";
+        } else if (type.equals("byte")) {
+            return "B";
+        } else if (type.equals("char")) {
+            return "C";
+        } else if (type.equals("boolean")) {
+            return "Z";
+        } else {
+            throw new RuntimeException("Unexpected primitve type: " + type);
+        }
     }
 
     // TODO: This is duplicated from the generator class.  
