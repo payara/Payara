ec187f7572eeca2251f07a60d3d1f3633e7655ca
yaminikb <yaminikb@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-22 08:58:17 +0000

    Changing the getter/setter since monitoring node name changed
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54187 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/ModuleMonitoringLevels.java
+++ nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/ModuleMonitoringLevels.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -439,7 +439,7 @@ public interface ModuleMonitoringLevels extends ConfigBeanProxy, Injectable, Pro
      */
     @Attribute (defaultValue="OFF")
     @Pattern(regexp="(OFF|LOW|HIGH)")
-    public String getCloudIaas();
+    public String getCloudIaasMgmt();
 
     /**
      * Sets the value of the cloud IMS property.
@@ -447,7 +447,7 @@ public interface ModuleMonitoringLevels extends ConfigBeanProxy, Injectable, Pro
      * @param value allowed object is
      *              {@link String }
      */
-    public void setCloudIaas(String value) throws PropertyVetoException;
+    public void setCloudIaasMgmt(String value) throws PropertyVetoException;
     /**
         Properties as per {@link PropertyBag}
      */
