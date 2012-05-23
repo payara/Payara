b2322c09ec23ceb8d5bfb2759046a5d4a0be34e7
kalali <kalali@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-23 08:54:32 +0000

    Fixing copyright header's year.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54211 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/util/zeroconfig/commands/CreateDefaultConfigCommand.java
+++ nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/util/zeroconfig/commands/CreateDefaultConfigCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,7 +38,6 @@
  * holder.
  */
 
-
 package com.sun.enterprise.config.util.zeroconfig.commands;
 
 import com.sun.enterprise.config.serverbeans.Config;
@@ -52,7 +51,10 @@ import com.sun.enterprise.util.SystemPropertyConstants;
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.I18n;
 import org.glassfish.api.Param;
-import org.glassfish.api.admin.*;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.ExecuteOn;
+import org.glassfish.api.admin.RuntimeType;
 import org.glassfish.api.admin.config.ConfigExtension;
 import org.glassfish.config.support.CommandTarget;
 import org.glassfish.config.support.TargetType;
@@ -65,7 +67,6 @@ import javax.inject.Inject;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
