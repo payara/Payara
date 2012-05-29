6ebb113bb247d2e2af6e727e1aeac07dac2ee989
simeng_oracle <simeng_oracle@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-29 01:30:07 +0000

    Fix GLASSFISH-4773 - Change the min value of addresslist iterations to -1
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54303 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/jms/gf-jms-connector/src/main/java/com/sun/enterprise/connectors/jms/config/JmsService.java
+++ appserver/jms/gf-jms-connector/src/main/java/com/sun/enterprise/connectors/jms/config/JmsService.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -259,7 +259,7 @@ public interface JmsService extends ConfigExtension, Injectable, PropertyBag {
      *         {@link String }
      */
     @Attribute (defaultValue="3")
-    @Min(value=1)
+    @Min(value=-1)
     String getAddresslistIterations();
 
     /**
