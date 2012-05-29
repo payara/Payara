fe10e70382a9a07da438ef0bd43c44588a8a0b4b
cf126330 <cf126330@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-05-29 17:07:12 +0000

    revert rev 54280 due to cts smoke webservices tests failures caused by rev 54265.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54313 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/ejb/runtime/beancache/ASEjbBCMaxCacheSize.java
+++ appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/ejb/runtime/beancache/ASEjbBCMaxCacheSize.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,10 +40,10 @@
 
 package com.sun.enterprise.tools.verifier.tests.ejb.runtime.beancache;
 
+import com.sun.enterprise.deployment.EjbDescriptor;
 import com.sun.enterprise.tools.verifier.Result;
 import com.sun.enterprise.tools.verifier.Verifier;
 import com.sun.enterprise.tools.verifier.tests.ComponentNameConstructor;
-import org.glassfish.ejb.deployment.descriptor.EjbDescriptor;
 
 /** ejb [0,n]
  *    bean-cache ?
@@ -61,7 +61,7 @@ import org.glassfish.ejb.deployment.descriptor.EjbDescriptor;
 public class ASEjbBCMaxCacheSize extends ASEjbBeanCache
 {
     
-    public Result check(EjbDescriptor descriptor)
+    public Result check(EjbDescriptor descriptor) 
     {
 
 	Result result = getInitializedResult();
--- appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/ejb/runtime/beancache/ASEjbBCVictimPolicy.java
+++ appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/ejb/runtime/beancache/ASEjbBCVictimPolicy.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,9 +40,9 @@
 
 package com.sun.enterprise.tools.verifier.tests.ejb.runtime.beancache;
 
+import com.sun.enterprise.deployment.EjbDescriptor;
 import com.sun.enterprise.tools.verifier.Result;
 import com.sun.enterprise.tools.verifier.tests.ComponentNameConstructor;
-import org.glassfish.ejb.deployment.descriptor.EjbDescriptor;
 
 
 /** ejb [0,n]
@@ -62,7 +62,7 @@ import org.glassfish.ejb.deployment.descriptor.EjbDescriptor;
 public class ASEjbBCVictimPolicy extends ASEjbBeanCache
 {
     
-    public Result check(EjbDescriptor descriptor)
+    public Result check(EjbDescriptor descriptor) 
     {
         Result result = getInitializedResult();
         ComponentNameConstructor compName = getVerifierContext().getComponentNameConstructor();
