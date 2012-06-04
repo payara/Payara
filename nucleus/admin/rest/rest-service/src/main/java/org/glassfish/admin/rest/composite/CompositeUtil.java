b743b8eb5e98263faf5cb8853d62b92f3daf0fdf
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-06-04 22:08:35 +0000

    Minor code reorg prior to larger changes
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54427 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
@@ -265,6 +265,10 @@ public class CompositeUtil {
                : ("L" + getInternalName(type.getName()) + ";");
     }
 
+    private String getPropertyName(String name) {
+        return name.substring(0,1).toLowerCase() + name.substring(1);
+    }
+
     /**
      * This method starts the class definition, adding the JAX-B annotations to allow for marshalling via JAX-RS
      */
@@ -313,64 +317,6 @@ public class CompositeUtil {
         method.visitMaxs(1, 1);
         method.visitEnd();
     }
-
-    /**
-     * This enum encapsulates the metadata for primitives needed for generating fields, getters and setters
-     */
-    static enum Primitive {
-
-        DOUBLE("D", DRETURN, DLOAD),
-        FLOAT("F", FRETURN, FLOAD),
-        LONG("J", LRETURN, LLOAD),
-        SHORT("S", IRETURN, ILOAD),
-        INT("I", IRETURN, ILOAD),
-//        CHAR   ("C", IRETURN, ILOAD),
-        BYTE("B", IRETURN, ILOAD),
-        BOOLEAN("Z", IRETURN, ILOAD);
-        private final int returnOpcode;
-        private final int setOpcode;
-        private final String internalType;
-
-        Primitive(String type, int returnOpcode, int setOpcode) {
-            this.internalType = type;
-            this.returnOpcode = returnOpcode;
-            this.setOpcode = setOpcode;
-        }
-
-        public int getReturnOpcode() {
-            return returnOpcode;
-        }
-
-        public int getSetOpCode() {
-            return setOpcode;
-        }
-
-        public String getInternalType() {
-            return internalType;
-        }
-
-        static Primitive getPrimitive(String type) {
-            if ("S".equals(type) || "short".equals(type)) {
-                return SHORT;
-            } else if ("J".equals(type) || "long".equals(type)) {
-                return LONG;
-            } else if ("I".equals(type) || "int".equals(type)) {
-                return INT;
-            } else if ("F".equals(type) || "float".equals(type)) {
-                return FLOAT;
-            } else if ("D".equals(type) || "double".equals(type)) {
-                return DOUBLE;
-//            } else if ("C".equals(type) || "char".equals(type)) {
-//                return CHAR;
-            } else if ("B".equals(type) || "byte".equals(type)) {
-                return BYTE;
-            } else if ("Z".equals(type) || "boolean".equals(type)) {
-                return BOOLEAN;
-            } else {
-                throw new RuntimeException("Unknown primitive type: " + type);
-            }
-        }
-    };
 
     /*
      * This method generates the byte code to set the default value for a given field. Efforts are made to determine the
