9ddf33fd5e43d564ffda25c21f2c8c860f01552f
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-06-15 11:34:53 +0000

    ignore the test, I did use junit @Ignore instead of testng @Test(enabled=false)
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@54605 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/tests/quicklook/src/test/java/org/glassfish/nucleus/quicklook/RestTest.java
+++ nucleus/tests/quicklook/src/test/java/org/glassfish/nucleus/quicklook/RestTest.java
@@ -42,12 +42,10 @@ package org.glassfish.nucleus.quicklook;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.junit.Ignore;
 import static org.testng.AssertJUnit.*;
 import org.testng.annotations.Test;
 
-@Ignore
-@Test
+@Test(enabled=false)
 public class RestTest {
     public void testManagementEndpoint() {
         try {
