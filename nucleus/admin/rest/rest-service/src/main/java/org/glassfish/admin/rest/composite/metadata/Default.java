5181af3b46a352477836e3945aa84428594c29db
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-08-14 19:24:58 +0000

    Added missed files Adding export to osgi.bundle
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@55467 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/metadata/Default.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.admin.rest.composite.metadata;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
+
+/**
+ *
+ * @author jdlee
+ */
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Default {
+    Class<? extends DefaultGenerator> generator();
+}
---
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/metadata/DefaultGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.admin.rest.composite.metadata;
+
+import org.glassfish.grizzly.http.server.Request;
+
+/**
+ *
+ * @author jdlee
+ */
+public interface DefaultGenerator {
+    String getDefaultValue();
+}
