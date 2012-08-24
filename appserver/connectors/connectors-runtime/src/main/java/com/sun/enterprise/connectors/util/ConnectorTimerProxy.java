ea9f48ddb0ed9548c4d42b11aa561b58f2f4b18c
sm157516 <sm157516@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-08-24 10:49:31 +0000

    Fixing Findbugs : DC_DOUBLECHECK
    Tests run : QL, jdbc devtests
    
    
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@55620 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/util/ConnectorTimerProxy.java
+++ appserver/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/util/ConnectorTimerProxy.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -50,7 +50,7 @@ import java.util.logging.Logger;
 
 public class ConnectorTimerProxy extends Timer {
     
-    private static ConnectorTimerProxy connectorTimer;
+    private volatile static ConnectorTimerProxy connectorTimer;
     private Timer timer;
     private boolean timerException = false;
     private final Object getTimerLock = new Object();
