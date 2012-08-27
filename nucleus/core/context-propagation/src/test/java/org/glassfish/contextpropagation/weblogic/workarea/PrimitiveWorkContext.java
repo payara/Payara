e2a9a7d89a68b7ca34acce4f95ae39b8c5610e32
jean.drolet@oracle.com <jean.drolet@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-08-27 23:05:40 +0000

    repairing copyright notices
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@55657 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/context-propagation/src/test/java/org/glassfish/contextpropagation/weblogic/workarea/PrimitiveWorkContext.java
+++ nucleus/core/context-propagation/src/test/java/org/glassfish/contextpropagation/weblogic/workarea/PrimitiveWorkContext.java
@@ -1,20 +1,45 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package org.glassfish.contextpropagation.weblogic.workarea;
 
-
-
-/**
- * <code>PrimitiveWorkContext</code> is a marker interface used for
- * marshaling and unmarshaling primitive user data in a
- * <code>WorkArea</code>. <code>PrimitiveWorkContext</code>s contain
- * only a single data item that can be accessed via
- * {@link #get}. They are provided, not only as a
- * convenience, but because they can be passed between servers without
- * users needing to provide a {@link WorkContext} implementation
- * on the target. <code>PrimitiveWorkContext</code> implementations
- * must provide a public no-arg constructor.
- *
- * @author Copyright (c) 2003 by BEA Systems Inc. All Rights Reserved.
- */
 public interface PrimitiveWorkContext extends WorkContext {
   /**
    * Returns the data associated with this <code>PrimitiveWorkContext</code>.
