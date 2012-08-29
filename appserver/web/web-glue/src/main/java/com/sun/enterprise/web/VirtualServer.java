89b7abab28adf09650e30b2803461d50adb1671c
oleksiys <oleksiys@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-08-29 15:19:14 +0000

    Integrate Grizzly 2.3-beta1
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@55708 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/web-glue/src/main/java/com/sun/enterprise/web/VirtualServer.java
+++ appserver/web/web-glue/src/main/java/com/sun/enterprise/web/VirtualServer.java
@@ -2627,9 +2627,10 @@ public class VirtualServer extends StandardHost
                     responsePacket.setStatus(HttpStatus.BAD_REQUEST_400);
 
                     org.glassfish.grizzly.http.server.Request request = org.glassfish.grizzly.http.server.Request.create();
-                    org.glassfish.grizzly.http.server.Response response = org.glassfish.grizzly.http.server.Response.create();
+//                    org.glassfish.grizzly.http.server.Response response = org.glassfish.grizzly.http.server.Response.create();
+                    org.glassfish.grizzly.http.server.Response response = request.getResponse();
 
-                    request.initialize(response, requestPacket, FilterChainContext.create(connection), null);
+                    request.initialize(/*response,*/ requestPacket, FilterChainContext.create(connection), null);
                     response.initialize(request, responsePacket, FilterChainContext.create(connection), null, null);
 
                     Response res = new Response();
