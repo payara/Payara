b66c3cea069aa28a55e99ebfd9df46f5a605eab4
jungicz <jungicz@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-09-12 16:00:36 +0000

    BUG 14576445 - GFV3.1.2.2: CANNOT DEPLOY EJB-BASED WEBSERVICE IF DAS HAS NO HTTP-LISTENERS DEFI
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@55921 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/WsUtil.java
+++ appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/WsUtil.java
@@ -8,7 +8,7 @@
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
@@ -1504,6 +1504,7 @@ public class WsUtil {
             }
         } else {
             wsi.setHttpVS(new VirtualServerInfo("http", "localhost", 0));
+            wsi.setHttpsVS(new VirtualServerInfo("https", "localhost", 0));
         }
         return wsi;
     }
