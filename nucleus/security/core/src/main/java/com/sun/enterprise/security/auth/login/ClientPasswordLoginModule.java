9b82f36d0fc15305afd41080ab91a6220a69eec8
JeffTancill <JeffTancill@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-02 00:15:05 +0000

    FindBugs fix, drop first of double assignment
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56210 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/security/core/src/main/java/com/sun/enterprise/security/auth/login/ClientPasswordLoginModule.java
+++ nucleus/security/core/src/main/java/com/sun/enterprise/security/auth/login/ClientPasswordLoginModule.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -324,7 +324,6 @@ public class ClientPasswordLoginModule implements LoginModule {
     public boolean logout() throws LoginException {
 
 	subject.getPrincipals().remove(userPrincipal);
-	succeeded = false;
 	succeeded = commitSucceeded;
 	username = null;
 	if (password != null) {
