65d58ff83e4d9edf7ec918f2903e885210f2e223
JeffTancill <JeffTancill@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-02 19:12:16 +0000

    FindBugs fix
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56221 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/security/ejb.security/src/main/java/com/sun/enterprise/iiop/security/ConnectionContext.java
+++ appserver/security/ejb.security/src/main/java/com/sun/enterprise/iiop/security/ConnectionContext.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -56,7 +56,7 @@ public final class ConnectionContext implements Serializable {
     private boolean ssl = false;
     private IOR ior = null;
     private transient Socket socket = null;
-    private SocketInfo endpoint = null;
+    private transient SocketInfo endpoint = null;
 
     /**
      * Default constructor.
