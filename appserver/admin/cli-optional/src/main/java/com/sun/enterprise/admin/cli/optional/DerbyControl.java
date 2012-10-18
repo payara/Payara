67c842369d214230af5c73306cbc4fa5b5835dbb
jc129909 <jc129909@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-18 17:01:35 +0000

    Fix findbugs bug: In the database commands replace System.exit with Runtime.getRuntime().exit to make findbugs happy.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56588 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/DerbyControl.java
+++ appserver/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/DerbyControl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -104,7 +104,7 @@ public final class DerbyControl
             catch (Throwable t) {
 	            t.printStackTrace();
 	            //exit with an error code of 2
-	            System.exit(2);
+	            Runtime.getRuntime().exit(2);
             }
         }
         //do not set derby.system.home if derbyHome is empty
@@ -159,7 +159,7 @@ public final class DerbyControl
         }
         catch (Throwable t) {
 	        t.printStackTrace();
-	        System.exit(2);
+	        Runtime.getRuntime().exit(2);
         }
     }
 
