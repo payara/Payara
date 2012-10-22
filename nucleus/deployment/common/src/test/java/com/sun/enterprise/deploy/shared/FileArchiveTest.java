3d78be9f4db880efec377a282795851a3a13d78d
jwells <jwells@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-22 17:44:32 +0000

    Remov unused import of Habitat
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56676 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/deployment/common/src/test/java/com/sun/enterprise/deploy/shared/FileArchiveTest.java
+++ nucleus/deployment/common/src/test/java/com/sun/enterprise/deploy/shared/FileArchiveTest.java
@@ -68,7 +68,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.jvnet.hk2.component.Habitat;
 
 import static org.junit.Assert.*;
 
