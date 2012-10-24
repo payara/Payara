1e3b5f9a79cec8122dd0e2e22255266e5ae0812d
jc129909 <jc129909@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-24 22:10:37 +0000

    Fix findbugs bug:  Close input and output streams in finally block.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56730 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/services/WindowsService.java
+++ nucleus/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/services/WindowsService.java
@@ -235,10 +235,21 @@ public class WindowsService extends NonSMFServiceAdapter {
 
         if (!sourceWin32Exe.isFile()) {
             // copy it from inside this jar to the file system
-            InputStream in = WindowsService.class.getResourceAsStream("/lib/" + SOURCE_WIN32_EXE_FILENAME);
-            FileOutputStream out = new FileOutputStream(sourceWin32Exe);
-            copyStream(in, out);
-            trace("Copied from inside the jar to " + sourceWin32Exe);
+            InputStream in = null;
+            FileOutputStream out = null;
+            try {
+                in = WindowsService.class.getResourceAsStream("/lib/" + SOURCE_WIN32_EXE_FILENAME);
+                out = new FileOutputStream(sourceWin32Exe);
+                copyStream(in, out);
+                trace("Copied from inside the jar to " + sourceWin32Exe);
+            }
+            finally {
+                if (in != null)
+                    in.close();
+                
+                if (out != null)
+                    out.close();
+            }
         }
         trace("Source executable: " + sourceWin32Exe);
     }
@@ -249,8 +260,6 @@ public class WindowsService extends NonSMFServiceAdapter {
         while ((len = in.read(buf)) >= 0) {
             out.write(buf, 0, len);
         }
-        in.close();
-        out.close();
     }
 
     /**
