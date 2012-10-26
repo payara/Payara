e2b0c3fb4be19a56a04f5bd54a15fffb99cff6e7
mtaube <mtaube@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-10-26 14:19:36 +0000

    removing hk2 superfluous tracing
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56747 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
+++ nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
@@ -95,7 +95,6 @@ import org.glassfish.deployment.versioning.VersioningSyntaxException;
 import org.glassfish.deployment.versioning.VersioningUtils;
 
 import org.glassfish.deployment.versioning.VersioningService;
-import org.jvnet.hk2.tracing.TracingUtilities;
 
 
 /**
@@ -552,10 +551,7 @@ public class DeployCommand extends DeployCommandParameters implements AdminComma
                         "Error while closing deployable artifact : ",
                         path.getAbsolutePath()), e);
             }
-            if (tracing!=null) {
-                tracing.print(System.out);
-                TracingUtilities.dump("org.glassfish.javaee.core.deployment.DolProvider", System.out);
-            }
+            
             if (report.getActionExitCode().equals(ActionReport.ExitCode.SUCCESS)) {
                 // Set the app name in the result so that embedded deployer can retrieve it.
                 report.setResultType(String.class, name);
