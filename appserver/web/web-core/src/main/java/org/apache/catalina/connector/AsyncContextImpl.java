d27daab9def7ae6449b4b690734588801268c99e
swchan2 <swchan2@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-11-06 21:28:29 +0000

    fix issue http://java.net/jira/browse/GLASSFISH-19212 ("Servlet AsyncListener PreDestroy method not called")
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56872 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/web-core/src/main/java/org/apache/catalina/connector/AsyncContextImpl.java
+++ appserver/web/web-core/src/main/java/org/apache/catalina/connector/AsyncContextImpl.java
@@ -40,6 +40,7 @@
 
 package org.apache.catalina.connector;
 
+import org.apache.catalina.ContainerEvent;
 import org.apache.catalina.Globals;
 import org.apache.catalina.core.ApplicationDispatcher;
 import org.apache.catalina.core.ApplicationHttpRequest;
@@ -52,6 +53,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
@@ -106,6 +109,8 @@ class AsyncContextImpl implements AsyncContext {
     private AtomicBoolean isOkToConfigure = new AtomicBoolean(true); 
 
     private long asyncTimeoutMillis = DEFAULT_ASYNC_TIMEOUT_MILLIS;
+
+    private final Queue<AsyncListener> listenerQueue = new ConcurrentLinkedQueue<AsyncListener>();
 
     private final LinkedList<AsyncListenerContext> asyncListenerContexts =
         new LinkedList<AsyncListenerContext>();
@@ -292,6 +297,7 @@ class AsyncContextImpl implements AsyncContext {
         if (ctx != null) {
             try {
                 listener = ctx.createListenerInstance(clazz);
+                listenerQueue.add(listener);
             } catch (Throwable t) {
                 throw new ServletException(t);
             }
@@ -522,6 +528,17 @@ class AsyncContextImpl implements AsyncContext {
         synchronized(asyncListenerContexts) {
             asyncListenerContexts.clear();
         }
+
+        StandardContext ctx = (StandardContext) origRequest.getContext();
+        if (ctx != null) {
+            for (AsyncListener l : listenerQueue) {
+                ctx.fireContainerEvent(ContainerEvent.PRE_DESTROY, l);
+            }
+        }
+        listenerQueue.clear();
+        servletRequest = null;
+        servletResponse = null;
+        origRequest = null;
     }
 
     /**
