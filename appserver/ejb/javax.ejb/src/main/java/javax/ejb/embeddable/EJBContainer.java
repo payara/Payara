164f6b6edeb49bed3f5defaf8c5ef545a3980bf7
mvatkina <mvatkina@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-11-09 23:11:49 +0000

    EJBContainer implements AutoCloseable
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@56955 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/javax.ejb/src/main/java/javax/ejb/embeddable/EJBContainer.java
+++ appserver/ejb/javax.ejb/src/main/java/javax/ejb/embeddable/EJBContainer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2006-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -57,7 +57,7 @@ import javax.ejb.spi.EJBContainerProvider;
   *
   * @since EJB 3.1
   */
-public abstract class EJBContainer {
+public abstract class EJBContainer implements AutoCloseable {
 
     /**
      * Standard property name for specifying the embeddable container implementation bootstrap
