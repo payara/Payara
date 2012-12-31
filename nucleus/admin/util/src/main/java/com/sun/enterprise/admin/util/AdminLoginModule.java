cca326bd3a11a878d37f70dae7a7b60b72bb739c
tjquinn <tjquinn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2012-12-31 15:52:47 +0000

    Fix FindBugs error (even though this class is supposedly obsolete).
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@57867 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/util/src/main/java/com/sun/enterprise/admin/util/AdminLoginModule.java
+++ nucleus/admin/util/src/main/java/com/sun/enterprise/admin/util/AdminLoginModule.java
@@ -97,6 +97,8 @@ public class AdminLoginModule implements LoginModule {
     private Subject subject;
     private CallbackHandler callbackHandler;
 
+    private String authRealm = null;
+
     // Holds principals and credentials that should be added to the real
     // subject during commit, if it is ever invoked.
     private final Subject subjectToAssemble = new Subject();
