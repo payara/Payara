e950a3ff75532e467f16f29ff9fa1c75a10a4781
tmueller <tmueller@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-02 17:36:14 +0000

    GLASSFISH-19486 - enforce the use of JDK 7 with GlassFish Passed QL.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@57887 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/bootstrap/src/main/java/com/sun/enterprise/glassfish/bootstrap/ASMainHelper.java
+++ nucleus/core/bootstrap/src/main/java/com/sun/enterprise/glassfish/bootstrap/ASMainHelper.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -74,8 +74,8 @@ public class ASMainHelper {
     static void checkJdkVersion() {
         int minor = getMinorJdkVersion();
 
-        if (minor < 6) {
-            logger.severe("GlassFish requires JDK 6, you are using JDK version " + minor);
+        if (minor < 7) {
+            logger.severe("GlassFish requires JDK 7, you are using JDK version " + minor);
             System.exit(1);
         }
     }
