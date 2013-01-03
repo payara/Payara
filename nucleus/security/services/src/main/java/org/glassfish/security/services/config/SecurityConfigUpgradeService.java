70c799b3c5d2aaf0f6744922185582a29bb967ee
kalali <kalali@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-03 11:03:16 +0000

    Fixing copyright year.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@57903 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/security/services/src/main/java/org/glassfish/security/services/config/SecurityConfigUpgradeService.java
+++ nucleus/security/services/src/main/java/org/glassfish/security/services/config/SecurityConfigUpgradeService.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -210,7 +210,6 @@ public class SecurityConfigUpgradeService implements ConfigurationUpgrade, PostC
         final SecurityProvider sp_w = addAuthzProvider(as_w, SIMPLE_PROVIDER_NAME, SIMPLE_PROVIDER_PROVIDER_NAME, SIMPLE_PROVIDER_TYPE);
         addAuthorizationConfig(sp_w, SIMPLE_PROVIDER_CONFIG_NAME, SUPPORT_POLICY_DEPLOY);
         return sp_w;
-
     }
 
     private void addAuthorizationConfig(SecurityProvider sp_w, String configName, String supportPolicyDeploy) throws TransactionFailure, PropertyVetoException {
