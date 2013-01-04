1df36150a7141d212a70dade86ef2834a50e8051
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-04 17:55:13 +0000

    Minor code reformatting
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@57966 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/tests/admin/src/test/java/org/glassfish/nucleus/admin/rest/RestTestBase.java
+++ nucleus/tests/admin/src/test/java/org/glassfish/nucleus/admin/rest/RestTestBase.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -159,8 +159,7 @@ public class RestTestBase {
     }
 
     protected void authenticate() {
-        getClient().
-                register(basicAuthFilter);
+        getClient().register(basicAuthFilter);
     }
 
     protected <T> T getTestClass(Class<T> clazz) {
