5ab025b6835b582118afc6d582e9b611a09498da
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-07 17:19:45 +0000

    Created shell for new tests
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58184 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ nucleus/tests/admin/src/test/java/org/glassfish/nucleus/admin/rest/ExceptionFilterTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.nucleus.admin.rest;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author jdlee
+ */
+@Test(enabled = false)
+public class ExceptionFilterTest {
+    public void testExceptionFiler() {
+        // TODO
+    }
+}
