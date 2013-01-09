c46fb6512dc1369407f33227b2380f888baa17fa
amy.yang <amy.yang@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-09 03:46:38 +0000

    findbugs issue UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR in InterceptorBindingDescriptor. passed quicklook_all and apsdevtests_ejb
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58224 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/ejb-container/src/main/java/org/glassfish/ejb/deployment/descriptor/InterceptorBindingDescriptor.java
+++ appserver/ejb/ejb-container/src/main/java/org/glassfish/ejb/deployment/descriptor/InterceptorBindingDescriptor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -84,7 +84,7 @@ public class InterceptorBindingDescriptor extends Descriptor
     }
 
     public BindingType getBindingType() {
-        if( ejbName.equals("*") ) {
+        if( "*".equals(ejbName) ) {
             return BindingType.DEFAULT;
         } else if( businessMethod == null ) {
             return BindingType.CLASS;
