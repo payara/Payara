b7a343d92aaca3c009cbd78b152750b212ba76d6
amy.yang <amy.yang@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-09 06:52:15 +0000

    findbugs issue UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR in ConcurrentMethodDescriptor.java. passed quicklook_all & apsdevtests_ejb
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58228 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/ejb-container/src/main/java/org/glassfish/ejb/deployment/descriptor/ConcurrentMethodDescriptor.java
+++ appserver/ejb/ejb-container/src/main/java/org/glassfish/ejb/deployment/descriptor/ConcurrentMethodDescriptor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -86,11 +86,11 @@ public class ConcurrentMethodDescriptor extends Descriptor {
     }
     
     public long getAccessTimeoutValue() {
-        return accessTimeout.getValue();
+        return accessTimeout != null ? accessTimeout.getValue() : 0;
     }
 
     public TimeUnit getAccessTimeoutUnit() {
-        return accessTimeout.getUnit();
+        return accessTimeout != null ? accessTimeout.getUnit() : null;
     }
 
 
