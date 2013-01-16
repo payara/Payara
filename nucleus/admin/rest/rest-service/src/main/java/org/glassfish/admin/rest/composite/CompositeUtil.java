d876184e5206f8bc6b4047b68098885e8b12861c
tmoreau <tmoreau@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-16 18:39:31 +0000

    Change executeDeleteCommand to throw BAD_REQUEST when the CLI fails, now the resources are responsible for verifying that the entity exists before calling executeDeleteComand
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58617 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/composite/CompositeUtil.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -402,7 +402,7 @@ public class CompositeUtil {
      * @return
      */
     public ActionReporter executeDeleteCommand(Subject subject, String command, ParameterMap parameters) {
-        return executeCommand(subject, command, parameters, false, true);
+        return executeCommand(subject, command, parameters, true, true);
     }
 
     /**
