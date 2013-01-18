110d4f4f1600ff93fa664e8584679e81e6be2fb3
sandeep.shrivastava <sandeep.shrivastava@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-18 13:00:31 +0000

    GLASSFISH-19490. Add the default logging properties in the REST data.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58649 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/logging/src/main/java/com/sun/enterprise/server/logging/commands/ListLogAttributes.java
+++ nucleus/core/logging/src/main/java/com/sun/enterprise/server/logging/commands/ListLogAttributes.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -179,6 +179,7 @@ public class ListLogAttributes implements AdminCommand {
             }
             Properties restData = new Properties();
             restData.put("logAttributes", logAttributes);
+            restData.put("defaultLoggingProperties", loggingConfig.getDefaultLoggingProperties());
             report.setExtraProperties(restData);
 
         } catch (IOException ex) {
