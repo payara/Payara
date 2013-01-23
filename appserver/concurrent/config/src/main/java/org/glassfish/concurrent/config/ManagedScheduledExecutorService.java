3beb71c8230e30ee65eb1db147ea4ebb9750c932
hzhang_jn <hzhang_jn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-23 14:03:35 +0000

    Add the initial version of the config bean classes for JSR236 managed-executor-service resource element, managed-scheduled-executor-service resource element, and their sub-element pool-info. Ran QL.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58755 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/concurrent/config/src/main/java/org/glassfish/concurrent/config/ManagedScheduledExecutorService.java
+++ appserver/concurrent/config/src/main/java/org/glassfish/concurrent/config/ManagedScheduledExecutorService.java
@@ -38,36 +38,29 @@
  * holder.
  */
 
-package org.glassfish.resourcebase.resources;
+package org.glassfish.concurrent.config;
+
+import com.sun.enterprise.config.serverbeans.customvalidators.ReferenceConstraint;
+import org.glassfish.admin.cli.resources.ResourceConfigCreator;
+import org.glassfish.api.admin.RestRedirect;
+import org.glassfish.api.admin.RestRedirects;
+import org.glassfish.admin.cli.resources.UniqueResourceNameConstraint;
+import org.jvnet.hk2.config.*;
+import org.glassfish.resourcebase.resources.ResourceTypeOrder;
+import org.glassfish.resourcebase.resources.ResourceDeploymentOrder;
 
 /**
- * Created with IntelliJ IDEA.
- * User: naman
- * Date: 3/1/13
- * Time: 11:10 AM
- * To change this template use File | Settings | File Templates.
+ * Concurrency managed scheduled executor service resource definition
  */
-public enum ResourceDeploymentOrder {
 
-    /*
-    The number indicates the deployment order for particular resources.
-
-    To add new resource order add constant here and give the deployment order number for that resource.
-    Define @ResourceTypeOrder(deploymentOrder=ResourceDeploymentOrder.<your resource>) for your resource. For example
-    check JdbcResource class.
-     */
-
-    JDBC_RESOURCE(1) , JDBC_POOL(2), CONNECTOR_RESOURCE(3), CONNECTOR_POOL(4), ADMIN_OBJECT_RESOURCE(5),
-    DIAGNOSTIC_RESOURCE(6), MAIL_RESOURCE(7), CUSTOM_RESOURCE(8), EXTERNALJNDI_RESOURCE(9),
-    RESOURCEADAPTERCONFIG_RESOURCE(10), WORKSECURITYMAP_RESOURCE(11), PERSISTENCE_RESOURCE(12), CONTEXT_SERVICE(13), MANAGED_THREAD_FACTORY(14), MANAGED_EXECUTOR_SERVICE(15), MANAGED_SCHEDULED_EXECUTOR_SERVICE(16);
-
-    private int value;
-
-    private ResourceDeploymentOrder(int value) {
-        this.value = value;
-    }
-
-    public int getResourceDeploymentOrder() {
-        return value;
-    }
-};
+@Configured
+@ResourceConfigCreator(commandName="create-managed-scheduled-executor-service")
+@RestRedirects({
+ @RestRedirect(opType = RestRedirect.OpType.POST, commandName = "create-managed-scheduled-executor-service"),
+ @RestRedirect(opType = RestRedirect.OpType.DELETE, commandName = "delete-managed-scheduled-executor-service")
+})
+@ResourceTypeOrder(deploymentOrder=ResourceDeploymentOrder.MANAGED_SCHEDULED_EXECUTOR_SERVICE)
+@ReferenceConstraint(skipDuringCreation=true, payload=ManagedScheduledExecutorService.class)
+@UniqueResourceNameConstraint(message="{resourcename.isnot.unique}", payload=ManagedScheduledExecutorService.class)
+public interface ManagedScheduledExecutorService extends ManagedExecutorService {
+}
