adc63a25ad5cf6e5910f3c045be03c1a776821eb
jainalok101 <jainalok101@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-01-28 09:04:47 +0000

    Updating the copyright and removing the unwanted imports.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@58819 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/common/common-util/src/main/java/com/sun/appserv/server/util/Version.java
+++ nucleus/common/common-util/src/main/java/com/sun/appserv/server/util/Version.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -49,8 +49,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
 *
