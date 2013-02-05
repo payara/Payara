0909e0623a97647cf30667c97a2cfdce2f6c3369
tmoreau <tmoreau@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-02-05 02:50:09 +0000

    Add a static utility for converting java Properties to a string (so that the REST layer can easily share it)
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@59109 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/common/common-util/src/main/java/org/glassfish/common/util/admin/ParameterMapExtractor.java
+++ nucleus/common/common-util/src/main/java/org/glassfish/common/util/admin/ParameterMapExtractor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -185,7 +185,7 @@ public class ParameterMapExtractor {
         return sb.toString();
     }
 
-    public String propertiesValue(final Properties props, final char sep) {
+    public static String propertiesToPropertiesString(final Properties props, final char sep) {
         final StringBuilder sb = new StringBuilder();
         final String sepString = String.valueOf(sep);
         final String sepQuote = new StringBuilder("\\\\").append(sep).toString();
@@ -202,6 +202,10 @@ public class ParameterMapExtractor {
             currentSep = sepString;
         }
         return sb.toString();
+    }
+
+    public String propertiesValue(final Properties props, final char sep) {
+        return propertiesToPropertiesString(props, sep);
     }
 
     private Collection<? extends String> multipleValue(final Param p, final Object value) {
