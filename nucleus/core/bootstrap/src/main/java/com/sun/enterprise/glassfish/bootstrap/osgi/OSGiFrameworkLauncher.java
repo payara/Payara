6b26a5c3c23f85ae367c03929157148c6305ff4e
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-08 09:38:18 +0000

    Fix findbugs error
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60211 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/bootstrap/src/main/java/com/sun/enterprise/glassfish/bootstrap/osgi/OSGiFrameworkLauncher.java
+++ nucleus/core/bootstrap/src/main/java/com/sun/enterprise/glassfish/bootstrap/osgi/OSGiFrameworkLauncher.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -101,6 +101,9 @@ public class OSGiFrameworkLauncher {
     }
 
     public <T> T getService(Class<T> type) throws Exception {
+        if (framework == null) {
+            throw new IllegalStateException("OSGi framework has not yet been launched.");
+        }
         final BundleContext context = framework.getBundleContext();
         ServiceTracker tracker = new ServiceTracker(context, type.getName(), null);
         try {
