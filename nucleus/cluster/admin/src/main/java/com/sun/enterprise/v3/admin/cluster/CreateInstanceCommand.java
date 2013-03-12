0e0e7159a1fa0d6227bdb29002763088fedd07e4
tjquinn <tjquinn@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-12 21:30:42 +0000

    Fix FindBugs error.
    FindBugs complained because the logger field is not set in the constructor and is dereferenced from boostrapSecureAdminRemotely and reportFailure.  Yet the value is assigned very early in the execute method - the only non-private method - and all dereferences are in private methods invoked after that assignment is done, so the field will have a value by the time it is dereferenced.
    
    Anyway, initializing it to null placates FindBugs.
    
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60392 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/CreateInstanceCommand.java
+++ nucleus/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/CreateInstanceCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -117,7 +117,7 @@ public class CreateInstanceCommand implements AdminCommand {
     private String systemProperties;
     @Param(name = "instance_name", primary = true)
     private String instance;
-    private Logger logger;
+    private Logger logger = null; // set in execute and all references occur after that assignment
     private AdminCommandContext ctx;
     private Node theNode = null;
     private String nodeHost = null;
