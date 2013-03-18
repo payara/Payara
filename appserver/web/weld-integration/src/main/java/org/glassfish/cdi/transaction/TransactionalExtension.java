ed55fab9a4c7d7fc9d120688d69832143a8bdbfe
jjsnyder83 <jjsnyder83@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-18 20:57:34 +0000

    Transactional interceptors fix: http://java.net/jira/browse/GLASSFISH-19921
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60546 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/weld-integration/src/main/java/org/glassfish/cdi/transaction/TransactionalExtension.java
+++ appserver/web/weld-integration/src/main/java/org/glassfish/cdi/transaction/TransactionalExtension.java
@@ -40,155 +40,35 @@
 
 package org.glassfish.cdi.transaction;
 
-import com.sun.logging.LogDomains;
-
-import javax.enterprise.context.Dependent;
-import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.Any;
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.*;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.util.AnnotationLiteral;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.logging.Logger;
 
 /**
  * The CDI Portable Extension for @Transactional.
  */
 public class TransactionalExtension implements Extension {
 
-  private static Logger _logger = LogDomains.getLogger(
-            TransactionalInterceptorMandatory.class, LogDomains.JTA_LOGGER);
+    public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery beforeBeanDiscoveryEvent, final BeanManager beanManager) {
+        // Register the interceptors so no beans.xml is needed
+        AnnotatedType annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorMandatory.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
 
-  public void afterBeanDiscovery( @Observes AfterBeanDiscovery event, BeanManager manager ) {
-      _logger.fine("org.glassfish.cdi.transaction.TransactionalExtension.afterBeanDiscovery event = [" + event + "], manager = [" + manager + "] start");
-//      Bean transactionalInterceptorBaseBean = createLocalBean(manager, TransactionalInterceptorBase.class);
-//      event.addBean(transactionalInterceptorBaseBean);
-      Bean transactionalInterceptorMandatoryBean = createLocalBean(manager, TransactionalInterceptorMandatory.class);
-      event.addBean(transactionalInterceptorMandatoryBean);
-      Bean transactionalInterceptorNeverBean = createLocalBean(manager, TransactionalInterceptorNever.class);
-      event.addBean(transactionalInterceptorNeverBean);
-      Bean transactionalInterceptorNotSupportedBean = createLocalBean(manager, TransactionalInterceptorNotSupported.class);
-      event.addBean(transactionalInterceptorNotSupportedBean);
-      Bean transactionalInterceptorRequiredBean = createLocalBean(manager, TransactionalInterceptorRequired.class);
-      event.addBean(transactionalInterceptorRequiredBean);
-      Bean transactionalInterceptorRequiresNewBean = createLocalBean(manager, TransactionalInterceptorRequiresNew.class);
-      event.addBean(transactionalInterceptorRequiresNewBean);
-      Bean transactionalInterceptorSupportsBean = createLocalBean(manager, TransactionalInterceptorSupports.class);
-      event.addBean(transactionalInterceptorSupportsBean);
-      _logger.fine("org.glassfish.cdi.transaction.TransactionalExtension.afterBeanDiscovery event = [" + event + "], manager = [" + manager + "] end");
-    }
+        annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorNever.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
 
-    private Bean createLocalBean(BeanManager beanManager, Class beanClass) {
-        AnnotatedType annotatedType = beanManager.createAnnotatedType(beanClass);
-        LocalBean localBean = new LocalBean(beanClass);
-        InjectionTargetFactory injectionTargetFactory = beanManager.getInjectionTargetFactory(annotatedType);
-        localBean.setInjectionTarget(injectionTargetFactory.createInjectionTarget(localBean));
-        return localBean;
-    }
+        annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorNotSupported.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
 
-    public class LocalBean implements Bean {
-        private Class beanClass;
-        private InjectionTarget injectionTarget;
+        annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorRequired.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
 
-        public LocalBean(Class beanClass) {
-            this.beanClass = beanClass;
-        }
+        annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorRequiresNew.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
 
-        public LocalBean(Class beanClass, InjectionTarget injectionTarget) {
-            this.beanClass = beanClass;
-            this.injectionTarget = injectionTarget;
-        }
-
-        public void setInjectionTarget(InjectionTarget injectionTarget) {
-            this.injectionTarget = injectionTarget;
-        }
-
-        @Override
-        public Class<?> getBeanClass() {
-            return beanClass;
-        }
-
-        @Override
-        public Set<InjectionPoint> getInjectionPoints() {
-            return injectionTarget.getInjectionPoints();
-        }
-
-        @Override
-        public String getName() {
-            return beanClass.getName();
-        }
-
-        @Override
-        public Set<Annotation> getQualifiers() {
-            Set<Annotation> qualifiers = new HashSet<Annotation>();
-//            qualifiers.add(new AnnotationLiteral<Default>() {});
-//            qualifiers.add(new AnnotationLiteral<Any>() {});
-            return qualifiers;
-        }
-
-        @Override
-        public Class<? extends Annotation> getScope() {
-            return Dependent.class;
-        }
-
-        @Override
-        public Set<Class<? extends Annotation>> getStereotypes() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public Set<Type> getTypes() {
-            Set<Type> types = new HashSet<Type>();
-            types.add(beanClass);
-            boolean loop = true;
-            Class clazz = beanClass;
-            while (loop) {
-                Class[] interfaces = clazz.getInterfaces();
-                for (Class t : interfaces) {
-                    types.add(t);
-                }
-                clazz = clazz.getSuperclass();
-                if (clazz == null) {
-                    loop = false;
-                    break;
-                } else {
-                    types.add(clazz);
-                }
-            }
-            return types;
-        }
-
-        @Override
-        public boolean isAlternative() {
-            return false;
-        }
-
-        @Override
-        public boolean isNullable() {
-            return false;
-        }
-
-        @Override
-        public Object create(CreationalContext ctx) {
-            Object instance = injectionTarget.produce(ctx);
-            injectionTarget.inject(instance, ctx);
-            injectionTarget.postConstruct(instance);
-            return instance;
-        }
-
-        @Override
-        public void destroy(Object instance, CreationalContext ctx) {
-            injectionTarget.preDestroy(instance);
-            injectionTarget.dispose(instance);
-            ctx.release();
-        }
+        annotatedType = beanManager.createAnnotatedType( TransactionalInterceptorSupports.class );
+        beforeBeanDiscoveryEvent.addAnnotatedType( annotatedType );
     }
 
  }
