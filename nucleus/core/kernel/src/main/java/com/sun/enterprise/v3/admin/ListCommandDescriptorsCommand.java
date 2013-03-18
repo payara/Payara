fb89af432988bb4d878ba5ca693e8c2d8f811e6b
bnevins <bnevins@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-18 23:08:09 +0000

    The list-descriptors command pre-dated the convention for prepending underscores to hide commands. I changed the name of this command to _list-descriptors
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60549 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/admin/ListCommandDescriptorsCommand.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/admin/ListCommandDescriptorsCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2006-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -60,7 +60,7 @@ import org.jvnet.hk2.annotations.Service;
  * @author Jerome Dochez
  * 
  */
-@Service(name="list-descriptors", metadata="mode=debug")
+@Service(name="_list-descriptors")
 @PerLookup
 @I18n("list.commands")
 @AccessRequired(resource="domain", action="dump")
