870b264c8f76ac58562d806704ed72feb0a6729a
japod <japod@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-20 23:19:23 +0000

    jersey upgrade: 2.0-m13-2 -> 2.0-m13-3, hk2 upgrade: 2.1.73->2.1.76 reviewed by Romain
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60646 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/weld-integration/src/main/java/org/glassfish/cdi/hk2/HK2IntegrationExtension.java
+++ appserver/web/weld-integration/src/main/java/org/glassfish/cdi/hk2/HK2IntegrationExtension.java
@@ -63,88 +63,88 @@ import org.glassfish.hk2.api.ServiceLocator;
 
 /**
  * The touch-point for hk2 integration with CDI
- * 
+ *
  * @author jwells
  *
  */
 public class HK2IntegrationExtension implements Extension {
     private final HashMap<Long, ActiveDescriptor<?>> foundWithHK2 = new HashMap<Long, ActiveDescriptor<?>>();
     private final ServiceLocator locator = HK2IntegrationUtilities.getApplicationServiceLocator();
-    
+
     /**
      * Called by CDI, gathers up all of the injection points known to hk2
-     * 
+     *
      * @param pit The injection target even from CDI
      */
     @SuppressWarnings("unused")
     private <T> void injectionTargetObserver(@Observes ProcessInjectionTarget<T> pit) {
         if (locator == null) return;
-        
+
         InjectionTarget<?> injectionTarget = pit.getInjectionTarget();
         Set<InjectionPoint> injectionPoints = injectionTarget.getInjectionPoints();
-        
+
         for (InjectionPoint injectionPoint : injectionPoints) {
             Injectee injectee = HK2IntegrationUtilities.convertInjectionPointToInjectee(injectionPoint);
-            
+
             ActiveDescriptor<?> descriptor = locator.getInjecteeDescriptor(injectee);
-            if (descriptor == null) continue;
-            
+            if (descriptor == null || descriptor.getServiceId() == null) continue;
+
             // using a map removes duplicates
             foundWithHK2.put(descriptor.getServiceId(), descriptor);
         }
     }
-    
+
     /**
      * Called by CDI after going through all of the injection points.  For each
      * service known to hk2 adds a CDI bean
-     * 
+     *
      * @param abd This is used just to mark the type of the event
      */
     @SuppressWarnings({ "unused", "unchecked", "rawtypes" })
     private void afterDiscoveryObserver(@Observes AfterBeanDiscovery abd) {
         if (locator == null) return;
-        
+
         HashSet<Class<? extends Annotation>> customScopes = new HashSet<Class<? extends Annotation>>();
-        
+
         for (ActiveDescriptor<?> descriptor : foundWithHK2.values()) {
             abd.addBean(new HK2CDIBean(locator, descriptor));
-            
+
             customScopes.add(descriptor.getScopeAnnotation());
         }
-        
+
         customScopes.remove(PerLookup.class);
         customScopes.remove(Singleton.class);
-        
+
         List<org.glassfish.hk2.api.Context> hk2Contexts = locator.getAllServices(org.glassfish.hk2.api.Context.class);
-        
+
         for (org.glassfish.hk2.api.Context hk2Context : hk2Contexts) {
             if (!customScopes.contains(hk2Context.getScope())) {
                 continue;
             }
-            
+
             abd.addContext(new HK2ContextBridge(hk2Context));
         }
-        
+
         foundWithHK2.clear();  // No need to keep these references around
     }
-    
+
     /**
      * Called by CDI after it has been completely validated.  Will add the JIT resolver to HK2
      * with the BeanManager
-     * 
+     *
      * @param event This is just to mark the type of the event
      * @param manager The manager that will be used to get references
      */
     @SuppressWarnings("unused")
     private void afterDeploymentValidation(@Observes AfterDeploymentValidation event) {
         if (locator == null) return;
-        
+
         DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
         DynamicConfiguration config = dcs.createDynamicConfiguration();
-        
+
         config.addActiveDescriptor(CDISecondChanceResolver.class);
         config.addActiveDescriptor(CDIContextBridge.class);
-        
+
         config.commit();
     }
 
