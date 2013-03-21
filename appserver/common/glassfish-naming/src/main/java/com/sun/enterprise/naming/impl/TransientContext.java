04d7853cf162d4827a77a100f13987f6130cc99c
guojun.shan@oracle.com <guojun.shan@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-21 02:20:26 +0000

    GLASSFISH-19952 Typo in TransientContext.drillDownAndCreateSubcontext(TransientContext.java:140)
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60654 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/common/glassfish-naming/src/main/java/com/sun/enterprise/naming/impl/TransientContext.java
+++ appserver/common/glassfish-naming/src/main/java/com/sun/enterprise/naming/impl/TransientContext.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2006-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -127,7 +127,7 @@ public class TransientContext implements Context, Serializable {
             if (n.size() <= 1) { // bottom
                 if (bindings.containsKey(name)) {
                     throw new NameAlreadyBoundException("Subcontext " +
-                            name + "already present");
+                            name + " already present");
                 }
 
                 TransientContext ctx = null;
