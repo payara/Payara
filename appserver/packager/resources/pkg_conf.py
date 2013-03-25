03d07bd1e4946ff6cf41798cab0f8640c6d9f929
jungicz <jungicz@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-03-25 10:30:45 +0000

    integrate metro 2.3-b259
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@60763 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/packager/resources/pkg_conf.py
+++ appserver/packager/resources/pkg_conf.py
@@ -66,7 +66,7 @@ javadb_version="10.5.3.0,0-0"
 corba_version="4.0.0,0-4"
 jsf_version="2.2.0,0-12"
 grizzly_version="2.3,0-12"
-metro_version="2.3,0-240"
+metro_version="2.3,0-259"
 javahelp_version="2.0.2,0-1"
 shoal_version="1.6.18,0-0"
 
