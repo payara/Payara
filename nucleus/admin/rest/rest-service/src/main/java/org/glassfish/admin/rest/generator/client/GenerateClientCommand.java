40c2151eb90328fe675d128535aa1577f9685df0
jdlee <jdlee@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-04-01 14:35:59 +0000

    Disable generate-client-command for this release.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61063 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/generator/client/GenerateClientCommand.java
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/generator/client/GenerateClientCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -77,7 +77,8 @@ import org.glassfish.api.admin.AccessRequired;
  *
  * @author jdlee
  */
-@Service(name = "generate-rest-client")
+// TODO: This command is not quite ready yet, so we disable it until
+//@Service(name = "__generate-rest-client")
 @PerLookup
 @CommandLock(CommandLock.LockType.NONE)
 @ExecuteOn({RuntimeType.DAS})
