a2f3efd72f78f74dd95d780b9c63e72b6676a0ec
shaun.pei@oracle.com <shaun.pei@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-04-09 19:36:00 +0000

    copyright fix
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61268 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/security/core-ee/src/main/java/com/sun/enterprise/security/perms/VoidPermission.java
+++ appserver/security/core-ee/src/main/java/com/sun/enterprise/security/perms/VoidPermission.java
@@ -1,4 +1,5 @@
-/* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+/* 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
  *
