67ea272a2a383ab493ad21f5db536c6d3a0e5e6a
jwells <jwells@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-04-17 16:16:32 +0000

    GLASSFISH-20206 Reenable multi-threaded boot services
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61469 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/AppServerStartup.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/AppServerStartup.java
@@ -192,9 +192,9 @@ public class AppServerStartup implements PostConstruct, ModuleStartup {
             }
         }
         else {
-            // if (platform == null || !(platform.equals(FELIX_PLATFORM) || platform.equals(STATIC_PLATFORM))) {
+            if (platform == null || !(platform.equals(FELIX_PLATFORM) || platform.equals(STATIC_PLATFORM))) {
                 runLevelController.setThreadingPolicy(RunLevelController.ThreadingPolicy.USE_NO_THREADS);
-            // }
+            }
         }
         
         int numThreads = Integer.getInteger(MAX_STARTUP_THREAD_PROPERTY, DEFAULT_STARTUP_THREADS);
