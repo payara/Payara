63df6e80cd058fdb69b5f61d18fdcb2b82dc3964
guojun.shan@oracle.com <guojun.shan@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-04-28 02:14:22 +0000

    GLASSFISH-20407 GlassFish "client without the ACC" mode incorrectly returns a value for java:comp/InAppClientContainer
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61695 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/common/container-common/src/main/java/com/sun/enterprise/container/common/impl/JavaCompNamingProxy.java
+++ appserver/common/container-common/src/main/java/com/sun/enterprise/container/common/impl/JavaCompNamingProxy.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -60,6 +60,8 @@ public final class JavaCompNamingProxy implements NamedNamingObjectProxy {
 			Boolean isInAppClientContainer = Boolean.FALSE;
 			if (processEnv.getProcessType() == ProcessEnvironment.ProcessType.ACC) {
 				isInAppClientContainer = Boolean.TRUE;
+			} else if (processEnv.getProcessType() == ProcessEnvironment.ProcessType.Other) {
+				throw new NamingException("Lookup failed for '" + name + "'");
 			}
 			return isInAppClientContainer;
 		}
