4469f74724912df719afc84600ba7ec9f1126367
shaun.pei@oracle.com <shaun.pei@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-05-06 12:11:30 +0000

    Bug16761878 - porting fix from 4.0 to main (CTS test client side permission issue)
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61839 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/security/ejb.security/src/main/java/com/sun/enterprise/iiop/security/SecurityContextUtil.java
+++ appserver/security/ejb.security/src/main/java/com/sun/enterprise/iiop/security/SecurityContextUtil.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -98,7 +98,12 @@ public class SecurityContextUtil implements PostConstruct {
     }
 
     public void postConstruct() {
-        policy = Policy.getPolicy();
+        AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            public Object run() {
+                policy = Policy.getPolicy();
+                return null;
+            }
+        });
     }
     /**
      * This is called by the CSIv2 interceptor on the client before
