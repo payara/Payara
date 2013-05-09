65b841cd04202ee791ac97cb05fa0989d5514cae
hvilekar <hvilekar@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-05-09 16:47:10 +0000

    Add diagnostics warning message
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61944 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/GlassFishORBInitializer.java
+++ appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/GlassFishORBInitializer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -145,7 +145,9 @@ public class GlassFishORBInitializer extends org.omg.CORBA.LocalObject
             }
 
         } catch (Exception e) {
-            fineLog( "Exception registering interceptors", e);
+            if (_logger.isLoggable(Level.WARNING)) {
+                _logger.log(Level.WARNING, "Exception registering interceptors", e ) ;
+            }
             throw new RuntimeException(e.getMessage(), e);
         }
     }
