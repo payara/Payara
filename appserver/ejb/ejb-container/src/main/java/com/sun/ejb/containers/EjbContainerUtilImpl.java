d7273b09fb18c46f27b6ec57cb1a9580a59e96e0
mvatkina <mvatkina@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-05-15 23:00:09 +0000

    Fix NPE on addWork if container is being shut down
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@61998 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/EjbContainerUtilImpl.java
+++ appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/EjbContainerUtilImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -392,7 +392,9 @@ public class EjbContainerUtilImpl
     }
 
     public void addWork(Runnable task) {
-        defaultThreadPoolExecutor.submit(task);
+        if (defaultThreadPoolExecutor != null) {
+            defaultThreadPoolExecutor.submit(task);
+        }
     }
 
     public EjbDescriptor ejbIdToDescriptor(long ejbId) {
