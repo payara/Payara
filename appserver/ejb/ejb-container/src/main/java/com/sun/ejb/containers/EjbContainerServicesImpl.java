8d55db40a0b90cbf94238c3c7f5281de7781cc60
phil.zampino <phil.zampino@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-06-12 13:09:22 +0000

    GLASSFISH-20323 - Overlooked removal of Serializable from the implements clause when the other related changes were made. Reviewed by marina.vatkina@oracle.com . Passed QL, EJB & CDI devtests
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62197 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/EjbContainerServicesImpl.java
+++ appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/EjbContainerServicesImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -67,7 +67,7 @@ import com.sun.logging.LogDomains;
  *
  */
 @Service
-public class EjbContainerServicesImpl implements EjbContainerServices, Serializable {
+public class EjbContainerServicesImpl implements EjbContainerServices {
 
 
 
