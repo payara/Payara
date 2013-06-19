44b8f49b5f74c7871ec7312d51311b93526ec7b4
ss141213 <ss141213@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-06-19 05:19:55 +0000

    GLASSFISH-20646: [regression] felix-web-console-extension fails with insufficient security permission
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62246 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/osgi-platforms/felix-webconsole-extension/src/main/java/org/glassfish/osgi/felixwebconsoleextension/GlassFishSecurityProvider.java
+++ appserver/osgi-platforms/felix-webconsole-extension/src/main/java/org/glassfish/osgi/felixwebconsoleextension/GlassFishSecurityProvider.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -50,9 +50,14 @@ import org.glassfish.embeddable.GlassFishException;
 import org.glassfish.security.services.api.authentication.AuthenticationService;
 import org.osgi.framework.BundleContext;
 
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
 /**
  *
  * @author tangyong@cn.fujitsu.com
+ * @author sanjeeb.sahoo@oracle.com
  */
 public class GlassFishSecurityProvider implements WebConsoleSecurityProvider {
 	
@@ -78,7 +83,7 @@ public class GlassFishSecurityProvider implements WebConsoleSecurityProvider {
 		gf = getGlassFish();
 		AuthenticationService authService = null;
 		try{
-		    authService = gf.getService(AuthenticationService.class);
+            authService = getAuthService();
 		}catch(GlassFishException gfe){
 			gfe.printStackTrace();
 			return null;
@@ -96,7 +101,24 @@ public class GlassFishSecurityProvider implements WebConsoleSecurityProvider {
           return fs;		
 	}
 
-	@Override
+    private AuthenticationService getAuthService() throws GlassFishException {
+        // Authentication Service is protected, so we need to access within doPrivileged
+        // It must be done irrespective of security manager, because the permission is enforced
+        // when a security policy file is present.
+        try {
+            return AccessController.doPrivileged(new PrivilegedExceptionAction<AuthenticationService>() {
+                @Override
+                public AuthenticationService run() throws GlassFishException {
+                    return gf.getService(AuthenticationService.class);
+                }
+            });
+        } catch (PrivilegedActionException e) {
+            throw GlassFishException.class.cast(e.getException());
+        }
+
+    }
+
+    @Override
 	public boolean authorize(Object user, String role) {
 		// TODO Auto-generated method stub
 		return false;
