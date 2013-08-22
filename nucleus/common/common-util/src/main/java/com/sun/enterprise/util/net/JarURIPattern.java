0353f30cabd8659eb29946634274ba34a853b97a
swchan2 <swchan2@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-08-22 20:44:21 +0000

    fix FindBugs RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62594 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/common/common-util/src/main/java/com/sun/enterprise/util/net/JarURIPattern.java
+++ nucleus/common/common-util/src/main/java/com/sun/enterprise/util/net/JarURIPattern.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -78,7 +78,7 @@ public class JarURIPattern {
         String fileName = file.getName();
 
         // only look at jar file
-        if (fileName != null && fileName.endsWith(".jar")) {
+        if (fileName.endsWith(".jar")) {
             JarFile jarFile = null;
             try {
                 jarFile = new JarFile(new File(uri));
