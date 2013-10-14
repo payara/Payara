5b9f4a2629ab5022eb8e310684ce9e5919edf071
tvlatas <tvlatas@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-10-14 22:21:09 +0000

    Low priority FindBugs
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62785 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/flashlight/framework/src/main/java/org/glassfish/flashlight/impl/client/ProbeProviderClassFileTransformer.java
+++ nucleus/flashlight/framework/src/main/java/org/glassfish/flashlight/impl/client/ProbeProviderClassFileTransformer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -82,7 +82,7 @@ public class ProbeProviderClassFileTransformer implements ClassFileTransformer {
     private String providerClassName = null;
     private Map<String, FlashlightProbe> probes = new HashMap<String, FlashlightProbe>();
     private ClassWriter cw;
-    private boolean enabled = false;
+    private volatile boolean enabled = false;
     private boolean allProbesTransformed = true;
     private boolean transformerAdded = false;
     private int count = 0;  // Only used for debug so we can look at the before/after class dumps for each iteration
