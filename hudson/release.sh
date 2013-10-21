9e00f183a186f4e074fdc021b783f0bd4f26164a
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-10-21 08:32:45 +0000

    fix typo
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62794 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- hudson/release.sh
+++ hudson/release.sh
@@ -74,7 +74,7 @@ fi
 # value can be either "HEAD" or an SVN revision
 if [ ! -z $SYNCHTO ] && [ ${#SYNCHTO} -gt 0 ]
 then
-    printf "\n%s\n\n" "Synchronizing to $SYCHTO"
+    printf "\n%s\n\n" "Synchronizing to $SYNCHTO"
 else
     svn co --depth=files $GF_WORKSPACE_URL_SSH/trunk/main tmp
     SVN_REVISION=`svn propget svn:keyword main | grep 'clean_' | sed s@'clean_'@@g | awk '{print $2}'`
