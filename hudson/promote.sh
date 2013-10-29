d42a1c04621314e886bf1d432d7d66ea9e50239e
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-10-29 16:21:20 +0000

    uncomment, promote should work now
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62911 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- hudson/promote.sh
+++ hudson/promote.sh
@@ -2,14 +2,14 @@
 
 source `dirname $0`/common.sh
 
-#init_storage_area
+init_storage_area
 
-#promote_bundle $PROMOTED_BUNDLES/web-ips-ml.zip $PRODUCT_GF-$PRODUCT_VERSION_GF-web-$BUILD_ID-ml.zip
-#promote_bundle $PROMOTED_BUNDLES/glassfish-ips-ml.zip $PRODUCT_GF-$PRODUCT_VERSION_GF-$BUILD_ID-ml.zip
-#promote_bundle $PROMOTED_BUNDLES/nucleus-new.zip nucleus-$PRODUCT_VERSION_GF-$BUILD_ID.zip
-#promote_bundle $PROMOTED_BUNDLES/version-info.txt version-info-$BUILD_ID.txt
+promote_bundle $PROMOTED_BUNDLES/web-ips-ml.zip $PRODUCT_GF-$PRODUCT_VERSION_GF-web-$BUILD_ID-ml.zip
+promote_bundle $PROMOTED_BUNDLES/glassfish-ips-ml.zip $PRODUCT_GF-$PRODUCT_VERSION_GF-$BUILD_ID-ml.zip
+promote_bundle $PROMOTED_BUNDLES/nucleus-new.zip nucleus-$PRODUCT_VERSION_GF-$BUILD_ID.zip
+promote_bundle $PROMOTED_BUNDLES/version-info.txt version-info-$BUILD_ID.txt
 
-#create_symlinks
+create_symlinks
 
 if [ "weekly" == "$1" ]
 then
@@ -19,4 +19,4 @@ then
    	printf "\n%s \n\n" "===== TODO, RECORD REVISION ====="
 fi
 
-#send_notification
+send_notification
