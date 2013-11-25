521afe3399a8d5232e008d32c55e7ed59bccbc0b
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-11-25 10:15:18 +0000

    use product version for version-info.txt
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@62968 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- hudson/promote.sh
+++ hudson/promote.sh
@@ -7,7 +7,7 @@ init_storage_area
 promote_bundle ${PROMOTED_BUNDLES}/web-ips-ml.zip ${PRODUCT_GF}-${PRODUCT_VERSION_GF}-web-${BUILD_ID}-ml.zip
 promote_bundle ${PROMOTED_BUNDLES}/glassfish-ips-ml.zip ${PRODUCT_GF}-${PRODUCT_VERSION_GF}-${BUILD_ID}-ml.zip
 promote_bundle ${PROMOTED_BUNDLES}/nucleus-new.zip nucleus-${PRODUCT_VERSION_GF}-${BUILD_ID}.zip
-promote_bundle ${PROMOTED_BUNDLES}/version-info.txt version-info-${BUILD_ID}.txt
+promote_bundle ${PROMOTED_BUNDLES}/version-info.txt version-info-${PRODUCT_VERSION_GF}-${BUILD_ID}.txt
 
 create_symlinks
 
