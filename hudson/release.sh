9c4c6150775aaa3da3ad3113d9310343df76887f
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2013-12-09 17:12:59 +0000

    fix typo
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63012 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- hudson/release.sh
+++ hudson/release.sh
@@ -16,12 +16,12 @@ then
     delete_svn_tag ${RELEASE_VERSION}
     svn_checkout ${SVN_REVISION}
     release_prepare
-    build_release "clean deploy" "release-phase2,ips,embedded,javaee-api"
+    release_build "clean deploy" "release-phase2,ips,embedded,javaee-api"
 elif [ "${BUILD_KIND}" == "nightly" ]
 then
     svn_checkout ${SVN_REVISION}
     run_findbugs
-    build_release "clean install" "ips,javaee-api"
+    release_build "clean install" "ips,javaee-api"
 fi
 
 archive_bundles
