a8d94ec949d08cce4f219dece2df212000db0b65
jjsnyder83 <jjsnyder83@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2014-06-24 14:56:02 +0000

    Fix NPE when groups are null
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63380 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/security/core/src/main/java/com/sun/enterprise/security/auth/realm/file/FileRealm.java
+++ nucleus/security/core/src/main/java/com/sun/enterprise/security/auth/realm/file/FileRealm.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -295,6 +295,9 @@ public final class FileRealm extends IASRealm
         throws NoSuchUserException
     {
         String groups[] = helper.getGroupNames(username);
+        if ( groups == null ) {
+          groups = new String[]{};
+        }
         groups = addAssignGroups(groups);
         return Collections.enumeration(Arrays.asList(groups));
     }
