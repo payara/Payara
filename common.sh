46ec7a078147b889114b2bc17c7d9ecc21919dd9
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2014-07-08 12:36:39 +0000

    fix create_changes_info
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63440 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- common.sh
+++ common.sh
@@ -578,7 +578,7 @@ create_changes_info(){
     curl $JOB_URL/lastSuccessfulBuild/bundles/version-info.txt > ${WORKSPACE}/previous-version-info.txt
     PREVIOUS_SVN_REV=`cat ${WORKSPACE}/previous-version-info.txt | head -1 | awk '{print $2}'`
     touch ${WORKSPACE}/changes.txt
-    if [ "${PREVIOUS_SVN_REV}" -ne "${SVN_REVISION}" ] ; then
+    if [ "${PREVIOUS_SVN_REV}" != "${SVN_REVISION}" ] ; then
         svn log -r ${PREVIOUS_SVN_REV}:${SVN_REVISION} $WORKSPACE/main > ${WORKSPACE}/changes.txt
         printf "\n%s\n\n" "==== CHANGELOG ===="
         cat ${WORKSPACE}/changes.txt
