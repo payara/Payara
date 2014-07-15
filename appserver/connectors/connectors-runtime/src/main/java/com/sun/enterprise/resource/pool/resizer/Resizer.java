4ec588d138b5f9408df0f588a4989b372483df0c
kdevaras <kdevaras@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2014-07-15 02:34:42 +0000

    GLASSFISH-19476. Add valid connections to activeResources for prefer-validate-over-recreate.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63492 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/resizer/Resizer.java
+++ appserver/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/resizer/Resizer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -200,6 +200,7 @@ public class
                         //preferValidateOverrecreate true and connection is valid within SPS
                         validConnectionsCounter++;
                         idleConnKeptInSteadyCounter++;
+                        activeResources.add(h);
                         debug("PreferValidateOverRecreate: Keeping idle resource "
                                 + h + " in the steady part of the free pool "
                                 + "as the RA reports it to be valid (" + validConnectionsCounter
