b39a0400b7ba3ebf524b8a34eb265256dea50de9
kchung <kchung@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2014-08-04 20:57:22 +0000

    Fix memory leak with CDI: https://java.net/jira/browse/GLASSFISH-21083 Thanks smillidge-c2b2 for the patch.
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63568 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/web-glue/src/main/java/com/sun/enterprise/web/jsp/ResourceInjectorImpl.java
+++ appserver/web/web-glue/src/main/java/com/sun/enterprise/web/jsp/ResourceInjectorImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -116,6 +116,7 @@ public class ResourceInjectorImpl implements ResourceInjector {
         if (desc != null) {
             try {
                 injectionMgr.invokeInstancePreDestroy(handler, desc);
+                injectionMgr.destroyManagedObject(handler);
             } catch (Exception e) {
                 String msg = _rb.getString(EXCEPTION_DURING_JSP_TAG_HANDLER_PREDESTROY);
                 msg = MessageFormat.format(msg, handler);
