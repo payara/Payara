f74a74808240cbdb5e1ace81aaebc3b87f6dfe39
aribandy <aribandy@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-04-05 07:47:48 +0000

    Fix for GLASSFISH-21251
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63831 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/concurrent/concurrent-impl/src/main/java/org/glassfish/concurrent/admin/ManagedExecutorServiceBaseManager.java
+++ appserver/concurrent/concurrent-impl/src/main/java/org/glassfish/concurrent/admin/ManagedExecutorServiceBaseManager.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -200,6 +200,9 @@ public abstract class ManagedExecutorServiceBaseManager implements ResourceManag
         managedExecutorService.setKeepAliveSeconds(keepAliveSeconds);
         managedExecutorService.setThreadLifetimeSeconds(threadLifetimeSeconds);
         managedExecutorService.setEnabled(enabled);
+        //Fix for GLASSFISH-21251
+        managedExecutorService.setLongRunningTasks(longRunningTasks);
+        //end of fix GLASSFISH-21251
         if (properties != null) {
             for ( Map.Entry e : properties.entrySet()) {
                 Property prop = managedExecutorService.createChild(Property.class);
