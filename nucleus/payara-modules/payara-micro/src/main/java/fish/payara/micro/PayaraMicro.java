ee891451047e3fa606d262b7a174d4fa7c029037
Steve Millidge <smillidge@c2b2.co.uk>
2015-04-05 11:37:25 +0100

    Added license headers and javadocs

--- nucleus/payara-modules/payara-micro/src/main/java/fish/payara/micro/PayaraMicro.java
+++ nucleus/payara-modules/payara-micro/src/main/java/fish/payara/micro/PayaraMicro.java
@@ -35,7 +35,8 @@ import org.glassfish.embeddable.GlassFishProperties;
 import org.glassfish.embeddable.GlassFishRuntime;
 
 /**
- * Main class for Bootstrapping Payara Micro Edition
+ * Main class for Bootstrapping Payara Micro Edition This class is used from
+ * applications to creare a full JavaEE runtime environment and deploywar files.
  *
  * @author steve
  */
@@ -58,16 +59,54 @@ public class PayaraMicro {
     private boolean noCluster = false;
     private static PayaraMicro instance;
 
+    /**
+     * Runs a Payara Micro server used via java -jar payara-micro.jar
+     *
+     * @param args Command line arguments for PayaraMicro Usage: --noCluster
+     * Disables clustering<br/>
+     * --port sets the http port<br/>
+     * --sslPort sets the https port number<br/>
+     * --mcAddress sets the cluster multicast group<br/>
+     * --mcPort sets the cluster multicast port<br/>
+     * --startPort sets the cluster start port number<br/>
+     * --name sets the instance name<br/>
+     * --rootDir Sets the root configuration directory and saves the
+     * configuration across restarts<br/>
+     * --deploymentDir if set to a valid directory all war files in this
+     * directory will be deployed<br/>
+     * --deploy specifies a war file to deploy<br/>
+     * --domainConfig overrides the complete server configuration with an
+     * alternative domain.xml file<br/>
+     * --minHttpThreads the minimum number of threads in the HTTP thread
+     * pool<br/>
+     * --maxHttpThreads the maximum number of threads in the HTTP thread
+     * pool<br/>
+     * --help Shows this message and exits\n
+     * @throws BootstrapException If there is a problem booting the server
+     */
     public static void main(String args[]) throws BootstrapException {
         PayaraMicro main = getInstance();
         main.scanArgs(args);
         main.bootStrap();
     }
 
+    /**
+     * Obtains the static singleton instance of the Payara Micro Server. If it
+     * does not exist it will be create.
+     *
+     * @return The singleton instance
+     */
     public static PayaraMicro getInstance() {
         return getInstance(true);
     }
 
+    /**
+     *
+     * @param create If false the instance won't be created if it has not been
+     * initialised
+     * @return null if no instance exists and create is false. Otherwise returns
+     * the singleton instance
+     */
     public static PayaraMicro getInstance(boolean create) {
         if (instance == null) {
             instance = new PayaraMicro();
@@ -75,92 +114,178 @@ public class PayaraMicro {
         return instance;
     }
 
-    private PayaraMicro() {
-        addShutdownHook();
-    }
-
-    private PayaraMicro(String args[]) {
-        scanArgs(args);
-        addShutdownHook();
-    }
-
+    /**
+     * Gets the cluster group
+     * @return The Multicast Group that will beused for the Hazelcast clustering 
+     */
     public String getClusterMulticastGroup() {
         return hzMulticastGroup;
     }
 
+    /**
+     * Sets the cluster group used for Payara Micro clustering
+     * used for cluster communications and discovery. Each Payara Micro cluster should
+     * have different values for the MulticastGroup
+     * @param hzMulticastGroup String representation of the multicast group 
+     * @return 
+     */
     public PayaraMicro setClusterMulticastGroup(String hzMulticastGroup) {
         this.hzMulticastGroup = hzMulticastGroup;
         return this;
     }
 
+    /**
+     * Gets the cluster multicast  port used for cluster communications
+     * @return The configured cluster port
+     */
     public int getClusterPort() {
         return hzPort;
     }
 
+    /**
+     * Sets the multicast group used for Payara Micro clustering used for cluster
+     * communication and discovery. Each Payara Micro cluster should have different
+     * values for the cluster port
+     * @param hzPort The port number
+     * @return 
+     */
     public PayaraMicro setClusterPort(int hzPort) {
         this.hzPort = hzPort;
         return this;
     }
 
+    /**
+     * Gets the instance listen port number used by clustering. 
+     * This number will be incremented automatically
+     * if the port is unavailable due to another instance running on the same host,
+     * @return The start port number
+     */
     public int getClusterStartPort() {
         return hzStartPort;
     }
 
+    /**
+     * Sets the start port number for the Payara Micro to listen on for cluster
+     * communications.
+     * @param hzStartPort Start port number
+     * @return 
+     */
     public PayaraMicro setClusterStartPort(int hzStartPort) {
         this.hzStartPort = hzStartPort;
         return this;
     }
 
+    /**
+     * The configured port Payara Micro will use for HTTP requests.
+     * @return The HTTP port
+     */
     public int getHttpPort() {
         return httpPort;
     }
 
+    /**
+     * Sets the port used for HTTP requests
+     * @param httpPort The port number
+     * @return 
+     */
     public PayaraMicro setHttpPort(int httpPort) {
         this.httpPort = httpPort;
         return this;
     }
 
+    /**
+     * The configured port for HTTPS requests
+     * @return  The HTTPS port
+     */
     public int getSslPort() {
         return sslPort;
     }
 
+    /**
+     * Sets the configured port for HTTPS requests.
+     * If this is not set HTTPS is disabled
+     * @param sslPort The HTTPS port
+     * @return 
+     */
     public PayaraMicro setSslPort(int sslPort) {
         this.sslPort = sslPort;
         return this;
     }
 
+    /**
+     * Gets the logical name for this PayaraMicro Server within the server cluster
+     * @return The configured instance name
+     */
     public String getInstanceName() {
         return instanceName;
     }
 
+    /**
+     * Sets the logical instance name for this PayaraMicro server within the server cluster
+     * If this is not set a UUID is generated
+     * @param instanceName The logical server name
+     * @return 
+     */
     public PayaraMicro setInstanceName(String instanceName) {
         this.instanceName = instanceName;
         return this;
     }
 
+    /**
+     * A directory which will be scanned for archives to deploy
+     * @return 
+     */
     public File getDeploymentDir() {
         return deploymentRoot;
     }
 
+    /**
+     * Sets a directory to scan for archives to deploy on boot. This directory is not monitored 
+     * while running for changes. Therefore archives in this directory will NOT be redeployed during runtime.
+     * @param deploymentRoot File path to the directory
+     * @return 
+     */
     public PayaraMicro setDeploymentDir(File deploymentRoot) {
         this.deploymentRoot = deploymentRoot;
         return this;
     }
 
+    /**
+     * The path to an alternative domain.xml for PayaraMicro to use at boot
+     * @return The path to the domain.xml
+     */
     public File getAlternateDomainXML() {
         return alternateDomainXML;
     }
 
+    /**
+     * Sets the path to a domain.xml file PayaraMicro should use to boot. If this is not
+     * set PayaraMicro will use an appropriate domain.xml from within its jar file
+     * @param alternateDomainXML
+     * @return 
+     */
     public PayaraMicro setAlternateDomainXML(File alternateDomainXML) {
         this.alternateDomainXML = alternateDomainXML;
         return this;
     }
 
+    /**
+     * Adds an archive to the list of archives to be deployed at boot. These archives are not 
+     * monitored for changes during running so are not redeployed without restarting the server
+     * @param pathToWar File path to the deployment archive
+     * @return 
+     */
     public PayaraMicro addDeployment(String pathToWar) {
         File file = new File(pathToWar);
         return addDeploymentFile(file);
     }
 
+    /**
+     * Adds an archive to the list of archives to be deployed at boot. These archives are not 
+     * monitored for changes during running so are not redeployed without restarting the server
+     * @param file File path to the deployment archive
+     * @return 
+     */    
     public PayaraMicro addDeploymentFile(File file) {
 
         if (deployments == null) {
@@ -170,31 +295,173 @@ public class PayaraMicro {
         return this;
     }
 
+    /**
+     * Indicated whether clustering is enabled
+     * @return 
+     */
     public boolean isNoCluster() {
         return noCluster;
     }
 
+    /**
+     * Enables or disables clustering before bootstrap
+     * @param noCluster set to true to disable clustering
+     * @return 
+     */
     public PayaraMicro setNoCluster(boolean noCluster) {
         this.noCluster = noCluster;
         return this;
     }
 
+    /**
+     * The maximum threads in the HTTP(S) threadpool processing HTTP(S) requests.
+     * Setting this will determine how many concurrent HTTP requests can be processed.
+     * The default value is 200.
+     * This value is shared by both HTTP and HTTP(S) requests.
+     * @return 
+     */
     public int getMaxHttpThreads() {
         return maxHttpThreads;
     }
 
+    /**
+     * The maximum threads in the HTTP(S) threadpool processing HTTP(S) requests.
+     * Setting this will determine how many concurrent HTTP requests can be processed.
+     * The default value is 200
+     * @param maxHttpThreads Maximum threads in the HTTP(S) threadpool
+     * @return 
+     */
     public PayaraMicro setMaxHttpThreads(int maxHttpThreads) {
         this.maxHttpThreads = maxHttpThreads;
         return this;
     }
 
+    /**
+     * The minimum number of threads in the HTTP(S) threadpool
+     * Default value is 10
+     * @return The minimum threads to be created in the threadpool
+     */
     public int getMinHttpThreads() {
         return minHttpThreads;
     }
 
+    /**
+     * The minimum number of threads in the HTTP(S) threadpool
+     * Default value is 10
+     * @param minHttpThreads
+     * @return 
+     */
     public PayaraMicro setMinHttpThreads(int minHttpThreads) {
         this.minHttpThreads = minHttpThreads;
         return this;
+    }
+
+    /**
+     * The File path to a directory that PayaraMicro should use for storing its
+     * configuration files
+     * @return 
+     */
+    public File getRootDir() {
+        return rootDir;
+    }
+
+    /**
+     * Sets the File path to a directory PayaraMicro should use to install its
+     * configuration files. If this is set the PayaraMicro configuration files will be
+     * stored in the directory and persist across server restarts. If this is not set the
+     * configuration files are created in a temporary location and not persisted across server restarts.
+     * @param rootDir Path to a valid directory
+     */
+    public PayaraMicro setRootDir(File rootDir) {
+        this.rootDir = rootDir;
+        return this;
+    }
+    
+
+    /**
+     * Boots the Payara Micro Server. All parameters are checked at this point
+     * @throws BootstrapException 
+     */
+    public void bootStrap() throws BootstrapException {
+        BootstrapProperties bprops = new BootstrapProperties();
+        GlassFishRuntime runtime;
+        try {
+            runtime = GlassFishRuntime.bootstrap();
+            GlassFishProperties gfproperties = new GlassFishProperties();
+            if (httpPort != Integer.MIN_VALUE) {
+                gfproperties.setPort("http-listener", httpPort);
+            }
+
+            if (sslPort != Integer.MIN_VALUE) {
+                gfproperties.setPort("https-listener", sslPort);
+
+            }
+
+            if (alternateDomainXML != null) {
+                gfproperties.setConfigFileURI("file://" + alternateDomainXML.getAbsolutePath());
+            } else {
+                if (noCluster) {
+                    gfproperties.setConfigFileURI(Thread.currentThread().getContextClassLoader().getResource("microdomain-nocluster.xml").toExternalForm());
+
+                } else {
+                    gfproperties.setConfigFileURI(Thread.currentThread().getContextClassLoader().getResource("microdomain.xml").toExternalForm());
+                }
+            }
+
+            if (rootDir != null) {
+                gfproperties.setInstanceRoot(rootDir.getAbsolutePath());
+                File configFile = new File(rootDir.getAbsolutePath() + File.separator + "config" + File.separator + "domain.xml");
+                if (!configFile.exists()) {
+                    installFiles(gfproperties);
+                } else {
+                    gfproperties.setConfigFileURI("file://" + rootDir.getAbsolutePath() + File.separator + "config" + File.separator + "domain.xml");
+                }
+
+            }
+
+            if (this.hzPort != Integer.MIN_VALUE) {
+                gfproperties.setProperty("embedded-glassfish-config.server.hazelcast-runtime-configuration.multicastPort", Integer.toString(hzPort));
+            }
+
+            if (this.hzMulticastGroup != null) {
+                gfproperties.setProperty("embedded-glassfish-config.server.hazelcast-runtime-configuration.multicastGroup", hzMulticastGroup);
+            }
+
+            if (this.maxHttpThreads != Integer.MIN_VALUE) {
+                gfproperties.setProperty("embedded-glassfish-config.server.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size", Integer.toString(maxHttpThreads));
+            }
+
+            if (this.minHttpThreads != Integer.MIN_VALUE) {
+                gfproperties.setProperty("embedded-glassfish-config.server.thread-pools.thread-pool.http-thread-pool.min-thread-pool-size", Integer.toString(minHttpThreads));
+            }
+
+            gf = runtime.newGlassFish(gfproperties);
+            gf.start();
+            deployAll();
+        } catch (GlassFishException ex) {
+            throw new BootstrapException(ex.getMessage(), ex);
+        }
+    }
+    
+    /**
+     * Stops and then shutsdown the Payara Micro Server
+     * @throws BootstrapException 
+     */
+    public void shutdown() throws BootstrapException {
+        try {
+            this.gf.dispose();
+        } catch (GlassFishException ex) {
+            throw new BootstrapException("Unable to stop Payara Micro", ex);
+        }
+    }
+
+    private PayaraMicro() {
+        addShutdownHook();
+    }
+
+    private PayaraMicro(String args[]) {
+        scanArgs(args);
+        addShutdownHook();
     }
 
     private void scanArgs(String[] args) {
@@ -324,73 +591,11 @@ public class PayaraMicro {
                         + "--deploymentDir if set to a valid directory all war files in this directory will be deployed\n"
                         + "--deploy specifies a war file to deploy\n"
                         + "--domainConfig overrides the complete server configuration with an alternative domain.xml file\n"
-                        + "--minHttpThreads the minimum number of threads in the HTTP thread pool"
-                        + "--maxHttpThreads the maximum number of threads in the HTTP thread pool"
+                        + "--minHttpThreads the minimum number of threads in the HTTP thread pool\n"
+                        + "--maxHttpThreads the maximum number of threads in the HTTP thread pool\n"
                         + "--help Shows this message and exits\n");
                 System.exit(-1);
             }
-        }
-    }
-
-    public void bootStrap() throws BootstrapException {
-        BootstrapProperties bprops = new BootstrapProperties();
-        GlassFishRuntime runtime;
-        try {
-            runtime = GlassFishRuntime.bootstrap();
-            GlassFishProperties gfproperties = new GlassFishProperties();
-            if (httpPort != Integer.MIN_VALUE) {
-                gfproperties.setPort("http-listener", httpPort);
-            }
-
-            if (sslPort != Integer.MIN_VALUE) {
-                gfproperties.setPort("https-listener", sslPort);
-
-            }
-
-            if (alternateDomainXML != null) {
-                gfproperties.setConfigFileURI("file://" + alternateDomainXML.getAbsolutePath());
-            } else {
-                if (noCluster) {
-                    gfproperties.setConfigFileURI(ClassLoader.getSystemResource("microdomain-nocluster.xml").toExternalForm());
-
-                } else {
-                    gfproperties.setConfigFileURI(ClassLoader.getSystemResource("microdomain.xml").toExternalForm());
-                }
-            }
-
-            if (rootDir != null) {
-                gfproperties.setInstanceRoot(rootDir.getAbsolutePath());
-                File configFile = new File(rootDir.getAbsolutePath() + File.separator + "config" + File.separator + "domain.xml");
-                if (!configFile.exists()) {
-                    installFiles(gfproperties);
-                } else {
-                    gfproperties.setConfigFileURI("file://" + rootDir.getAbsolutePath() + File.separator + "config" + File.separator + "domain.xml");
-                }
-
-            }
-
-            if (this.hzPort != Integer.MIN_VALUE) {
-                gfproperties.setProperty("embedded-glassfish-config.server.hazelcast-runtime-configuration.multicastPort", Integer.toString(hzPort));
-            }
-
-            if (this.hzMulticastGroup != null) {
-                gfproperties.setProperty("embedded-glassfish-config.server.hazelcast-runtime-configuration.multicastGroup", hzMulticastGroup);
-            }
-            
-            if (this.maxHttpThreads != Integer.MIN_VALUE) {
-                gfproperties.setProperty("embedded-glassfish-config.server.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size", Integer.toString(maxHttpThreads));
-            } 
-            
-            if (this.minHttpThreads != Integer.MIN_VALUE) {
-                gfproperties.setProperty("embedded-glassfish-config.server.thread-pools.thread-pool.http-thread-pool.min-thread-pool-size", Integer.toString(minHttpThreads));
-            }   
-            
-
-            gf = runtime.newGlassFish(gfproperties);
-            gf.start();
-            deployAll();
-        } catch (GlassFishException ex) {
-            throw new BootstrapException(ex.getMessage(), ex);
         }
     }
 
