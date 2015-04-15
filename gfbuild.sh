4f0b17f8b3b3a4b774202f073d4ccea1ba8731ae
romain_grecourt <romain_grecourt@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-04-15 21:14:18 +0000

    PAYARA-243 fix no args usage of gfbuild.sh
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63843 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- gfbuild.sh
+++ gfbuild.sh
@@ -2,7 +2,7 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
-# Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
 #
 # The contents of this file are subject to the terms of either the GNU
 # General Public License Version 2 only ("GPL") or the Common Development
@@ -162,8 +162,8 @@ then
     eval "$build_arg1"
 else
     if [ "$maven_repo" = "" ]; then
-        mvn $mvn_env $update "$build_args" | tee gfbuild.log
+        mvn $mvn_env $update $build_args | tee gfbuild.log
     else
-        mvn -Dmaven.repo.local=$maven_repo $mvn_env $update "$build_args" | tee gfbuild.log
+        mvn -Dmaven.repo.local=$maven_repo $mvn_env $update $build_args | tee gfbuild.log
     fi
 fi
