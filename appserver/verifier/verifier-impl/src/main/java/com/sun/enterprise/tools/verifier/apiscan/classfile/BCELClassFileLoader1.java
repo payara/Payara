f7562829cac3ee0cf6e8916fd6a76cb97f3f19f9
Steve Millidge <smillidge@c2b2.co.uk>
2015-04-29 20:04:56 +0100

    Now compiles on IBM JDK in the payara-blue branch #248

--- appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/apiscan/classfile/BCELClassFileLoader1.java
+++ appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/apiscan/classfile/BCELClassFileLoader1.java
@@ -50,7 +50,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
-import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.apache.bcel.util.ClassPath;
 
 /**
  * Yet another factory for {@link BCELClassFile}. This is not a public class, as
