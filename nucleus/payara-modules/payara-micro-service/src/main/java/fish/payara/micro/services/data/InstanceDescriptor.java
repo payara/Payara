6f5c75a1b9d7bee88beaf902e129b6da75e21a75
Steve Millidge <smillidge@c2b2.co.uk>
2015-05-31 12:09:02 +0100

    Added instance descriptors to the cache

---
+++ nucleus/payara-modules/payara-micro-service/src/main/java/fish/payara/micro/services/data/InstanceDescriptor.java
@@ -0,0 +1,130 @@
+/*
+
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.micro.services.data;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import org.glassfish.internal.data.ApplicationInfo;
+
+/**
+ *
+ * @author steve
+ */
+public class InstanceDescriptor implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String memberUUID;
+    private int httpPort;
+    private int httpsPort;
+    private InetAddress hostName;
+    private Map<String, ApplicationDescriptor> deployedApplications;
+
+    public InstanceDescriptor() throws UnknownHostException {
+        hostName = InetAddress.getLocalHost();
+    }
+
+    public void addApplication(ApplicationInfo info) {
+        if (deployedApplications == null) {
+            deployedApplications = new HashMap<>(3);
+        }
+        
+        ApplicationDescriptor ad = new ApplicationDescriptor(info);
+        deployedApplications.put(ad.getName(), ad);
+    }
+    
+    /**
+     * @return the memberUUID
+     */
+    public String getMemberUUID() {
+        return memberUUID;
+    }
+
+    /**
+     * @param memberUUID the memberUUID to set
+     */
+    public void setMemberUUID(String memberUUID) {
+        this.memberUUID = memberUUID;
+    }
+
+    /**
+     * @return the httpPort
+     */
+    public int getHttpPort() {
+        return httpPort;
+    }
+
+    /**
+     * @param httpPort the httpPort to set
+     */
+    public void setHttpPort(int httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    @Override
+    public String toString() {
+        return "InstanceDescriptor{" + "memberUUID=" + memberUUID + ", httpPort=" + httpPort + ", httpsPort=" + httpsPort + ", hostName=" + hostName + ", deployedApplications=" + deployedApplications + '}';
+    }
+
+    /**
+     * @return the hostName
+     */
+    public InetAddress getHostName() {
+        return hostName;
+    }
+
+    /**
+     * @param hostName the hostName to set
+     */
+    public void setHostName(InetAddress hostName) {
+        this.hostName = hostName;
+    }
+
+    /**
+     * @return the deployedApplications
+     */
+    public Collection<ApplicationDescriptor> getDeployedApplications() {
+        return deployedApplications.values();
+    }
+
+    /**
+     * @return the httpsPort
+     */
+    public int getHttpsPort() {
+        return httpsPort;
+    }
+
+    /**
+     * @param httpsPort the httpsPort to set
+     */
+    public void setHttpsPort(int httpsPort) {
+        this.httpsPort = httpsPort;
+    }
+
+    public void removeApplication(ApplicationInfo applicationInfo) {
+        if (deployedApplications == null) {
+            deployedApplications = new HashMap<>(3);
+        }
+
+        deployedApplications.remove(applicationInfo.getName());
+    }
+    
+}
