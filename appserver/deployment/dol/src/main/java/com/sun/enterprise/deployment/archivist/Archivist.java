ce189413aec4972ddf583645fd481ccc047da04e
aribandy <aribandy@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-06-07 07:08:46 +0000

    GLASSFISH-21131 fix
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63930 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/archivist/Archivist.java
+++ appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/archivist/Archivist.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -417,6 +417,7 @@ public abstract class Archivist<T extends BundleDescriptor> {
         postStandardDDsRead(descriptor, contentArchive, extensions);
 
         readAnnotations(contentArchive, descriptor, extensions);
+        postStandardDDsRead(descriptor, contentArchive, extensions);
         postAnnotationProcess(descriptor, contentArchive);
 
         // now read the runtime deployment descriptors
