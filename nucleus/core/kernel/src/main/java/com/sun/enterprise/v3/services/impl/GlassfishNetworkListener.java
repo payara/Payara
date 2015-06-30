c1193597c5d16cfbf6223a130dd9550abd585c28
oleksiys <oleksiys@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-06-30 17:53:34 +0000

    + Glassfish part of the fix of the https://java.net/jira/browse/GRIZZLY-1786
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@63983 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2007-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -165,6 +165,7 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
             final ContainerMapper containerMapper = new ContainerMapper(grizzlyService, this);
             containerMapper.setMapper(mapper);
             containerMapper.setDefaultHost(http.getDefaultVirtualServer());
+            containerMapper.setRequestURIEncoding(http.getUriEncoding());
             containerMapper.configureMapper();
 
             VirtualServer vs = null;
