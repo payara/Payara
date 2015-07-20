6f41fddd1fcf22a13ae4d23b3db369ca8090c542
mriem <mriem@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-07-20 23:34:25 +0000

    POM changes for Mojarra 2.2.12
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@64007 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/packager/resources/pkg_conf.py
+++ appserver/packager/resources/pkg_conf.py
@@ -63,7 +63,7 @@
 glassfish_version="4.1,0-${build.id}"
 felix_version="4.0.2,0-0"
 corba_version="4.0.0,0-4"
-jsf_version="2.2.7,0-0"
+jsf_version="2.2.12,0-0"
 grizzly_version="2.3.19,0-0"
 metro_version="2.3.2,0-608"
 javahelp_version="2.0.2,0-1"
