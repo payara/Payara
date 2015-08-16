44973e27c2deb42158a7ef6c410fbb5c52fc97d1
mk111283 <mk111283@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2015-08-16 01:34:28 +0000

    Add Create and Drop sql scripts
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@64054 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

---
+++ appserver/batch/batch-database/src/main/resources/glassfish/lib/install/databases/oracle/job_status_ddl.sql
@@ -0,0 +1,29 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+ vCtr     Number;
+ vSQL     VARCHAR2(1000); 
+ vcurrSchema VARCHAR2(256);
+BEGIN
+
+  SELECT sys_context( 'userenv', 'current_schema' ) into vcurrSchema from dual;
+  dbms_output.put_line('Current Schema: '||vcurrSchema);
+
+  SELECT COUNT(*)  
+    INTO vCtr  
+    FROM user_tables  
+    WHERE table_name = 'JOBSTATUS';
+ 
+  IF vCtr = 0 THEN
+    dbms_output.put_line('Creating JOBSTATUS table');
+    vSQL := 'CREATE TABLE JOBSTATUS
+    (	
+         id            NUMBER(19,0) PRIMARY KEY,
+         obj           BLOB,
+         CONSTRAINT JOBSTATUS_JOBINST_FK FOREIGN KEY (id) REFERENCES JOBINSTANCEDATA (jobinstanceid) ON DELETE CASCADE
+    )';  
+   EXECUTE IMMEDIATE vSQL;   
+  END IF;
+
+END;
+/
