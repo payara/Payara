3b4187b36619c8ab1ee77fe64ead8edbd8171332
Mert ÇALIŞKAN <mert@t2.com.tr>
2015-11-20 09:51:13 +0200

    added 2 healthcheck service, one for GC, one for CPU usage, along with the core implementation for a healthcheck service

---
+++ nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/HealthCheckService.java
@@ -0,0 +1,87 @@
+/*
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.nucleus.healthcheck;
+
+import fish.payara.nucleus.healthcheck.preliminary.BaseHealthCheck;
+import fish.payara.nucleus.healthcheck.preliminary.CpuUsageHealthCheck;
+import fish.payara.nucleus.healthcheck.preliminary.GarbageCollectorHealthCheck;
+import org.glassfish.api.StartupRunLevel;
+import org.glassfish.api.event.EventListener;
+import org.glassfish.api.event.EventTypes;
+import org.glassfish.api.event.Events;
+import org.glassfish.hk2.runlevel.RunLevel;
+import org.jvnet.hk2.annotations.Service;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author steve
+ */
+@Service(name = "healthcheck-core")
+@RunLevel(StartupRunLevel.VAL)
+public class HealthCheckService implements EventListener {
+
+    private static final Logger logger = Logger.getLogger(HealthCheckService.class.getCanonicalName());
+
+    @Inject
+    private Events events;
+
+    private ScheduledExecutorService executor;
+
+    private final Map<String, HealthCheckTask> registeredTasks = new HashMap<String, HealthCheckTask>(5);
+
+    public void event(Event event) {
+        if (event.is(EventTypes.SERVER_SHUTDOWN)) {
+            executor.shutdownNow();
+        }
+        else if (event.is(EventTypes.SERVER_READY)) {
+            for (HealthCheckTask registeredTask : registeredTasks.values()) {
+                logger.info("Scheduling Health Check " + registeredTask.getName());
+                executor.scheduleAtFixedRate(registeredTask, 0,
+                        registeredTask.getCheck().getOptions().getTime(),
+                        registeredTask.getCheck().getOptions().getUnit());
+            }
+        }
+    }
+
+    public void setCheckEnabled(String name, boolean state) {
+        HealthCheckTask task = registeredTasks.get(name);
+        if (task != null) {
+            task.setEnabled(state);
+        }
+    }
+
+    public void registerCheck(String name, BaseHealthCheck check) {
+        registeredTasks.put(name, new HealthCheckTask(name, check));
+    }
+
+    @PostConstruct
+    void postConstruct() {
+        executor = Executors.newScheduledThreadPool(2);
+        events.register(this);
+        logger.info("Payara Health Check Service Started");
+
+        //TODO these configurations should be moved to domain.xml with custom tag defs. I guess!
+        registerCheck("GC", new GarbageCollectorHealthCheck(new HealthCheckExecutionOptions(10, TimeUnit.SECONDS)));
+        registerCheck("CPU", new CpuUsageHealthCheck(new HealthCheckExecutionOptions(3, TimeUnit.SECONDS)));
+    }
+}
