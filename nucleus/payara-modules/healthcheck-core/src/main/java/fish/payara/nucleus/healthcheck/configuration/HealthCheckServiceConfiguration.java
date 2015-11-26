73fa90d74b54388eb004603bf6bceb8d3ec0f7b6
Mert ÇALIŞKAN <mert@t2.com.tr>
2015-11-26 17:47:38 +0200

    now configuration is done through domain.xml
    now configuration is done through domain.xml

---
+++ nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/configuration/HealthCheckServiceConfiguration.java
@@ -0,0 +1,55 @@
+/*
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.nucleus.healthcheck.configuration;
+
+import org.glassfish.api.admin.config.ConfigExtension;
+import org.jvnet.hk2.config.*;
+
+import java.beans.PropertyVetoException;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author mertcaliskan
+ *
+ */
+@Configured
+public interface HealthCheckServiceConfiguration extends ConfigBeanProxy, ConfigExtension {
+
+    Logger logger = Logger.getLogger(HealthCheckServiceConfiguration.class.getCanonicalName());
+
+
+    @Attribute(defaultValue="false",dataType=Boolean.class)
+    Boolean getCheckEnabled();
+    void setCheckEnabled(String value) throws PropertyVetoException;
+
+    @Element("*")
+    List<Checker> getCheckerList();
+
+    @DuckTyped
+    <T extends Checker> T getCheckerByType(Class<T> type);
+
+    class Duck {
+        public static <T extends Checker> T getCheckerByType(HealthCheckServiceConfiguration config, Class<T> type) {
+            for (Checker checker : config.getCheckerList()) {
+                try {
+                    return type.cast(checker);
+                } catch (Exception e) {
+                    // ignore, not the right type.
+                }
+            }
+            return null;
+        }
+    }
+}
