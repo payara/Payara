3190c6f2373596d5c2b7734923a7edc2078efef2
danPowell52 <dan.powell@sky.com>
2016-04-18 17:30:09 +0100

    start of service

---
+++ nucleus/payara-modules/BackupConfigService/src/main/java/fish/payara/backup/service/BackupConfigService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fish.payara.backup.service;
+
+import javax.inject.Inject;
+import org.glassfish.api.StartupRunLevel;
+import org.glassfish.api.event.EventListener;
+import org.glassfish.api.event.EventTypes;
+import org.glassfish.api.event.Events;
+import org.glassfish.hk2.api.PostConstruct;
+import org.glassfish.hk2.runlevel.RunLevel;
+import org.jvnet.hk2.annotations.Service;
+
+/**
+ *
+ * @author Daniel
+ */
+@Service(name="backup-config-service")
+@RunLevel(StartupRunLevel.VAL)
+public class BackupConfigService implements EventListener,PostConstruct {
+    
+    @Inject
+    private Events events;
+
+    @Override
+    public void event(Event event) {
+        if (event.is(EventTypes.SERVER_STARTUP)){
+            System.out.println("PREPARE YOURSELF");
+        }
+        
+    }
+
+    @Override
+    public void postConstruct() {
+        events.register(this);
+        System.out.println("");
+    }
+}
