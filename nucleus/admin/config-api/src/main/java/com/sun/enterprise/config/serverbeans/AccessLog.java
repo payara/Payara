9c57f0d35b47323205dde7b9135c494a687fbad7
Andrew Pielage <andrew.pielage@payara.fish>
2016-07-29 08:31:46 +0100

    PAYARA-935 Copyright

--- nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/AccessLog.java
+++ nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/AccessLog.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2014-2016] [C2B2 Consulting Limited]
+// Portions Copyright [2014-2016] [C2B2 Consulting Limited and/or its affiliates]
 
 package com.sun.enterprise.config.serverbeans;
 
