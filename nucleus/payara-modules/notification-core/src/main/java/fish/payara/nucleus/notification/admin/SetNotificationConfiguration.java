93c93fa63d7e5940fe5f391554af10ccef05ee60
MeroRai <susan.rai04@gmail.com>
2016-08-12 15:04:41 +0100

    Fixed "notification-configure" command

--- nucleus/payara-modules/notification-core/src/main/java/fish/payara/nucleus/notification/admin/SetNotificationConfiguration.java
+++ nucleus/payara-modules/notification-core/src/main/java/fish/payara/nucleus/notification/admin/SetNotificationConfiguration.java
@@ -105,11 +105,7 @@ public class SetNotificationConfiguration implements AdminCommand {
         CommandRunner runner = serviceLocator.getService(CommandRunner.class);
         ActionReport subReport = context.getActionReport().addSubActionsReport();
 
-        if (target.equals("server-config")) {
-            inv = runner.getCommandInvocation("notification-configure-das", subReport, context.getSubject());
-        } else {
-            inv = runner.getCommandInvocation("notification-configure", subReport, context.getSubject());
-        }
+        inv = runner.getCommandInvocation("notification-configure", subReport, context.getSubject());
 
         ParameterMap params = new ParameterMap();
         params.add("enabled", enabled.toString());
