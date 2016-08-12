d2650cb7bab594d640dca30553119e1e6e575e9f
MeroRai <susan.rai04@gmail.com>
2016-08-12 10:39:56 +0100

    Fixed "healthcheck-configure" command.

---
+++ nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/admin/_enableHealthCheckConfigurerOnInstance.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fish.payara.nucleus.healthcheck.admin;
+
+import com.sun.enterprise.config.serverbeans.Domain;
+import com.sun.enterprise.util.LocalStringManagerImpl;
+import fish.payara.nucleus.healthcheck.HealthCheckService;
+import java.util.Properties;
+import javax.inject.Inject;
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.CommandLock;
+import org.glassfish.api.admin.ExecuteOn;
+import org.glassfish.api.admin.RestEndpoint;
+import org.glassfish.api.admin.RestEndpoints;
+import org.glassfish.api.admin.RuntimeType;
+import org.glassfish.config.support.CommandTarget;
+import org.glassfish.config.support.TargetType;
+import org.glassfish.hk2.api.PerLookup;
+import org.jvnet.hk2.annotations.Service;
+
+/**
+ *
+ * @author Susan Rai
+ */
+@Service(name = "__enable-healthcheck-configurer-on-instance")
+@PerLookup
+@CommandLock(CommandLock.LockType.NONE)
+@I18n("__enable-healthcheck-configurer-on-instance")
+@ExecuteOn(RuntimeType.INSTANCE)
+@TargetType(value = {CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTER, CommandTarget.CLUSTERED_INSTANCE, CommandTarget.CONFIG})
+@RestEndpoints({
+    @RestEndpoint(configBean = Domain.class,
+            opType = RestEndpoint.OpType.GET,
+            path = "__enable-healthcheck-configurer-on-instance",
+            description = "Enables Healthcheck service on Instance")
+})
+public class _enableHealthCheckConfigurerOnInstance implements AdminCommand {
+
+    final private static LocalStringManagerImpl strings = new LocalStringManagerImpl(HealthCheckConfigurer.class);
+
+    @Inject
+    HealthCheckService service;
+
+    @Param(name = "target", optional = true, defaultValue = "server")
+    protected String target;
+
+    @Param(name = "enabled", optional = false)
+    private Boolean enabled;
+
+    @Override
+    public void execute(AdminCommandContext context) {
+        final ActionReport actionReport = context.getActionReport();
+        Properties extraProperties = actionReport.getExtraProperties();
+        if (extraProperties == null) {
+            extraProperties = new Properties();
+            actionReport.setExtraProperties(extraProperties);
+        }
+        
+        service.setEnabled(enabled);
+          actionReport.appendMessage("Health Check Service staus set to " + enabled + " on " + target );
+    }
+
+}
