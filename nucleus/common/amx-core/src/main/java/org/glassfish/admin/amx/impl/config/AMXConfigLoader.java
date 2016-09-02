518529568835a74d8a41cc6f20df80ffcadc0f09
Andrew Pielage <andrew.pielage@payara.fish>
2016-09-02 16:55:40 +0100

    PAYARA-956 Copyright

--- nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/impl/config/AMXConfigLoader.java
+++ nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/impl/config/AMXConfigLoader.java
@@ -37,6 +37,9 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+ 
+ // Portions Copyright [2016] [Payara Foundation and/or its affiliates]
+ 
 package org.glassfish.admin.amx.impl.config;
 
 import com.sun.enterprise.config.serverbeans.Domain;
