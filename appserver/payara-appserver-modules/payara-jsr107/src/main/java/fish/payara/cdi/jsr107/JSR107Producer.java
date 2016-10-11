a7e428f1775d1b0f949e88d246f72f1321008131
Jaromir Hamala <jaromir.hamala@gmail.com>
2016-10-11 22:26:21 +0300

    JCache beans created by JSR107Producer use generics (#1125)
    JCache beans created by JSR107Producer use generics (PAYARA-1107)

--- appserver/payara-appserver-modules/payara-jsr107/src/main/java/fish/payara/cdi/jsr107/JSR107Producer.java
+++ appserver/payara-appserver-modules/payara-jsr107/src/main/java/fish/payara/cdi/jsr107/JSR107Producer.java
@@ -75,9 +75,8 @@ public class JSR107Producer {
      * @return 
      */
     @Produces
-    @SuppressWarnings("unchecked")
-    public Cache createCache(InjectionPoint ip) {
-        Cache result = null;
+    public <K,V> Cache<K, V> createCache(InjectionPoint ip) {
+        Cache<K, V> result;
 
         //determine the cache name first start with the default name
         String cacheName = ip.getMember().getDeclaringClass().getCanonicalName();
@@ -94,7 +93,7 @@ public class JSR107Producer {
             Class valueClass = ncqualifier.valueClass();           
             result = manager.getCache(cacheName, keyClass, valueClass);
             if (result == null) {
-                MutableConfiguration<Object, Object> config = new MutableConfiguration<>();
+                MutableConfiguration<K, V> config = new MutableConfiguration<>();
                 config.setTypes(keyClass, valueClass);
                 
                 // determine the expiry policy
@@ -138,7 +137,7 @@ public class JSR107Producer {
             }
             result = manager.getCache(cacheName);
             if (result == null) {
-                MutableConfiguration<Object, Object> config = new MutableConfiguration<>();
+                MutableConfiguration<K, V> config = new MutableConfiguration<>();
                 config.setManagementEnabled(true);
                 config.setStatisticsEnabled(true);
                 result = manager.createCache(cacheName, config);
