e9aeb5c71a7951ebf728289caf36115a80cc0eb4
Andrew Pielage <andrew.pielage@payara.fish>
2017-01-06 16:43:12 +0000

    PAYARA-1264 Copyright and remove unused import

--- appserver/web/web-glue/src/main/java/org/glassfish/web/jsp/JSPCompiler.java
+++ appserver/web/web-glue/src/main/java/org/glassfish/web/jsp/JSPCompiler.java
@@ -54,12 +54,13 @@
  * @author byron.nevins@sun.com
  */
 
+// Portions Copyright [2017] [Payara Foundation and/or its affiliates]
+
 package org.glassfish.web.jsp;
 
 import com.sun.enterprise.deployment.WebBundleDescriptor;
 import com.sun.enterprise.deployment.WebComponentDescriptor;
 import com.sun.enterprise.deployment.web.InitializationParameter;
-import com.sun.enterprise.util.LocalStringManagerImpl;
 import com.sun.enterprise.util.io.FileUtils;
 import org.apache.jasper.JspC;
 import org.glassfish.deployment.common.DeploymentException;
