a43cd05fda20fdea4142c8a371d6839f4c3ffc45
swchan2 <swchan2@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2017-02-01 23:58:09 +0000

    fix GLASSFISH-20917 a wrong implementation for web-fragment.xml with <distributable/> issue
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@64467 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- appserver/web/web-glue/src/main/java/org/glassfish/web/deployment/util/WebBundleValidator.java
+++ appserver/web/web-glue/src/main/java/org/glassfish/web/deployment/util/WebBundleValidator.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -98,9 +98,6 @@ public class WebBundleValidator extends ApplicationValidator implements WebBundl
 
         if (descriptor.getSessionConfig() == null) {
             descriptor.setSessionConfig(new SessionConfigDescriptor());
-        }
-        if (descriptor.isDistributable() == null) {
-            descriptor.setDistributable(Boolean.FALSE);
         }
     }
 
