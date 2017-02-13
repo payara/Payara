0135819b98918435676ce3aad704141a27c23c96
srinivas.krishnan@oracle.com <srinivas.krishnan@oracle.com@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2017-02-13 05:51:47 +0000

    (GLASSFISH-21648) corethreads and maxthreads are always zero
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@64531 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2007-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -105,8 +105,6 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
 
     @Override
     public void start() throws IOException {
-        registerMonitoringStatsProviders();
-        
         super.start();
     }
 
