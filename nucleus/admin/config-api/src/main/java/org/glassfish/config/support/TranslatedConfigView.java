0495c7b6facc10dcef14dfe35e8632f9752b81ac
mskdeepak <mskdeepak@6f3ba3e3-413c-0410-a8aa-90bee3ab43b5>
2017-02-20 05:57:27 +0000

    Fix for GLASSFISH-21546: password alias is not updated until restart the process Removing the if check for domainPasswordAliasStore so that it gets updated
    git-svn-id: https://svn.java.net/svn/glassfish~svn/trunk/main@64569 6f3ba3e3-413c-0410-a8aa-90bee3ab43b5

--- nucleus/admin/config-api/src/main/java/org/glassfish/config/support/TranslatedConfigView.java
+++ nucleus/admin/config-api/src/main/java/org/glassfish/config/support/TranslatedConfigView.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -197,16 +197,14 @@ public class TranslatedConfigView implements ConfigView {
     }
     
     private static DomainScopedPasswordAliasStore domainPasswordAliasStore = null;
-    private static synchronized DomainScopedPasswordAliasStore domainPasswordAliasStore() {
-        if (domainPasswordAliasStore == null) {
-            domainPasswordAliasStore =
-                AccessController.doPrivileged(
-                        new PrivilegedAction<DomainScopedPasswordAliasStore>() {
-                            public DomainScopedPasswordAliasStore run() {
-            					    return habitat.getService(DomainScopedPasswordAliasStore.class);
-                            }
-                });
-        }
+    private static DomainScopedPasswordAliasStore domainPasswordAliasStore() {
+        domainPasswordAliasStore = AccessController.doPrivileged(
+                new PrivilegedAction<DomainScopedPasswordAliasStore>() {
+                    public DomainScopedPasswordAliasStore run() {
+                        return habitat.getService(DomainScopedPasswordAliasStore.class);
+                    }
+                }
+        );
         return domainPasswordAliasStore;
     }
     
