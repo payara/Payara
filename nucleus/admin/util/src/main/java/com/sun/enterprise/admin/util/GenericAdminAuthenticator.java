cb73aa7ca35cabfe783bf79e5ef177a1f765abfd
Steve Millidge <steve.millidge@payara.fish>
2017-03-06 18:13:51 +0000

    PAYARA-1251 corrected the date

--- nucleus/admin/util/src/main/java/com/sun/enterprise/admin/util/GenericAdminAuthenticator.java
+++ nucleus/admin/util/src/main/java/com/sun/enterprise/admin/util/GenericAdminAuthenticator.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2016] [Payara Foundation and/or it's affiliates]
+// Portions Copyright [2017] [Payara Foundation and/or it's affiliates]
 package com.sun.enterprise.admin.util;
 
 import com.sun.enterprise.config.serverbeans.SecureAdmin;
