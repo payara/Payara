3cbdb954fa040a3b9daf08f14ce30af71c1dbcf2
Cousjava <jonathan.coustick@payara.fish>
2017-04-24 15:56:30 +0100

    PAYARA-1382 can now save active notifiers configuration

--- appserver/admingui/payara-console-extras/src/main/java/fish/payara/admingui/extras/rest/PayaraRestApiHandlers.java
+++ appserver/admingui/payara-console-extras/src/main/java/fish/payara/admingui/extras/rest/PayaraRestApiHandlers.java
@@ -1,18 +1,41 @@
-/**
+/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright (c) 2016 Payara Foundation and/or its affiliates. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License.  You can
- * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
- * or packager/legal/LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at packager/legal/LICENSE.txt.
+ *    Copyright (c) [2016-2017] Payara Foundation and/or its affiliates. All rights reserved.
+ * 
+ *     The contents of this file are subject to the terms of either the GNU
+ *     General Public License Version 2 only ("GPL") or the Common Development
+ *     and Distribution License("CDDL") (collectively, the "License").  You
+ *     may not use this file except in compliance with the License.  You can
+ *     obtain a copy of the License at
+ *     https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *     See the License for the specific
+ *     language governing permissions and limitations under the License.
+ * 
+ *     When distributing the software, include this License Header Notice in each
+ *     file and include the License file at glassfish/legal/LICENSE.txt.
+ * 
+ *     GPL Classpath Exception:
+ *     The Payara Foundation designates this particular file as subject to the "Classpath"
+ *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *     file that accompanied this code.
+ * 
+ *     Modifications:
+ *     If applicable, add the following below the License Header, with the fields
+ *     enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyright [year] [name of copyright owner]"
+ * 
+ *     Contributor(s):
+ *     If you wish your version of this file to be governed by only the CDDL or
+ *     only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *     elects to include this software in this distribution under the [CDDL or GPL
+ *     Version 2] license."  If you don't indicate a single choice of license, a
+ *     recipient has the option to distribute your version of this file under
+ *     either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *     its licensees as provided above.  However, if you add GPL Version 2 code
+ *     and therefore, elected the GPL Version 2 license, then the option applies
+ *     only if the new code is made subject to such option by the copyright
+ *     holder.
  */
 package fish.payara.admingui.extras.rest;
 
@@ -20,8 +43,8 @@ import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
-import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -212,24 +235,45 @@ public class PayaraRestApiHandlers
         logger.log(Logger.Level.ERROR, disabled.toString());
         handlerctx.setOutputValue("disabled", disabled);
         handlerctx.setOutputValue("enabled", enabled);
-        /*JsonObject notifiers = Json.createReader(new StringReader((String) handlerctx.getInputValue("requestTracingNotifiers"))).readObject();
-        for (int i = 0; i < notifiers.size(); i++){
-            
-            JsonObject service = notifiers.getJsonObject((String)notifiers.keySet().toArray()[i]);
-            //JsonObject service = notifiers.get(i).asJsonObject();
-            String name = service.getString("notifierName");
-            if (service.getBoolean("notifierEnabled")){
-                enabled.add(name);          
+                
+    }
+    
+    /**
+     * Updates the request tracing notifiers to be enabled or disabled
+     * @param handlerCtx 
+     */
+    @Handler(id="py.updateRequesttracingNotifiers",
+            input={
+                @HandlerInput(name="endpoint", type=String.class, required=true),
+                @HandlerInput(name="selected", type=String[].class, required=true),
+                @HandlerInput(name="notifiers", type=String[].class, required=true),
+                @HandlerInput(name="dynamic", type=Boolean.class, required=true),
+                @HandlerInput(name="quiet", type=boolean.class, defaultValue="false"),
+                @HandlerInput(name="throwException", type=boolean.class, defaultValue="true")
+            })
+    public static void updateRequesttracingNotifiers(HandlerContext handlerCtx){
+        String[] notifiers = (String[]) handlerCtx.getInputValue("notifiers");
+        String[] enabled = (String[]) handlerCtx.getInputValue("selected");
+        String endpoint = (String) handlerCtx.getInputValue("endpoint");
+        Boolean dynamic = (Boolean) handlerCtx.getInputValue("dynamic");
+        Boolean quiet = (Boolean) handlerCtx.getInputValue("quiet");
+        Boolean throwException = (Boolean) handlerCtx.getInputValue("throwException");
+        List<String> enabledNotifiers = Arrays.asList(enabled);
+        for (String notifier : notifiers){
+            String name = notifier.split("-")[1];
+            String restEndpoint = endpoint + "/requesttracing-" + name + "-notifier-configure";
+            HashMap<String, Object> attrs = new HashMap<String, Object>();
+            if (enabledNotifiers.contains(notifier)){
+                attrs.put("enabled", "true");                
             } else {
-                disabled.add(name);
+                attrs.put("enabled", "false");
             }
-            avaliable.remove(name);
+            if (dynamic){
+                attrs.put("dynamic", "true");
+            } else {
+                attrs.put("dynamic", "false");
+            }
+            RestUtil.restRequest(restEndpoint, attrs, "post", handlerCtx, quiet, throwException);
         }
-        for (String left : avaliable){
-            disabled.add(left);
-        }*/
-
-        
-        
     }
 }
