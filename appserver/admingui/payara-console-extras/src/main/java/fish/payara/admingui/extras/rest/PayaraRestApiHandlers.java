dcf8f351a0b303fcddf3bcd524585042d4f897c5
Cousjava <outingbrains-websitesgeneric@yahoo.co.uk>
2017-05-08 12:19:39 +0100

    PAYARA-1382, PAYARA-1574 admin console notifiers integration (#1557)
    * PAYARA-1382 Added avaliable notifiers to requesttracing page in admin console
    
    * PAYARA-1382 active notifiers now appear
    
    * PAYARA-1382 can now save active notifiers configuration
    
    * Removed extraneous logging
    
    * PAYARA-1574 admin console integration for selecting notifiers for healthheck service
    
    * Removed log notifier checkbox.
    
    Also deprecated notiferenabled argument for asadmin command as now there are the notifer configure commands

--- appserver/admingui/payara-console-extras/src/main/java/fish/payara/admingui/extras/rest/PayaraRestApiHandlers.java
+++ appserver/admingui/payara-console-extras/src/main/java/fish/payara/admingui/extras/rest/PayaraRestApiHandlers.java
@@ -1,43 +1,39 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+/* 
+ *     Copyright (c) [2016-2017] Payara Foundation and/or its affiliates. All rights reserved.
  * 
- * Copyright (c) 2016-2017 Payara Foundation and/or its affiliates. All rights reserved.
- *
- * Copyright (c) 2016-2017 Payara Foundation and/or its affiliates. All rights reserved.
- *
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License.  You can
- * obtain a copy of the License at
- * https://github.com/payara/Payara/blob/master/LICENSE.txt
- * See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/legal/LICENSE.txt.
- *
- * GPL Classpath Exception:
- * The Payara Foundation designates this particular file as subject to the "Classpath"
- * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
- * file that accompanied this code.
- *
- * Modifications:
- * If applicable, add the following below the License Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyright [year] [name of copyright owner]"
- *
- * Contributor(s):
- * If you wish your version of this file to be governed by only the CDDL or
- * only the GPL Version 2, indicate your decision by adding "[Contributor]
- * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate a single choice of license, a
- * recipient has the option to distribute your version of this file under
- * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
- * and therefore, elected the GPL Version 2 license, then the option applies
- * only if the new code is made subject to such option by the copyright
- * holder.
+ *     The contents of this file are subject to the terms of either the GNU
+ *     General Public License Version 2 only ("GPL") or the Common Development
+ *     and Distribution License("CDDL") (collectively, the "License").  You
+ *     may not use this file except in compliance with the License.  You can
+ *     obtain a copy of the License at
+ *     https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *     See the License for the specific
+ *     language governing permissions and limitations under the License.
+ * 
+ *     When distributing the software, include this License Header Notice in each
+ *     file and include the License file at glassfish/legal/LICENSE.txt.
+ * 
+ *     GPL Classpath Exception:
+ *     The Payara Foundation designates this particular file as subject to the "Classpath"
+ *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *     file that accompanied this code.
+ * 
+ *     Modifications:
+ *     If applicable, add the following below the License Header, with the fields
+ *     enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyright [year] [name of copyright owner]"
+ * 
+ *     Contributor(s):
+ *     If you wish your version of this file to be governed by only the CDDL or
+ *     only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *     elects to include this software in this distribution under the [CDDL or GPL
+ *     Version 2] license."  If you don't indicate a single choice of license, a
+ *     recipient has the option to distribute your version of this file under
+ *     either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *     its licensees as provided above.  However, if you add GPL Version 2 code
+ *     and therefore, elected the GPL Version 2 license, then the option applies
+ *     only if the new code is made subject to such option by the copyright
+ *     holder.
  */
 package fish.payara.admingui.extras.rest;
 
@@ -47,6 +43,7 @@ import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -290,6 +287,128 @@ public class PayaraRestApiHandlers
         GuiUtil.prepareAlert("success", "Command sent successfully", null);
     }
     
+    @Handler(id="py.sortRequestTracingEnabledNotifierStatus",
+    	input={
+            @HandlerInput(name="specifiedNotifiers", type=String.class, required=true),
+            @HandlerInput(name="avaliableNotifiers", type=List.class, required=true )},
+        output={
+            @HandlerOutput(name="enabled", type=List.class),
+            @HandlerOutput(name="disabled", type=List.class)})
+    public static void sortRequestTracingEnabledNotifierStatus(HandlerContext handlerctx){
+        List<String> enabled = new ArrayList<String>();
+        List<String> disabled = new ArrayList<String>();
+        
+        List<String> avaliable = (List) handlerctx.getInputValue("avaliableNotifiers");
+        
+        String notifiersString = (String) handlerctx.getInputValue("specifiedNotifiers");
+        notifiersString = notifiersString.substring(1, notifiersString.length() - 2);
+        String[] notifiers = notifiersString.split("\\}\\,");
+        for (String notifier : notifiers){
+            String name = notifier.split("notifierName=", 2)[1];
+            if (notifier.contains("notifierEnabled=true")){
+                enabled.add(name);
+            } else {
+                disabled.add(name);
+            }
+            avaliable.remove(name);
+        }
+        for (String unused : avaliable){
+            disabled.add(unused);
+        }        
+        handlerctx.setOutputValue("disabled", disabled);
+        handlerctx.setOutputValue("enabled", enabled);
+                
+    }
+    
+        @Handler(id="py.sortHealthcheckEnabledNotifierStatus",
+    	input={
+            @HandlerInput(name="specifiedNotifiers", type=String.class, required=true),
+            @HandlerInput(name="avaliableNotifiers", type=List.class, required=true )},
+        output={
+            @HandlerOutput(name="enabled", type=List.class),
+            @HandlerOutput(name="disabled", type=List.class)})
+    public static void sortHealthcheckEnabledNotifierStatus(HandlerContext handlerctx){
+        List<String> enabled = new ArrayList<String>();
+        List<String> disabled = new ArrayList<String>();
+        
+        List<String> avaliable = (List) handlerctx.getInputValue("avaliableNotifiers");
+        
+        String notifiersString = (String) handlerctx.getInputValue("specifiedNotifiers");
+        notifiersString = notifiersString.substring(1, notifiersString.length() - 2);
+        
+        String[] notifiers = notifiersString.split("[\\}\\]]\\,");
+        for (String notifier : notifiers){
+            //Check to see if this is actually a notifier
+            notifier = notifier.trim();
+            if (!notifier.startsWith("notifierList")){
+                continue;
+            }
+            
+            String name = "service-" + notifier.split("notifierName=", 2)[1].toLowerCase();
+            if (notifier.contains("notifierEnabled=true")){
+                enabled.add(name);
+            } else {
+                disabled.add(name);
+            }
+            avaliable.remove(name);
+        }
+        for (String unused : avaliable){
+            disabled.add(unused);
+        }        
+        handlerctx.setOutputValue("disabled", disabled);
+        handlerctx.setOutputValue("enabled", enabled);
+                
+    }
+    
+    /**
+     * Updates the request tracing notifiers to be enabled or disabled
+     * @param handlerCtx 
+     */
+    @Handler(id="py.updateNotifiers",
+            input={
+                @HandlerInput(name="endpoint", type=String.class, required=true),
+                @HandlerInput(name="selected", type=String[].class, required=true),
+                @HandlerInput(name="notifiers", type=String[].class, required=true),
+                @HandlerInput(name="dynamic", type=Boolean.class, required=true),
+                @HandlerInput(name="quiet", type=boolean.class, defaultValue="false"),
+                @HandlerInput(name="throwException", type=boolean.class, defaultValue="true")
+            })
+    public static void updateNotifiers(HandlerContext handlerCtx) {
+        String[] notifiers = (String[]) handlerCtx.getInputValue("notifiers");
+        String[] enabled = (String[]) handlerCtx.getInputValue("selected");
+        String endpoint = (String) handlerCtx.getInputValue("endpoint");
+        Boolean dynamic = (Boolean) handlerCtx.getInputValue("dynamic");
+        Boolean quiet = (Boolean) handlerCtx.getInputValue("quiet");
+        Boolean throwException = (Boolean) handlerCtx.getInputValue("throwException");
+        List<String> enabledNotifiers = Arrays.asList(enabled);
+        for (String notifier : notifiers){
+            String name = notifier.split("-")[1];
+            String restEndpoint;
+            if (endpoint.contains("request-tracing-service-configuration")){
+                restEndpoint = endpoint + "/requesttracing-" + name + "-notifier-configure";
+            } else if (endpoint.contains("health-check-service-configuration")){
+                restEndpoint = endpoint + "/healthcheck-" + name + "-notifier-configure";
+            } else {
+                //Unknown service being configured
+                throw new UnknownConfigurationException();
+            }
+            
+            HashMap<String, Object> attrs = new HashMap<String, Object>();
+            if (enabledNotifiers.contains(notifier)){
+                attrs.put("enabled", "true");                
+            } else {
+                attrs.put("enabled", "false");
+            }
+            if (dynamic){
+                attrs.put("dynamic", "true");
+            } else {
+                attrs.put("dynamic", "false");
+            }
+            RestUtil.restRequest(restEndpoint, attrs, "post", handlerCtx, quiet, throwException);
+        }
+    }
+  
+  
     @Handler(id = "py.getHistoricHealthcheckMessages",
             input = @HandlerInput(name = "parentEndpoint", type = String.class, required = true),
             output = @HandlerOutput(name = "result", type = java.util.List.class))
