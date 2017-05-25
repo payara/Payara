81a28347e1f8eda64bad8a067cdfaffa30f5015c
Michael Ranaldo <michael.ranaldo@payara.fish>
2017-05-25 13:55:54 +0100

    PAYARA-1564 Updated license headers

--- appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/ApplicationHandlers.java
+++ appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/ApplicationHandlers.java
@@ -38,7 +38,7 @@
  * holder.
  */
 
-// Portions Copyright [2016] [Payara Foundation and/or its affiliates]]
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]]
 
 /*
  * ApplicationHandlers.java
