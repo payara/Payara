40a6c8ef1df6d4e4a2514440d1cc058da06f3e96
Mert Caliskan <mcaliskan@gmail.com>
2017-05-28 03:33:37 +0300

    PAYARA-1619-Upon-start-some-healthchecks-do-not-run-when-enabled (#1599)

--- nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/HistoricHealthCheckEvent.java
+++ nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/HistoricHealthCheckEvent.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2016 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016-2017 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,12 +38,13 @@
  */
 package fish.payara.nucleus.healthcheck;
 
+import java.io.Serializable;
 import java.util.logging.Level;
 
 /**
  * @author mertcaliskan
  */
-public class HistoricHealthCheckEvent implements Comparable<HistoricHealthCheckEvent> {
+public class HistoricHealthCheckEvent implements Comparable<HistoricHealthCheckEvent>, Serializable {
 
     private long occurringTime;
     private Level level;
