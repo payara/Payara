37a502a4d5e97cce6dfa9ddae81542c06c7d5b79
Andrew Pielage <pandrex247@hotmail.com>
2017-06-01 10:05:17 +0100

    Copyright

--- nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherLogger.java
+++ nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherLogger.java
@@ -37,6 +37,9 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+
+// Portions Copyright [2017] [Payara Foundation and/or its affiliates.]
+
 package com.sun.enterprise.admin.launcher;
 
 import com.sun.enterprise.server.logging.ODLLogFormatter;
