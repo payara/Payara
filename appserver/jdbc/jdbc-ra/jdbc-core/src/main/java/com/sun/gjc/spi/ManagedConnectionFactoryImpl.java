bed3a8e28fbaf3a2afe764e6ad575e78c3e74dc6
jGauravGupta <gaurav.gupta.jc@gmail.com>
2017-06-06 17:40:17 +0530

    Portions Copyright

--- appserver/jdbc/jdbc-ra/jdbc-core/src/main/java/com/sun/gjc/spi/ManagedConnectionFactoryImpl.java
+++ appserver/jdbc/jdbc-ra/jdbc-core/src/main/java/com/sun/gjc/spi/ManagedConnectionFactoryImpl.java
@@ -37,7 +37,8 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2016] [Payara Foundation and/or its affiliates]
+// Portions Copyright [2017] [Payara Foundation and/or its affiliates]
+
 package com.sun.gjc.spi;
 
 import com.sun.appserv.connectors.internal.spi.MCFLifecycleListener;
