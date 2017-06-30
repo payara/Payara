3961e3eaa9eb21298f9e76300b70f8a8c762d9d2
Mert ÇALIŞKAN <mert@t2.com.tr>
2017-06-30 10:18:22 +0300

    PAYARA-1700-Add-threshold-configuration-to-the-GC-health-check

--- nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/admin/GetHealthCheckConfiguration.java
+++ nucleus/payara-modules/healthcheck-core/src/main/java/fish/payara/nucleus/healthcheck/admin/GetHealthCheckConfiguration.java
@@ -1,20 +1,40 @@
 /*
- DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-
-
- Copyright (c) 2016 Payara Foundation. All rights reserved.
-
-
- The contents of this file are subject to the terms of the Common Development
- and Distribution License("CDDL") (collectively, the "License").  You
- may not use this file except in compliance with the License.  You can
- obtain a copy of the License at
- https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
- or packager/legal/LICENSE.txt.  See the License for the specific
- language governing permissions and limitations under the License.
-
- When distributing the software, include this License Header Notice in each
- file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * Copyright (c) 2016-2017 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://github.com/payara/Payara/blob/master/LICENSE.txt
+ * See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * The Payara Foundation designates this particular file as subject to the "Classpath"
+ * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 package fish.payara.nucleus.healthcheck.admin;
 
@@ -247,7 +267,7 @@ public class GetHealthCheckConfiguration implements AdminCommand, HealthCheckCon
         }
         
         // Populate the extraProps with defaults for any checker that isn't present
-        baseExtraProps = checkCheckerPropertyPresence(baseExtraProps, garbageCollectorPropertyName);
+        baseExtraProps = checkCheckerPropertyPresence(thresholdDiagnosticsExtraProps, garbageCollectorPropertyName);
         hoggingThreadsExtraProps = checkCheckerPropertyPresence(hoggingThreadsExtraProps, hoggingThreadsPropertyName);
         thresholdDiagnosticsExtraProps = checkCheckerPropertyPresence(thresholdDiagnosticsExtraProps, 
                 cpuUsagePropertyName);
@@ -305,6 +325,9 @@ public class GetHealthCheckConfiguration implements AdminCommand, HealthCheckCon
             case CPU_USAGE:
                 thresholdDiagnosticsExtraProps.put(cpuUsagePropertyName, extraPropsMap);
                 break;
+            case GARBAGE_COLLECTOR:
+                thresholdDiagnosticsExtraProps.put(garbageCollectorPropertyName, extraPropsMap);
+                break;
             case HEAP_MEMORY_USAGE:
                 thresholdDiagnosticsExtraProps.put(heapMemoryUsagePropertyName, extraPropsMap);
                 break;
@@ -322,17 +345,6 @@ public class GetHealthCheckConfiguration implements AdminCommand, HealthCheckCon
         extraPropsMap.put("enabled", checker.getEnabled());
         extraPropsMap.put("time", checker.getTime());
         extraPropsMap.put("unit", checker.getUnit());
-
-        // Get the checker type
-        ConfigView view = ConfigSupport.getImpl(checker);
-        CheckerConfigurationType annotation = view.getProxyType().getAnnotation(CheckerConfigurationType.class);
-        
-        // Add the extraPropsMap as a property with a name matching its checker type
-        switch (annotation.type()) {
-            case GARBAGE_COLLECTOR:
-                baseExtraProps.put(garbageCollectorPropertyName, extraPropsMap);
-                break;
-        }
     }
     
     private Properties checkCheckerPropertyPresence(Properties extraProps, String checkerName) {
@@ -341,7 +353,7 @@ public class GetHealthCheckConfiguration implements AdminCommand, HealthCheckCon
             Map<String, Object> extraPropsMap;
             switch (checkerName) {
                 case garbageCollectorPropertyName:
-                    extraPropsMap = new HashMap<>(4);
+                    extraPropsMap = new HashMap<>(7);
                     extraPropsMap.put("checkerName", DEFAULT_GARBAGE_COLLECTOR_NAME);
                     extraProps.put(checkerName, populateDefaultValuesMap(extraPropsMap));
                     break;
@@ -383,11 +395,10 @@ public class GetHealthCheckConfiguration implements AdminCommand, HealthCheckCon
         extraPropsMap.put("unit", DEFAULT_UNIT);
         
         // Add default properties for hogging threads checker, or add default properties for a thresholdDiagnostics checker
-        // if the checker type isn't a hogging thread or garbage collector checker (the only current base checker)
         if (extraPropsMap.containsValue(DEFAULT_HOGGING_THREADS_NAME)) {
             extraPropsMap.put("threshold-percentage", DEFAULT_THRESHOLD_PERCENTAGE);
             extraPropsMap.put("retry-count", DEFAULT_RETRY_COUNT);
-        } else if (!extraPropsMap.containsValue(DEFAULT_GARBAGE_COLLECTOR_NAME)) {
+        } else {
             extraPropsMap.put("thresholdCritical", THRESHOLD_DEFAULTVAL_CRITICAL);
             extraPropsMap.put("thresholdWarning", THRESHOLD_DEFAULTVAL_WARNING);
             extraPropsMap.put("thresholdGood", THRESHOLD_DEFAULTVAL_GOOD);
