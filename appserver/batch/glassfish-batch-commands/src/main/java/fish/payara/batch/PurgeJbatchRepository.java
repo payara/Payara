39ff1352986357c2107927583d61b67538d19491
Cousjava <jonathan.coustick@payara.fish>
2017-07-27 12:12:25 +0100

    PAYARA-571 asadmin command to purge Jbatch repository

---
+++ appserver/batch/glassfish-batch-commands/src/main/java/fish/payara/batch/PurgeJbatchRepository.java
@@ -0,0 +1,79 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ *    Copyright (c) [2017] Payara Foundation and/or its affiliates. All rights reserved.
+ * 
+ *     The contents of this file are subject to the terms of either the GNU
+ *     General Public License Version 2 only ("GPL") or the Common Development
+ *     and Distribution License("CDDL") (collectively, the "License").  You
+ *     may not use this file except in compliance with the License.  You can
+ *     obtain a copy of the License at
+ *     https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *     See the License for the specific
+ *     language governing permissions and limitations under the License.
+ * 
+ *     When distributing the software, include this License Header Notice in each
+ *     file and include the License file at glassfish/legal/LICENSE.txt.
+ * 
+ *     GPL Classpath Exception:
+ *     The Payara Foundation designates this particular file as subject to the "Classpath"
+ *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *     file that accompanied this code.
+ * 
+ *     Modifications:
+ *     If applicable, add the following below the License Header, with the fields
+ *     enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyright [year] [name of copyright owner]"
+ * 
+ *     Contributor(s):
+ *     If you wish your version of this file to be governed by only the CDDL or
+ *     only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *     elects to include this software in this distribution under the [CDDL or GPL
+ *     Version 2] license."  If you don't indicate a single choice of license, a
+ *     recipient has the option to distribute your version of this file under
+ *     either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *     its licensees as provided above.  However, if you add GPL Version 2 code
+ *     and therefore, elected the GPL Version 2 license, then the option applies
+ *     only if the new code is made subject to such option by the copyright
+ *     holder.
+ */
+package fish.payara.batch;
+
+import com.ibm.jbatch.container.servicesmanager.ServicesManager;
+import com.ibm.jbatch.container.servicesmanager.ServicesManagerImpl;
+import com.ibm.jbatch.container.services.IPersistenceManagerService;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.CommandLock;
+import org.glassfish.api.admin.ExecuteOn;
+import org.glassfish.api.admin.RuntimeType;;
+import org.glassfish.hk2.api.PerLookup;
+import org.jvnet.hk2.annotations.Service;
+
+/**
+ * Purges the JBatch repository of all history of a specified apptag
+ * 
+ * The default apptag form is server-config:/applicationName_/applicationName
+ * 
+ * @since 4.1.2.173
+ * @author jonathan coustick
+ */
+@Service(name = "purge-jbatch-repository")
+@PerLookup
+@CommandLock(CommandLock.LockType.NONE)
+@ExecuteOn(value = {RuntimeType.INSTANCE})
+public class PurgeJbatchRepository implements AdminCommand {
+
+    @Param(name="apptag", primary = true, optional = false)
+    private String apptag;
+    
+    @Override
+    public void execute(AdminCommandContext context) {
+        ServicesManager manager = ServicesManagerImpl.getInstance();
+        IPersistenceManagerService ps = manager.getPersistenceManagerService();
+        ps.purge(apptag);
+       
+    }
+    
+}
