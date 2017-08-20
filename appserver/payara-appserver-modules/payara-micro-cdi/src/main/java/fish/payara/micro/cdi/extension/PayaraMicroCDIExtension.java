c8a9e2fff4d86844a39a2deed27f885116e51a0b
Mert Caliskan <mcaliskan@gmail.com>
2017-08-20 22:30:29 +0300

    PAYARA-1951-Refactor-usage-of-deprecated-addAnnotatedType-method-from-BeforeBeanDiscovery (#1882)

--- appserver/payara-appserver-modules/payara-micro-cdi/src/main/java/fish/payara/micro/cdi/extension/PayaraMicroCDIExtension.java
+++ appserver/payara-appserver-modules/payara-micro-cdi/src/main/java/fish/payara/micro/cdi/extension/PayaraMicroCDIExtension.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2016 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2016-2017] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -55,10 +55,10 @@ public class PayaraMicroCDIExtension implements Extension {
     void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
 
         AnnotatedType<PayaraMicroProducer> at = bm.createAnnotatedType(PayaraMicroProducer.class);
-        bbd.addAnnotatedType(at);
+        bbd.addAnnotatedType(at, PayaraMicroProducer.class.getName());
         
         AnnotatedType<ClusteredCDIEventBusImpl> at3 = bm.createAnnotatedType(ClusteredCDIEventBusImpl.class);
-        bbd.addAnnotatedType(at3);
+        bbd.addAnnotatedType(at3, ClusteredCDIEventBusImpl.class.getName());
 
     }
 
