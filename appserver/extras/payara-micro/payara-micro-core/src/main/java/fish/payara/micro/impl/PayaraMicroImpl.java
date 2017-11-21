68bf3e9ded59fcb5ce89f92b3542e1b0cb598e88
Matt Gill <MattGill98@users.noreply.github.com>
2017-11-21 17:49:56 +0000

    PAYARA-2282 Payara Micro Doesn't Sort Deployments (#2161)
    * Sorted deployments based on file type
    
    * Moved the deployment comparator into a separate file
    
    * Removed unused imports
    
    * Updated deployment comparator to handle files, and applied sorting only to deployment directory

--- appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
+++ appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -89,6 +89,7 @@ import fish.payara.micro.data.InstanceDescriptor;
 import fish.payara.nucleus.requesttracing.RequestTracingService;
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -1366,13 +1367,13 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
         int deploymentCount = 0;
         Deployer deployer = gf.getDeployer();
 
-        // search MICRO-INF/deploy for deployments
+        // search and deploy from MICRO-INF/deploy directory.
         // if there is a deployment called ROOT deploy to the root context /
         URL url = this.getClass().getClassLoader().getResource("MICRO-INF/deploy");
         if (url != null) {
             String entryName = "";
             try {
-                List<String> entriesToDeploy = new LinkedList<>();
+                List<String> microInfEntries = new LinkedList<>();
                 JarURLConnection urlcon = (JarURLConnection) url.openConnection();
                 JarFile jFile = urlcon.getJarFile();
                 Enumeration<JarEntry> entries = jFile.entries();
@@ -1380,11 +1381,11 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
                     JarEntry entry = entries.nextElement();
                     entryName = entry.getName();
                     if (!entry.isDirectory() && !entryName.endsWith(".properties") && !entryName.endsWith(".xml") && !entryName.endsWith(".gitkeep") && entryName.startsWith("MICRO-INF/deploy")) {
-                        entriesToDeploy.add(entryName);
+                        microInfEntries.add(entryName);
                     }
                 }
 
-                for (String entry : entriesToDeploy) {
+                for (String entry : microInfEntries) {
                     File file = new File(entry);
                     String contextRoot = file.getName();
                     String name = contextRoot.substring(0, contextRoot.length() - 4);
@@ -1409,6 +1410,7 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
             LOGGER.info("No META-INF/deploy directory");
         }
 
+        // Deploy command line provided files
         if (deployments != null) {
             for (File war : deployments) {
                 if (war.exists() && war.canRead()) {
@@ -1426,13 +1428,18 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
 
         // Deploy from deployment directory
         if (deploymentRoot != null) {
-            for (File war : deploymentRoot.listFiles()) {
-                String warPath = war.getAbsolutePath();
-                if (war.isFile() && war.canRead() && (warPath.endsWith(".war") || warPath.endsWith(".ear") || warPath.endsWith(".jar") || warPath.endsWith(".rar"))) {
-                    if (war.getName().startsWith("ROOT.")) {
-                        deployer.deploy(war, "--availabilityenabled=true", "--force=true", "--contextroot=/");
+            
+            // Get all files in the directory, and sort them by file type
+            List<File> deploymentDirEntries = Arrays.asList(deploymentRoot.listFiles());
+            deploymentDirEntries.sort(new DeploymentComparator());
+            
+            for (File entry : deploymentDirEntries) {
+                String entryPath = entry.getAbsolutePath();
+                if (entry.isFile() && entry.canRead() && (entryPath.endsWith(".war") || entryPath.endsWith(".ear") || entryPath.endsWith(".jar") || entryPath.endsWith(".rar"))) {
+                    if (entry.getName().startsWith("ROOT.")) {
+                        deployer.deploy(entry, "--availabilityenabled=true", "--force=true", "--contextroot=/");
                     } else {
-                        deployer.deploy(war, "--availabilityenabled=true", "--force=true");
+                        deployer.deploy(entry, "--availabilityenabled=true", "--force=true");
                     }
                     deploymentCount++;
                 }
