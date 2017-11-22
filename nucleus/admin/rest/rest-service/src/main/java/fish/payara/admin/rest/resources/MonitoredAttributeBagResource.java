5e296a600cf97d1006ea03e60106105f3cca6ba7
Matt Gill <matthew.gill@live.co.uk>
2017-11-22 16:08:24 +0000

    Made rest errors work properly

--- nucleus/admin/rest/rest-service/src/main/java/fish/payara/admin/rest/resources/MonitoredAttributeBagResource.java
+++ nucleus/admin/rest/rest-service/src/main/java/fish/payara/admin/rest/resources/MonitoredAttributeBagResource.java
@@ -4,13 +4,13 @@ import org.glassfish.admin.rest.resources.*;
 import com.sun.enterprise.util.LocalStringManagerImpl;
 
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.glassfish.admin.rest.utils.Util;
@@ -19,7 +19,9 @@ import org.glassfish.admin.rest.results.OptionsResult;
 import org.glassfish.admin.rest.utils.ResourceUtil;
 import org.glassfish.admin.rest.utils.xml.RestActionReporter;
 import org.glassfish.api.ActionReport;
+import org.glassfish.config.support.TranslatedConfigView;
 import org.jvnet.hk2.config.Dom;
+import org.jvnet.hk2.config.TransactionFailure;
 
 /**
  * Resource for managing monitored attributes. Supports GET, POST, PUT and
@@ -67,34 +69,51 @@ public class MonitoredAttributeBagResource extends AbstractResource {
     }
 
     /**
-     * Creates new monitored-attributes. Existing monitored-attributes will be
-     * ignored, and others will be created.
-     *
-     * @param data the list of monitored-attributes to be created.
-     * @return a list of the monitored-attributes after the transaction.
-     */
-    @POST
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_FORM_URLENCODED})
-    @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public ActionReportResult post(List<Map<String, String>> data) {
-        List<Map<String, String>> currentData = getMonitoredAttributes();
-        data.addAll(currentData);
-        return put(data);
-    }
-
-    /**
      * Creates new monitored-attributes. This method deletes all of the existing
      * monitored-attributes.
      *
-     * @param data the list of monitored-attributes to be created.
+     * @param attributes the list of monitored-attributes to be created.
      * @return a list of the monitored-attributes after the transaction.
      */
     @PUT
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_FORM_URLENCODED})
     @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public ActionReportResult put(List<Map<String, String>> data) {
-        setMonitoredAttributes(data);
-        return get();
+    public ActionReportResult put(List<Map<String, String>> attributes) {
+
+        RestActionReporter ar = new RestActionReporter();
+        ar.setActionExitCode(ActionReport.ExitCode.SUCCESS);
+        ar.setActionDescription("monitored-attribute");
+
+        try {
+            setMonitoredAttributes(attributes);
+
+            List monitoredAttributes = getMonitoredAttributes();
+
+            Properties extraProperties = new Properties();
+            extraProperties.put("monitoredAttributes", monitoredAttributes);
+            ar.setExtraProperties(extraProperties);
+        } catch (TransactionFailure ex) {
+            ar.setActionExitCode(ActionReport.ExitCode.FAILURE);
+            ar.setMessage(ex.getMessage());
+        }
+
+        return new ActionReportResult(tagName, ar, new OptionsResult(Util.getResourceName(uriInfo)));
+    }
+
+    /**
+     * Creates new monitored-attributes. Existing monitored-attributes will be
+     * ignored, and others will be created.
+     *
+     * @param attributes the list of monitored-attributes to be created.
+     * @return a list of the monitored-attributes after the transaction.
+     */
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_FORM_URLENCODED})
+    @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public ActionReportResult post(List<Map<String, String>> attributes) {
+        List<Map<String, String>> currentData = getMonitoredAttributes();
+        attributes.addAll(currentData);
+        return put(attributes);
     }
 
     /**
@@ -115,7 +134,7 @@ public class MonitoredAttributeBagResource extends AbstractResource {
      * @return a list of the monitored attributes
      */
     public List<Map<String, String>> getMonitoredAttributes() {
-        List<Map<String, String>> monitoredAttributes = new ArrayList<>();
+        List<Map<String, String>> attributes = new ArrayList<>();
 
         for (Dom child : entity) {
             Map<String, String> entry = new HashMap<>();
@@ -127,21 +146,51 @@ public class MonitoredAttributeBagResource extends AbstractResource {
                 entry.put("description", description);
             }
 
-            monitoredAttributes.add(entry);
+            attributes.add(entry);
         }
-        return monitoredAttributes;
+        return attributes;
     }
 
-    public void setMonitoredAttributes(List<Map<String, String>> monitoredAttributes) {
-        for (Map<String, String> currentAttribute : getMonitoredAttributes()) {
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("delattribute", String.format("attributeName=%s objectName=%s", currentAttribute.get("attributeName"), currentAttribute.get("objectName")));
-            ResourceUtil.runCommand("set-monitoring-configuration", parameters, getSubject());
-        }
-        for (Map<String, String> data : monitoredAttributes) {
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("addattribute", String.format("attributeName=%s objectName=%s", data.get("attributeName"), data.get("objectName")));
-            ResourceUtil.runCommand("set-monitoring-configuration", parameters, getSubject());
+    /**
+     * Sets the monitored attribute list to the specified list.
+     *
+     * @param attributes the intended list of attributes.
+     * @throws TransactionFailure if an error occurs in removing or adding
+     * attributes.
+     */
+    public void setMonitoredAttributes(List<Map<String, String>> attributes) throws TransactionFailure {
+        TranslatedConfigView.doSubstitution.set(false);
+        List<Map<String, String>> currentAttributes = getMonitoredAttributes();
+
+        // Don't attempt to add any attributes that already exist
+        List<Map<String, String>> attributesToAdd = new ArrayList<>(attributes);
+        attributesToAdd.removeAll(currentAttributes);
+
+        // Get list of attributes that are intended to be deleted
+        List<Map<String, String>> attributesToDelete = new ArrayList<>(currentAttributes);
+        attributesToDelete.removeAll(attributes);
+
+        try {
+            // Add all required attributes
+            for (Map<String, String> attribute : attributesToAdd) {
+                Map<String, String> parameters = new HashMap<>();
+                parameters.put("addattribute", String.format("attributeName=%s objectName=%s", attribute.get("attributeName"), attribute.get("objectName")));
+                RestActionReporter reporter = ResourceUtil.runCommand("set-monitoring-configuration", parameters, getSubject());
+                if (reporter.isFailure()) {
+                    throw new TransactionFailure(reporter.getMessage());
+                }
+            }
+            // Delete all unrequired attributes
+            for (Map<String, String> attribute : attributesToDelete) {
+                Map<String, String> parameters = new HashMap<>();
+                parameters.put("delattribute", String.format("attributeName=%s objectName=%s", attribute.get("attributeName"), attribute.get("objectName")));
+                RestActionReporter reporter = ResourceUtil.runCommand("set-monitoring-configuration", parameters, getSubject());
+                if (reporter.isFailure()) {
+                    throw new TransactionFailure(reporter.getMessage());
+                }
+            }
+        } finally {
+            TranslatedConfigView.doSubstitution.set(true);
         }
         entity = parent.nodeElements("monitored-attributes");
     }
