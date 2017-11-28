payara/Payara

commit 8f54e86db126658d23b203ae0d2f39ce61638c36

PAYARA-2095 Fix EJB timers broken in Payara 5 alpha1 (#1994)

appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -987,18 +987,11 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
             
             // boot the server
             preBootCommands.executeCommands(gf.getCommandRunner());
-            gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=false");
             gf.start();
             postBootCommands.executeCommands(gf.getCommandRunner());
             this.runtime = new PayaraMicroRuntimeImpl(gf, gfruntime);
 
-            // load all applications, but do not start them until Hazelcast gets a chance to initialize
             deployAll();
-            if(!noCluster) {
-                gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=true");
-                gf.getCommandRunner().run("set-hazelcast-configuration", "--enabled", "true", "--dynamic", "true");
-            }
-
             gf.getCommandRunner().run("initialize-all-applications");
 
             postBootActions();

appserver/payara-appserver-modules/hazelcast-ejb-timer/src/main/java/fish/payara/ejb/timer/hazelcast/HazelcastTimerStore.java
@@ -95,6 +95,10 @@ public class HazelcastTimerStore extends EJBTimerService {
     }
 
     public HazelcastTimerStore(HazelcastCore core) throws Exception {
+        
+        if (!core.isEnabled()) {
+            throw new Exception("Hazelcast MUST be enabled when using the HazelcastTimerStore");
+        }
         pkCache = core.getInstance().getMap(EJB_TIMER_CACHE_NAME);
         containerCache = core.getInstance().getMap(EJB_TIMER_CONTAINER_CACHE_NAME);
         applicationCache = core.getInstance().getMap(EJB_TIMER_APPLICAION_CACHE_NAME);

commit fd14c7edcbc670883af44f91be213118af1aab72

re-introduced deferred Hz startup Broken in https://github.com/payara/Payara/pull/1994

appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -989,11 +989,18 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
 
             // boot the server
             preBootCommands.executeCommands(gf.getCommandRunner());
+            gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=false");
             gf.start();
             postBootCommands.executeCommands(gf.getCommandRunner());
             this.runtime = new PayaraMicroRuntimeImpl(gf, gfruntime);
 
+            // load all applications, but do not start them until Hazelcast gets a chance to initialize
             deployAll();
+            if(!noCluster) {
+                 gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=true");
+                 gf.getCommandRunner().run("set-hazelcast-configuration", "--enabled", "true", "--dynamic", "true");
+            }
+
             gf.getCommandRunner().run("initialize-all-applications");
 
             postBootActions();