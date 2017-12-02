payara/Payara

commit 8f54e86db126658d23b203ae0d2f39ce61638c36

PAYARA-2095 Fix EJB timers broken in Payara 5 alpha1 (#1994)

appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -987,18 +987,11 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
             
             // boot the server
             preBootCommands.executeCommands(gf.getCommandRunner());
-            gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=false");
             gf.start();
             postBootCommands.executeCommands(gf.getCommandRunner());
             this.runtime = new PayaraMicroRuntimeImpl(gf, gfruntime);
 
-            // load all applications, but do not start them until Hazelcast gets a chance to initialize
             deployAll();
-            if(!noCluster) {
-                gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=true");
-                gf.getCommandRunner().run("set-hazelcast-configuration", "--enabled", "true", "--dynamic", "true");
-            }
-
             gf.getCommandRunner().run("initialize-all-applications");
 
             postBootActions();

appserver/payara-appserver-modules/hazelcast-ejb-timer/src/main/java/fish/payara/ejb/timer/hazelcast/HazelcastTimerStore.java
@@ -95,6 +95,10 @@ public class HazelcastTimerStore extends EJBTimerService {
     }
 
     public HazelcastTimerStore(HazelcastCore core) throws Exception {
+        
+        if (!core.isEnabled()) {
+            throw new Exception("Hazelcast MUST be enabled when using the HazelcastTimerStore");
+        }
         pkCache = core.getInstance().getMap(EJB_TIMER_CACHE_NAME);
         containerCache = core.getInstance().getMap(EJB_TIMER_CONTAINER_CACHE_NAME);
         applicationCache = core.getInstance().getMap(EJB_TIMER_APPLICAION_CACHE_NAME);

commit 4e1579066e49c69f8cae0f931aef34b230303658

PAYARA-2243 and PAYARA-2095 EJB Timer initialization in Payara Micro 5 (#2190)
* re-introduced deferred Hz startup
Broken in https://github.com/payara/Payara/pull/1994

* Defer application container loading until the initialization phase

* restored previous lost changes to HazelcastCore

* Broke out post-Hz initialize() method

* missed null check

appserver/ejb/ejb-container/src/main/java/com/sun/ejb/Container.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]
 
 package com.sun.ejb;
 
@@ -217,6 +218,11 @@ public interface Container {
      * Called by webservice code to do ejb invocation post processing.
      */
     void webServicePostInvoke(EjbInvocation inv);
+
+    /**
+     * Called after creation when Hazelcast is available
+     */
+    default void initialize() {};
 
     /**
      * Called by the EJB(Local)Home after invoking ejbCreate on an EntityBean.

appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/BaseContainer.java
@@ -831,22 +831,6 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
                 }
                 
             }
-            if( isTimedObject_ ) {
-                if( !isStatefulSession ) {
-                    // EJBTimerService should be accessed only if needed 
-                    // not to cause it to be loaded if it's not used.
-                    EJBTimerService timerService = EJBTimerService.getEJBTimerService();
-                    if( timerService != null ) {
-                        timerService.timedObjectCount();
-                    }
-                } else {
-                    isTimedObject_ = false;
-                    throw new EJBException(localStrings.getLocalString(
-                            "ejb.stateful_cannot_be_timed_object", 
-                            "EJB {0} is invalid. Stateful session ejbs cannot be Timed Objects",
-                            ejbDescriptor.getName()));
-                }
-            }
 
             preInitialize(ejbDesc, loader);
             
@@ -880,6 +864,26 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
 		+ "; containerId: " + ejbDescriptor.getUniqueId();
 	_logger.log(Level.FINE, "Instantiated container for: "
 		+ _debugDescription);
+    }
+
+    @Override
+    public void initialize() {
+        if (isTimedObject_) {
+            if (!isStatefulSession) {
+                // EJBTimerService should be accessed only if needed
+                // not to cause it to be loaded if it's not used.
+                EJBTimerService timerService = EJBTimerService.getEJBTimerService();
+                if (timerService != null) {
+                    timerService.timedObjectCount();
+                }
+            } else {
+                isTimedObject_ = false;
+                throw new EJBException(localStrings.getLocalString(
+                        "ejb.stateful_cannot_be_timed_object",
+                        "EJB {0} is invalid. Stateful session ejbs cannot be Timed Objects",
+                        ejbDescriptor.getName()));
+            }
+        }
     }
 
     protected ProtocolManager getProtocolManager() {

appserver/ejb/ejb-container/src/main/java/org/glassfish/ejb/startup/EjbApplication.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2016] [Payara Foundation]
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]
 
 package org.glassfish.ejb.startup;
 
@@ -243,6 +243,11 @@ public class EjbApplication
         }
         
         return true;
+    }
+
+    @Override
+    public void initialize() {
+        containers.forEach(Container::initialize);
     }
 
     public boolean stop(ApplicationContext stopContext) {

appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -989,11 +989,18 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
 
             // boot the server
             preBootCommands.executeCommands(gf.getCommandRunner());
+            gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=false");
             gf.start();
             postBootCommands.executeCommands(gf.getCommandRunner());
             this.runtime = new PayaraMicroRuntimeImpl(gf, gfruntime);
 
+            // load all applications, but do not start them until Hazelcast gets a chance to initialize
             deployAll();
+            if(!noCluster) {
+                 gf.getCommandRunner().run("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=true");
+                 gf.getCommandRunner().run("set-hazelcast-configuration", "--enabled", "true", "--dynamic", "true");
+            }
+
             gf.getCommandRunner().run("initialize-all-applications");
 
             postBootActions();

nucleus/common/glassfish-api/src/main/java/org/glassfish/api/deployment/ApplicationContainer.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]
 
 package org.glassfish.api.deployment;
 
@@ -56,6 +57,12 @@ public interface ApplicationContainer<T> {
      */
     public T getDescriptor();
     
+    /**
+     * Initialize the container post-startup of Hazelcast
+     * Used for EJB timers right now
+     */
+    default public void initialize() {};
+
     /**
      * Starts an application container. 
      * ContractProvider starting should not throw an exception but rather should
@@ -98,5 +105,4 @@ public interface ApplicationContainer<T> {
      * @return ClassLoader for this app
      */
     public ClassLoader getClassLoader();
-    
 }

nucleus/common/internal-api/src/main/java/org/glassfish/internal/data/ApplicationInfo.java
@@ -374,6 +374,11 @@ public class ApplicationInfo extends ModuleInfo {
 
     }
 
+    public void initialize() {
+        getModuleInfos().forEach(module -> module.getEngineRefs().forEach(EngineRef::initialize));
+    }
+
+
     public void stop(ExtendedDeploymentContext context, Logger logger) {
 
         ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();

nucleus/common/internal-api/src/main/java/org/glassfish/internal/data/EngineRef.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2016-2017] [Payara Foundation and/or its affiliates]
 
 package org.glassfish.internal.data;
 
@@ -183,4 +184,9 @@ public class EngineRef {
         }
     }
 
+    void initialize() {
+        if(appCtr != null) {
+            appCtr.initialize();
+        }
+    }
 }

nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
@@ -523,6 +523,7 @@ public class ApplicationLifecycle implements Deployment, PostConstruct {
         if (loadOnCurrentInstance(context)) {
             try {
                 notifyLifecycleInterceptorsBefore(ExtendedDeploymentContext.Phase.START, context);
+                appInfo.initialize();
                 appInfo.start(context, tracker);
                 notifyLifecycleInterceptorsAfter(ExtendedDeploymentContext.Phase.START, context);
             } catch (Throwable loadException) {

nucleus/payara-modules/hazelcast-bootstrap/src/main/java/fish/payara/nucleus/hazelcast/HazelcastCore.java
@@ -53,6 +53,7 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.nio.serialization.Serializer;
 import com.hazelcast.nio.serialization.StreamSerializer;
+import com.sun.enterprise.util.Utility;
 import fish.payara.nucleus.events.HazelcastEvents;
 import fish.payara.nucleus.hazelcast.contextproxy.CachingProviderProxy;
 import java.io.File;
@@ -74,12 +75,12 @@ import javax.naming.NamingException;
 import org.glassfish.api.StartupRunLevel;
 import org.glassfish.api.admin.ServerEnvironment;
 import org.glassfish.api.event.EventListener;
-import org.glassfish.api.event.EventTypes;
 import org.glassfish.api.event.Events;
 import org.glassfish.hk2.runlevel.RunLevel;
 import org.glassfish.internal.api.ClassLoaderHierarchy;
 import org.glassfish.internal.api.JavaEEContextUtil;
 import org.glassfish.internal.api.ServerContext;
+import org.glassfish.internal.deployment.Deployment;
 import org.jvnet.hk2.annotations.Optional;
 import org.jvnet.hk2.annotations.Service;
 
@@ -224,10 +225,16 @@ public class HazelcastCore implements EventListener {
 
     @Override
     public void event(Event event) {
-        if (event.is(EventTypes.SERVER_SHUTDOWN)) {
+        if (event.is(Deployment.ALL_APPLICATIONS_STOPPED)) {
             shutdownHazelcast();
-        } else if (event.is(EventTypes.SERVER_STARTUP)) {
-            bootstrapHazelcast();
+        } else if (event.is(Deployment.ALL_APPLICATIONS_LOADED)) {
+            ClassLoader oldCL = Utility.getClassLoader();
+            try {
+                Utility.setContextClassLoader(clh.getCommonClassLoader());
+                bootstrapHazelcast();
+            } finally {
+                Utility.setContextClassLoader(oldCL);
+            }
         }
     }
 
@@ -353,6 +360,8 @@ public class HazelcastCore implements EventListener {
 
     private void shutdownHazelcast() {
         if (theInstance != null) {
+            enabled = false;
+            events.send(new Event(HazelcastEvents.HAZELCAST_SHUTDOWN_STARTED, true));
             unbindFromJNDI();
             hazelcastCachingProvider.getCacheManager().close();
             hazelcastCachingProvider.close();