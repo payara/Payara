8d13f4fa3d95f798bcc52732cb45773abb9922da
lprimak <lenny@flowlogix.com>
2018-01-27 19:58:30 -0600

    portions copyright

--- appserver/jms/gf-jms-injection/src/main/java/org/glassfish/jms/injection/InjectableJMSContext.java
+++ appserver/jms/gf-jms-injection/src/main/java/org/glassfish/jms/injection/InjectableJMSContext.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2016-2018] [Payara Foundation and/or its affiliates]
 
 package org.glassfish.jms.injection;
 
