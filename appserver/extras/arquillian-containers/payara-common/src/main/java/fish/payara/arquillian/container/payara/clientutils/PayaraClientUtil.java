e6d020cf2b283a4cf51a93a161af8812a8934153
arjantijms <arjan.tijms@gmail.com>
2018-02-03 23:28:41 +0100

    PAYARA-2452 Updated copyright headers

--- appserver/extras/arquillian-containers/payara-common/src/main/java/fish/payara/arquillian/container/payara/clientutils/PayaraClientUtil.java
+++ appserver/extras/arquillian-containers/payara-common/src/main/java/fish/payara/arquillian/container/payara/clientutils/PayaraClientUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017-2018 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
--- appserver/web/admin/src/main/java/org/glassfish/web/admin/cli/ListVirtualServers.java
+++ appserver/web/admin/src/main/java/org/glassfish/web/admin/cli/ListVirtualServers.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
+//Portions Copyright [2018] Payara Foundation and/or affiliates
 package org.glassfish.web.admin.cli;
 
 import static org.glassfish.api.admin.CommandLock.LockType.NONE;
