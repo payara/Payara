548a753dc09205d2e379026045f325addd571322
Matt Gill <MattGill98@users.noreply.github.com>
2018-02-14 09:59:20 +0000

    PAYARA-2427 Payara Micro Deployed Application Produces Log Spam (#2388)
    * Changed InstanceDescriptor printing in Payara Micro to a JSON format.
    
    * Updated name in Payara Micro poms.
    
    * added --showservletmappings parameter.
    
    * Added copyright headers
    
    * Updated case in system property configuration
    
    * Removed default value for primitive boolean (defaults to false).
    
    * Imported static JsonGenerator value

--- appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/data/ModuleDescriptor.java
+++ appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/data/ModuleDescriptor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2016 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016-2018 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,9 +40,7 @@
 package fish.payara.micro.data;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Describes an application module
