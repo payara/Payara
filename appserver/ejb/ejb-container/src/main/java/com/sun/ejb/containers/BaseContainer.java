53c865d5c8129d23a60faa331c9514e13ad34ffd
lprimak <lenny@flowlogix.com>
2018-02-22 14:49:27 -0600

    - added created and more intormative toString() to WebappClassLoader for debugging - Making sure that Cluster updates do not happen while app is stopped, especially in EJB timers - Setting context class loader so race conditions don't happen with class loading

--- appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/BaseContainer.java
+++ appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/BaseContainer.java
@@ -137,6 +137,7 @@ import org.glassfish.flashlight.provider.ProbeProviderFactory;
 import org.glassfish.hk2.api.PostConstruct;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.internal.api.Globals;
+import org.glassfish.internal.api.JavaEEContextUtil;
 import org.glassfish.logging.annotation.LogMessageInfo;
 import javax.annotation.PreDestroy;
 import javax.interceptor.AroundConstruct;
@@ -486,7 +487,7 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
     protected static final int CONTAINER_UNDEPLOYED = 3;
     protected static final int CONTAINER_ON_HOLD = 4;
     
-    protected int containerState = CONTAINER_INITIALIZING;
+    protected volatile int containerState = CONTAINER_INITIALIZING;
     
     protected HashMap			    methodMonitorMap;
     protected boolean			    monitorOn = false;
@@ -521,6 +522,7 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
     private Set<Class> monitoredGeneratedClasses = new HashSet<Class>();
 
     protected InvocationManager invocationManager;
+    protected JavaEEContextUtil ctxUtil;
 
     protected InjectionManager injectionManager;
 
@@ -587,6 +589,7 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
             logParams = new Object[1];
             logParams[0] =  ejbDesc.getName();
             invocationManager = ejbContainerUtilImpl.getInvocationManager();
+            ctxUtil = ejbContainerUtilImpl.getServices().getService(JavaEEContextUtil.class);
             injectionManager = ejbContainerUtilImpl.getInjectionManager();
             namingManager = ejbContainerUtilImpl.getGlassfishNamingManager();
             transactionManager = ejbContainerUtilImpl.getTransactionManager();
@@ -4018,6 +4021,7 @@ public abstract class BaseContainer implements Container, EjbContainerFacade, Ja
         SafeProperties safeProps = new SafeProperties();
         safeProps.copy(env);
         envProps = safeProps;
+        ctxUtil.setInstanceComponentId(componentId);
     }
     
     /**
