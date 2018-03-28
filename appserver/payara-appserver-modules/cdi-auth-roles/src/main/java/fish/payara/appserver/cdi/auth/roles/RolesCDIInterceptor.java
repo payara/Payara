b6ecbf82b1c6f1ce3ddcbbf37742fe3b1e00ae0b
Mero Rai <susan.rai04@gmail.com>
2018-03-28 16:25:52 +0100

    PAYARA-2578 CDI roles Extension on Payara 4 doesn't have Arjan's rework (#2523)
    * PAYARA-2534 CDI roles - fixed several issues, specifically extension not found one (#2478)
    
    * PAYARA-2534 Fixed several issues, specifically extension not found one
    
    * PAYARA-2534 Added copyright header
    
    * Replaced lamda code
    
    * Removed JDK8 specific code

--- appserver/payara-appserver-modules/cdi-auth-roles/src/main/java/fish/payara/appserver/cdi/auth/roles/RolesCDIInterceptor.java
+++ appserver/payara-appserver-modules/cdi-auth-roles/src/main/java/fish/payara/appserver/cdi/auth/roles/RolesCDIInterceptor.java
@@ -1,9 +1,9 @@
 /*
  *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *  
- *   Copyright (c) [2017-2018] Payara Foundation and/or its affiliates. 
+ *
+ *   Copyright (c) [2017-2018] Payara Foundation and/or its affiliates.
  *   All rights reserved.
- *  
+ *
  *   The contents of this file are subject to the terms of either the GNU
  *   General Public License Version 2 only ("GPL") or the Common Development
  *   and Distribution License("CDDL") (collectively, the "License").  You
@@ -12,20 +12,20 @@
  *   https://github.com/payara/Payara/blob/master/LICENSE.txt
  *   See the License for the specific
  *   language governing permissions and limitations under the License.
- *  
+ *
  *   When distributing the software, include this License Header Notice in each
  *   file and include the License file at glassfish/legal/LICENSE.txt.
- *  
+ *
  *   GPL Classpath Exception:
- *   The Payara Foundation designates this particular file as subject to the 
- *   "Classpath" exception as provided by the Payara Foundation in the GPL 
+ *   The Payara Foundation designates this particular file as subject to the
+ *   "Classpath" exception as provided by the Payara Foundation in the GPL
  *   Version 2 section of the License file that accompanied this code.
- *  
+ *
  *   Modifications:
  *   If applicable, add the following below the License Header, with the fields
  *   enclosed by brackets [] replaced by your own identifying information:
  *   "Portions Copyright [year] [name of copyright owner]"
- *  
+ *
  *   Contributor(s):
  *   If you wish your version of this file to be governed by only the CDDL or
  *   only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -40,80 +40,96 @@
  */
 package fish.payara.appserver.cdi.auth.roles;
 
-import com.sun.enterprise.universal.i18n.LocalStringsImpl;
-import fish.payara.cdi.auth.roles.Roles;
-import fish.payara.cdi.auth.roles.LogicalOperator;
-import java.io.Serializable;
-import java.util.Arrays;
+import static fish.payara.cdi.auth.roles.LogicalOperator.AND;
+import static fish.payara.cdi.auth.roles.LogicalOperator.OR;
+import static java.util.Arrays.asList;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Queue;
+import java.util.Set;
+
 import javax.annotation.Priority;
+import javax.enterprise.inject.Intercepted;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import javax.security.enterprise.SecurityContext;
-import javax.ws.rs.NotAuthorizedException;
+
+import fish.payara.cdi.auth.roles.CallerAccessException;
+import fish.payara.cdi.auth.roles.RolesPermitted;
 
 /**
- * The Roles CDI Interceptor authenticates requests to methods and classes annotated with the @Roles annotation. If the
- * security context cannot find a role within the requestor which matches either all (if using the AND semantic within
- * the Roles annotation) or one of (if using the OR semantic within the Roles annotation), then a NotAuthorizedException
- * is thrown.
+ * The Roles CDI Interceptor authenticates requests to methods and classes
+ * annotated with the @Roles annotation. If the security context cannot find a
+ * role within the requestor which matches either all (if using the AND semantic
+ * within the Roles annotation) or one of (if using the OR semantic within the
+ * Roles annotation), then a NotAuthorizedException is thrown.
  *
  * @author Michael Ranaldo <michael@ranaldo.co.uk>
+ * @author Arjan Tijms
  */
 @Interceptor
-@Roles
+@RolesPermitted
 @Priority(Interceptor.Priority.PLATFORM_AFTER)
-public class RolesCDIInterceptor implements Serializable {
-
-    private final static LocalStringsImpl STRINGS = new LocalStringsImpl(RolesCDIInterceptor.class);
+public class RolesCDIInterceptor {
 
     private final SecurityContext securityContext;
 
-    public RolesCDIInterceptor() {
+    private BeanManager beanManager;
+
+    private Bean<?> interceptedBean;
+
+    @Inject
+    public RolesCDIInterceptor(@Intercepted Bean<?> interceptedBean, BeanManager beanManager) {
         this.securityContext = CDI.current().select(SecurityContext.class).get();
+        this.interceptedBean = interceptedBean;
+        this.beanManager = beanManager;
     }
 
     /**
-     * Method invoked whenever a method annotated with @Roles, or a method within a class annotated with @Roles is
-     * called.
+     * Method invoked whenever a method annotated with @Roles, or a method
+     * within a class annotated with @Roles is called.
      *
      * @param invocationContext Context provided by Weld.
      * @return Proceed to next interceptor in chain.
      */
     @AroundInvoke
-    public Object method(InvocationContext invocationContext) {
-        if (checkRoles(invocationContext.getMethod().getAnnotation(Roles.class))
-                || (invocationContext.getMethod().getAnnotation(Roles.class) == null
-                && checkRoles(invocationContext.getClass().getAnnotation(Roles.class)))) {
-            try {
-                Object result = invocationContext.proceed();
-                return result;
-            } catch (Exception ex) {
-                Logger.getLogger(RolesCDIInterceptor.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    public Object method(InvocationContext invocationContext) throws Exception {
+        RolesPermitted roles = getRolesPermitted(invocationContext);
+
+        boolean isAccessPermitted = checkAccessPermitted(roles);
+
+        if (!isAccessPermitted) {
+            throw new CallerAccessException("Caller was not permitted access to a protected resource");
         }
-        throw new NotAuthorizedException(STRINGS.get("access.restricted.resource.failed"));
+
+        return invocationContext.proceed();
     }
 
     /**
-     * Check that the roles allowed by the class or method match the roles currently granted to the caller.
+     * Check that the roles allowed by the class or method match the roles
+     * currently granted to the caller.
      *
      * @param roles The roles declared within the @Roles annotation.
-     * @return True or False
+     * @return True if access is allowed, false otherwise
      */
-    public boolean checkRoles(Roles roles) {
-        List<String> permittedRoles = Arrays.asList(roles.allowed());
-        if (roles.semantics().equals(LogicalOperator.OR)) {
+    public boolean checkAccessPermitted(RolesPermitted roles) {
+        List<String> permittedRoles = asList(roles.value());
+
+        if (roles.semantics().equals(OR)) {
             for (String role : permittedRoles) {
                 if (securityContext.isCallerInRole(role)) {
                     return true;
                 }
             }
-        } else if (roles.semantics().equals(LogicalOperator.AND)) {
+        } else if (roles.semantics().equals(AND)) {
             for (String role : permittedRoles) {
                 if (!securityContext.isCallerInRole(role)) {
                     return false;
@@ -121,6 +137,103 @@ public class RolesCDIInterceptor implements Serializable {
             }
             return true;
         }
+
         return false;
+    }
+
+    private RolesPermitted getRolesPermitted(InvocationContext invocationContext) {
+
+        RolesPermitted rolesPermitted = null;
+        @SuppressWarnings("unchecked")
+        Set<Annotation> bindings = (Set<Annotation>) invocationContext.getContextData().get("org.jboss.weld.interceptor.bindings");
+        if (bindings != null) {
+            for (Annotation annotation : bindings) {
+                if (annotation.annotationType().equals(RolesPermitted.class)) {
+                    rolesPermitted = RolesPermitted.class.cast(annotation);
+                }
+            }
+
+            if (rolesPermitted != null) {
+                return rolesPermitted;
+            }
+        }
+
+        // Failing the Weld binding, check the method first
+        rolesPermitted = getAnnotationFromMethod(beanManager, invocationContext.getMethod(), RolesPermitted.class);
+        if (rolesPermitted != null) {
+            return rolesPermitted;
+        }
+
+        // If nothing found on the method, check the the bean class
+        rolesPermitted = getAnnotationFromClass(beanManager, interceptedBean.getBeanClass(), RolesPermitted.class);
+        if (rolesPermitted != null) {
+            return rolesPermitted;
+        }
+
+        // If still not found; throw. Since we're being called the annotation has to be there. Failing to
+        // find it signals a critical error.
+        throw new IllegalStateException("@RolesPermitted not found on " + interceptedBean.getBeanClass());
+    }
+
+    public static <A extends Annotation> A getAnnotationFromMethod(BeanManager beanManager, Method annotatedMethod, Class<A> annotationType) {
+
+        if (annotatedMethod.isAnnotationPresent(annotationType)) {
+            if (annotatedMethod.getAnnotation(annotationType) != null) {
+                return annotatedMethod.getAnnotation(annotationType);
+            }
+        }
+
+        Queue<Annotation> annotations = new LinkedList<>(asList(annotatedMethod.getAnnotations()));
+
+        while (!annotations.isEmpty()) {
+            Annotation annotation = annotations.remove();
+
+            if (annotation.annotationType().equals(annotationType)) {
+                if (annotationType.cast(annotation) != null) {
+                    return annotationType.cast(annotation);
+                }
+            }
+
+            if (beanManager.isStereotype(annotation.annotationType())) {
+                annotations.addAll(
+                        beanManager.getStereotypeDefinition(
+                                annotation.annotationType()
+                        )
+                );
+            }
+        }
+
+        return null;
+    }
+
+    public static <A extends Annotation> A getAnnotationFromClass(BeanManager beanManager, Class<?> annotatedClass, Class<A> annotationType) {
+
+        if (annotatedClass.isAnnotationPresent(annotationType)) {
+            if (annotatedClass.getAnnotation(annotationType) != null) {
+                return annotatedClass.getAnnotation(annotationType);
+            }
+        }
+
+        Queue<Annotation> annotations = new LinkedList<>(asList(annotatedClass.getAnnotations()));
+
+        while (!annotations.isEmpty()) {
+            Annotation annotation = annotations.remove();
+
+            if (annotation.annotationType().equals(annotationType)) {
+                if (annotationType.cast(annotation) != null) {
+                    return annotationType.cast(annotation);
+                }
+            }
+
+            if (beanManager.isStereotype(annotation.annotationType())) {
+                annotations.addAll(
+                        beanManager.getStereotypeDefinition(
+                                annotation.annotationType()
+                        )
+                );
+            }
+        }
+
+        return null;
     }
 }
