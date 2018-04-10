6d023e5530e6c0280aa21a0385100506ad3caff9
lprimak <lprimak@hope.nyc.ny.us>
2018-04-10 11:16:39 -0500

    PAYARA-2263 per-jdk version domain options (#2585)
    * added min-version and max-version to jdk-option field
    
    * introduced JvmOption class that contains JDK versions
    
    * work in progress
    
    * revert unneeded files
    
    * basic functionality complete - using the prefix of [[<min_vers>|<max_vers>]] to denote JDK versions
    
    * version pattern fixes
    
    * fixed test
    
    * updated usage text
    
    * working admin console, security settings
    
    * preserve JDK versions in security handler for consolee
    
    * formatting and copyrights
    
    * fixed bug where saving JVM options would modify internal JSF list to remove selected field, thus causing errors
    
    * add support for versions such as 1.8.0u162 (the 'u')
    
    * added support for versions that are missing minor / subminor / update versions
    added correct comparison support for those versions
    
    * refactored constants, added equals/hashCode() for JvmOptions class
    
    * removed a leftover TODO, using fuctional method to iterate through options
    
    * Update CollectionLeafResource.java
    
    * Update CollectionLeafResource.java
    
    * Update MiniXmlParser.java
    
    * Update JDK.java
    
    * Update DeleteJvmOptions.java
    
    * Update InstanceHandler.java
    
    * fixed style / formatting, added comments and more tests per code review
    
    * fixed incorrect version unit tests
    
    * enhanced comments, new test

--- appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/InstanceHandler.java
+++ appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/InstanceHandler.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2018] [Payara Foundation and/or its affiliates]
 
 /*
  * InstanceHandler.java
@@ -52,6 +53,7 @@
  */
 package org.glassfish.admingui.common.handlers;
 
+import com.sun.enterprise.universal.xml.MiniXmlParser.JvmOption;
 import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
@@ -66,6 +68,12 @@ import java.util.Map;
 import java.util.logging.Level;
 
 public class InstanceHandler {
+    private static final String SELECTED = "selected";
+    private static final String PROFILER = "profiler";
+    static final String TARGET = "target";
+    static final String MAX_VERSION = "maxVersion";
+    static final String MIN_VERSION = "minVersion";
+    static final String JVM_OPTION = "jvmOption";
 
     /** Creates a new instance of InstanceHandler */
     public InstanceHandler() {
@@ -79,10 +87,16 @@ public class InstanceHandler {
         output={
             @HandlerOutput(name="result", type=java.util.List.class)})
     public static void getJvmOptionsValues(HandlerContext handlerCtx) {
-        try{
-            ArrayList<String> list = getJvmOptions(handlerCtx);
-            handlerCtx.setOutputValue("result", GuiUtil.convertArrayToListOfMap(list.toArray(), "value"));
-        }catch (Exception ex){
+        try {
+            List<Map<String, String>> list = getJvmOptions(handlerCtx);
+            List<Map<String, Object>> optionValues = new ArrayList<>();
+            for (Map<String, String> item : list) {
+                Map<String, Object> valueMap = new HashMap<>(item);
+                valueMap.put(SELECTED, false);
+                optionValues.add(valueMap);
+            }
+            handlerCtx.setOutputValue("result", optionValues);
+        } catch (Exception ex){
             handlerCtx.setOutputValue("result", new HashMap());
             GuiUtil.getLogger().info(GuiUtil.getCommonMessage("log.error.getJvmOptionsValues") + ex.getLocalizedMessage());
             if (GuiUtil.getLogger().isLoggable(Level.FINE)){
@@ -91,46 +105,53 @@ public class InstanceHandler {
         }
     }
     
-     public static ArrayList getJvmOptions(HandlerContext handlerCtx) {
-        ArrayList<String> list;
+    public static List<Map<String, String>> getJvmOptions(HandlerContext handlerCtx) {
+        ArrayList<Map<String, String>> list;
         String endpoint = (String) handlerCtx.getInputValue("endpoint");
         if (!endpoint.endsWith(".json"))
             endpoint = endpoint + ".json";
         Map<String, Object> attrs = (Map<String, Object>) handlerCtx.getInputValue("attrs");
-        Map result = (HashMap) RestUtil.restRequest(endpoint, attrs, "get", handlerCtx, false).get("data");
-        list = (ArrayList<String>) ((Map<String, Object>) result.get("extraProperties")).get("leafList");
+        Map<String, Map> result = (Map<String, Map>) RestUtil.restRequest(endpoint, attrs, "get", handlerCtx, false).get("data");
+        list = (ArrayList<Map<String, String>>) result.get("extraProperties").get("leafList");
         if (list == null)
-            list = new ArrayList<String>();
+            list = new ArrayList<>();
         return list;
     }
  
    @Handler(id="saveJvmOptionValues",
         input={
             @HandlerInput(name="endpoint",   type=String.class, required=true),
-            @HandlerInput(name="target",   type=String.class, required=true),
+            @HandlerInput(name=TARGET,   type=String.class, required=true),
             @HandlerInput(name="attrs", type=Map.class, required=false),
-            @HandlerInput(name="profiler", type=String.class, required=true),
+            @HandlerInput(name=PROFILER, type=String.class, required=true),
             @HandlerInput(name="options",   type=List.class),
             @HandlerInput(name="deleteProfileEndpoint",   type=String.class),
             @HandlerInput(name="origList",   type=List.class)
             } )
    public static void saveJvmOptionValues(HandlerContext handlerCtx) {
         String endpoint = (String) handlerCtx.getInputValue("endpoint");
-        String target = (String) handlerCtx.getInputValue("target");
+        String target = (String) handlerCtx.getInputValue(TARGET);
         try {
-            List<Map> options = (List<Map>) handlerCtx.getInputValue("options");
-            ArrayList<String> newList = new ArrayList();
-            for (Map oneRow : options) {
-                newList.add((String) oneRow.get(PROPERTY_VALUE));
+            @SuppressWarnings("unchecked")
+            List<Map<String, Object>> options = (List<Map<String, Object>>) handlerCtx.getInputValue("options");
+            List<Map<String, Object>> newList = new ArrayList<>();
+            for (Map<String, Object> oneRow : options) {
+                Map<String, Object> newRow = new HashMap<>();
+                oneRow.forEach((key, value) -> {
+                    if (!SELECTED.equalsIgnoreCase(key)) {
+                        newRow.put(key, value);
+                    }
+                });
+                newList.add(newRow);
             }
-            ArrayList<String> oldList = getJvmOptions(handlerCtx);
+            List<Map<String, String>> oldList = getJvmOptions(handlerCtx);
             if (newList.equals(oldList)) {
                 // if old list is same as new list, return without saving anything
                 return;
             }
-            Map<String, Object> payload = new HashMap<String, Object>();
-            payload.put("profiler", (String)handlerCtx.getInputValue("profiler"));
-            prepareJvmOptionPayload(payload, target, options);
+            Map<String, Object> payload = new HashMap<>();
+            payload.put(PROFILER, (String)handlerCtx.getInputValue(PROFILER));
+            prepareJvmOptionPayload(payload, target, newList);
             RestUtil.restRequest(endpoint, payload, "POST", handlerCtx, false, true);
         } catch (Exception ex) {
             //If this is called during create profile, we want to delete the profile which was created, and stay at the same
@@ -138,16 +159,16 @@ public class InstanceHandler {
             String deleteProfileEndpoint = (String) handlerCtx.getInputValue("deleteProfileEndpoint");
             if (!GuiUtil.isEmpty(deleteProfileEndpoint)){
                 Map attrMap = new HashMap();
-                attrMap.put("target", (String) handlerCtx.getInputValue("target"));
+                attrMap.put(TARGET, (String) handlerCtx.getInputValue(TARGET));
                 RestUtil.restRequest(deleteProfileEndpoint, attrMap, "DELETE", handlerCtx, false, false);
             }
 
             //If the origList is not empty,  we want to restore it. Since POST remove all options first and then add it back. As a
             //result, all previous existing option is gone.
-            List<Map> origList = (List<Map>) handlerCtx.getInputValue("origList");
+            List<Map<String, Object>> origList = (List<Map<String, Object>>) handlerCtx.getInputValue("origList");
             Map<String, Object> payload1 = new HashMap<String, Object>();
-            if (endpoint.contains("profiler")) {
-                payload1.put("profiler", "true");
+            if (endpoint.contains(PROFILER)) {
+                payload1.put(PROFILER, "true");
             }
             if ( (origList != null) && origList.size()>0){
                 prepareJvmOptionPayload(payload1, target, origList);
@@ -157,12 +178,13 @@ public class InstanceHandler {
         }
     }
 
-    private static void prepareJvmOptionPayload(Map payload, String target, List<Map> options){
-        payload.put("target", target);
-        for (Map oneRow : options) {
-            String str = (String) oneRow.get(PROPERTY_VALUE);
-            String str1 = UtilHandlers.escapePropertyValue(str);         //refer to GLASSFISH-19069
-            ArrayList kv = getKeyValuePair(str1);
+    private static void prepareJvmOptionPayload(Map<String, Object> payload, String target, List<Map<String, Object>> options) {
+        payload.put(TARGET, target);
+        for (Map<String, Object> oneRow : options) {
+            String jvmOptionUnescaped = new JvmOption((String) oneRow.get(JVM_OPTION),
+                    (String) oneRow.get(MIN_VERSION), (String) oneRow.get(MAX_VERSION)).toString();
+            String jvmOptionEscape = UtilHandlers.escapePropertyValue(jvmOptionUnescaped);         //refer to GLASSFISH-19069
+            ArrayList kv = getKeyValuePair(jvmOptionEscape);
             payload.put((String)kv.get(0), kv.get(1));
         }
     }
@@ -184,8 +206,4 @@ public class InstanceHandler {
         list.add(1, value);
         return list;
     }
-
-    private static final String PROPERTY_VALUE = "value";
 }
-        
- 
