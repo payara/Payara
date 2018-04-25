6cc4970f77da0e6c06995993560e22a684757d1a
Cousjava <jonathan.coustick@payara.fish>
2018-04-25 17:05:29 +0100

    PAYARA-1394 OAuth 2 client integration (#2650)
    * PAYARA-1394 OAuth2 integration - new annotation and handler for it
    
    * PAYARA-1394 Compiling version of basic mechanism
    
    Does not work due to OSGi issues
    
    * PAYARA-1394 resolved OSGi errors
    
    * PAYARA-1394 removed HK2 annotations, using pure CDi
    
    * PAYARA-1394 added import for annotation
    
    * PAYARA-1394 oauth mechanism handler successfuly created, although innjection in it fails
    
    * PAYARA-1394 Another way to produce the mechanism
    
    Fails to inject OAuth2State
    
    * PAYARA-1394 OAuth handler now works
    
    * PAYARA-1394 Added default identity store
    
    OAuth integration now appears to work, but needs further testing
    
    * PAYARA-1394 authentication mechanism is now only used when a request is made to a secured page
    
    Also split up OAuthState so some methods are part of the public api
    
    * PAYARA-1394 OAuth integration now only is called when necessary
    
    * PAYARA-1394 Split state into two files, added moduleinfo and javadoc comments
    
    * PAYARA-1394 minor bug fixes, now  only use one logger name
    
    * PAYARA-1394 Added enviroment variable substitution for the annotation
    
    * PAYARA-1394 Correct scope name, remove extraenous code
    
    * PAYARA-1394 add comma in imports; fix incorrect merge

---
+++ appserver/payara-appserver-modules/security-oauth2/src/main/java/fish/payara/security/oauth2/OAuth2MechanismHandler.java
@@ -0,0 +1,139 @@
+/*
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ *  Copyright (c) [2018] Payara Foundation and/or its affiliates. All rights reserved.
+ * 
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common Development
+ *  and Distribution License("CDDL") (collectively, the "License").  You
+ *  may not use this file except in compliance with the License.  You can
+ *  obtain a copy of the License at
+ *  https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *  See the License for the specific
+ *  language governing permissions and limitations under the License.
+ * 
+ *  When distributing the software, include this License Header Notice in each
+ *  file and include the License file at glassfish/legal/LICENSE.txt.
+ * 
+ *  GPL Classpath Exception:
+ *  The Payara Foundation designates this particular file as subject to the "Classpath"
+ *  exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *  file that accompanied this code.
+ * 
+ *  Modifications:
+ *  If applicable, add the following below the License Header, with the fields
+ *  enclosed by brackets [] replaced by your own identifying information:
+ *  "Portions Copyright [year] [name of copyright owner]"
+ * 
+ *  Contributor(s):
+ *  If you wish your version of this file to be governed by only the CDDL or
+ *  only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *  elects to include this software in this distribution under the [CDDL or GPL
+ *  Version 2] license."  If you don't indicate a single choice of license, a
+ *  recipient has the option to distribute your version of this file under
+ *  either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *  its licensees as provided above.  However, if you add GPL Version 2 code
+ *  and therefore, elected the GPL Version 2 license, then the option applies
+ *  only if the new code is made subject to such option by the copyright
+ *  holder.
+ */
+package fish.payara.security.oauth2;
+
+import java.util.logging.Level;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import fish.payara.security.oauth2.annotation.OAuth2AuthenticationDefinition;
+import fish.payara.security.oauth2.api.OAuth2State;
+import fish.payara.security.oauth2.api.OAuthIdentityStore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.CDI;
+import javax.enterprise.inject.spi.DefinitionException;
+import javax.enterprise.inject.spi.ProcessBean;
+import javax.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
+import javax.security.enterprise.identitystore.IdentityStore;
+import org.glassfish.soteria.cdi.CdiProducer;
+
+/**
+ * Handles the {@link OAuth2AuthenticationDefinition} annotation
+ *
+ * @author jonathan
+ * @since 4.1.2.172
+ */
+public class OAuth2MechanismHandler implements Extension {
+
+    private List<OAuth2AuthenticationDefinition> annotations;
+    private Logger logger = Logger.getLogger("OAuth2Mechanism");
+
+    public OAuth2MechanismHandler() {
+        annotations = new ArrayList<>();
+    }
+
+    /**
+     * This method tries to find the {@link OAuth2AuthenticationDefinition} annotation and if does flags that fact.
+     *
+     * @param <T>
+     * @param eventIn
+     * @param beanManager
+     */
+    public <T> void findOAuth2DefinitionAnnotation(@Observes ProcessBean<T> eventIn, BeanManager beanManager) {
+
+        ProcessBean<T> event = eventIn;
+
+        OAuth2AuthenticationDefinition annotation = event.getAnnotated().getAnnotation(OAuth2AuthenticationDefinition.class);
+        if (annotation != null && !annotations.contains(annotation)) {
+            logger.log(Level.FINE, "Processing annotation {0}", annotation);
+            annotations.add(annotation);
+            for (String param : annotation.extraParameters()){
+                if (param.split("=").length != 2){
+                    throw new DefinitionException("Exception processing OAuth2AuthenticationDefinition: "
+                            + "extraParameter on annotation " + annotation.toString() + " is not of the format key=value");
+                }
+            }
+        }
+    }
+
+    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery event, BeanManager manager) {
+        logger.log(Level.FINER, "OAuth2Handler - BeforeBeanDiscovery" + event.toString());
+        event.addAnnotatedType(manager.createAnnotatedType(OAuth2AuthenticationMechanism.class), "OAuth2 Mechanism");
+        event.addAnnotatedType(manager.createAnnotatedType(OAuth2StateHolder.class), "OAuth2Token");
+        event.addAnnotatedType(manager.createAnnotatedType(OAuthIdentityStore.class), "OAuth2IdentityStore");
+        event.addAnnotatedType(manager.createAnnotatedType(OAuth2State.class), "OAuth2State");
+
+    }
+
+    void afterBeanDiscovery(@Observes AfterBeanDiscovery afterBean, BeanManager beanManager) {
+        logger.log(Level.FINE, "Creating OAuth2 Mechanism");
+        
+        if (!annotations.isEmpty() && beanManager.getBeans(IdentityStore.class).isEmpty()) {
+            afterBean.addBean()
+                    .scope(ApplicationScoped.class)
+                    .types(IdentityStore.class, Object.class)
+                    .beanClass(IdentityStore.class)
+                    .createWith(obj -> {
+                        return CDI.current()
+                                .select(OAuthIdentityStore.class).get();
+                    });
+
+        }
+        for (OAuth2AuthenticationDefinition annotation : annotations) {
+
+            afterBean.addBean(new CdiProducer<HttpAuthenticationMechanism>().
+                    scope(ApplicationScoped.class)
+                    .beanClass(HttpAuthenticationMechanism.class)
+                    .types(HttpAuthenticationMechanism.class, Object.class)
+                    .create(obj -> {return CDI.current()
+                               .select(OAuth2AuthenticationMechanism.class).get().setDefinition(annotation);}
+                    )); // --> Leads to NPE in CdiProducer.create(104)
+            logger.log(Level.FINE, "OAuth2 Mechanism created successfully");
+
+        }
+        annotations.clear();
+    }
+
+}
