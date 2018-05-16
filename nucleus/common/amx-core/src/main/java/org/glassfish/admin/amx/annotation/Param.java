1d4c985d14f0b23f88689360f81ad09053413714
Cousjava <jonathan.coustick@payara.fish>
2018-05-16 20:58:34 +0100

    PAYARA-2559 Boot AMX seperatly from JMX (#2716)
    * PAYARA-2559 Added command and config options to start AMX
    
    * PAYARA-2559 AMX is now only started through service
    
    * PAYARA-2559 Admin console integration for starting AMX
    
    Checkbox for AMX is now on the general tab of monitoring instead of JMX.
    
    * PAYARA-2559 add unprocessedChangeEvent if AMX is disabled

--- nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/annotation/Param.java
+++ nucleus/common/amx-core/src/main/java/org/glassfish/admin/amx/annotation/Param.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2018] Payara Foundation and/or affiliates
 
 package org.glassfish.admin.amx.annotation;
 
@@ -50,7 +51,7 @@ import org.glassfish.external.arc.Taxonomy;
 import org.glassfish.external.arc.Stability;
 
 /**
- * Annotates the paramete rof a method.
+ * Annotates the parameter of a method.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({PARAMETER, LOCAL_VARIABLE, FIELD})
