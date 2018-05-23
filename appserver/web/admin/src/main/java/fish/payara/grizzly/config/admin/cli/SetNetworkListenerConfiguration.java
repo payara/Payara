9824d6c92db99a56fa737041a5bda5deb2e2d9a0
Jonathan Coustick <jonathan.coustick@payara.fish>
2018-05-23 11:34:35 +0100

    PAYARA-2580 Added spaces and fixed error on shutdown

--- appserver/web/admin/src/main/java/fish/payara/grizzly/config/admin/cli/SetNetworkListenerConfiguration.java
+++ appserver/web/admin/src/main/java/fish/payara/grizzly/config/admin/cli/SetNetworkListenerConfiguration.java
@@ -41,7 +41,6 @@ package fish.payara.grizzly.config.admin.cli;
 
 import com.sun.enterprise.config.serverbeans.Config;
 import com.sun.enterprise.util.SystemPropertyConstants;
-import fish.payara.nucleus.hazelcast.HazelcastRuntimeConfiguration;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyVetoException;
 import java.text.MessageFormat;
@@ -69,7 +68,6 @@ import org.glassfish.api.event.EventTypes;
 import org.glassfish.api.event.Events;
 import org.glassfish.config.support.CommandTarget;
 import org.glassfish.config.support.TargetType;
-import org.glassfish.grizzly.Grizzly;
 import org.glassfish.grizzly.config.dom.NetworkConfig;
 import org.glassfish.grizzly.config.dom.NetworkListener;
 import org.glassfish.grizzly.config.dom.NetworkListeners;
@@ -78,9 +76,6 @@ import org.glassfish.internal.api.Target;
 import org.glassfish.internal.config.UnprocessedConfigListener;
 import org.glassfish.web.admin.LogFacade;
 import org.jvnet.hk2.annotations.Service;
-import org.jvnet.hk2.config.ConfigBeanProxy;
-import org.jvnet.hk2.config.ConfigCode;
-import org.jvnet.hk2.config.ConfigListener;
 import org.jvnet.hk2.config.ConfigSupport;
 import org.jvnet.hk2.config.SingleConfigCode;
 import org.jvnet.hk2.config.TransactionFailure;
@@ -93,7 +88,7 @@ import org.jvnet.hk2.config.UnprocessedChangeEvents;
  * @since 4.1.2.182
  */
 @ExecuteOn({RuntimeType.DAS, RuntimeType.INSTANCE})
-@TargetType(value = {CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTER, CommandTarget.CONFIG, CommandTarget.DEPLOYMENT_GROUP})
+@TargetType(value = {CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTER, CommandTarget.CONFIG})
 @Service(name = "set-network-listener-configuration")
 @CommandLock(CommandLock.LockType.NONE)
 @PerLookup
@@ -123,16 +118,22 @@ public class SetNetworkListenerConfiguration implements AdminCommand, EventListe
 
     @Param(name = "listenerPortRange", alias = "listenerportrange", optional = true)
     private String portRange;
+    
     @Param(name = "threadpool", optional = true, alias = "threadPool")
     private String threadPool;
+    
     @Param(name = "protocol", optional = true)
     private String protocol;
+    
     @Param(name = "name", primary = true)
     private String listenerName;
+    
     @Param(name = "transport", optional = true)
     private String transport;
+    
     @Param(name = "jkenabled", optional = true, alias = "jkEnabled")
     private Boolean jkEnabled;
+    
     @Param(name = "target", optional = true, defaultValue = SystemPropertyConstants.DAS_SERVER_NAME)
     private String target;
 
@@ -283,7 +284,6 @@ public class SetNetworkListenerConfiguration implements AdminCommand, EventListe
                 }
             }
         }
-        ucl.getUnprocessedChangeEvents().removeAll(processed);
     }
 
 }
