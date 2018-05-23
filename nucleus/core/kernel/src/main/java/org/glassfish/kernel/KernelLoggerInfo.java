0efab5920ffb38e5be264252d3544341810cf51e
Andrew Pielage <pandrex247@hotmail.com>
2018-05-23 15:35:42 +0100

    WIP refactor

--- nucleus/core/kernel/src/main/java/org/glassfish/kernel/KernelLoggerInfo.java
+++ nucleus/core/kernel/src/main/java/org/glassfish/kernel/KernelLoggerInfo.java
@@ -653,4 +653,15 @@ public class KernelLoggerInfo {
             level = "INFO")
     public static final String grizzlyUnableToBind = LOGMSG_PREFIX + "-00097";
     
+    @LogMessageInfo(
+            message = "Propagated trace ID \"{0}\" is not a valid UUID - starting a new trace instead",
+            cause="The trace ID extracted from the request header is not a valid UUID",
+            level = "WARNING")
+    public static final String invalidPropagatedTraceId = LOGMSG_PREFIX + "-00099";
+    
+    @LogMessageInfo(
+            message = "Propagated parent ID \"{0}\" is not a valid UUID - starting a new trace instead",
+            cause="The parent ID extracted from the request header is either null or not a valid UUID",
+            level = "WARNING")
+    public static final String invalidPropagatedParentId = LOGMSG_PREFIX + "-00100";
 }
