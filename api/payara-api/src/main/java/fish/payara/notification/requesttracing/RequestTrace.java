9fc0e1325db15e3586df915113092a63105f8133
Andrew Pielage <pandrex247@hotmail.com>
2018-05-29 18:44:40 +0100

    PAYARA-2547 Extra javadoc

--- api/payara-api/src/main/java/fish/payara/notification/requesttracing/RequestTrace.java
+++ api/payara-api/src/main/java/fish/payara/notification/requesttracing/RequestTrace.java
@@ -226,7 +226,7 @@ public class RequestTrace implements Serializable, Comparable<RequestTrace> {
     /**
      * Returns a list of all the events that make up the trace.
      *
-     * @return
+     * @return A list containing all of the Spans that constitute this trace.
      */
     public LinkedList<RequestTraceSpan> getTraceSpans() {
         return trace;
@@ -235,7 +235,7 @@ public class RequestTrace implements Serializable, Comparable<RequestTrace> {
     /**
      * Gets the Instant when the span was started See {@link java.time.Instant#now()} for how this time is generated.
      *
-     * @return
+     * @return The Instant for when this span was started.
      */
     public long getStartTime() {
         return startTime;
