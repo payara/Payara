add487f1da9fd08d6c1aee15b3dd76150ed187cc
Cousjava <jonathan.coustick@payara.fish>
2018-05-31 11:26:05 +0100

    PAYARA-2781 Exception thrown in logs when shutting down Payara (#2799)
    * PAYARA-2781 Exception thrown in logs when shutting down Payara

--- appserver/common/amx-javaee/src/main/java/org/glassfish/admin/amx/impl/j2ee/loader/AMXJ2EEStartupService.java
+++ appserver/common/amx-javaee/src/main/java/org/glassfish/admin/amx/impl/j2ee/loader/AMXJ2EEStartupService.java
@@ -242,9 +242,13 @@ public final class AMXJ2EEStartupService implements org.glassfish.hk2.api.PostCo
 
     @Override
     public synchronized void unloadAMXMBeans() {
-        final J2EEDomain j2eeDomain = getJ2EEDomainProxy();
-        if (j2eeDomain != null) {
-            ImplUtil.unregisterAMXMBeans(j2eeDomain);
+        try {
+            final J2EEDomain j2eeDomain = getJ2EEDomainProxy();
+            if (j2eeDomain != null) {
+                ImplUtil.unregisterAMXMBeans(j2eeDomain);
+            }
+        } catch (NullPointerException e){
+            logger.log(Level.FINEST, "NullPointerException when stopping AMX, AMX may not have been started", e);
         }
     }
 }
