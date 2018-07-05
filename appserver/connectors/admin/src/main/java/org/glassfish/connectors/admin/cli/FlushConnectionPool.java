3120907a0f5a8d41a6c27313ef47a5e3f44a5df7
Cousjava <jonathan.coustick@payara.fish>
2018-07-05 09:52:15 +0100

    removed deployment group

--- appserver/connectors/admin/src/main/java/org/glassfish/connectors/admin/cli/FlushConnectionPool.java
+++ appserver/connectors/admin/src/main/java/org/glassfish/connectors/admin/cli/FlushConnectionPool.java
@@ -69,8 +69,7 @@ import org.glassfish.config.support.TargetType;
 @Service(name = "flush-connection-pool")
 @PerLookup
 @I18n("flush.connection.pool")
-@TargetType(value = {CommandTarget.DOMAIN, CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTER, CommandTarget.DEPLOYMENT_GROUP,
-    CommandTarget.CLUSTERED_INSTANCE})
+@TargetType(value = {CommandTarget.DOMAIN, CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTER, CommandTarget.CLUSTERED_INSTANCE})
 @ExecuteOn(value = {RuntimeType.ALL})
 @RestEndpoints({
     @RestEndpoint(configBean=Resources.class,
