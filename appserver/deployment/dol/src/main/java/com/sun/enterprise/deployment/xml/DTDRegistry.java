316d28b003be2643ba718fe32f02bced33f66ebb
Jonathan Coustick <jonathan.coustick@payara.fish>
2018-07-17 15:05:54 +0100

    payara-web.xml now works

--- appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/xml/DTDRegistry.java
+++ appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/xml/DTDRegistry.java
@@ -297,7 +297,7 @@ public final class DTDRegistry {
     
     // Payara DTDs
     
-    public static final String PAYARA_WEBAPP_4_DTD_PUBLIC_ID = "-//Payara.fish//DTD Payara Application Server 4 Servlet 3.0//EN";
+    public static final String PAYARA_WEBAPP_4_DTD_PUBLIC_ID = "-//Payara.fish//DTD Payara Server 4 Servlet 3.0//EN";
     public static final String PAYARA_WEBAPP_4_DTD_SYSTEM_ID = "https://raw.githubusercontent.com/payara/Payara/master/appserver/deployment/dtds/src/main/resources/glassfish/lib/dtds/payara-web-app_4.dtd";
     
 }
