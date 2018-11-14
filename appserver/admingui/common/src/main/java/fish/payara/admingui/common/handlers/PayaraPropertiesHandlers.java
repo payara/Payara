f5847608f8a55cee66a12ed457864364666b5a19
merorai <susan.rai04@gmail.com>
2018-11-14 09:19:45 +0000

    Added separate MicroProfile Config properties page for Application

--- appserver/admingui/common/src/main/java/fish/payara/admingui/common/handlers/PayaraPropertiesHandlers.java
+++ appserver/admingui/common/src/main/java/fish/payara/admingui/common/handlers/PayaraPropertiesHandlers.java
@@ -44,7 +44,6 @@ import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
