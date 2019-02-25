78ef1cc468ff72f4d574410e0cc969e240b9d1ed
arjantijms <arjan.tijms@gmail.com>
2019-02-25 22:45:31 +0100

    PAYARA-3440 Throw UnsupportedOperationException from empty methods

--- appserver/ejb/ejb-rest-remoting/client/src/main/java/fish/payara/ejb/rest/client/RemoteEJBContext.java
+++ appserver/ejb/ejb-rest-remoting/client/src/main/java/fish/payara/ejb/rest/client/RemoteEJBContext.java
@@ -251,112 +251,112 @@ class RemoteEJBContext implements Context {
 
     @Override
     public void bind(Name name, Object obj) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void bind(String name, Object obj) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void rebind(Name name, Object obj) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void rebind(String name, Object obj) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void unbind(Name name) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void unbind(String name) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void rename(Name oldName, Name newName) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void rename(String oldName, String newName) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void destroySubcontext(Name name) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void destroySubcontext(String name) throws NamingException {
-        
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Context createSubcontext(Name name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Context createSubcontext(String name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Object lookupLink(Name name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Object lookupLink(String name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NameParser getNameParser(Name name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public NameParser getNameParser(String name) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Name composeName(Name name, Name prefix) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String composeName(String name, String prefix) throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -381,7 +381,7 @@ class RemoteEJBContext implements Context {
 
     @Override
     public String getNameInNamespace() throws NamingException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
 }
