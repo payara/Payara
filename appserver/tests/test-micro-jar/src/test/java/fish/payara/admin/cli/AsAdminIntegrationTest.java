c2ea35a8f354e7cf59cbc26570b4d3cac122f083
Jan Bernitt <jaanbernitt@gmail.com>
2019-03-04 15:51:18 +0100

    QA-192 extracted common setup of test module and utils

---
+++ appserver/tests/test-micro-jar/src/test/java/fish/payara/admin/cli/AsAdminIntegrationTest.java
@@ -0,0 +1,209 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2019 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://github.com/payara/Payara/blob/master/LICENSE.txt
+ * See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * The Payara Foundation designates this particular file as subject to the "Classpath"
+ * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package fish.payara.admin.cli;
+
+import static fish.payara.micro.ClusterCommandResult.ExitStatus.FAILURE;
+import static fish.payara.micro.ClusterCommandResult.ExitStatus.SUCCESS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+
+import fish.payara.micro.ClusterCommandResult;
+import fish.payara.micro.ClusterCommandResult.ExitStatus;
+import fish.payara.test.util.IntegrationTest;
+import fish.payara.test.util.PayaraMicroServer;
+
+@Category(IntegrationTest.class)
+public abstract class AsAdminIntegrationTest {
+
+    private static final String UNSATISFIED_DEPENDENCY_EXCEPTION_CLASS_NAME = 
+            "org.jvnet.hk2.config.UnsatisfiedDependencyException";
+    private static final String UNACCEPTABLE_VALUE_EXCEPTION_CLASS_NAME = 
+            "org.glassfish.common.util.admin.UnacceptableValueException";
+
+    protected final PayaraMicroServer server;
+    private final String[] args;
+
+    protected AsAdminIntegrationTest() {
+        this(new String[0]);
+    }
+
+    protected AsAdminIntegrationTest(String... args) {
+        this.args = args;
+        this.server = PayaraMicroServer.newInstance();
+    }
+
+    @Before
+    public void serverSetUp() throws Exception {
+        if (args.length == 0) {
+            server.start();
+        } else {
+            server.start(args);
+        }
+    }
+
+    protected static void assertUnchanged(boolean expected, boolean actual) {
+        assertEquals(Boolean.valueOf(expected), Boolean.valueOf(actual));
+    }
+
+    protected static void assertContains(String expected, String actual) {
+        assertThat(actual, CoreMatchers.containsString(expected));
+    }
+
+    protected static void assertSuccess(ClusterCommandResult result) {
+        ExitStatus actual = result.getExitStatus();
+        if (SUCCESS != actual) {
+            String msg = result.getOutput();
+            if (actual == FAILURE && result.getFailureCause() != null) {
+                StringWriter writer = new StringWriter();
+                result.getFailureCause().printStackTrace(new PrintWriter(writer));
+                msg = writer.toString();
+            }
+            if (msg != null) {
+                assertEquals(msg, SUCCESS, actual);
+            } else {
+                assertEquals(SUCCESS, actual);
+            }
+        }
+    }
+
+    protected static void assertFailure(ClusterCommandResult result) {
+        assertEquals(FAILURE, result.getExitStatus());
+    }
+
+    private static void assertStatesUsage(ClusterCommandResult result) {
+        assertTrue("No usage was given.", result.getOutput().contains("Usage: "));
+    }
+
+    protected final void assertMissingParameter(String name, ClusterCommandResult result) {
+        assertFailure(result);
+        Class<?> expectedExceptionType = server.getClass(UNSATISFIED_DEPENDENCY_EXCEPTION_CLASS_NAME);
+        Throwable cause = result.getFailureCause();
+        assertNotNull(cause);
+        assertTrue("Error is not caused by a missing parameter but " + cause.getClass().getName(),
+                expectedExceptionType.isAssignableFrom(cause.getClass()));
+        String text = result.getOutput();
+        int afterName = text.indexOf(" with class ");
+        int beforeName = text.substring(0, afterName).lastIndexOf('.');
+        String actualName = text.substring(beforeName + 1, afterName);
+        assertEquals("Error was about another parameter", name, actualName);
+        assertStatesUsage(result);
+    }
+
+    protected final void assertUnacceptableParameter(String name, ClusterCommandResult result) {
+        assertFailure(result);
+        Class<?> expectedExceptionType = server.getClass(UNACCEPTABLE_VALUE_EXCEPTION_CLASS_NAME);
+        Throwable cause = result.getFailureCause();
+        assertNotNull(cause);
+        assertTrue("Error is not caused by a unacceptable parameter but " + cause.getClass().getName(),
+                expectedExceptionType.isAssignableFrom(cause.getClass()));
+        String text = result.getOutput();
+        try {
+            assertContains("Invalid parameter: " + name, text);
+        } catch (AssertionError e) {
+            assertContains("on parameter [ "+name+" ]", text);
+        }
+        assertStatesUsage(result);
+    }
+
+    /**
+     * Runs an as-admin command on the {@link #server} 
+     */
+    protected final ClusterCommandResult asadmin(String command, String...args) {
+        return new PlainClusterCommandResult(server.getInstance().executeLocalAsAdmin(command, args));
+    }
+
+    protected final Class<?> getClass(String name) {
+        return server.getClass(name);
+    }
+
+    protected final <T> T getExtensionByType(String target, Class<T> type) {
+        return server.getExtensionByType(target, type);
+    }
+
+    private static final class PlainClusterCommandResult implements ClusterCommandResult {
+
+        final ClusterCommandResult wrapped;
+
+        public PlainClusterCommandResult(ClusterCommandResult wrapped) {
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public ExitStatus getExitStatus() {
+            return wrapped.getExitStatus();
+        }
+
+        @Override
+        public Throwable getFailureCause() {
+            return wrapped.getFailureCause();
+        }
+
+        @Override
+        public String getOutput() {
+            String output = wrapped.getOutput();
+            if (!output.startsWith("PlainTextActionReporter")) {
+                return output;
+            }
+            int index = output.indexOf("SUCCESS");
+            if (index < 0) {
+                index = output.indexOf("FAILURE");
+            }
+            if (index > 0) {
+                return output.substring(index + 7);
+            }
+            return output;
+        }
+
+        @Override
+        public String toString() {
+            return getExitStatus() + ": " + getOutput();
+        }
+    }
+
+}
---
+++ appserver/tests/test-micro-jar/src/test/java/fish/payara/test/util/BeanProxy.java
@@ -0,0 +1,158 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2019 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://github.com/payara/Payara/blob/master/LICENSE.txt
+ * See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * The Payara Foundation designates this particular file as subject to the "Classpath"
+ * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package fish.payara.test.util;
+
+import static org.junit.Assert.fail;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.concurrent.Callable;
+
+/**
+ * Base class for test wrappers on beans of classes only available at runtime.
+ */
+public class BeanProxy {
+
+    private final Object bean;
+
+    public BeanProxy(Object bean) {
+        this.bean = bean;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        return obj != null && obj.getClass() == getClass() && bean.equals(((BeanProxy)obj).bean);
+    }
+
+    @Override
+    public final int hashCode() {
+        return bean.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return bean.getClass().getSimpleName();
+    }
+
+    public final boolean booleanValue(String name) {
+        Object res = propertyValue(name, Boolean.class);
+        return res instanceof Boolean ? ((Boolean)res).booleanValue() : Boolean.parseBoolean((String)res);
+    }
+
+    public final int intValue(String name) {
+        Integer res = integerValue(name);
+        return res == null ? 0 : res.intValue();
+    }
+
+    public final Integer integerValue(String name) {
+        Object res = propertyValue(name, Integer.class);
+        return res instanceof Integer || res == null ? ((Integer)res) : Integer.valueOf((String)res);
+    }
+
+    public final Long longValue(String name) {
+        Object res = propertyValue(name, Long.class);
+        return res instanceof Long || res == null ? ((Long)res) : Long.valueOf((String)res);
+    }
+
+    public final String stringValue(String name) {
+        Object res = propertyValue(name, String.class);
+        return res instanceof String || res == null ? (String) res : String.valueOf(res);
+    }
+
+    public final Enum<?> enumValue(String name) {
+        return (Enum<?>) propertyValue(name, Enum.class);
+    }
+    @SuppressWarnings("unchecked")
+    public final <E extends Enum<E>> E enumValue(String name, Class<E> type) {
+        Object val = propertyValue(name, type);
+        if (val == null || type.isAssignableFrom(val.getClass())) {
+            return (E) val;
+        }
+        if (val instanceof String) {
+            return Enum.valueOf(type, (String)val);
+        }
+        fail("Unexpected value type for enum: " + name);
+        return null;
+    }
+
+    public final Object propertyValue(String name) {
+        return propertyValue(name, Object.class);
+    }
+
+    public final Object propertyValue(String name, Class<?> type) {
+        String errorText = "Failed to get " + type.getSimpleName() + " value for property " + name;
+        return name.startsWith("get") || name.startsWith("is") || bean instanceof Annotation
+                ? callMethod(name, errorText)
+                : accessField(name, errorText);
+    }
+
+    public final Object callMethod(String name, String errorText) {
+        return failAsAssertionError(errorText, () ->  bean.getClass().getMethod(name, new Class[0]).invoke(bean));
+    }
+
+    public final <P> Object callMethod(String name, String errorText, Class<P> parameter, P argument) {
+        return failAsAssertionError(errorText + " for method " + name,
+                () -> bean.getClass().getMethod(name, new Class[] { parameter }).invoke(bean, argument));
+    }
+
+    public final Object accessField(String name, String errorText) {
+        return failAsAssertionError(errorText + " for field " + name, () -> {
+            Field field = bean.getClass().getDeclaredField(name);
+            field.setAccessible(true);
+            return field.get(bean);
+        });
+    }
+
+    public static AssertionError asAssertionError(String msg, Throwable e) {
+        return e.getClass() == AssertionError.class ? (AssertionError)e : new AssertionError(msg, e);
+    }
+
+    public final static <T> T failAsAssertionError(Callable<T> operation) {
+        return failAsAssertionError("Failed to run operation: ", operation);
+    }
+
+    public final static <T> T failAsAssertionError(String msg, Callable<T> operation) {
+        try {
+            return operation.call();
+        } catch (Throwable e) {
+            throw asAssertionError(msg, e);
+        }
+    }
+}
