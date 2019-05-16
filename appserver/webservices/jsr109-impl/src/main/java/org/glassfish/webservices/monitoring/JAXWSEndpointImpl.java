68b2698138d1eb9fe4ca66f934c477f743314a64
arjantijms <arjan.tijms@gmail.com>
2019-05-16 10:26:08 +0200

    PAYARA-3385 Updated copyrights
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/monitoring/JAXWSEndpointImpl.java
+++ appserver/webservices/jsr109-impl/src/main/java/org/glassfish/webservices/monitoring/JAXWSEndpointImpl.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
+// Portions Copyright [2019] [Payara Foundation and/or its affiliates]
 package org.glassfish.webservices.monitoring;
 
 import static java.util.logging.Level.WARNING;
