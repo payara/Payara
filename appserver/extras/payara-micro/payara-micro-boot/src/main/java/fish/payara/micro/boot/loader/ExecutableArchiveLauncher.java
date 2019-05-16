62e1df2bc47366dd6724e8097da9924491241460
Jan Bernitt <jaanbernitt@gmail.com>
2019-05-16 17:03:31 +0200

    PAYARA-3798 added try-catch against multiple instances

--- appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/boot/loader/ExecutableArchiveLauncher.java
+++ appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/boot/loader/ExecutableArchiveLauncher.java
@@ -18,6 +18,7 @@
 package fish.payara.micro.boot.loader;
 
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -72,7 +73,11 @@ public abstract class ExecutableArchiveLauncher extends Launcher {
 
 	@Override
 	protected List<Archive> getClassPathArchives() throws Exception {
-		FileSystems.newFileSystem(this.archive.getUrl().toURI(), Collections.emptyMap());
+	    try {
+	        FileSystems.newFileSystem(this.archive.getUrl().toURI(), Collections.emptyMap());
+	    } catch (FileSystemAlreadyExistsException e) {
+	        // fine
+	    }
 		List<Archive> archives = new ArrayList<Archive>(
 				this.archive.getNestedArchives(new EntryFilter() {
 
