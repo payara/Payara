2626363e54b26ff4c8eaae4b2cb514eccae18297
Jan Bernitt <jaanbernitt@gmail.com>
2019-05-28 10:03:42 +0200

    sonar: try with resource

--- nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/generator/TextResourcesGenerator.java
+++ nucleus/admin/rest/rest-service/src/main/java/org/glassfish/admin/rest/generator/TextResourcesGenerator.java
@@ -83,25 +83,17 @@ public class TextResourcesGenerator extends ResourcesGeneratorBase {
     public String endGeneration() {
         //generate date info in 1 single file
         File file = new File(generationDir+ "/codegeneration.properties");
-        BufferedWriter out = null;
         try {
             if (file.createNewFile()) {
-                FileWriter fstream = new FileWriter(file);
-                out = new BufferedWriter(fstream);
-                out.write("generation_date=" + new Date() + "\n");
+                try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
+                    out.write("generation_date=" + new Date() + "\n");
+                }
             } else {
                 RestLogging.restLogger.log(Level.SEVERE, RestLogging.FILE_CREATION_FAILED, "codegeneration.properties");
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException ex) {
-                    RestLogging.restLogger.log(Level.SEVERE, null, ex);
-                }
-            }
+            RestLogging.restLogger.log(Level.SEVERE, null, e);
         }
 
         return  "Code Generation done at : " + generationDir;
