b833611c84fd366422ab510d324b6912c780e9cc
Jan Bernitt <jaanbernitt@gmail.com>
2019-05-29 17:37:16 +0200

    updated copyright headers

--- appserver/appclient/server/core/src/main/java/org/glassfish/appclient/server/core/jws/RestrictedContentAdapter.java
+++ appserver/appclient/server/core/src/main/java/org/glassfish/appclient/server/core/jws/RestrictedContentAdapter.java
@@ -37,6 +37,8 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2019] Payara Foundation and/or affiliates
+
 package org.glassfish.appclient.server.core.jws;
 
 import java.io.File;
