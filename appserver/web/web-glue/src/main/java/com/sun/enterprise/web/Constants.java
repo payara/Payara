4c1b33b08a4ede9598cc34f5ce061b060d66981b
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-06-10 14:54:51 +0530

    PAYARA-3101 remove copyright

--- appserver/web/web-glue/src/main/java/com/sun/enterprise/web/Constants.java
+++ appserver/web/web-glue/src/main/java/com/sun/enterprise/web/Constants.java
@@ -37,7 +37,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2019] [Payara Foundation and/or its affiliates]
 
 package com.sun.enterprise.web;
 
