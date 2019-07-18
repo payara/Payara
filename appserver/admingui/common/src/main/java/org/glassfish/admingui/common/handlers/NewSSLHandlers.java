a4f9612f6979609f78c0a45360b5664e27d7b0c0
Sven Diedrichsen <sdiedrichsen@yahoo.de>
2019-07-18 22:33:42 +0200

    replaced synchronized classes with unsynchronized. extracted a method for code copy.

--- appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/NewSSLHandlers.java
+++ appserver/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/NewSSLHandlers.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright [2019] [Payara Foundation and/or its affiliates]
 
 /*
  * SSLHandlers.java
@@ -47,14 +48,13 @@
 
 package org.glassfish.admingui.common.handlers;
 
-import java.util.Arrays;
-import java.util.Vector;
-
-import com.sun.jsftemplating.annotation.Handler; 
+import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 import org.glassfish.admingui.common.util.GuiUtil;
+
+import java.util.*;
 
 /**
  *
@@ -62,20 +62,24 @@ import org.glassfish.admingui.common.util.GuiUtil;
  *
  */
 public class NewSSLHandlers {
-    
-    static String[] COMMON_CIPHERS = {"SSL_RSA_WITH_RC4_128_MD5", "SSL_RSA_WITH_RC4_128_SHA",
-        "TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA", "SSL_RSA_WITH_3DES_EDE_CBC_SHA"};
-    
-    static String[] BIT_CIPHERS = {"SSL_RSA_WITH_DES_CBC_SHA", "SSL_DHE_RSA_WITH_DES_CBC_SHA", "SSL_DHE_DSS_WITH_DES_CBC_SHA",
-        "SSL_RSA_EXPORT_WITH_RC4_40_MD5", "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA",
-        "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA"};
-    
+
+    private static Set<String> COMMON_CIPHERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+        "SSL_RSA_WITH_RC4_128_MD5", "SSL_RSA_WITH_RC4_128_SHA", "TLS_RSA_WITH_AES_128_CBC_SHA",
+        "TLS_RSA_WITH_AES_256_CBC_SHA", "SSL_RSA_WITH_3DES_EDE_CBC_SHA"
+    )));
+
+    private static Set<String> BIT_CIPHERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+        "SSL_RSA_WITH_DES_CBC_SHA", "SSL_DHE_RSA_WITH_DES_CBC_SHA", "SSL_DHE_DSS_WITH_DES_CBC_SHA",
+        "SSL_RSA_EXPORT_WITH_RC4_40_MD5", "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA",
+        "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA", "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA"
+    )));
+
     public NewSSLHandlers() {
     }
-    
+
     @Handler(id="convertToDifferentCiphersGroup",
     input={
-        @HandlerInput(name="ciphers",   type=Object.class)},
+        @HandlerInput(name="ciphers")},
     output={
         @HandlerOutput(name="CommonCiphersList",    type=String[].class),
         @HandlerOutput(name="EphemeralCiphersList", type=String[].class),
@@ -83,21 +87,20 @@ public class NewSSLHandlers {
         @HandlerOutput(name="EccCiphersList",       type=String[].class)}
     )
     public static void convertToDifferentCiphersGroup(HandlerContext handlerCtx) {
-
-        Vector ciphersVector = null;
-        Object ciphers = (Object)handlerCtx.getInputValue("ciphers");
+        List<String> ciphersList = null;
+        Object ciphers = handlerCtx.getInputValue("ciphers");
         if (ciphers != null) {
             if (ciphers instanceof String) {
                 String[] ary = getSelectedCiphersList((String) ciphers);
-                ciphersVector = getCiphersVector(ary);
+                ciphersList = getCiphers(ary);
             } else {
-                ciphersVector = getCiphersVector((String[]) ciphers);
+                ciphersList = getCiphers((String[]) ciphers);
             }
         }
-        handlerCtx.setOutputValue("CommonCiphersList", getCommonCiphers(ciphersVector));
-        handlerCtx.setOutputValue("EphemeralCiphersList", getEphemeralCiphers(ciphersVector));
-        handlerCtx.setOutputValue("OtherCiphersList", getOtherCiphers(ciphersVector));
-        handlerCtx.setOutputValue("EccCiphersList", getEccCiphers(ciphersVector));
+        handlerCtx.setOutputValue("CommonCiphersList", getCommonCiphers(ciphersList));
+        handlerCtx.setOutputValue("EphemeralCiphersList", getEphemeralCiphers(ciphersList));
+        handlerCtx.setOutputValue("OtherCiphersList", getOtherCiphers(ciphersList));
+        handlerCtx.setOutputValue("EccCiphersList", getEccCiphers(ciphersList));
     }
 
 
@@ -108,7 +111,7 @@ public class NewSSLHandlers {
         @HandlerInput(name="other",     type=String[].class),
         @HandlerInput(name="ecc",       type=String[].class)},
     output={
-        @HandlerOutput(name="ciphers",   type=Object.class)}
+        @HandlerOutput(name="ciphers")}
     )
     public static void convertCiphersItemsToStr(HandlerContext handlerCtx) {
 
@@ -126,100 +129,85 @@ public class NewSSLHandlers {
     }
 
 
-        
-        private static String[] getSelectedCiphersList(String selectedCiphers){
-            Vector selItems = new Vector();
-            if(selectedCiphers != null){
-                String[] sel = selectedCiphers.split(","); //NOI18N
-                for(int i=0; i<sel.length; i++){
-                    String cName = sel[i];
-                    if(cName.startsWith("+")){ //NOI18N
-                        cName = cName.substring(1, cName.length());
-                        selItems.add(cName);
-                    }
-                }    
+
+    private static String[] getSelectedCiphersList(String selectedCiphers){
+        List<String> selItems = new ArrayList<>();
+        if(selectedCiphers != null){
+            String[] sel = selectedCiphers.split(","); //NOI18N
+            for (String cName : sel) {
+                if (cName.startsWith("+")) { //NOI18N
+                    selItems.add(cName.substring(1));
+                }
             }
-            return (String[])selItems.toArray(new String[selItems.size()]);
         }
-        
-        private static String processSelectedCiphers(String[] selectedCiphers, String ciphers){
-            StringBuilder sb = new StringBuilder();
-            String sep = "";
-            if ( ! GuiUtil.isEmpty(ciphers)){
-                sb.append(ciphers);
+        return selItems.toArray(new String[0]);
+    }
+
+    private static String processSelectedCiphers(String[] selectedCiphers, String ciphers){
+        StringBuilder sb = new StringBuilder();
+        String sep = "";
+        if ( ! GuiUtil.isEmpty(ciphers)){
+            sb.append(ciphers);
+            sep = ",";
+        }
+        if(selectedCiphers != null){
+            for (String selectedCipher : selectedCiphers) {
+                sb.append(sep).append("+").append(selectedCipher);
                 sep = ",";
             }
-            if(selectedCiphers != null){
-                for (int i = 0; i < selectedCiphers.length; i++) {
-                    sb.append(sep).append("+").append(selectedCiphers[i]);
-                    sep = ",";
-                }
-            }
-            return sb.toString();
         }
+        return sb.toString();
+    }
 
-        private static Vector getCiphersVector(String[] allCiphers){
-            Vector ciphers = new Vector();
-            if (allCiphers != null){
-                for(int i=0; i<allCiphers.length; i++){
-                    ciphers.add(allCiphers[i]);
-                }
-            }
-            return ciphers;
+    private static List<String> getCiphers(String[] allCiphers){
+        List<String> ciphers = new ArrayList<>();
+        if (allCiphers != null){
+            Collections.addAll(ciphers, allCiphers);
         }
-        
-        private static String[] getCommonCiphers(Vector ciphers){
-            Vector commonCiphers = filterCiphers(ciphers, COMMON_CIPHERS);
-            String[] ciphersList = (String[])commonCiphers.toArray(new String[commonCiphers.size()]);
-            return ciphersList;
+        return ciphers;
+    }
+
+    private static String[] getCommonCiphers(List<String> ciphers){
+        return filterCiphers(ciphers, COMMON_CIPHERS).toArray(new String[0]);
+    }
+
+    private static String[] getEccCiphers(List<String> ciphers){
+        List<String> eccCiphers = breakUpCiphers(new ArrayList<>(), ciphers, "_ECDH_"); //NOI18N
+        eccCiphers = breakUpCiphers(eccCiphers, ciphers, "_ECDHE_"); //NOI18N
+        return eccCiphers.toArray(new String[0]);
+    }
+
+    private static String[] getEphemeralCiphers(List<String> ciphers){
+        List<String> ephmCiphers = breakUpCiphers(new ArrayList<>(), ciphers, "_DHE_RSA_"); //NOI18N
+        ephmCiphers = breakUpCiphers(ephmCiphers, ciphers, "_DHE_DSS_"); //NOI18N
+        return ephmCiphers.toArray(new String[0]);
+    }
+
+    private static String[] getOtherCiphers(List<String> ciphers){
+        return filterCiphers(ciphers, BIT_CIPHERS).toArray(new String[0]);
+    }
+
+    private static List<String> filterCiphers(List<String> ciphers, Set<String> filterList){
+        List<String> listCiphers = new ArrayList<>();
+        if (ciphers != null) {
+            listCiphers.addAll(ciphers);
+            listCiphers.retainAll(filterList);
         }
-        
-        private static String[] getEccCiphers(Vector ciphers){
-            Vector eccCiphers = breakUpCiphers(new Vector(), ciphers, "_ECDH_"); //NOI18N
-            eccCiphers = breakUpCiphers(eccCiphers, ciphers, "_ECDHE_"); //NOI18N
-            String[] ciphersList = (String[])eccCiphers.toArray(new String[eccCiphers.size()]);
-            return ciphersList;
-        }    
-        
-        private static String[] getEphemeralCiphers(Vector ciphers){
-            Vector ephmCiphers = breakUpCiphers(new Vector(), ciphers, "_DHE_RSA_"); //NOI18N
-            ephmCiphers = breakUpCiphers(ephmCiphers, ciphers, "_DHE_DSS_"); //NOI18N
-            String[] ciphersList = (String[])ephmCiphers.toArray(new String[ephmCiphers.size()]);
-            return ciphersList;
-        }
-        
-        private static String[] getOtherCiphers(Vector ciphers){
-            Vector bitCiphers = filterCiphers(ciphers, BIT_CIPHERS);
-            String[] ciphersList = (String[])bitCiphers.toArray(new String[bitCiphers.size()]);
-            return ciphersList;
-        }
-        
-        private static Vector filterCiphers(Vector ciphers, String[] filterList){
-            Vector listCiphers = new Vector();
-            if (ciphers != null){
-                for(int i=0; i<ciphers.size(); i++){
-                    String cipherName = ciphers.get(i).toString();
-                    if (Arrays.asList(filterList).contains(cipherName)){
-                        listCiphers.add(ciphers.get(i));
+        return listCiphers;
+    }
+
+
+    private static List<String> breakUpCiphers(List<String> cipherSubset, List<String> allCiphers, String type){
+        if (allCiphers != null){
+            for (String cipherName : allCiphers) {
+                if (cipherName.contains(type)) {
+                    if (!BIT_CIPHERS.contains(cipherName)) {
+                        cipherSubset.add(cipherName);
                     }
                 }
             }
-            return listCiphers;
         }
+        return cipherSubset;
+    }
 
-
-    private static Vector breakUpCiphers(Vector cipherSubset, Vector allCiphers, String type){
-            if (allCiphers != null){
-                for(int i=0; i<allCiphers.size(); i++){
-                    String cipherName = allCiphers.get(i).toString();
-                    if(cipherName.indexOf(type) != -1) {
-                        if(! Arrays.asList(BIT_CIPHERS).contains(cipherName)){
-                            cipherSubset.add(cipherName);
-                        }
-                    }
-                }
-            }
-            return cipherSubset;
-        }
-     
 }
