38c0f6bc2f90e8369254f0d7a8ec92b3437b2b45
David Matejcek <david.matejcek@payara.fish>
2019-09-16 11:58:54 +0200

    PAYARA-4123 Refactored server JAXRS Request Tracing
    - removed filter + exception mapper solution, which failed
      TCK's completitioncallback tests
    - introduced EventListener solution, depends on Jersey
    - removed trailing whitespaces in the module
    - added logging and several generics
    - reduced nesting of blocks in favor of readibility
    - EnvironmentConfigSource uses AccessController to avoid security exceptions

--- appserver/payara-appserver-modules/microprofile/opentracing/src/main/java/fish/payara/microprofile/opentracing/cdi/TracedInterceptor.java
+++ appserver/payara-appserver-modules/microprofile/opentracing/src/main/java/fish/payara/microprofile/opentracing/cdi/TracedInterceptor.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  *    Copyright (c) [2018-2019] Payara Foundation and/or its affiliates. All rights reserved.
- * 
+ *
  *     The contents of this file are subject to the terms of either the GNU
  *     General Public License Version 2 only ("GPL") or the Common Development
  *     and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  *     https://github.com/payara/Payara/blob/master/LICENSE.txt
  *     See the License for the specific
  *     language governing permissions and limitations under the License.
- * 
+ *
  *     When distributing the software, include this License Header Notice in each
  *     file and include the License file at glassfish/legal/LICENSE.txt.
- * 
+ *
  *     GPL Classpath Exception:
  *     The Payara Foundation designates this particular file as subject to the "Classpath"
  *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
  *     file that accompanied this code.
- * 
+ *
  *     Modifications:
  *     If applicable, add the following below the License Header, with the fields
  *     enclosed by brackets [] replaced by your own identifying information:
  *     "Portions Copyright [year] [name of copyright owner]"
- * 
+ *
  *     Contributor(s):
  *     If you wish your version of this file to be governed by only the CDDL or
  *     only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -39,9 +39,18 @@
  */
 package fish.payara.microprofile.opentracing.cdi;
 
+import fish.payara.opentracing.OpenTracingService;
+
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.log.Fields;
+import io.opentracing.tag.Tags;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Priority;
@@ -63,121 +72,113 @@ import org.glassfish.api.invocation.InvocationManager;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.internal.api.Globals;
 
-import fish.payara.opentracing.OpenTracingService;
-import io.opentracing.Scope;
-import io.opentracing.Span;
-import io.opentracing.Tracer;
-import io.opentracing.log.Fields;
-import io.opentracing.tag.Tags;
+import static fish.payara.microprofile.opentracing.cdi.OpenTracingCdiUtils.getAnnotation;
 
 /**
  * Interceptor for MicroProfile Traced annotation.
- * 
+ *
  * @author Andrew Pielage <andrew.pielage@payara.fish>
+ * @author David Matejcek
  */
 @Interceptor
 @Traced
 @Priority(Interceptor.Priority.PLATFORM_AFTER)
 public class TracedInterceptor implements Serializable {
 
-    private static final Logger logger = Logger.getLogger(TracedInterceptor.class.getName());
+    private static final long serialVersionUID = -7772038254542420659L;
+    private static final Logger LOG = Logger.getLogger(TracedInterceptor.class.getName());
+    @SuppressWarnings("rawtypes")
+    private static final Class[] HTTP_METHODS = //
+        new Class[] {GET.class, POST.class, DELETE.class, PUT.class, HEAD.class, PATCH.class, OPTIONS.class};
 
     @Inject
     private BeanManager beanManager;
 
+
+    /**
+     * If the tracing is enabled and possible for the invoked method, traces it's execution.
+     * If not, only executes the invocation context.
+     *
+     * @param invocationContext the context to be executed
+     * @return result of the invocation context execution.
+     * @throws Exception any execution thrown from the invocation context execution.
+     */
     @AroundInvoke
-    public Object traceCdiCall(InvocationContext invocationContext) throws Exception {
+    public Object traceCdiCall(final InvocationContext invocationContext) throws Exception {
+        LOG.fine(() -> "traceCdiCall(" + invocationContext + ")");
         // Get the required HK2 services
-        ServiceLocator serviceLocator = Globals.getDefaultBaseServiceLocator();
-        OpenTracingService openTracing = serviceLocator.getService(OpenTracingService.class);
-        InvocationManager invocationManager = serviceLocator.getService(InvocationManager.class);
-        
-        // Initialise return value
-        Object proceed = null;
+        final ServiceLocator serviceLocator = Globals.getDefaultBaseServiceLocator();
+        final OpenTracingService openTracing = serviceLocator.getService(OpenTracingService.class);
+        final InvocationManager invocationManager = serviceLocator.getService(InvocationManager.class);
 
         // If Request Tracing is enabled, and this isn't a JaxRs method
-        if (openTracing != null && openTracing.isEnabled() && !isJaxRsMethod(invocationContext) && !isWebServiceMethod(invocationContext, invocationManager)) {
-            // Get the Traced annotation present on the method or class
-            Traced traced = OpenTracingCdiUtils.getAnnotation(beanManager, Traced.class, invocationContext);
-
-            // Get the operationName variable from a config override, or from the annotation if there is no override
-            String operationName = (String) OpenTracingCdiUtils
-                    .getConfigOverrideValue(Traced.class, "operationName", invocationContext, String.class)
-                    .orElse(traced.operationName());
-            
-            // If the operation name is blank, set it to the full method signature
-            if (operationName.equals("")) {
-                operationName = invocationContext.getMethod().getDeclaringClass().getCanonicalName()
-                        + "."
-                        + invocationContext.getMethod().getName();
-            }
-
-            // Get the enabled (value) variable from a config override, or from the annotation if there is no override
-            boolean tracingEnabled = (boolean) OpenTracingCdiUtils
-                    .getConfigOverrideValue(Traced.class, "value", invocationContext, boolean.class)
-                    .orElse(traced.value());
-
-            // Only trace if we've explicitly been told to (which is the default behaviour)
-            if (tracingEnabled) {
-                // If we *have* been told to, get the application's Tracer instance and start an active span.
-                Tracer tracer = openTracing.getTracer(openTracing.getApplicationName(invocationManager, invocationContext));
-                Span activeSpan = tracer.buildSpan(operationName).start();
-                try (Scope scope = tracer.scopeManager().activate(activeSpan, true)) { // TODO Check whether this should be true or false
-
-                    // Proceed the invocation
-                    try {
-                        proceed = invocationContext.proceed();
-                    } catch (Exception ex) {
-                        // If an exception occurs during processing the method, add error info to the span
-                        activeSpan.setTag(Tags.ERROR.getKey(), true);
-                        Map<String, Object> errorInfoMap = new HashMap<>();
-                        errorInfoMap.put(Fields.EVENT, "error");
-                        errorInfoMap.put(Fields.ERROR_OBJECT, ex);
-                        activeSpan.log(errorInfoMap);
-                        // Don't deal with the error here, let it propagate upwards
-                        throw ex;
-                    }
-                }
-            } else {
-                // If we've explicitly been told not to trace the method: don't!
-                proceed = invocationContext.proceed();
-            }
-        } else {
+        if (openTracing == null || !openTracing.isEnabled() //
+            || isJaxRsMethod(invocationContext) || isWebServiceMethod(invocationContext, invocationManager)) {
             // If request tracing was turned off, or this is a JaxRs method, just carry on
-            proceed = invocationContext.proceed();
+            LOG.finest("The call is already monitored by some different component, proceeding the invocation.");
+            return invocationContext.proceed();
         }
 
-        return proceed;
+        // Get the Traced annotation present on the method or class
+        final Traced traced = getAnnotation(beanManager, Traced.class, invocationContext);
+
+        // Get the enabled (value) variable from a config override, or from the annotation if there is no override
+        final boolean tracingEnabled = OpenTracingCdiUtils
+                .getConfigOverrideValue(Traced.class, "value", invocationContext, boolean.class)
+                .orElse(traced.value());
+
+        // If we've explicitly been told not to trace the method: don't!
+        if (!tracingEnabled) {
+            LOG.finest("Tracing is not enabled, nothing to do.");
+            return invocationContext.proceed();
+        }
+
+        // If we *have* been told to, get the application's Tracer instance and start an active span.
+        final String applicationName = openTracing.getApplicationName(invocationManager, invocationContext);
+        final Tracer tracer = openTracing.getTracer(applicationName);
+        final String operationName = getOperationName(invocationContext, traced);
+        final Span activeSpan = tracer.buildSpan(operationName).start();
+        try (Scope scope = tracer.scopeManager().activate(activeSpan, true)) {
+            try {
+                return invocationContext.proceed();
+            } catch (final Exception ex) {
+                LOG.log(Level.FINEST, "Setting the error to the active span ...", ex);
+                activeSpan.setTag(Tags.ERROR.getKey(), true);
+                final Map<String, Object> errorInfoMap = new HashMap<>();
+                errorInfoMap.put(Fields.EVENT, "error");
+                errorInfoMap.put(Fields.ERROR_OBJECT, ex.getClass().getName());
+                activeSpan.log(errorInfoMap);
+                throw ex;
+            }
+        }
     }
 
     /**
-     * Helper method that determines if the annotated method is a JaxRs method or not by inspecting it for HTTP Method 
+     * Helper method that determines if the annotated method is a JaxRs method or not by inspecting it for HTTP Method
      * annotations. JaxRs method tracing is handled by the Client/Container filters, so we don't process them using this
      * interceptor.
-     * 
+     *
      * @param invocationContext The invocation context from the AroundInvoke method.
      * @return True if the method is a JaxRs method.
      */
+    @SuppressWarnings({"unchecked", "rawtypes"}) // safe here.
     private boolean isJaxRsMethod(InvocationContext invocationContext) {
-        // Initialise an Array with all supported JaxRs HTTP methods
-        Class[] httpMethods = {GET.class, POST.class, DELETE.class, PUT.class, HEAD.class, PATCH.class, OPTIONS.class};
-
         // Check if any of the HTTP Method annotations are present on the intercepted method
-        for (Class httpMethod : httpMethods) {
-            if (OpenTracingCdiUtils.getAnnotation(beanManager, httpMethod, invocationContext) != null) {
+        for (Class httpMethod : HTTP_METHODS) {
+            if (getAnnotation(beanManager, httpMethod, invocationContext) != null) {
                 return true;
             }
         }
 
         return false;
     }
-    
+
     /**
-     * Helper method that determines if the annotated method is the monitored JAX-WS method or not by inspecting the invocation manager. 
+     * Helper method that determines if the annotated method is the monitored JAX-WS method or not by inspecting the invocation manager.
      * <p>
      * JaxWs method tracing is handled by the JAX-WS monitoring pipe/tube, so we don't process them using this
      * interceptor.
-     * 
+     *
      * @param invocationContext The invocation context from the AroundInvoke method.
      * @param invocationManager The current invocation manager for this thread
      * @return True if the method is a JaxRs method.
@@ -185,5 +186,23 @@ public class TracedInterceptor implements Serializable {
     private boolean isWebServiceMethod(InvocationContext invocationContext, InvocationManager invocationManager) {
         return invocationContext.getMethod().equals(invocationManager.peekWebServiceMethod());
     }
-    
+
+
+    /**
+     * Get the operationName variable from a config override, or from the annotation if there is no override
+     *
+     * @return the name of the operation. Never null.
+     */
+    private String getOperationName(final InvocationContext invocationContext, final Traced traced) {
+        String operationName = OpenTracingCdiUtils
+            .getConfigOverrideValue(Traced.class, "operationName", invocationContext, String.class)
+            .orElse(traced.operationName());
+
+        if (!"".equals(operationName)) {
+            return operationName;
+        }
+        // If the operation name is blank, set it to the full method signature
+        return invocationContext.getMethod().getDeclaringClass().getCanonicalName() + "."
+            + invocationContext.getMethod().getName();
+    }
 }
---
+++ appserver/payara-appserver-modules/microprofile/opentracing/src/main/java/fish/payara/microprofile/opentracing/jaxrs/OpenTracingApplicationEventListener.java
@@ -0,0 +1,117 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *    Copyright (c) 2019 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *     The contents of this file are subject to the terms of either the GNU
+ *     General Public License Version 2 only ("GPL") or the Common Development
+ *     and Distribution License("CDDL") (collectively, the "License").  You
+ *     may not use this file except in compliance with the License.  You can
+ *     obtain a copy of the License at
+ *     https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *     See the License for the specific
+ *     language governing permissions and limitations under the License.
+ *
+ *     When distributing the software, include this License Header Notice in each
+ *     file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *     GPL Classpath Exception:
+ *     The Payara Foundation designates this particular file as subject to the "Classpath"
+ *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *     file that accompanied this code.
+ *
+ *     Modifications:
+ *     If applicable, add the following below the License Header, with the fields
+ *     enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyright [year] [name of copyright owner]"
+ *
+ *     Contributor(s):
+ *     If you wish your version of this file to be governed by only the CDDL or
+ *     only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *     elects to include this software in this distribution under the [CDDL or GPL
+ *     Version 2] license."  If you don't indicate a single choice of license, a
+ *     recipient has the option to distribute your version of this file under
+ *     either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *     its licensees as provided above.  However, if you add GPL Version 2 code
+ *     and therefore, elected the GPL Version 2 license, then the option applies
+ *     only if the new code is made subject to such option by the copyright
+ *     holder.
+ */
+package fish.payara.microprofile.opentracing.jaxrs;
+
+import fish.payara.nucleus.requesttracing.RequestTracingService;
+import fish.payara.opentracing.OpenTracingService;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+
+import org.glassfish.api.invocation.InvocationManager;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.internal.api.Globals;
+import org.glassfish.jersey.server.monitoring.ApplicationEvent;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.glassfish.jersey.server.monitoring.RequestEvent;
+import org.glassfish.jersey.server.monitoring.RequestEventListener;
+
+/**
+ * {@link ApplicationEventListener} for configuring the {@link OpenTracingRequestEventListener}
+ * instances for tracked requests.
+ *
+ * @author David Matejcek
+ */
+@Priority(Priorities.HEADER_DECORATOR - 500)
+public class OpenTracingApplicationEventListener implements ApplicationEventListener {
+
+    private static final Logger LOG = Logger.getLogger(OpenTracingApplicationEventListener.class.getName());
+
+    private RequestTracingService requestTracing;
+    private OpenTracingService openTracing;
+    private String applicationName;
+
+    @Context
+    private ResourceInfo resourceInfo;
+
+
+    /**
+     * Initialization of internal services.
+     */
+    @PostConstruct
+    public void postConstruct() {
+        LOG.finest("postConstruct()");
+        final ServiceLocator serviceLocator = Globals.getDefaultBaseServiceLocator();
+        if (serviceLocator == null) {
+            LOG.config("Default base service locator is null, JAX-RS server tracing is disabled.");
+            return;
+        }
+        this.requestTracing = serviceLocator.getService(RequestTracingService.class);
+        this.openTracing = serviceLocator.getService(OpenTracingService.class);
+        this.applicationName = this.openTracing.getApplicationName(serviceLocator.getService(InvocationManager.class));
+    }
+
+
+    @Override
+    public void onEvent(final ApplicationEvent event) {
+        LOG.config(() -> "onEvent(event.type=" + event.getType() + ")");
+    }
+
+
+    @Override
+    public RequestEventListener onRequest(final RequestEvent event) {
+        LOG.finer(() -> "onRequest(event.type=" + event.getType() + ")");
+        if (!isRequestTracingInProgress()) {
+            LOG.finest("isRequestTracingInProgress() returned false, nothing to do.");
+            return null;
+        }
+        return new OpenTracingRequestEventListener(this.applicationName, this.resourceInfo, this.openTracing);
+    }
+
+
+    private boolean isRequestTracingInProgress() {
+        return requestTracing != null && requestTracing.isRequestTracingEnabled() && requestTracing.isTraceInProgress();
+    }
+}
