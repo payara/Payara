payara/Payara

commit 81f87239ac175e26affc4fc2fbe0c5338a618696

Command is now replicated among instances in a Deployment Group

nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/Domain.java
@@ -47,6 +47,15 @@ import com.sun.enterprise.util.StringUtils;
 import fish.payara.enterprise.config.serverbeans.DGServerRef;
 import fish.payara.enterprise.config.serverbeans.DeploymentGroup;
 import fish.payara.enterprise.config.serverbeans.DeploymentGroups;
+import java.beans.PropertyVetoException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.validation.constraints.NotNull;
 import org.glassfish.api.admin.config.ApplicationName;
 import org.glassfish.api.admin.config.PropertiesDesc;
 import org.glassfish.api.admin.config.PropertyDesc;
@@ -60,16 +69,6 @@ import org.jvnet.hk2.config.DuckTyped;
 import org.jvnet.hk2.config.Element;
 import org.jvnet.hk2.config.types.Property;
 import org.jvnet.hk2.config.types.PropertyBag;
-
-import javax.validation.constraints.NotNull;
-import java.beans.PropertyVetoException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 /**
@@ -917,7 +916,7 @@ public interface Domain extends ConfigBeanProxy, PropertyBag, SystemPropertyBag,
             // only add non-clustered servers as the cluster 
             // targets will be separately added
             for (Server server : d.getServers().getServer()) {
-                if (server.getCluster() == null && server.getDeploymentGroup().isEmpty()) {
+                if (server.getCluster() == null) {
                     targets.add(server.getName());
                 }
             }

nucleus/admin/config-api/src/main/java/org/glassfish/config/support/CommandTarget.java
@@ -41,8 +41,6 @@
 package org.glassfish.config.support;
 
 import com.sun.enterprise.config.serverbeans.*;
-
-
 import org.glassfish.hk2.api.ServiceLocator;
 
 /**
@@ -140,7 +138,7 @@ public enum CommandTarget implements TargetValidator {
         }
     },
     /**
-     * a cluster configuration change
+     * a Deployment Group configuration change
      */
     DEPLOYMENT_GROUP {
         @Override

nucleus/cluster/cli/src/main/java/com/sun/enterprise/admin/cli/cluster/CreateLocalInstanceCommand.java
@@ -43,14 +43,6 @@
 
 package com.sun.enterprise.admin.cli.cluster;
 
-import static com.sun.enterprise.admin.servermgmt.domain.DomainConstants.MASTERPASSWORD_FILE;
-
-import java.io.File;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-
 import com.sun.enterprise.admin.cli.CLIConstants;
 import com.sun.enterprise.admin.cli.remote.RemoteCLICommand;
 import com.sun.enterprise.admin.servermgmt.KeystoreManager;
@@ -60,7 +52,13 @@ import com.sun.enterprise.universal.glassfish.TokenResolver;
 import com.sun.enterprise.util.OS;
 import com.sun.enterprise.util.SystemPropertyConstants;
 import com.sun.enterprise.util.io.FileUtils;
-
+import fish.payara.admin.cli.cluster.NamingHelper;
+import fish.payara.util.cluster.PayaraServerNameGenerator;
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Level;
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.I18n;
 import org.glassfish.api.Param;
@@ -70,8 +68,7 @@ import org.glassfish.hk2.api.PerLookup;
 import org.glassfish.security.common.FileProtectionUtility;
 import org.jvnet.hk2.annotations.Service;
 
-import fish.payara.admin.cli.cluster.NamingHelper;
-import fish.payara.util.cluster.PayaraServerNameGenerator;
+import static com.sun.enterprise.admin.servermgmt.domain.DomainConstants.MASTERPASSWORD_FILE;
 
 
 /**
@@ -86,12 +83,16 @@ import fish.payara.util.cluster.PayaraServerNameGenerator;
 public final class CreateLocalInstanceCommand extends CreateLocalInstanceFilesystemCommand {
     private static final String CONFIG = "config";
     private static final String CLUSTER = "cluster";
+    private static final String DEPLOYMENT_GROUP = "deploymentGroup";
 
     @Param(name = CONFIG, optional = true)
     private String configName;
 
     @Param(name = CLUSTER, optional = true)
     private String clusterName;
+    
+    @Param(name = DEPLOYMENT_GROUP, optional = true)
+    private String deploymentGroup;
 
     @Param(name="lbenabled", optional = true)
     private Boolean lbEnabled;
@@ -350,6 +351,10 @@ public final class CreateLocalInstanceCommand extends CreateLocalInstanceFilesys
         if (clusterName != null) {
             argsList.add("--cluster");
             argsList.add(clusterName);
+        }
+        if (deploymentGroup != null) {
+            argsList.add("--deploymentgroup");
+            argsList.add(deploymentGroup);
         }
         if (lbEnabled != null) {
             argsList.add("--lbenabled");

nucleus/common/amx-core/src/main/java/fish/payara/admin/amx/cli/SetAmxEnabled.java
@@ -84,7 +84,7 @@ import static org.glassfish.config.support.CommandTarget.STANDALONE_INSTANCE;
  */
 @Service(name = "set-amx-enabled")
 @PerLookup
-@ExecuteOn({RuntimeType.DAS})
+@ExecuteOn({RuntimeType.DAS, RuntimeType.INSTANCE})
 @TargetType({DAS, DEPLOYMENT_GROUP, STANDALONE_INSTANCE, CLUSTER, CLUSTERED_INSTANCE, CONFIG})
 @RestEndpoints({
     @RestEndpoint(configBean = Domain.class,

commit a8ec79deb59b24607d999bf6c7c0d45c9608400a

CUSTCOM-78 Fix redeployment for deployment groups
When redeploying applications for deployment groups, a HK2 bean locking
exception would be thrown as the server attempts to deploy the
application to the deployment group as well as each of the instances
within that deployment group. This change means that only the deployment
group is targeted so the application can deploy normally.

The broken functionality was caused by
81f87239ac175e26affc4fc2fbe0c5338a618696, but the included fix does
seem appropriate for the original issue. This change aims to complement
that commit.

nucleus/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/Domain.java
@@ -38,7 +38,7 @@
  * holder.
  */
 
-// Portions Copyright [2017-2018] [Payara Foundation and/or its affiliates]
+// Portions Copyright [2017-2020] [Payara Foundation and/or its affiliates]
 
 package com.sun.enterprise.config.serverbeans;
 
@@ -930,12 +930,36 @@ public interface Domain extends ConfigBeanProxy, PropertyBag, SystemPropertyBag,
             return targets;
         }
 
+        /**
+         * @param me      the current domain
+         * @param appName the name of the application
+         * @return all of the targets that have application-refs for an application. If
+         *         an instance is included by being in a deployment group or cluster it
+         *         won't appear in this list.
+         */
         public static List<String> getAllReferencedTargetsForApplication(
             Domain me, String appName) {
             List<String> referencedTargets = new ArrayList<String>();
             for (String target : me.getAllTargets()) {
                 if (me.getApplicationRefInTarget(appName, target) != null) {
                     referencedTargets.add(target);
+                }
+            }
+
+            // Remove any server targets if they are included by virtue of being in a
+            // deployment group
+            for (String reference : new ArrayList<>(referencedTargets)) {
+                DeploymentGroup dg = me.getDeploymentGroupNamed(reference);
+                if (dg != null) {
+                    for (Server instance : dg.getInstances()) {
+                        referencedTargets.remove(instance.getName());
+                    }
+                }
+                Cluster cluster = me.getClusterNamed(reference);
+                if (cluster != null) {
+                    for (Server instance : cluster.getInstances()) {
+                        referencedTargets.remove(instance.getName());
+                    }
                 }
             }
             return referencedTargets;