c71a030ed19ef9428bbdb041be2930f571b46bca
merorai <susan.rai04@gmail.com>
2020-02-07 09:50:30 +0000

    * Removed the use of continue for the loop * Updated the copyright header

--- nucleus/cluster/admin/src/main/java/fish/payara/admin/cluster/AddInstanceToDeploymentGroupCommand.java
+++ nucleus/cluster/admin/src/main/java/fish/payara/admin/cluster/AddInstanceToDeploymentGroupCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2017 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2017-2020] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -183,10 +183,9 @@ public class AddInstanceToDeploymentGroupCommand implements AdminCommand {
                 List<VirtualServer> hosts = httpService.getVirtualServer();
                 if (hosts != null) {
                     for (VirtualServer host : hosts) {
-                        if (("__asadmin").equals(host.getId())) {
-                            continue;
+                        if (!("__asadmin").equals(host.getId())) {
+                            virtualServers.add(host.getId());
                         }
-                        virtualServers.add(host.getId());
                     }
                 }
             }
