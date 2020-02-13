a201e8ce96b4925bab2a91a5ebfc9d6ac5450be0
Matt Gill <matthew.gill@live.co.uk>
2020-02-13 16:50:48 +0000

    CUSTCOM-70 Fix JAXRS client with embedded dependency
    When Payara Embedded is used as a library rather than starting the
    embedded server, an exception will be thrown when using the JAX-RS
    client caused by the request tracing service not having initialised.
    
    The previous CUSTCOM-70 fix allowed a JAX-RS client request to be
    built, but executing the request would still throw the same
    exception. This change allows the request to be executed as well.
    
    Additional references to service locators were restricted to the
    PayaraTracingServices class to coordinate all service discovery in
    request tracing. This should help prevent future service locator related
    problems in tracing, or at least make them easier to fix.
    
    Signed-off-by: Matt Gill <matthew.gill@live.co.uk>

--- appserver/tests/payara-samples/classpath/embedded-vs-jersey/src/test/java/fish/payara/samples/classpath/embeddedvsjersey/JerseyClasspathTest.java
+++ appserver/tests/payara-samples/classpath/embedded-vs-jersey/src/test/java/fish/payara/samples/classpath/embeddedvsjersey/JerseyClasspathTest.java
@@ -1,7 +1,7 @@
 /*
  *    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- *    Copyright (c) [2019] Payara Foundation and/or its affiliates. All rights reserved.
+ *    Copyright (c) [2019-2020] Payara Foundation and/or its affiliates. All rights reserved.
  *
  *    The contents of this file are subject to the terms of either the GNU
  *    General Public License Version 2 only ("GPL") or the Common Development
@@ -54,6 +54,6 @@ public class JerseyClasspathTest {
     @Test
     public void jerseyShouldInitialize() {
         Client client = ClientBuilder.newClient();
-        client.target("http://localhost:8080").request().buildGet();
+        client.target("https://google.com").request().get();
     }
 }
