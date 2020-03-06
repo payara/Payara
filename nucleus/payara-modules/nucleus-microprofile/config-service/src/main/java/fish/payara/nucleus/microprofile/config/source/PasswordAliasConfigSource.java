7ac1cdbc9706d924fe169c64f7fb4581c94ef440
Andrew Pielage <andrew.pielage@payara.fish>
2020-03-06 15:17:56 +0000

    APPSERV-20 refactor big method in TranslatedConfigView and translate values returned from password alias config provider

--- nucleus/payara-modules/nucleus-microprofile/config-service/src/main/java/fish/payara/nucleus/microprofile/config/source/PasswordAliasConfigSource.java
+++ nucleus/payara-modules/nucleus-microprofile/config-service/src/main/java/fish/payara/nucleus/microprofile/config/source/PasswordAliasConfigSource.java
@@ -40,10 +40,20 @@
 package fish.payara.nucleus.microprofile.config.source;
 
 import com.sun.enterprise.security.store.DomainScopedPasswordAliasStore;
+
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.eclipse.microprofile.config.spi.ConfigSource;
+import org.glassfish.config.support.TranslatedConfigView;
 import org.glassfish.internal.api.Globals;
 
 /**
@@ -53,7 +63,7 @@ import org.glassfish.internal.api.Globals;
 public class PasswordAliasConfigSource extends PayaraConfigSource implements ConfigSource {
 
     private final DomainScopedPasswordAliasStore store;
-    
+    private static final String ALIAS_TOKEN = "ALIAS";
     
     public PasswordAliasConfigSource() {
         store = Globals.getDefaultHabitat().getService(DomainScopedPasswordAliasStore.class);
@@ -70,7 +80,7 @@ public class PasswordAliasConfigSource extends PayaraConfigSource implements Con
         Map<String,String> properties = new HashMap<>();
         if (store != null) {
             Iterator<String> keys = store.keys();
-            while(keys.hasNext()){
+            while (keys.hasNext()){
                 String key = keys.next();
                 properties.put(key, new String(store.get(key)));
             }
@@ -80,10 +90,26 @@ public class PasswordAliasConfigSource extends PayaraConfigSource implements Con
 
     @Override
     public String getValue(String name) {
-        String value = null;
-        if (store != null && name != null && store.containsKey(name)) {
-            value = new String(store.get(name));
+        if (name == null || store == null) {
+            return null;
         }
+
+        String value = null;
+
+        if (store.containsKey(name)) {
+            value = new String(store.get(name));
+        } else if (TranslatedConfigView.getAlias(name, ALIAS_TOKEN) != null) {
+            try {
+                value = TranslatedConfigView.getRealPasswordFromAlias(name);
+            } catch (IllegalArgumentException iae) {
+                Logger.getLogger(PasswordAliasConfigSource.class.getName()).log(Level.FINEST, iae.getMessage());
+            } catch (IOException | CertificateException | NoSuchAlgorithmException |
+                    UnrecoverableKeyException | KeyStoreException exception) {
+                Logger.getLogger(PasswordAliasConfigSource.class.getName()).log(Level.FINE,
+                        "Exception caught reading from Password Alias store", exception);
+            }
+        }
+
         return value;
     }
 
