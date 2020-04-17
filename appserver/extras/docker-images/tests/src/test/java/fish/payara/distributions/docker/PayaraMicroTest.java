73cc89f8fe192e071febbef60defab651e1b3c36
David Matejcek <david.matejcek@payara.fish>
2020-04-17 11:27:28 +0200

    CUSTCOM-130 Fixed Payara Micro docker image
    - deployment did not work
    - extended test to deploy a trivial servlet application

--- appserver/extras/docker-images/tests/src/test/java/fish/payara/distributions/docker/PayaraMicroTest.java
+++ appserver/extras/docker-images/tests/src/test/java/fish/payara/distributions/docker/PayaraMicroTest.java
@@ -40,13 +40,17 @@
 
 package fish.payara.distributions.docker;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import fish.payara.distributions.docker.war.TestServlet;
+
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,32 +60,70 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.testcontainers.containers.BindMode.READ_ONLY;
 
+/**
+ * Test for Payara Micro docker container basic functionality.
+ *
+ * @author David Matejcek
+ */
 @Testcontainers
 public class PayaraMicroTest {
+
     private static final Logger LOG = LoggerFactory.getLogger(PayaraMicroTest.class);
+    private static final Class<TestServlet> SERVLET_CLASS = TestServlet.class;
+    private static final File WAR_FILE = createWar();
+    private static final String WEBAPP_CONTEXT = "/" + WAR_FILE.getName().substring(0, WAR_FILE.getName().length() - 4);
 
     @Container
-    private static final PayaraContainer CONTAINER = new PayaraContainer("payara/micro") //
-            .withExposedPorts(8080)
-            .withLogConsumer(new Slf4jLogConsumer(LOG))
-            .waitingFor(Wait.forLogMessage(".*Payara Micro.+ ready.+\\n", 1));
+    private final PayaraContainer container = new PayaraContainer("payara/micro") //
+        .withExposedPorts(8080)
+        .withLogConsumer(new Slf4jLogConsumer(LOG))
+        .withFileSystemBind(WAR_FILE.getAbsolutePath(), "/opt/payara/deployments/" + WAR_FILE.getName(), READ_ONLY)
+        .waitingFor(Wait.forLogMessage(".*Payara Micro.+ ready.+\\n", 1));
 
-    @Test
-    public void testStartedServerEndpoints() throws Exception {
-        assertTrue(CONTAINER.isRunning(), "server is running");
-        assertAll( //
-                () -> assertNotNull(CONTAINER.getMappedPort(8080), "http port") //
-        );
-        // health url is not present without a deployment so that's all we'd test for now
+    private static File createWar() {
+        try {
+            final WebArchive war = ShrinkWrap.create(WebArchive.class).addClass(SERVLET_CLASS);
+            final File warFileOnHost = File.createTempFile(TestServlet.class.getSimpleName(), ".war");
+            war.as(ZipExporter.class).exportTo(warFileOnHost, true);
+            return warFileOnHost;
+        } catch (Exception e) {
+            return fail(e);
+        }
     }
 
-    private String getPageContent(final URL url) throws IOException {
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(url.openConnection().getInputStream(), StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining("\n"));
+
+    /**
+     * Tests if Micro can be started and it the war application has been deployed and works.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testServlet() throws Exception {
+        assertTrue(container.isRunning(), "server is running");
+        assertAll( //
+            () -> assertNotNull(container.getMappedPort(8080), "http port"), //
+            () -> assertEquals(TestServlet.RESPONSE_TEXT, download()) //
+        );
+    }
+
+
+    private String download() throws Exception {
+        final URL url = new URL("http", container.getContainerIpAddress(), container.getMappedPort(8080), WEBAPP_CONTEXT);
+        final Object object = url.getContent();
+        if (object instanceof InputStream) {
+            final InputStream input = (InputStream) object;
+            try (Scanner scanner = new Scanner(input, StandardCharsets.UTF_8.name())) {
+                return scanner.nextLine();
+            } finally {
+                input.close();
+            }
         }
+        return fail("Expected input stream, but received this: " + object.toString());
     }
 }
