b86911b20011a4ff2827313faa9fdec05812a9de
Patrik Dudits <patrik.dudits@payara.fish>
2020-04-20 11:19:30 +0200

    CUSTOM-130: Retain CMD compatibility for Payara Micro image
    - deploymentDir is now CMD concern, overrideable by user
    - shell executes exec, so that java becomes the main process of container
    - shell expansion is not available in CMD, therefore DEPLOY_DIR used expanded,
      but this is usual Docker behavior

--- appserver/extras/docker-images/micro/src/main/docker/bin/entrypoint.sh
+++ appserver/extras/docker-images/micro/src/main/docker/bin/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar --deploymentDir "${DEPLOY_DIR}"
+exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@"
