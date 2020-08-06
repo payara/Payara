e148c021274c084a693549ca504c2dbf147f97fc
Andrew Pielage <pandrex247@hotmail.com>
2020-08-06 13:15:07 +0100

    FISH-244 Remove unused imports

--- nucleus/payara-modules/opentracing-adapter/src/main/java/fish/payara/opentracing/OpenTracingScope.java
+++ nucleus/payara-modules/opentracing-adapter/src/main/java/fish/payara/opentracing/OpenTracingScope.java
@@ -42,13 +42,6 @@
  */
 package fish.payara.opentracing;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import io.opentracing.Span;
 
 /**
