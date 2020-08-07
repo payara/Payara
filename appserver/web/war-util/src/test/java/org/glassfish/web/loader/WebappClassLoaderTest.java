23f7df02b82a4704480468a2f71168c7737a3e3c
Cousjava <jcoustick@sleightholme.com>
2020-08-07 14:42:11 +0100

    FISh-66 Prevent zip file closure on EJB initalisation

--- appserver/web/war-util/src/test/java/org/glassfish/web/loader/WebappClassLoaderTest.java
+++ appserver/web/war-util/src/test/java/org/glassfish/web/loader/WebappClassLoaderTest.java
@@ -56,6 +56,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import org.apache.naming.resources.FileDirContext;
+import org.apache.naming.resources.WebDirContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -99,8 +100,42 @@ public class WebappClassLoaderTest {
 
         CompletableFuture<Void> result = new CompletableFuture<>();
 
+        add(webappClassLoader);
         // Create the tasks to run
         Runnable lookupTask = waitAndDo(result, () -> lookup(webappClassLoader));
+        Runnable addTask = waitAndDo(result, () -> add(webappClassLoader));
+        Runnable closeTask = waitAndDo(result, () -> webappClassLoader.closeJARs(true));
+
+        try {
+            // Run the methods at the same time
+            for (int i = 0; i < EXECUTION_COUNT; i++) {
+                executor.execute(addTask);
+                executor.execute(lookupTask);
+                executor.execute(closeTask);
+            }
+
+            // Wait for tasks to execute
+            assertTrue("The tasks didn't finish in the allowed time.",
+                    latch.await(20, TimeUnit.SECONDS));
+
+            // Check to see if any tasks completed exceptionally
+            result.getNow(null);
+        } finally {
+            webappClassLoader.close();
+        }
+    }
+
+    @Test
+    public void check_findResources_thread_safety() throws Exception {
+        final WebappClassLoader webappClassLoader = new WebappClassLoader(getClass().getClassLoader(), null);
+        webappClassLoader.start();
+        webappClassLoader.setResources(new WebDirContext());
+        webappClassLoader.addRepository(junitJarFile.getAbsolutePath(), junitJarFile);
+
+        CompletableFuture<Void> result = new CompletableFuture<>();
+
+        // Create the tasks to run
+        Runnable lookupTask = waitAndDo(result, () -> findResources(webappClassLoader));
         Runnable addTask = waitAndDo(result, () -> add(webappClassLoader));
         Runnable closeTask = waitAndDo(result, () -> webappClassLoader.closeJARs(true));
 
@@ -140,6 +175,15 @@ public class WebappClassLoaderTest {
             for (JarEntry entry : Collections.list(jarFile.entries())) {
                 webappClassLoader.findResource(entry.getName());
                 // System.out.println("Looked up " + resourceEntry);
+                Thread.sleep(0, 100);
+            }
+        }
+    }
+
+    private void findResources(WebappClassLoader webappClassLoader) throws Exception {
+        for (JarFile jarFile : findJarFiles()) {
+            for (JarEntry entry : Collections.list(jarFile.entries())) {
+                webappClassLoader.findResources(entry.getName());
                 Thread.sleep(0, 100);
             }
         }
