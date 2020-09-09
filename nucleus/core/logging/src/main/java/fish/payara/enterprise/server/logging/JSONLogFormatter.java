fdbd3c80304921b6f3c65960efd75adece8e7b24
Andrew Pielage <pandrex247@hotmail.com>
2020-09-09 15:26:57 +0100

    FISH-141 Add test and fix copyright
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/JSONLogFormatter.java
+++ nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/JSONLogFormatter.java
@@ -37,7 +37,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
- // Portions Copyright [2018-2019] [Payara Foundation and/or its affiliates]
 package fish.payara.enterprise.server.logging;
 
 import com.sun.common.util.logging.GFLogRecord;
