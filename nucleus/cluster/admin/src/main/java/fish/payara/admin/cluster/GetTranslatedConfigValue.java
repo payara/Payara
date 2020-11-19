d6ecfed14c39c6a0bef2126e6089707e3402d294
Jonathan Coustick <jonathan.coustick@payara.fish>
2020-11-19 13:35:56 +0000

    Moved command to cluster-admin module

--- nucleus/cluster/admin/src/main/java/fish/payara/admin/cluster/GetTranslatedConfigValue.java
+++ nucleus/cluster/admin/src/main/java/fish/payara/admin/cluster/GetTranslatedConfigValue.java
@@ -37,7 +37,7 @@
  *  only if the new code is made subject to such option by the copyright
  *  holder.
  */
-package fish.payara.enterprise.config;
+package fish.payara.admin.cluster;
 
 import com.sun.enterprise.config.serverbeans.Config;
 import com.sun.enterprise.util.StringUtils;
