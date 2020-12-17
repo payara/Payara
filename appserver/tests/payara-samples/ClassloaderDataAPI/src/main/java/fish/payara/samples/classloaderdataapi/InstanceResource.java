ae0526a3da9e6e11c8f0b48036c25b25b4e6c2df
Cuba <cuba.stanley.guitar@gmail.com>
2020-12-17 12:09:10 +0000

    Create access api

---
+++ appserver/tests/payara-samples/ClassloaderDataAPI/src/main/java/fish/payara/samples/classloaderdataapi/InstanceResource.java
@@ -0,0 +1,33 @@
+package fish.payara.samples.classloaderdataapi;
+
+import java.lang.ref.Reference;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import org.glassfish.web.loader.WebappClassLoader;
+import org.glassfish.web.loader.WebappClassLoaderFinalizer;
+
+/**
+ * A simple REST endpoint to return the count of
+ * WebappClassLoader instances in the JVM
+ * 
+ * @author - Cuba Stanley
+ */
+@Path("instance-count")
+public class InstanceResource {
+
+    /**
+     * Method handling HTTP GET request for Instance Count
+     *
+     * @return int WebappClassLoader instance count
+     */
+    @GET
+    public int getClassLoaderCount() {
+        System.gc();
+        Reference<? extends WebappClassLoader> referenceFromQueue;
+        while((referenceFromQueue = WebappClassLoader.referenceQueue.poll()) != null) {
+            ((WebappClassLoaderFinalizer)referenceFromQueue).cleanupAction();
+            referenceFromQueue.clear();
+        }
+        return WebappClassLoader.getInstanceCount();
+    }
+}
