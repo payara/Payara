e8af024ff0e301a1590b160f288aa4ca23167d88
Oliver Bertuch <o.bertuch@fz-juelich.de>
2021-01-26 00:28:26 +0100

    refactor(mpconfig): Add unit tests for DirConfigSource. #5006

--- nucleus/payara-modules/nucleus-microprofile/config-service/src/test/java/fish/payara/nucleus/microprofile/config/source/DirConfigSourceTest.java
+++ nucleus/payara-modules/nucleus-microprofile/config-service/src/test/java/fish/payara/nucleus/microprofile/config/source/DirConfigSourceTest.java
@@ -39,7 +39,9 @@
  */
 package fish.payara.nucleus.microprofile.config.source;
 
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,23 +49,28 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class DirConfigSourceTest {
 
     private static Path testDirectory;
     private static DirConfigSource source;
+    private static ScheduledExecutorService exec = Executors.newScheduledThreadPool(3);
     
     @BeforeClass
     public static void setUp() throws IOException {
@@ -73,7 +80,9 @@ public class DirConfigSourceTest {
     }
 
     @AfterClass
-    public static void tearDown() throws IOException {
+    public static void tearDown() throws IOException, InterruptedException {
+        exec.shutdown();
+        exec.awaitTermination(1, TimeUnit.SECONDS);
         Files.walk(testDirectory)
             .sorted(Comparator.reverseOrder())
             .map(Path::toFile)
@@ -81,28 +90,98 @@ public class DirConfigSourceTest {
     }
     
     @Test
+    public void testIsAptDir() throws IOException {
+        // given
+        Map<Path, Boolean> examples = new HashMap<>();
+        examples.put(subpath( "aptdir"), TRUE);
+        examples.put(subpath( ".unaptdir"), FALSE);
+    
+        // when & then
+        assertEquals(FALSE, DirConfigSource.isAptDir(null));
+        assertEquals(FALSE, DirConfigSource.isAptDir(subpath( "aptnotexisting")));
+        for (Map.Entry<Path, Boolean> ex : examples.entrySet()) {
+            Files.createDirectories(ex.getKey());
+            assertEquals(ex.getValue(), DirConfigSource.isAptDir(ex.getKey()));
+        }
+    }
+    
+    @Test
+    public void testIsAptFile() throws IOException {
+        
+        Map<Path, Boolean> examples = new HashMap<>();
+        examples.put(subpath( "aptdir/aptfile"), TRUE);
+        examples.put(subpath( "aptdir/.unaptfile"), FALSE);
+        
+        assertEquals(FALSE, DirConfigSource.isAptFile(null, null));
+        assertEquals(FALSE, DirConfigSource.isAptFile(subpath( "aptdir/aptnotexisting"), null));
+        for (Map.Entry<Path, Boolean> ex : examples.entrySet()) {
+            BasicFileAttributes atts = writeFile(ex.getKey(), "test");
+            assertEquals(ex.getValue(), DirConfigSource.isAptFile(ex.getKey(), atts));
+        }
+        
+        Path file100k = subpath("aptdir/100k-file");
+        BasicFileAttributes atts100k = writeRandFile(file100k, 100);
+        assertEquals(TRUE, DirConfigSource.isAptFile(file100k, atts100k));
+        
+        Path file600k = subpath("aptdir/600k-file");
+        BasicFileAttributes atts600k = writeRandFile(file600k, 600);
+        assertEquals(TRUE, DirConfigSource.isAptFile(file600k, atts600k));
+    }
+    
+    @Test
     public void testParsePropertyNameFromPath() {
         // given
         Map<Path,String> examples = new HashMap<>();
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar/test/ex"), "foo.bar.test.ex");
-        examples.put(Paths.get(testDirectory.toString(), "foo.bar.test/ex"), "foo.bar.test.ex");
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar.test/ex"), "foo.bar.test.ex");
-        examples.put(Paths.get(testDirectory.toString(), "foo.bar/test/ex"), "foo.bar.test.ex");
+        examples.put(subpath( "foo/bar/test/ex"), "foo.bar.test.ex");
+        examples.put(subpath( "foo.bar.test/ex"), "foo.bar.test.ex");
+        examples.put(subpath( "foo/bar.test/ex"), "foo.bar.test.ex");
+        examples.put(subpath( "foo.bar/test/ex"), "foo.bar.test.ex");
         
-        // we ignore the last file extension. always. this might lead to unexpected behaviour for a user.
+        // we ignore the last file extension if not more than 3 chars.
+        // this might lead to unexpected behaviour for a user.
         // best advice: do not use dots in filename, only in directory names.
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar/test/ex.txt"), "foo.bar.test.ex");
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar/test/ex.tar.gz"), "foo.bar.test.ex.tar");
-        examples.put(Paths.get(testDirectory.toString(), "foo.bar/test.ex"), "foo.bar.test");
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar.test.ex"), "foo.bar.test");
-        examples.put(Paths.get(testDirectory.toString(), "foo.bar.test.ex"), "foo.bar.test");
-        examples.put(Paths.get(testDirectory.toString(), "foo/bar/test.ex"), "foo.bar.test");
+        examples.put(subpath( "foo/bar/test/ex.txt"), "foo.bar.test.ex");
+        examples.put(subpath( "foo/bar/test/ex.tar.gz"), "foo.bar.test.ex.tar");
+        examples.put(subpath( "foo/bar/test/ex.helo"), "foo.bar.test.ex.helo");
+        examples.put(subpath( "foo.bar/test.ex"), "foo.bar.test");
+        examples.put(subpath( "foo/bar.test.ex"), "foo.bar.test");
+        examples.put(subpath( "foo.bar.test.ex"), "foo.bar.test");
+        examples.put(subpath( "foo/bar/test.ex"), "foo.bar.test");
         
         // when & then
         for (Map.Entry<Path, String> ex : examples.entrySet()) {
-            System.out.println(ex.getKey()+" = "+ex.getValue());
-            assertEquals(ex.getValue(), source.parsePropertyNameFromPath(ex.getKey()));
+            //System.out.println(ex.getKey()+" = "+ex.getValue());
+            assertEquals(ex.getValue(), DirConfigSource.parsePropertyNameFromPath(ex.getKey(), testDirectory));
         }
+    }
+    
+    @Test
+    public void testReadPropertyFromPath() throws IOException {
+        // given
+        Path sut = subpath("aptdir/sut-read-property");
+        BasicFileAttributes attsSUT = writeFile(sut, "foobar");
+        DirConfigSource.DirProperty example = new DirConfigSource.DirProperty(
+          "foobar", attsSUT.lastModifiedTime(), sut, testDirectory
+        );
+        // when & then
+        assertEquals(example, DirConfigSource.readPropertyFromPath(sut, attsSUT, testDirectory));
+    }
+    
+    @Test
+    public void testCheckLongestMatchForPath_SamePath() {
+        // given
+        Map<String,DirConfigSource.DirProperty> props = new HashMap<>();
+        // a property with a most specific path
+        String property = "foo.bar.test.ex";
+        props.put(property,
+            new DirConfigSource.DirProperty(
+                "test", FileTime.from(Instant.now()),
+                subpath( "foo/bar/test/ex"),
+                testDirectory));
+        source.setProperties(props);
+        
+        // when & then
+        assertTrue(source.isLongestMatchForPath(property, subpath("foo/bar/test/ex")));
     }
     
     @Test
@@ -114,12 +193,12 @@ public class DirConfigSourceTest {
         props.put(property,
                   new DirConfigSource.DirProperty(
                       "test", FileTime.from(Instant.now()),
-                      Paths.get(testDirectory.toString(), "foo/bar/test/ex"),
+                      subpath( "foo/bar/test/ex"),
                       testDirectory));
         source.setProperties(props);
         
         // when & then
-        assertFalse(source.isLongerMatchForPath(property, Paths.get(testDirectory.toString(), "foo/bar.test/ex")));
+        assertFalse(source.isLongestMatchForPath(property, subpath( "foo/bar.test/ex")));
     }
     
     @Test
@@ -131,12 +210,12 @@ public class DirConfigSourceTest {
         props.put(property,
             new DirConfigSource.DirProperty(
                 "test", FileTime.from(Instant.now()),
-                Paths.get(testDirectory.toString(), "foo.bar/test/ex"),
+                subpath( "foo.bar/test/ex"),
                 testDirectory));
         source.setProperties(props);
         
         // when & then
-        assertTrue(source.isLongerMatchForPath(property, Paths.get(testDirectory.toString(), "foo/bar/test/ex")));
+        assertTrue(source.isLongestMatchForPath(property, subpath( "foo/bar/test/ex")));
     }
     
     @Test
@@ -148,12 +227,12 @@ public class DirConfigSourceTest {
         props.put(property,
             new DirConfigSource.DirProperty(
                 "test", FileTime.from(Instant.now()),
-                Paths.get(testDirectory.toString(), "foo.bar/test.ex/one"),
+                subpath( "foo.bar/test.ex/one"),
                 testDirectory));
         source.setProperties(props);
         
         // when & then
-        assertTrue(source.isLongerMatchForPath(property, Paths.get(testDirectory.toString(), "foo.bar/test/ex.one.txt")));
+        assertTrue(source.isLongestMatchForPath(property, subpath( "foo.bar/test/ex.one.txt")));
     }
     
     @Test
@@ -165,12 +244,12 @@ public class DirConfigSourceTest {
         props.put(property,
             new DirConfigSource.DirProperty(
                 "test", FileTime.from(Instant.now()),
-                Paths.get(testDirectory.toString(), "foo.bar/test.ex/one"),
+                subpath( "foo.bar/test.ex/one"),
                 testDirectory));
         source.setProperties(props);
         
         // when & then
-        assertTrue(source.isLongerMatchForPath("foo.bar.test.ex.two", Paths.get(testDirectory.toString(), "foo.bar/test/ex.two.txt")));
+        assertTrue(source.isLongestMatchForPath("foo.bar.test.ex.two", subpath( "foo.bar/test/ex.two.txt")));
     }
     
     @Test
@@ -182,58 +261,158 @@ public class DirConfigSourceTest {
         props.put(property,
             new DirConfigSource.DirProperty(
                 "test", FileTime.from(Instant.now()),
-                Paths.get(testDirectory.toString(), "foo/bar/test"),
+                subpath( "foo/bar/test"),
                 testDirectory));
         source.setProperties(props);
         assertEquals("test", source.getValue(property));
         
         // when
-        source.removePropertyFromPath(Paths.get(testDirectory.toString(), "foo/bar/test"));
+        source.removePropertyFromPath(subpath( "foo/bar/test"));
         // then
         assertTrue(source.getValue(property) == null);
         
     }
-
-    @Test
-    public void testInitializeProperties_SimpleFiles() throws IOException {
-        // given
-        // only the most specific should be picked up (=test3)
-        writeFile(testDirectory, "foo.bar.test", "test");
-        writeFile(testDirectory, "foo.bar/test", "test2");
-        writeFile(testDirectory, "foo/bar/test", "test3");
-        
-        //when
-        source.initializePropertiesFromPath(testDirectory);
-        
-        //then
-        assertEquals("test3", source.getValue("foo.bar.test"));
-    }
     
     @Test
-    public void testInitializeProperties_IgnoreHidden() throws IOException {
+    public void testUpsertPropertyFromPath_InsertCase() throws IOException {
         // given
-        // none of these should be picked up (hidden file or dir)
-        writeFile(testDirectory, ".hidden.bar.test", "test");
-        writeFile(testDirectory, ".hidden/bar.test", "test");
-        //when
-        source.initializePropertiesFromPath(testDirectory);
-        //then
-        assertEquals(null, source.getValue("hidden.bar.test"));
+        Path sut = subpath("aptdir/sut-upsert-property-insert");
+        BasicFileAttributes attsSUT = writeFile(sut, "foobar");
+        DirConfigSource.DirProperty example = new DirConfigSource.DirProperty(
+            "foobar", attsSUT.lastModifiedTime(), sut, testDirectory
+        );
+        
+        // when & then
+        assertEquals(true, source.upsertPropertyFromPath(sut, attsSUT));
+        assertEquals(example.propertyValue, source.getValue("aptdir.sut-upsert-property-insert"));
     }
     
-    @Test(expected = IOException.class)
-    public void testInitializeProperties_FailDirectory() throws IOException {
+    @Test
+    public void testUpsertPropertyFromPath_UpdateCase() throws IOException {
         // given
-        Path failDir = Paths.get("/tmp/fail-112312");
-        //when & then
-        source.initializePropertiesFromPath(failDir);
+        Path sut = subpath("aptdir/sut-upsert-property-update");
+        BasicFileAttributes attsSUT = writeFile(sut, "foobar");
+        DirConfigSource.DirProperty example = new DirConfigSource.DirProperty(
+            "foobar", attsSUT.lastModifiedTime(), sut, testDirectory
+        );
+        
+        assertEquals(true, source.upsertPropertyFromPath(sut, attsSUT));
+        assertEquals(example.propertyValue, source.getValue("aptdir.sut-upsert-property-update"));
+        
+        // when & then
+        BasicFileAttributes attsUpdate = writeFile(sut, "foobar2");
+        assertEquals(true, source.upsertPropertyFromPath(sut, attsUpdate));
+        assertEquals("foobar2", source.getValue("aptdir.sut-upsert-property-update"));
     }
     
-    public static Path writeFile(Path parentDir, String filename, String content) throws IOException {
-        Path file = Paths.get(parentDir.toString(), filename);
-        Files.createDirectories(file.getParent());
-        Files.write(file, content.getBytes(StandardCharsets.UTF_8));
-        return file;
+    @Test
+    public void testPropertyWatcher_RegisterAndInit() throws Exception {
+        // given
+        Map<Path,String> examples = new HashMap<>();
+        examples.put(subpath( "init-watcher/foo/bar/test/ex"), "init-watcher.foo.bar.test.ex");
+        examples.put(subpath( "init-watcher/foo.bar.test/hello"), "init-watcher.foo.bar.test.hello");
+        examples.put(subpath( "init-watcher/.foo/ex"), "init-watcher.foo.ex");
+        for (Map.Entry<Path, String> ex : examples.entrySet()) {
+            writeFile(ex.getKey(), "foobar");
+        }
+        Files.createSymbolicLink(subpath( "init-watcher/foo.hello"), subpath("init-watcher/.foo/ex"));
+        
+        // when
+        DirConfigSource.DirPropertyWatcher watcher = source.createWatcher(subpath("init-watcher"));
+        
+        // then
+        assertEquals("foobar", source.getValue("init-watcher.foo.bar.test.ex"));
+        assertEquals("foobar", source.getValue("init-watcher.foo.bar.test.hello"));
+        assertEquals(null, source.getValue("init-watcher.foo.ex"));
+        assertEquals("foobar", source.getValue("init-watcher.foo.hello"));
+    }
+    
+    @Test
+    public void testPropertyWatcher_RunFilesNewUpdate() throws Exception {
+        // given
+        writeFile(subpath("watcher-files/foobar"), "test");
+        writeFile(subpath("watcher-files/.hidden/foobar"), "hidden");
+        Files.createSymbolicLink(subpath("watcher-files/revealed"), subpath("watcher-files/.hidden/foobar"));
+        
+        DirConfigSource.DirPropertyWatcher watcher = source.createWatcher(subpath("watcher-files"));
+        exec.scheduleWithFixedDelay(watcher, 0, 10, TimeUnit.MILLISECONDS);
+        
+        assertEquals("test", source.getValue("watcher-files.foobar"));
+        assertEquals("hidden", source.getValue("watcher-files.revealed"));
+        
+        // when
+        writeFile(subpath("watcher-files/foobar"), "test2");
+        writeFile(subpath("watcher-files/example"), "test2");
+        writeFile(subpath("watcher-files/reveal/.hidden"), "test2");
+        writeFile(subpath("watcher-files/.hidden/foobar"), "showme");
+        Files.delete(subpath("watcher-files/revealed"));
+        Files.createSymbolicLink(subpath("watcher-files/revealed"), subpath("watcher-files/.hidden/foobar"));
+        Thread.sleep(100);
+        
+        // then
+        assertEquals("test2", source.getValue("watcher-files.foobar"));
+        assertEquals("test2", source.getValue("watcher-files.example"));
+        assertEquals("showme", source.getValue("watcher-files.revealed"));
+        assertEquals(null, source.getValue("watcher-files.reveal.hidden"));
+    }
+    
+    @Test
+    public void testPropertyWatcher_RunNewDir() throws Exception {
+        // given
+        writeFile(subpath("watcher-newdir/test"), "test");
+        
+        DirConfigSource.DirPropertyWatcher watcher = source.createWatcher(subpath("watcher-newdir"));
+        exec.scheduleWithFixedDelay(watcher, 0, 10, TimeUnit.MILLISECONDS);
+        
+        assertEquals("test", source.getValue("watcher-newdir.test"));
+        
+        // when
+        writeFile(subpath("watcher-newdir/foobar/test"), "test");
+        writeFile(subpath("watcher-newdir/.hidden/foobar"), "test");
+        Thread.sleep(100);
+        
+        // then
+        assertEquals("test", source.getValue("watcher-newdir.foobar.test"));
+        assertEquals(null, source.getValue("watcher-newdir.hidden.foobar"));
+    }
+    
+    @Test
+    public void testPropertyWatcher_RunRemove() throws Exception {
+        // given
+        writeFile(subpath("watcher-remove/test"), "test");
+        
+        DirConfigSource.DirPropertyWatcher watcher = source.createWatcher(subpath("watcher-remove"));
+        exec.scheduleWithFixedDelay(watcher, 0, 10, TimeUnit.MILLISECONDS);
+        
+        assertEquals("test", source.getValue("watcher-remove.test"));
+        
+        // when
+        Files.delete(subpath("watcher-remove/test"));
+        Thread.sleep(100);
+        
+        // then
+        assertEquals(null, source.getValue("watcher-remove.test"));
+    }
+    
+    public static BasicFileAttributes writeFile(Path filepath, String content) throws IOException {
+        Files.createDirectories(filepath.getParent());
+        Files.write(filepath, content.getBytes(StandardCharsets.UTF_8));
+        return Files.readAttributes(filepath, BasicFileAttributes.class);
+    }
+    
+    public static BasicFileAttributes writeRandFile(Path filepath, int bytes) throws IOException {
+        Files.createDirectories(filepath.getParent());
+        
+        Random rnd = new Random();
+        byte[] content = new byte[bytes];
+        rnd.nextBytes(content);
+        
+        Files.write(filepath, content);
+        return Files.readAttributes(filepath, BasicFileAttributes.class);
+    }
+    
+    private static Path subpath(String subpath) {
+        return Paths.get(testDirectory.toString(), subpath);
     }
 
 }
