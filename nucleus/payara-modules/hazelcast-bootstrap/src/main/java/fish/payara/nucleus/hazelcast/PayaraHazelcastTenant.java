fe80880cfba7f7af4824ddc8728b23bc3b46eadd
Lenny Primak <lenny@flowlogix.com>
2021-04-08 18:15:41 -0500

    - pushing exmpty context in ClusteredStore to prevent association of Hazelcast maps to tenant applications (#5188)
    that may be later undeployed and would leave maps orphan, which in turn will lead
      to Hazelcast blocking calls to those maps
    - minor cleanup

--- nucleus/payara-modules/hazelcast-bootstrap/src/main/java/fish/payara/nucleus/hazelcast/PayaraHazelcastTenant.java
+++ nucleus/payara-modules/hazelcast-bootstrap/src/main/java/fish/payara/nucleus/hazelcast/PayaraHazelcastTenant.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) [2016-2020] Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2016-2021] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -120,13 +120,13 @@ public class PayaraHazelcastTenant implements TenantControl, DataSerializable {
     @Override
     public void writeData(ObjectDataOutput out) throws IOException {
         out.writeObject(contextInstance);
-        out.writeUTF(moduleName);
+        out.writeString(moduleName);
     }
 
     @Override
     public void readData(ObjectDataInput in) throws IOException {
         contextInstance = in.readObject();
-        moduleName = in.readUTF();
+        moduleName = in.readString();
     }
 
     @Override
