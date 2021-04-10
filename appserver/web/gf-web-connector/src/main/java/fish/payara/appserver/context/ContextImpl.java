6d6d86ac0679fdebef145b7cc41fffde8ff80afc
Lenny Primak <lenny@flowlogix.com>
2021-04-09 22:36:57 -0500

    [FISH-1293] Empty context should not block due to absent tenant (#5191)
    * [FISH-1293]
    - Added system property fish.payara.tenantcontrol.blocking…disable to disable tenant control blocking
    - properly pushing empty context by removing invocations instead of pushing a fake one
    - Fixed NPEs in invocation manager
    - push empty context for clustered singleton CDI maps

--- appserver/web/gf-web-connector/src/main/java/fish/payara/appserver/context/ContextImpl.java
+++ appserver/web/gf-web-connector/src/main/java/fish/payara/appserver/context/ContextImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) [2016-2020] Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2016-2021] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -41,6 +41,7 @@ package fish.payara.appserver.context;
 
 import com.sun.enterprise.container.common.spi.util.ComponentEnvManager;
 import com.sun.enterprise.util.Utility;
+import java.util.List;
 import java.util.Map;
 import org.glassfish.api.invocation.ComponentInvocation;
 import org.glassfish.api.invocation.InvocationManager;
@@ -84,7 +85,6 @@ class ContextImpl {
     }
 
     public static class ClassLoaderContext implements JavaEEContextUtil.Context {
-
         private final ClassLoader oldClassLoader;
         private final boolean resetOldClassLoader;
 
@@ -98,6 +98,21 @@ class ContextImpl {
             if (resetOldClassLoader) {
                 Utility.setContextClassLoader(oldClassLoader);
             }
+        }
+    }
+
+    public static class EmptyContext implements JavaEEContextUtil.Context {
+        private final InvocationManager invocationManager;
+        private final List<? extends ComponentInvocation> previousInvocations;
+
+        EmptyContext(InvocationManager invocationManager) {
+            this.invocationManager = invocationManager;
+            previousInvocations = invocationManager.popAllInvocations();
+        }
+
+        @Override
+        public void close() {
+            invocationManager.putAllInvocations(previousInvocations);
         }
     }
 
