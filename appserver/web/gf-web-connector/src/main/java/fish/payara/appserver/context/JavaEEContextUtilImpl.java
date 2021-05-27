4c3baa65be21a6a9e079a62b8c858df8d68d6944
Lenny Primak <lenny@flowlogix.com>
2021-05-27 09:39:20 -0500

    [FISH-1346] Hazelcast Cache now works for EAR files - Part 2 (#5270)
    * [FISH-1346] fix ear application module matching
    
    * versioning fixes

--- appserver/web/gf-web-connector/src/main/java/fish/payara/appserver/context/JavaEEContextUtilImpl.java
+++ appserver/web/gf-web-connector/src/main/java/fish/payara/appserver/context/JavaEEContextUtilImpl.java
@@ -54,10 +54,12 @@ import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import org.glassfish.api.invocation.ComponentInvocation;
 import org.glassfish.api.invocation.InvocationManager;
+import org.glassfish.deployment.versioning.VersioningUtils;
 import org.glassfish.grizzly.utils.Holder;
 import org.glassfish.internal.api.Globals;
 import org.glassfish.internal.data.ApplicationInfo;
 import org.glassfish.internal.data.ApplicationRegistry;
+import org.glassfish.internal.data.ModuleInfo;
 import org.jboss.weld.context.bound.BoundRequestContext;
 import org.jvnet.hk2.annotations.Service;
 
@@ -124,6 +126,11 @@ public class JavaEEContextUtilImpl implements JavaEEContextUtil, Serializable {
         return inv != null ? isLoaded(inv.getComponentId(), inv) : false;
     }
 
+    @Override
+    public boolean moduleMatches(ModuleInfo moduleInfo, String modulNameToMatch) {
+        return VersioningUtils.getUntaggedName(moduleInfo.getName()).equals(modulNameToMatch);
+    }
+
     private static ClassLoader getClassLoaderForEnvironment(JndiNameEnvironment componentEnv) {
         if (componentEnv instanceof BundleDescriptor) {
             BundleDescriptor bd = (BundleDescriptor) componentEnv;
@@ -145,7 +152,7 @@ public class JavaEEContextUtilImpl implements JavaEEContextUtil, Serializable {
 
     private boolean isLoaded(String componentId, ComponentInvocation invocation) {
         if (componentId == null) {
-            // empty component are always loaded
+            // empty components are always loaded
             return true;
         }
         JndiNameEnvironment env = invocation != null ? ((JndiNameEnvironment) invocation.getJNDIEnvironment())
@@ -210,9 +217,11 @@ public class JavaEEContextUtilImpl implements JavaEEContextUtil, Serializable {
         private InstanceImpl(ComponentInvocation currentInvocation) {
             boolean isApplicationComponent = false;
             if (currentInvocation.getComponentId() != null) {
-                componentId = currentInvocation.getComponentId();
+                componentId = VersioningUtils.getUntaggedName(currentInvocation.getComponentId());
             } else if (currentInvocation.getJNDIEnvironment() instanceof JndiNameEnvironment) {
-                componentId = DOLUtils.getApplicationName((JndiNameEnvironment)currentInvocation.jndiEnvironment);
+                componentId = DOLUtils.toEarComponentId(
+                        DOLUtils.getApplicationName((JndiNameEnvironment)
+                                currentInvocation.jndiEnvironment));
                 isApplicationComponent = true;
             } else {
                 // checkState() later should error out due to this condition
