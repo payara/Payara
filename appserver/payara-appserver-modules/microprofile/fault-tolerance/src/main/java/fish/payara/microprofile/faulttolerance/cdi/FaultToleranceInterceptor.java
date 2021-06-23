80590c98d18fac7c93d5b1d5d506923185372f98
Andrew Pielage <andrew.pielage@payara.fish>
2021-06-23 15:44:27 +0100

    Merge pull request #5315 from Pandrex247/FISH-758
    FISH-758 Fault Tolerance Annotations Not Applied to Rest Client Interfaces

--- appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
@@ -39,23 +39,6 @@
  */
 package fish.payara.microprofile.faulttolerance.cdi;
 
-import java.io.Serializable;
-import java.lang.annotation.Annotation;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import jakarta.annotation.Priority;
-import jakarta.enterprise.context.control.RequestContextController;
-import jakarta.enterprise.inject.Instance;
-import jakarta.enterprise.inject.spi.BeanManager;
-import jakarta.inject.Inject;
-import jakarta.interceptor.AroundInvoke;
-import jakarta.interceptor.Interceptor;
-import jakarta.interceptor.InvocationContext;
-
 import fish.payara.microprofile.faulttolerance.FaultToleranceConfig;
 import fish.payara.microprofile.faulttolerance.FaultToleranceService;
 import fish.payara.microprofile.faulttolerance.policy.FaultTolerancePolicy;
@@ -63,9 +46,22 @@ import fish.payara.microprofile.faulttolerance.service.Stereotypes;
 import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
 import org.glassfish.internal.api.Globals;
 
-@Interceptor
-@FaultTolerance
-@Priority(Interceptor.Priority.PLATFORM_AFTER + 15)
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.control.RequestContextController;
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.inject.Inject;
+import jakarta.interceptor.InvocationContext;
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Dependent
 public class FaultToleranceInterceptor implements Stereotypes, Serializable {
 
     private static final Logger logger = Logger.getLogger(FaultToleranceInterceptor.class.getName());
@@ -78,8 +74,14 @@ public class FaultToleranceInterceptor implements Stereotypes, Serializable {
 
     private FaultToleranceService faultToleranceService;
 
-    @AroundInvoke
+    protected static final String PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED =
+            "fish.payara.microprofile.faulttolerance.executed";
+
     public Object intercept(InvocationContext context) throws Exception {
+        if (!shouldIntercept(context)) {
+            return context.proceed();
+        }
+        context.getContextData().put(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED, Boolean.TRUE);
         try {
             initialize();
             AtomicReference<FaultToleranceConfig> lazyConfig = new AtomicReference<>();
@@ -116,6 +118,17 @@ public class FaultToleranceInterceptor implements Stereotypes, Serializable {
     @Override
     public Set<Annotation> getStereotypeDefinition(Class<? extends Annotation> stereotype) {
         return beanManager.getStereotypeDefinition(stereotype);
+    }
+
+    protected boolean shouldIntercept(InvocationContext invocationContext) throws Exception {
+        Map<String, Object> contextData = invocationContext.getContextData();
+
+        if (contextData.get(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED) != null &&
+                (Boolean) contextData.get(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED)) {
+            return false;
+        }
+
+        return true;
     }
 
 }
