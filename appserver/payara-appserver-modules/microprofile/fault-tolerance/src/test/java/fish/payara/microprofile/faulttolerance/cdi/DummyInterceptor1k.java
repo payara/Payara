1564feea4c9c43c580a73392d961b55b175ccfc2
Andrew Pielage <pandrex247@hotmail.com>
2021-06-23 13:01:12 +0100

    FISH-758 Rework and add unit test
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

---
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/test/java/fish/payara/microprofile/faulttolerance/cdi/DummyInterceptor1k.java
@@ -0,0 +1,8 @@
+package fish.payara.microprofile.faulttolerance.cdi;
+
+import javax.annotation.Priority;
+
+@Priority(1000)
+public class DummyInterceptor1k {
+
+}
---
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/test/java/fish/payara/microprofile/faulttolerance/cdi/DummyInterceptor2k.java
@@ -0,0 +1,7 @@
+package fish.payara.microprofile.faulttolerance.cdi;
+
+import javax.annotation.Priority;
+
+@Priority(2000)
+public class DummyInterceptor2k {
+}
