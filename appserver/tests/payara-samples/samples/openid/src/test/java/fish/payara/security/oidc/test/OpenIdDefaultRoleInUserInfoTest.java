d4d828d30a938e9e991e2dc4175980636dfb4d67
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2021-06-29 16:26:33 +0200

    FISH-1521: tests for OIDC groups specified in different ways
    Conflicts:
    	appserver/tests/payara-samples/samples/openid/src/main/java/fish/payara/security/oidc/server/OidcProvider.java

--- appserver/tests/payara-samples/samples/openid/src/test/java/fish/payara/security/oidc/test/OpenIdDefaultRoleInUserInfoTest.java
+++ appserver/tests/payara-samples/samples/openid/src/test/java/fish/payara/security/oidc/test/OpenIdDefaultRoleInUserInfoTest.java
@@ -1,7 +1,7 @@
 /*
  *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- *  Copyright (c) [2018-2021] Payara Foundation and/or its affiliates. All rights reserved.
+ *  Copyright (c) [2021] Payara Foundation and/or its affiliates. All rights reserved.
  * 
  *  The contents of this file are subject to the terms of either the GNU
  *  General Public License Version 2 only ("GPL") or the Common Development
@@ -63,7 +63,7 @@ import org.junit.runner.RunWith;
  */
 @NotMicroCompatible
 @RunWith(PayaraArquillianTestRunner.class)
-public class OpenIdDefaultRolesInUserInfoTest {
+public class OpenIdDefaultRoleInUserInfoTest {
 
     private WebClient webClient;
 
