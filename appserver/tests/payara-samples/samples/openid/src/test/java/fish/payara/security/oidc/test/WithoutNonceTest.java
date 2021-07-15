a1eed2c75415d8a86f67eeccc8e4f19e8fe3ac92
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2021-07-15 22:16:07 +0200

    Revert "Merge pull request #5338 from OndroMih/FISH-1521-community"
    This reverts commit 520029bd21bdf4d0790d05b54adecaba24090e9a, reversing
    changes made to 16fab659b04663e18dc94c36f3dc3dab9ceda941.

--- appserver/tests/payara-samples/samples/openid/src/test/java/fish/payara/security/oidc/test/WithoutNonceTest.java
+++ appserver/tests/payara-samples/samples/openid/src/test/java/fish/payara/security/oidc/test/WithoutNonceTest.java
@@ -1,7 +1,7 @@
 /*
  *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- *  Copyright (c) [2018-2021] Payara Foundation and/or its affiliates. All rights reserved.
+ *  Copyright (c) [2018-2020] Payara Foundation and/or its affiliates. All rights reserved.
  *
  *  The contents of this file are subject to the terms of either the GNU
  *  General Public License Version 2 only ("GPL") or the Common Development
@@ -78,7 +78,7 @@ public class WithoutNonceTest {
     public static WebArchive createClientDeployment() {
         StringAsset mpConfig = new StringAsset(OPENID_MP_USE_NONCE + "=" + Boolean.FALSE.toString());
         return OpenIdTestUtil
-                .createClientDefaultDeployment()
+                .createClientDeployment()
                 .addAsWebInfResource(mpConfig, "classes/META-INF/microprofile-config.properties");
     }
 
