91d9d1bf16443a1f363ac9af2d86962a3fd55b6c
Rudy De Busscher <rudy.de.busscher@payara.fish>
2021-09-01 16:51:37 +0200

    FISH-5646: Client Certificate Validator API

---
+++ api/payara-api/src/main/java/fish/payara/security/client/ClientCertificateValidator.java
@@ -0,0 +1,62 @@
+/*
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *  Copyright (c) [2021] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common Development
+ *  and Distribution License("CDDL") (collectively, the "License").  You
+ *  may not use this file except in compliance with the License.  You can
+ *  obtain a copy of the License at
+ *  https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *  See the License for the specific
+ *  language governing permissions and limitations under the License.
+ *
+ *  When distributing the software, include this License Header Notice in each
+ *  file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *  GPL Classpath Exception:
+ *  The Payara Foundation designates this particular file as subject to the "Classpath"
+ *  exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *  file that accompanied this code.
+ *
+ *  Modifications:
+ *  If applicable, add the following below the License Header, with the fields
+ *  enclosed by brackets [] replaced by your own identifying information:
+ *  "Portions Copyright [year] [name of copyright owner]"
+ *
+ *  Contributor(s):
+ *  If you wish your version of this file to be governed by only the CDDL or
+ *  only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *  elects to include this software in this distribution under the [CDDL or GPL
+ *  Version 2] license."  If you don't indicate a single choice of license, a
+ *  recipient has the option to distribute your version of this file under
+ *  either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *  its licensees as provided above.  However, if you add GPL Version 2 code
+ *  and therefore, elected the GPL Version 2 license, then the option applies
+ *  only if the new code is made subject to such option by the copyright
+ *  holder.
+ */
+package fish.payara.security.client;
+
+import javax.security.auth.Subject;
+import javax.security.auth.x500.X500Principal;
+import java.security.cert.X509Certificate;
+
+/**
+ * The Client Certificate Validator API that can be used to perform additional checks on the presented
+ * Client certificate. When specified, this method is executed for each request so make sure the implementation
+ * considers some kind of caching for checks that take some considerable time.
+ */
+@FunctionalInterface
+public interface ClientCertificateValidator {
+
+    /**
+     * @param subject The Subject object for the authentication request.
+     * @param principal The Principal object from the user certificate.
+     * @param certificate The user certificate that was presented in the request.
+     * @return true when the Client Certificate Validation was successful and request can continue.
+     * In the case of false, a {@code LoginException} will be thrown.
+     */
+    boolean isValid(Subject subject, X500Principal principal, X509Certificate certificate);
+}
