a3b5191463a15c1f6782db2bcbf50766b15acbba
Andrew Pielage <pandrex247@hotmail.com>
2021-10-11 16:02:01 +0100

    Missed failure condition
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/tests/payara-samples/samples/corba-read-timeout/src/test/java/fish/payara/samples/corba/read/timeout/TimeoutTestClientIT.java
+++ appserver/tests/payara-samples/samples/corba-read-timeout/src/test/java/fish/payara/samples/corba/read/timeout/TimeoutTestClientIT.java
@@ -45,6 +45,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -82,6 +83,7 @@ public class TimeoutTestClientIT {
                 System.out.println("Error occured at try number: " + tryNumber + " after " + (System.currentTimeMillis() - startTime) + " millis");
                 e.printStackTrace();
                 running = false;
+                Assert.fail();
             }
         }
     }
