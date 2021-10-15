3c8dfc1123bed748ce8df4cea788ff72a886d480
Rudy De Busscher <rudy.de.busscher@payara.fish>
2021-10-15 13:05:33 +0200

    FISH-5802: exclude .gitkeep file from unpacking

--- appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/boot/loader/ExplodedURLClassloader.java
+++ appserver/extras/payara-micro/payara-micro-boot/src/main/java/fish/payara/micro/boot/loader/ExplodedURLClassloader.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2016-2018 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016-2021 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -142,7 +142,7 @@ public class ExplodedURLClassloader extends OpenURLClassLoader {
                             fileName = entry.getName().substring(LIB_DOMAIN_DIR.length());
                         }
 
-                        if (fileName != null) {
+                        if (fileName != null && !fileName.contains("gitkeep")) {
                             File outputFile = new File(runtimeDir, fileName);
                             registerForDeletion(outputFile);
                             super.addURL(outputFile.getAbsoluteFile().toURI().toURL());
