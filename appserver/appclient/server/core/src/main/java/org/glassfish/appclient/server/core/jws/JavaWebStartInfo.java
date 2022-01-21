f40445b3df52dc83412d2cc7ba91d6ea7967ac33
Andrew Pielage <pandrex247@hotmail.com>
2022-01-21 12:18:33 +0000

    FISH-5925 FISH-3236 FISH-3234 Reapply removal
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/appclient/server/core/src/main/java/org/glassfish/appclient/server/core/jws/JavaWebStartInfo.java
+++ appserver/appclient/server/core/src/main/java/org/glassfish/appclient/server/core/jws/JavaWebStartInfo.java
@@ -85,9 +85,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.jar.Attributes;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -114,9 +111,6 @@ public class JavaWebStartInfo implements ConfigListener {
 
     @Inject
     private DeveloperContentHandler dch;
-
-    @Inject
-    private ExtensionFileManager extensionFileManager;
 
     @Inject
     private ServerEnvironment serverEnv;
@@ -374,65 +368,6 @@ public class JavaWebStartInfo implements ConfigListener {
         logger.log(Level.INFO, JWS_STARTED,
             new Object[] {acServerApp.moduleExpression(),
             JWSAdapterManager.userFriendlyContextRoot(acServerApp)});
-    }
-
-    private void processExtensionReferences() throws IOException {
-
-        // TODO: needs to be expanded to handle signed library JARS, perhap signed by different certs
-        final URI fileURI = URI.create("file:" + helper.appClientServerOriginalAnchor(dc).getRawSchemeSpecificPart());
-        Set<ExtensionFileManager.Extension> exts = extensionFileManager.findExtensionTransitiveClosure(
-                new File(fileURI),
-                //new File(helper.appClientServerURI(dc)).getParentFile(),
-                dc.getSource().getManifest().getMainAttributes());
-        tHelper.setProperty(APP_LIBRARY_EXTENSION_PROPERTY_NAME,
-                jarElementsForExtensions(exts));
-        for (ExtensionFileManager.Extension e : exts) {
-            final URI uri = URI.create(JWSAdapterManager.publicExtensionLookupURIText(e));
-            final StaticContent newSystemContent = createSignedStaticContent(
-                    e.getFile(),
-                    signedFileForDomainFile(e.getFile()),
-                    uri,
-                    extensionName(e.getFile()));
-            jwsAdapterManager.addStaticSystemContent(
-                    uri.toString(),
-                    newSystemContent);
-        }
-
-    }
-
-    private String extensionName(final File f) throws IOException {
-        JarFile jf = null;
-        try {
-            jf = new JarFile(f);
-            final Manifest mf = jf.getManifest();
-            final Attributes mainAttrs = mf.getMainAttributes();
-            final String extName = mainAttrs.getValue(Attributes.Name.EXTENSION_NAME);
-            return (extName == null ? "" : extName);
-        } finally {
-            if (jf != null) {
-                jf.close();
-            }
-        }
-    }
-
-    private File signedFileForDomainFile(final File unsignedFile) {
-
-        final File rootForSignedFilesInDomain = jwsAdapterManager.rootForSignedFilesInDomain();
-        mkdirs(rootForSignedFilesInDomain);
-        final URI signedFileURI = rootForSignedFilesInDomain.toURI().resolve(relativeURIToDomainFile(unsignedFile));
-        return new File(signedFileURI);
-    }
-
-    private URI relativeURIToDomainFile(final File domainFile) {
-        return serverEnv.getInstanceRoot().toURI().relativize(domainFile.toURI());
-    }
-
-    private String jarElementsForExtensions(final Set<ExtensionFileManager.Extension> exts) {
-        final StringBuilder sb = new StringBuilder();
-        for (ExtensionFileManager.Extension e : exts) {
-            sb.append("<jar href=\"").append(JWSAdapterManager.publicExtensionHref(e)).append("\"/>");
-        }
-        return sb.toString();
     }
 
     private void stopJWSServices() throws EndpointRegistrationException {
