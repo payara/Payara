88c5ec6ffbf4581d8f3a114ac7ae3409d5d14155
Arie Kiswanto <arie@xtramilesolutions.com.au>
2022-03-11 00:17:52 +0800

    FISH-6043 Fix copyright

--- nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/PayaraNotificationFileHandler.java
+++ nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/PayaraNotificationFileHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2017-2018 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017-2022 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,7 +37,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright 2022 [Payara Foundation and/or its affiliates]
 package fish.payara.enterprise.server.logging;
 
 import com.sun.enterprise.server.logging.GFFileHandler;
