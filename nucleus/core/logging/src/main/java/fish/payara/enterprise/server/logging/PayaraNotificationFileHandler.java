61c9cd70c1aca4e18b8cb0cc8967674b14481d08
Arie Kiswanto <arie@xtramilesolutions.com.au>
2022-03-10 23:22:30 +0800

    FISH-6043 Add copyright

--- nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/PayaraNotificationFileHandler.java
+++ nucleus/core/logging/src/main/java/fish/payara/enterprise/server/logging/PayaraNotificationFileHandler.java
@@ -37,6 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+// Portions Copyright 2022 [Payara Foundation and/or its affiliates]
 package fish.payara.enterprise.server.logging;
 
 import com.sun.enterprise.server.logging.GFFileHandler;
