0b6f6ce7a674dcad9d41f42d2c6000d030f276e3
luiseufrasio <luis.eufrasio@gmail.com>
2022-03-28 07:17:32 -0300

    FISH-5978 initializing with TLS13 as default

--- appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/IIOPSSLSocketFactory.java
+++ appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/IIOPSSLSocketFactory.java
@@ -231,7 +231,7 @@ public class IIOPSSLSocketFactory implements ORBSocketFactory {
      * Return a default SSLInfo object.
      */
     private SSLInfo getDefaultSslInfo() throws Exception {
-       return init(null, false, null, true, null, true, true, true, false);
+       return init(null, false, null, true, null, true, false, true, true);
     }
 
     /**
