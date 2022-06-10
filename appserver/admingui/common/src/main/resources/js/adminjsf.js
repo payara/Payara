dfe24acdf89767eefb775633936b4f1714661c04
luiseufrasio <luis.eufrasio@gmail.com>
2022-06-10 08:17:08 -0300

    FISH-6307 comment explaining the other 2 comments

--- appserver/admingui/common/src/main/resources/js/adminjsf.js
+++ appserver/admingui/common/src/main/resources/js/adminjsf.js
@@ -2399,6 +2399,7 @@ admingui.ajax = {
         }
 
         // FIXME: These 2 functions (should) only need be replaced after FPR...
+        // FIXME: The next 2 lines were commented because webui is not recognized and the code breaks with it
         // webui.suntheme.hyperlink.submit = admingui.woodstock.hyperLinkSubmit;
         // webui.suntheme.jumpDropDown.changed = admingui.woodstock.dropDownChanged;
         var contextObj = {};
