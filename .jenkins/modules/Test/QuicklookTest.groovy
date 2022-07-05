d34cab73cb3e2ca57f7cc3b0f3cd923008582c57
Cuba Stanley <cuba.stanley@shibadogstudios.com>
2022-07-05 12:36:21 +0100

    Use Payara Versions

--- .jenkins/modules/Test/QuicklookTest.groovy
+++ .jenkins/modules/Test/QuicklookTest.groovy
@@ -1,11 +1,6 @@
-MPLPostStep('always') {
-    sh "${pwd()}/payara/bin/asadmin stop-domain ${CFG.domain_name}"
-    cleanWs()
-}
-
 withMaven(jdk: CFG.jdk, options: [artifactsPublisher(disabled: true)]) {
     sh """mvn -B -V -ff -e clean test --strict-checksums -Pall \
-        -Dglassfish.home=\"${pwd()}/appserver/distributions/payara/target/stage/payara5/glassfish\" \
+        -Dglassfish.home=\"${pwd()}/${getPayaraDirectoryName(CFG.'build.version')}/glassfish\" \
         -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
         -Djavax.xml.accessExternalSchema=all \
         -f appserver/tests/quicklook/pom.xml"""
--- .jenkins/modules/Test/Test.groovy
+++ .jenkins/modules/Test/Test.groovy
@@ -1,3 +1,8 @@
+MPLPostStep('always') {
+    sh "${pwd()}/${getPayaraDirectoryName(CFG.'build.version)}/bin/asadmin stop-domain ${CFG.domain_name}"
+    cleanWs()
+}
+
 // Perform suite specific test execution
 if(CFG.suite.equals("Payara-Samples")) {
     MPLModule('Payara Samples Test', CFG)
