6a689dc26cb50bcee16444e015dfe6474596f04e
Cuba Stanley <cuba.stanley@shibadogstudios.com>
2022-07-25 09:26:59 +0100

    Override Modules

---
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -0,0 +1,8 @@
+MPLModule('Test Setup', CFG);
+
+def ASADMIN = "${pwd()}/${getPayaraDirectoryName(CFG.'build.version')}/bin/asadmin"
+
+echo '*#*#*#*#*#*#*#*#*#*#*#*#  Setting up tests  *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#'
+sh "${ASADMIN} create-domain --nopassword ${CFG.domain_name}"
+sh "${ASADMIN} start-domain ${CFG.domain_name}"
+sh "${ASADMIN} start-database || true"
---
+++ .jenkins/modules/Test/Test.groovy
@@ -0,0 +1,5 @@
+if(CFG.suite.equals("Payara-Samples")) {
+    MPLModule('Payara Samples Test', CFG)
+} else {
+    MPLModule('Quicklook Test', CFG)
+}
