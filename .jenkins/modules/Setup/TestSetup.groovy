3cf895b577e036f0fe578436678a75b32a91a64e
Cuba Stanley <cuba.stanley@payara.fish>
2022-09-12 14:18:49 +0100

    compare value directly

--- .jenkins/modules/Setup/TestSetup.groovy
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -1,10 +1,8 @@
 // Perform the base behaviour
 MPLModule('Test Setup', CFG);
 
-def testSuite = CFG.suite.suite_name
-
 // Perform suite specific behaviour
-if(testSuite.equals("Payara-Samples")) {
+if(CFG.suite.suite_name.equals("Payara-Samples")) {
     MPLModule('Payara Samples Setup', CFG)
 } else {
     MPLModule('Quicklook Setup', CFG)
