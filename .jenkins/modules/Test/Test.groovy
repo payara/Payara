bcf3a92c70a035e40a914e5e2b9f6d87fcd7280b
Cuba Stanley <cuba.stanley@payara.fish>
2022-10-31 16:50:51 +0000

    Capture logs on test failure

--- .jenkins/modules/Test/Test.groovy
+++ .jenkins/modules/Test/Test.groovy
@@ -3,6 +3,11 @@ MPLPostStep('always') {
     cleanWs()
 }
 
+MPLPostStep('failure') {
+    echo "There are test failures, archiving server log"
+    archiveArtifacts artifacts: "./${${getPayaraDirectoryName}}/glassfish/domains/${getDomainName()}/logs/server.log"
+}
+
 // Perform suite specific test execution
 if(CFG.suite.suite_name.equals("Payara-Samples")) {
     MPLModule('Payara Samples Test', CFG)
