98eac19870de5da01027e160d34399027479abc5
Cuba Stanley <cuba.stanley@payara.fish>
2022-11-11 09:50:30 +0000

    Update .jenkins/modules/Test/PayaraSamplesTest.groovy
    Co-authored-by: Andrew Pielage <pandrex247@hotmail.com>

--- .jenkins/modules/Test/PayaraSamplesTest.groovy
+++ .jenkins/modules/Test/PayaraSamplesTest.groovy
@@ -2,8 +2,7 @@ withMaven(jdk: CFG.jdk, options: [artifactsPublisher(disabled: true)]) {
     sh """mvn -V -B -ff clean install --strict-checksums -Ppayara-server-remote \
         -Dpayara.version=${CFG.'build.version'} \
         -Dpayara.home=${pwd()}/${getPayaraDirectoryName(CFG.'build.version')} \
-        -Dpayara.domain=${CFG.domain_name} \
-        -Dpayara_domain=${CFG.domain_name} \
+        -Dpayara.domain.name=${CFG.domain_name} \
         -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
         -Djavax.xml.accessExternalSchema=all \
         -f appserver/tests/payara-samples """
