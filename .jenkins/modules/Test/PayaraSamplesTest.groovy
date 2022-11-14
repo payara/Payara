cc8210e3bc1d00ccdb34112f1bc50b247de407b5
Cuba Stanley <cuba.stanley@payara.fish>
2022-11-14 09:05:03 +0000

    Don't use withMaven for Payara Samples

--- .jenkins/modules/Test/PayaraSamplesTest.groovy
+++ .jenkins/modules/Test/PayaraSamplesTest.groovy
@@ -1,13 +1,10 @@
-withMaven(jdk: CFG.jdk, options: [artifactsPublisher(disabled: true)]) {
-    sh """
-    export MAVEN_OPTS="\$MAVEN_OPTS $JAVA_TOOL_OPTIONS"
-    unset JAVA_TOOL_OPTION
-    mvn -V -B -ff clean install \$(eval echo \$MAVEN_ADD_OPTIONS) \
-        --strict-checksums -Ppayara-server-remote \
-        -Dpayara.version=${CFG.'build.version'} \
-        -Dpayara.home=${pwd()}/${getPayaraDirectoryName(CFG.'build.version')} \
-        -Dpayara.domain.name=${CFG.domain_name} \
-        -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
-        -Djavax.xml.accessExternalSchema=all \
-        -f appserver/tests/payara-samples """
-}
+sh """
+mvn -V -B -ff clean install \
+    --strict-checksums -Ppayara-server-remote \
+    -Dpayara.version=${CFG.'build.version'} \
+    -Dpayara.home=${pwd()}/${getPayaraDirectoryName(CFG.'build.version')} \
+    -Dpayara.domain.name=${CFG.domain_name} \
+    -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
+    -Djavax.xml.accessExternalSchema=all \
+    -f appserver/tests/payara-samples """
+
