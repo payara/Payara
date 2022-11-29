1941b32f19ebe26a4819b8caf901358501aae022
Cuba Stanley <cuba.stanley@payara.fish>
2022-11-29 12:37:55 +0000

    Have QL produce test reports

--- .jenkins/modules/Test/QuicklookTest.groovy
+++ .jenkins/modules/Test/QuicklookTest.groovy
@@ -1,9 +1,9 @@
 MPLPostStep('always') {
-    testNG(reportFilenamePattern: 'appserver/tests/quicklook/**/*.xml')
+    testNG(reportFilenamePattern: 'appserver/tests/quicklook/test-output/**/*.xml')
 }
 
 withMaven(jdk: CFG.jdk, options: [artifactsPublisher(disabled: true)]) {
-    sh """mvn -B -V -ff -e clean test --strict-checksums -Pall \
+    sh """mvn -B -V -ff -e clean test --strict-checksums -Pall,report \
         -Dglassfish.home=\"${pwd()}/${getPayaraDirectoryName(CFG.'build.version')}/glassfish\" \
         -Djavax.net.ssl.trustStore=${env.JAVA_HOME}/jre/lib/security/cacerts \
         -Djavax.xml.accessExternalSchema=all \
