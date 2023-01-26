c3dc989b81851450deb395379628220e75544b02
Kalin Chan <73829904+kalinchan@users.noreply.github.com>
2023-01-26 12:08:16 +0000

    Delete DB File before Quicklook tests

--- .jenkins/modules/Setup/QuicklookSetup.groovy
+++ .jenkins/modules/Setup/QuicklookSetup.groovy
@@ -1,5 +1,5 @@
 def ASADMIN = "${pwd()}/${getPayaraDirectoryName(CFG.'build.version')}/bin/asadmin"
-
+sh "rm -rf ~/'test|sa.mv.db'"
 echo '*#*#*#*#*#*#*#*#*#*#*#*#  Setting up tests  *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#'
 sh "${ASADMIN} create-domain --nopassword ${CFG.domain_name}"
 sh "${ASADMIN} start-domain ${CFG.domain_name}"
