ccff271fe7e9cb26ad2685a4b488f798e30c41ff
Patrik Dudits <patrik.dudits@payara.fish>
2023-01-26 15:13:38 +0100

    FISH-6966: Package Opentelemetry tracing

---
+++ appserver/tests/payara-samples/samples/opentelemetry/src/main/java/fish/payara/samples/otel/manual/ManualTracing.java
@@ -0,0 +1,144 @@
+/*
+ *
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *  Copyright (c) 2023 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common Development
+ *  and Distribution License("CDDL") (collectively, the "License").  You
+ *  may not use this file except in compliance with the License.  You can
+ *  obtain a copy of the License at
+ *  https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *  See the License for the specific
+ *  language governing permissions and limitations under the License.
+ *
+ *  When distributing the software, include this License Header Notice in each
+ *  file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *  GPL Classpath Exception:
+ *  The Payara Foundation designates this particular file as subject to the "Classpath"
+ *  exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *  file that accompanied this code.
+ *
+ *  Modifications:
+ *  If applicable, add the following below the License Header, with the fields
+ *  enclosed by brackets [] replaced by your own identifying information:
+ *  "Portions Copyright [year] [name of copyright owner]"
+ *
+ *  Contributor(s):
+ *  If you wish your version of this file to be governed by only the CDDL or
+ *  only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *  elects to include this software in this distribution under the [CDDL or GPL
+ *  Version 2] license."  If you don't indicate a single choice of license, a
+ *  recipient has the option to distribute your version of this file under
+ *  either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *  its licensees as provided above.  However, if you add GPL Version 2 code
+ *  and therefore, elected the GPL Version 2 license, then the option applies
+ *  only if the new code is made subject to such option by the copyright
+ *  holder.
+ *
+ */
+
+package fish.payara.samples.otel.manual;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class ManualTracing {
+    private OpenTelemetrySdk openTelemetry;
+
+    private Tracer tracer;
+
+    @Inject
+    @ConfigProperty(name = "autoconfig", defaultValue = "false")
+    boolean autoConfig;
+
+    void initOtelManually() {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "manual-otel-app")));
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
+                .setResource(resource)
+                .build();
+
+        SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+                .registerMetricReader(PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().build()).build())
+                .setResource(resource)
+                .build();
+
+        openTelemetry = OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                //.setMeterProvider(sdkMeterProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .build();
+        tracer = openTelemetry.getTracer("fish.payara.opentelemetry");
+    }
+
+    void initOtel() {
+        if (autoConfig) {
+            initOtelAutoConfig();
+        } else {
+            initOtelManually();
+        }
+    }
+
+    @Inject
+    Config config;
+
+    void initOtelAutoConfig() {
+        Map<String,String> mpconfigProperties = new HashMap<>();
+
+        config.getPropertyNames().forEach(
+                name -> {
+                    if (name.startsWith("otel.")) {
+                        mpconfigProperties.put(name, config.getValue(name, String.class));
+                    }
+                }
+        );
+        AutoConfiguredOpenTelemetrySdk sdk = AutoConfiguredOpenTelemetrySdk.builder()
+                .addPropertiesSupplier(() -> mpconfigProperties)
+                .setServiceClassLoader(Thread.currentThread().getContextClassLoader())
+                .build();
+        tracer = sdk.getOpenTelemetrySdk().getTracer("fish.payara.opentelemetry");
+    }
+
+    public Tracer getTracer() {
+        return tracer;
+    }
+
+    @Produces
+    @RequestScoped
+    Span makeSpan() {
+        return tracer.spanBuilder("request").startSpan();
+    }
+
+    void endSpan(@Disposes Span span) {
+        span.end();
+    }
+}
