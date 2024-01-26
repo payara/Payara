bd8d9edb005400b3d86d5df11139fd52e54dc0ae
Luis Eufrasio Teixeira Neto <luis.eufrasio@gmail.com>
2024-01-26 10:18:14 -0300

    Update nucleus/security/core/src/main/java/com/sun/enterprise/security/GroupPrincipal.java
    Co-authored-by: Andrew Pielage <pandrex247@hotmail.com>

--- nucleus/security/core/src/main/java/com/sun/enterprise/security/GroupPrincipal.java
+++ nucleus/security/core/src/main/java/com/sun/enterprise/security/GroupPrincipal.java
@@ -13,7 +13,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-// Portions Copyright 2024 Payara Foundation and/or affiliates
 package com.sun.enterprise.security;
 
 import java.security.Principal;
