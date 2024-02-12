10be6baea15fde49bab84452875d92c8619c6eb4
Andrew Pielage <pandrex247@hotmail.com>
2024-02-12 09:05:09 +0000

    Leave comment on currently unused module
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/extras/embedded/common/bootstrap/src/main/java/org/glassfish/uberjar/bootstrap/UberJarOSGiGlassFishRuntimeBuilder.java
+++ appserver/extras/embedded/common/bootstrap/src/main/java/org/glassfish/uberjar/bootstrap/UberJarOSGiGlassFishRuntimeBuilder.java
@@ -187,6 +187,7 @@ public class UberJarOSGiGlassFishRuntimeBuilder implements RuntimeBuilder {
         System.setProperty(BootstrapProperties.INSTALL_ROOT_PROP_NAME, installRoot);
         System.setProperty(GlassFishProperties.INSTANCE_ROOT_PROP_NAME, instanceRoot);
 
+        // TODO: This likely needs versioning
         props.setProperty("org.osgi.framework.system.packages.extra", "org.glassfish.simpleglassfishapi");
 
 //        props.setProperty(org.osgi.framework.Constants.FRAMEWORK_BUNDLE_PARENT,
