5e0551d7868fe075308afd23474d0a9e2537eff0
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-06-19 17:15:55 +0100

    Update copyright header.

--- appserver/web/web-naming/src/main/java/org/apache/naming/resources/FileDirContext.java
+++ appserver/web/web-naming/src/main/java/org/apache/naming/resources/FileDirContext.java
@@ -55,7 +55,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Portions Copyright [2017-2023] [Payara Foundation and/or its affiliates]
+// Portions Copyright [2017-2024] [Payara Foundation and/or its affiliates]
 
 package org.apache.naming.resources;
 
