ff9621cbb3b395887dfdb5ea958986e331426358
NotedSalmon <moreiraandre2001@gmail.com>
2024-08-08 14:43:38 +0100

    FISH-9196 DBControl now has a charset UTF_8

--- appserver/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/DBControl.java
+++ appserver/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/DBControl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2017 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2017-2024] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -44,6 +44,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 import com.sun.enterprise.util.io.FileUtils;
 import org.glassfish.api.admin.CommandException;
@@ -84,7 +85,7 @@ public abstract class DBControl {
                 }
 
                 // Redirect stdout and stderr to a file
-                try (PrintStream printStream = new PrintStream(new FileOutputStream(dbLog, true), true)) {
+                try (PrintStream printStream = new PrintStream(new FileOutputStream(dbLog, true), true, StandardCharsets.UTF_8)) {
                     System.setOut(printStream);
                     System.setErr(printStream);
                 }
