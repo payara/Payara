83c1231fb36cb77ec67f384c468430ed3853ea5e
Alfonso Valdez <alfonso.altamirano@payara.fish>
2025-01-09 15:46:20 -0600

    Merge pull request #7147 from breakponchito/FISH-10295-fix-tck-issue-to-deploy-artifacts
    FISH-10295: adding cdi-tck fixes

--- appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ResourceHandler.java
+++ appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ResourceHandler.java
@@ -245,7 +245,7 @@ public class ResourceHandler extends AbstractResourceHandler {
             }
             
             //if value is empty we need to verify any other available descriptor that use same field with not empty value
-            if (!ok(desc.getValue())) {
+            if (target.getFieldName() != null && !ok(desc.getValue())) {
                 String valueFound = searchValueIfAvailable(target.getFieldName(), descriptors);
                 if (!valueFound.isEmpty()) {
                     desc.setValue(valueFound);
