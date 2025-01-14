6e0c893b93ce3019b642bd20cf8b1604057c83db
luiseufrasio <luis.eufrasio@gmail.com>
2025-01-14 17:48:45 -0300

    FISH-10015 : trying to debug

--- appserver/extras/docker-images/micro/src/main/docker/bin/entrypoint.sh
+++ appserver/extras/docker-images/micro/src/main/docker/bin/entrypoint.sh
@@ -42,9 +42,9 @@ set -e
 # Doing a Graceful Shutdown before container stops
 trap 'echo "Received SIGTERM. Stopping Payara-Micro...";
       pid=$!; wait $pid;
-      echo "Payara stopped. Exiting gracefully";' SIGTERM
+      echo "Payara-Micro stopped. Exiting gracefully";' SIGTERM
 
-exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@" &
+exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar payara-micro.jar "$@" &
 micro_pid=$!
 
 # Wait Payara-Micro Process before finish the container
