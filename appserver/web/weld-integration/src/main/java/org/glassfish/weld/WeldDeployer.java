867a7709b6f92ac10f028de7b9f1bcf15e70aad4
lprimak <lenny@flowlogix.com>
2025-01-15 17:37:15 -0600

    refactored WeldUtils class models into HK2 and Jandex variants

--- appserver/web/weld-integration/src/main/java/org/glassfish/weld/WeldDeployer.java
+++ appserver/web/weld-integration/src/main/java/org/glassfish/weld/WeldDeployer.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2016-2024] [Payara Foundation and/or its affiliates]
+// Portions Copyright [2016-2025] [Payara Foundation and/or its affiliates]
 
 package org.glassfish.weld;
 
@@ -882,7 +882,7 @@ public class WeldDeployer extends SimpleDeployer<WeldContainer, WeldApplicationC
             // From the spec: "The container must also fire an event for every Jakarta EE component class supporting
             // injection that may be instantiated by the container at runtime". Stress on the "may".
             Collection<String> injectionTargetClassNames = WeldUtils.getInjectionTargetClassNames(
-                    deploymentImpl.getTypes(), beanDeploymentArchive.getKnownClasses());
+                    deploymentImpl.context, beanDeploymentArchive.getKnownClasses());
             for (String injectionTargetClassName : injectionTargetClassNames) {
                 // Don't fire twice
                 if (beanDeploymentArchive.getBeanClasses().contains(injectionTargetClassName)) {
