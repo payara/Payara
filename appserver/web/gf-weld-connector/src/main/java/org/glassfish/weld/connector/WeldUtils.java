1b567861b516dd8c41216a06c65c8710bc963c2f
lprimak <lenny@flowlogix.com>
2025-02-26 13:58:38 -0600

    removed inject scanning

--- appserver/web/gf-weld-connector/src/main/java/org/glassfish/weld/connector/WeldUtils.java
+++ appserver/web/gf-weld-connector/src/main/java/org/glassfish/weld/connector/WeldUtils.java
@@ -138,7 +138,6 @@ public class WeldUtils {
         cdi.add(Dependent.class);
         cdi.add(Singleton.class);
         cdi.add(Model.class);
-        cdi.add(Inject.class);
 
         cdiScopeAnnotationClasses = Collections.unmodifiableSet(cdi);
         cdiScopeAnnotations = cdiScopeAnnotationClasses.stream().map(Class::getName).collect(Collectors.toSet());
