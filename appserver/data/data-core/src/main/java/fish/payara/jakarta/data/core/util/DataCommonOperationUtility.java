2ac96325b6827326b03605102a0054e042ed3f74
Kalin Chan <kalin.chan@payara.fish>
2025-07-28 09:10:50 +0100

    FISH-11591 Add resolution for multiple persistence units

--- appserver/data/data-core/src/main/java/fish/payara/jakarta/data/core/util/DataCommonOperationUtility.java
+++ appserver/data/data-core/src/main/java/fish/payara/jakarta/data/core/util/DataCommonOperationUtility.java
@@ -64,13 +64,13 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
+
 import org.glassfish.internal.api.Globals;
 import org.glassfish.internal.data.ApplicationInfo;
 import org.glassfish.internal.data.ApplicationRegistry;
@@ -124,10 +124,26 @@ public class DataCommonOperationUtility {
         ApplicationInfo applicationInfo = applicationRegistry.get(applicationName);
         List<EntityManagerFactory> factoryList = applicationInfo.getTransientAppMetaData(EntityManagerFactory.class.toString(), List.class);
         if (factoryList.size() == 1) {
-            EntityManagerFactory factory = factoryList.get(0);
+            EntityManagerFactory factory = factoryList.getFirst();
             return factory.createEntityManager();
         }
-        return null;
+
+        List<EntityManagerFactory> result = factoryList.stream().filter(factory -> {
+            Map<String, Object> properties = factory.getProperties();
+            if (properties == null || properties.isEmpty()) {
+                return false;
+            }
+            if (!properties.containsKey("fish.payara.jakarta.data.usePU")) {
+                return false;
+            }
+            return Boolean.parseBoolean((String) properties.get("fish.payara.jakarta.data.usePU"));
+        }).toList();
+
+        if (result.size() == 1) {
+            EntityManagerFactory factory = result.getFirst();
+            return factory.createEntityManager();
+        }
+        throw new UnambiguousPersistenceUnitException(String.format("Multiple persistence units found for application '%s', try setting the 'fish.payara.jakarta.data.usePU' property to true", applicationName));
     }
 
     public static ApplicationRegistry getRegistry() {
