c780658bf3b7ce44a9b4435406316262f79e9bcf
Andrew Pielage <pandrex247@hotmail.com>
2025-08-05 09:54:52 +0100

    FISH-11585 Adjust property name
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/transaction/jts/src/main/java/com/sun/jts/jta/TransactionServiceProperties.java
+++ appserver/transaction/jts/src/main/java/com/sun/jts/jta/TransactionServiceProperties.java
@@ -322,7 +322,7 @@ public class TransactionServiceProperties {
                         interval = Integer.parseInt(value);
                     }
                     new RecoveryHelperThread(serviceLocator, interval,
-                            Boolean.parseBoolean(properties.getProperty("lazy-lookup")))
+                            Boolean.parseBoolean(properties.getProperty("pending-txn-lazy-lookup")))
                             .start();
                 }
                 // Release all locks
