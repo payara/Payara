2ee90b8b7ccd94660d3a99a9971ab9a525de3375
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-08-08 13:48:20 +0100

    Update appserver/web/web-naming/src/main/java/org/apache/naming/resources/WebDirContext.java
    Co-authored-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/web/web-naming/src/main/java/org/apache/naming/resources/WebDirContext.java
+++ appserver/web/web-naming/src/main/java/org/apache/naming/resources/WebDirContext.java
@@ -176,8 +176,7 @@ public class WebDirContext extends FileDirContext {
             WebDirContext tempContext = new WebDirContext(env);
             if (file == null) {
                 tempContext.docBase = name;
-            }
-            else {
+            } else {
                 tempContext.setDocBase(file.getAbsolutePath());
             }
             tempContext.setAllowLinking(getAllowLinking());
