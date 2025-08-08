4e520ce7f03fe3f0e8eddd53b97750ccec01b7fb
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-08-08 14:10:13 +0100

    FISH-10821: Update copyright.

--- appserver/web/web-naming/src/main/java/org/apache/naming/resources/WebDirContext.java
+++ appserver/web/web-naming/src/main/java/org/apache/naming/resources/WebDirContext.java
@@ -55,7 +55,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Portions Copyright [2024] Payara Foundation and/or affiliates
+// Portions Copyright [2024-2025] Payara Foundation and/or affiliates
 
 package org.apache.naming.resources;
 
