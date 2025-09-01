62848eb4052d46f67f5ca6acaeafdac5a082ef06
Adrian S <timestreamer@gmail.com>
2025-09-01 21:59:48 +0300

    FISH-11746: fixed package name

--- appserver/extras/docker-images/tests/src/test/java/fish/payara/jcache/JCacheRestTest.java
+++ appserver/extras/docker-images/tests/src/test/java/fish/payara/jcache/JCacheRestTest.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package fish.payara.jcache_test;
+package fish.payara.jcache;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
