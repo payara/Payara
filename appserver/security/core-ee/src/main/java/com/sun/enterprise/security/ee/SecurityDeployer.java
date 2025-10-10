7baf7fe52b8fe359c472833ccd8ee89fcf3c9b09
Kalin Chan <kchan2002@outlook.com>
2025-10-10 11:14:30 +0100

    FISH-12069 Fix copyright

--- appserver/security/core-ee/src/main/java/com/sun/enterprise/security/ee/SecurityDeployer.java
+++ appserver/security/core-ee/src/main/java/com/sun/enterprise/security/ee/SecurityDeployer.java
@@ -38,7 +38,7 @@
  * holder.
  */
 
-// Portions Copyright [2016-2025] [Payara Foundation and/or its affiliates]
+// Portions Copyright 2016-2025 Payara Foundation and/or its affiliates
 
 package com.sun.enterprise.security.ee;
 
