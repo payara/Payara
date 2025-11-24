b132bfc6e0b4d05dfe05bcc70f93b8215586afdb
lprimak <lenny@flowlogix.com>
2025-11-24 11:28:15 -0600

    bugfix: prevent ClassLoader leaks from what are possible EAR libraries

--- nucleus/common/common-util/src/main/java/com/sun/enterprise/loader/CacheCleaner.java
+++ nucleus/common/common-util/src/main/java/com/sun/enterprise/loader/CacheCleaner.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) [2024] Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2024-2025] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -77,8 +77,8 @@ public class CacheCleaner {
         try {
             Class<?> eagerBeans = CachingReflectionUtil
                     .getClassFromCache("org.omnifaces.cdi.eager.EagerBeansRepository", classLoader);
-            if (eagerBeans != null && eagerBeans.getClassLoader() instanceof CurrentBeforeParentClassLoader) {
-                Field instance = CachingReflectionUtil.getFieldFromCache(eagerBeans, "instance", true);
+            if (checkClassLoader(eagerBeans)) {
+                Field instance = CachingReflectionUtil.getField(eagerBeans, "instance", true);
                 instance.set(null, null);
             }
         } catch (Exception e) {
@@ -89,15 +89,20 @@ public class CacheCleaner {
     private static void clearJNACache(ClassLoader classLoader) {
         try {
             Class<?> cleanerClass = CachingReflectionUtil
-                    .getClassFromCache("com.sun.jna.internal.Cleaner", classLoader);
-            if (cleanerClass != null && cleanerClass.getClassLoader() instanceof CurrentBeforeParentClassLoader) {
-                Field instanceField = CachingReflectionUtil.getFieldFromCache(cleanerClass, "INSTANCE", true);
+                    .getClass("com.sun.jna.internal.Cleaner", classLoader);
+            if (checkClassLoader(cleanerClass)) {
+                Field instanceField = CachingReflectionUtil.getField(cleanerClass, "INSTANCE", true);
                 Object instance = instanceField.get(null);
-                CachingReflectionUtil.getFieldFromCache(instance.getClass(), "cleanerThread", true)
+                CachingReflectionUtil.getField(instance.getClass(), "cleanerThread", true)
                         .set(instance, null);
             }
         } catch (Exception e) {
             logger.log(Level.WARNING, "Error clearing JNA cache", e);
         }
+    }
+
+    private static boolean checkClassLoader(Class<?> cls) {
+        return cls != null && (cls.getClassLoader() instanceof CurrentBeforeParentClassLoader
+                || "org.glassfish.web.loader.WebappClassLoader".equals(cls.getClassLoader().getClass().getName()));
     }
 }
