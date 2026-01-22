c77c47dbde06951661050dbc790c9445dcde3dd3
Kalin Chan <kchan2002@outlook.com>
2026-01-22 12:31:03 +0000

    FISH-8650 Update copyright

--- nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/CreateApplicationRefCommand.java
+++ nucleus/deployment/admin/src/main/java/org/glassfish/deployment/admin/CreateApplicationRefCommand.java
@@ -37,7 +37,8 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2017-2021] [Payara Foundation and/or its affiliates]
+// Portions Copyright 2017-2026 Payara Foundation and/or its affiliates
+
 package org.glassfish.deployment.admin;
 
 import com.sun.enterprise.admin.util.ClusterOperationUtil;
--- nucleus/deployment/common/src/test/java/org/glassfish/deployment/versioning/VersioningUtilsTest.java
+++ nucleus/deployment/common/src/test/java/org/glassfish/deployment/versioning/VersioningUtilsTest.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2017-2018] [Payara Foundation and/or its affiliates]
+// Portions Copyright 2017-2026 Payara Foundation and/or its affiliates
 
 package org.glassfish.deployment.versioning;
 
