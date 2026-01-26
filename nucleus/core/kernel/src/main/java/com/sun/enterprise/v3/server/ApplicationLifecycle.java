19978c133c157696e33e8b580b26996532cb36a6
lprimak <lenny@flowlogix.com>
2026-01-26 09:40:45 -0600

    bugfix: initialze WebContainer before HK2 class scanning so Rest APIs get loaded properly

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
@@ -38,7 +38,7 @@
  * holder.
  */
 
-// Portions Copyright 2016-2024 Payara Foundation and/or its affiliates.
+// Portions Copyright 2016-2025 Payara Foundation and/or its affiliates.
 
 package com.sun.enterprise.v3.server;
 
@@ -47,6 +47,7 @@ import fish.payara.nucleus.hotdeploy.ApplicationState;
 import com.sun.enterprise.config.serverbeans.*;
 import com.sun.enterprise.deploy.shared.ArchiveFactory;
 import com.sun.enterprise.deploy.shared.FileArchive;
+import com.sun.enterprise.loader.CachingReflectionUtil;
 import com.sun.enterprise.util.LocalStringManagerImpl;
 import com.sun.enterprise.util.io.FileUtils;
 import fish.payara.enterprise.config.serverbeans.DeploymentGroup;
@@ -394,10 +395,16 @@ public class ApplicationLifecycle implements Deployment, PostConstruct {
                 return null;
             }
 
+            span.start(DeploymentTracing.AppStage.PREPARE, "ClassLoaderHierarchy");
+            ClassLoaderHierarchy clh = habitat.getService(ClassLoaderHierarchy.class);
+
             span.start(DeploymentTracing.AppStage.CLASS_SCANNING);
 
             Types types = null;
             if (handler.requiresAnnotationScanning(context.getSource())) {
+                // web container needs to be loaded before deployable classes can be determined correctly
+                habitat.getService(CachingReflectionUtil
+                        .getClass("com.sun.enterprise.web.WebContainer", clh.getCommonClassLoader()));
                 types = getDeployableTypes(context);
             }
 
@@ -417,10 +424,6 @@ public class ApplicationLifecycle implements Deployment, PostConstruct {
             sniffers = getSniffers(handler, sniffers, context);
             final Collection<? extends Sniffer> selectedSniffers = sniffers;
             appState.ifPresent(s -> s.setSniffers(selectedSniffers));
-
-            span.start(DeploymentTracing.AppStage.PREPARE, "ClassLoaderHierarchy");
-
-            ClassLoaderHierarchy clh = habitat.getService(ClassLoaderHierarchy.class);
 
             span.start(DeploymentTracing.AppStage.PREPARE, "ClassLoader");
 
