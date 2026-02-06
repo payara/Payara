5cc3f9d5105d2a887d7bb7bb130a1833c4749f25
Andrew Pielage <pandrex247@hotmail.com>
2026-02-06 17:52:39 +0000

    FISH-13091 Fix IiopFolbGmsClient#resolvePort
    This method didn't understand what to do if the value wasn't a property
    
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/IiopFolbGmsClient.java
+++ appserver/orb/orb-iiop/src/main/java/org/glassfish/enterprise/iiop/impl/IiopFolbGmsClient.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright [2017-2018] [Payara Foundation and/or its affiliates]
+// Portions Copyright 2017-2026 Payara Foundation and/or its affiliates
 
 package org.glassfish.enterprise.iiop.impl;
 
@@ -268,17 +268,21 @@ public class IiopFolbGmsClient implements ClusterListener {
     }
 
     private int resolvePort( Server server, IiopListener listener ) {
-        fineLog( "resolvePort: server {0} listener {1}", server, listener ) ;
+        fineLog( "resolvePort: server {0} listener {1}", server.getName(), listener .getId()) ;
 
         IiopListener ilRaw = GlassFishConfigBean.getRawView( listener ) ;
-        fineLog( "resolvePort: ilRaw {0}", ilRaw ) ;
+
 
         PropertyResolver pr = new PropertyResolver( domain, server.getName() ) ;
-        fineLog( "resolvePort: pr {0}", pr ) ;
+
 
         String port = pr.getPropertyValue( ilRaw.getPort() ) ;
         fineLog( "resolvePort: port {0}", port ) ;
 
+        if (port == null) {
+            fineLog("resolvePort: getPropertyValue returned null, setting port to raw value {0}", ilRaw.getPort());
+            port = ilRaw.getPort();
+        }
         return Integer.parseInt(port) ;
     }
 
