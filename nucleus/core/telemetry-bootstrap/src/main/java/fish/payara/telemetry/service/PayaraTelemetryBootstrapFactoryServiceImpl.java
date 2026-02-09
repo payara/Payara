e878a66cc2afd338236f5ca99025e21bc5f0ddbc
Andrew Pielage <pandrex247@hotmail.com>
2026-02-09 14:42:37 +0000

    Merge pull request #7927 from breakponchito/FISH-12815-creating-module-to-start-sdk-telemetry
    FISH-12815: adding module to register server runtime telemetry

---
+++ nucleus/core/telemetry-bootstrap/src/main/java/fish/payara/telemetry/service/PayaraTelemetryBootstrapFactoryServiceImpl.java
@@ -0,0 +1,115 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *    Copyright (c) [2026] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *     The contents of this file are subject to the terms of either the GNU
+ *     General Public License Version 2 only ("GPL") or the Common Development
+ *     and Distribution License("CDDL") (collectively, the "License").  You
+ *     may not use this file except in compliance with the License.  You can
+ *     obtain a copy of the License at
+ *     https://github.com/payara/Payara/blob/main/LICENSE.txt
+ *     See the License for the specific
+ *     language governing permissions and limitations under the License.
+ *
+ *     When distributing the software, include this License Header Notice in each
+ *     file and include the License file at legal/OPEN-SOURCE-LICENSE.txt.
+ *
+ *     GPL Classpath Exception:
+ *     The Payara Foundation designates this particular file as subject to the "Classpath"
+ *     exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *     file that accompanied this code.
+ *
+ *     Modifications:
+ *     If applicable, add the following below the License Header, with the fields
+ *     enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyright [year] [name of copyright owner]"
+ *
+ *     Contributor(s):
+ *     If you wish your version of this file to be governed by only the CDDL or
+ *     only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *     elects to include this software in this distribution under the [CDDL or GPL
+ *     Version 2] license."  If you don't indicate a single choice of license, a
+ *     recipient has the option to distribute your version of this file under
+ *     either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *     its licensees as provided above.  However, if you add GPL Version 2 code
+ *     and therefore, elected the GPL Version 2 license, then the option applies
+ *     only if the new code is made subject to such option by the copyright
+ *     holder.
+ */
+package fish.payara.telemetry.service;
+
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.glassfish.common.util.Constants;
+import org.glassfish.hk2.api.Rank;
+import org.glassfish.hk2.runlevel.RunLevel;
+import org.glassfish.internal.api.InitRunLevel;
+import org.jvnet.hk2.annotations.Service;
+
+@Service(name = "telemetry-runtime-config-service")
+@RunLevel(InitRunLevel.VAL)
+@Rank(Constants.IMPORTANT_RUN_LEVEL_SERVICE)
+public class PayaraTelemetryBootstrapFactoryServiceImpl implements PayaraTelemetryBootstrapFactoryService {
+    
+    private OpenTelemetry runtimeSdk = null;
+    
+    private static final String OTEL_PROPERTIES_PREFIX = "otel";
+    private static final String OTEL_SYSTEM_PROPERTY_NAME = "otel.sdk.disabled";
+    private static final String OTEL_ENVIRONMENT_PROPERTY_NAME = "OTEL_SDK_DISABLED";
+
+    @PostConstruct
+    public void init() {
+        runtimeSdk = createTelemetryRuntimeInstance();
+    }
+
+    @Override
+    public OpenTelemetry createTelemetryRuntimeInstance() {
+        if (!isOtelEnabled()) {
+            // need to read otel properties
+            final Map<String, String> props = new HashMap<>(readOtelProperties());
+            return AutoConfiguredOpenTelemetrySdk.builder().setServiceClassLoader(Thread.currentThread().getContextClassLoader())
+                    .disableShutdownHook()
+                    .addPropertiesSupplier(() -> props).build().getOpenTelemetrySdk();
+        }
+        return runtimeSdk;
+    }
+
+    @Override
+    public OpenTelemetry getAvailableReference() {
+        return runtimeSdk;
+    }
+
+    private boolean isOtelEnabled() {
+        if (System.getProperty("otel.sdk.disabled") != null) {
+            return !"false".equalsIgnoreCase(System.getProperty(OTEL_SYSTEM_PROPERTY_NAME, "true"));
+        }
+
+        if (System.getenv("OTEL_SDK_DISABLED") != null) {
+            return !"false".equalsIgnoreCase(System.getenv(OTEL_ENVIRONMENT_PROPERTY_NAME));
+        }
+        return true;
+    }
+
+    private Map<String, String> readOtelProperties() {
+        Map<String, String> props = new HashMap<>();
+        Map<String, String> systemOtelPropsAvailable = System.getProperties().entrySet().stream()
+                .filter(e -> ((String) e.getKey()).startsWith(OTEL_PROPERTIES_PREFIX))
+                .map(e -> Map.entry((String) e.getKey(), (String) e.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Map<String, String> environmentOtelPropsAvailable = System.getenv().entrySet().stream()
+                .filter(e -> e.getKey().startsWith(OTEL_PROPERTIES_PREFIX))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        props.putAll(systemOtelPropsAvailable);
+        props.putAll(environmentOtelPropsAvailable);
+        return props;
+    }
+
+
+}
