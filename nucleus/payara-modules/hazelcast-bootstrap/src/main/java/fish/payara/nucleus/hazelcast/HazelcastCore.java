f0bb1f7946ec2a955383cf55fbc8a24be88b5ba1
Andrew Pielage <pandrex247@hotmail.com>
2026-03-05 16:07:22 +0000

    FISH-12994 Add the instance name to the MemberEvent config and use it if value is not present in the live instance config map. Also makes the group name follow the same semantics, and fixes the instance group attribute not being set

--- nucleus/payara-modules/hazelcast-bootstrap/src/main/java/fish/payara/nucleus/hazelcast/HazelcastCore.java
+++ nucleus/payara-modules/hazelcast-bootstrap/src/main/java/fish/payara/nucleus/hazelcast/HazelcastCore.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2016-2024 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016-2026 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -581,6 +581,14 @@ public class HazelcastCore implements EventListener, ConfigListener {
             try {
                 // remove "CP Subsystem Unsafe" warning on startup
                 cpSubsystemLogger.setLevel(Level.SEVERE);
+
+                if (env.isMicro()) {
+                    if (Boolean.valueOf(configuration.getGenerateNames()) || memberName == null) {
+                        memberName = PayaraMicroNameGenerator.generateName();
+                    }
+                }
+
+                config.getMemberAttributeConfig().setAttribute(INSTANCE_ATTRIBUTE, memberName);
                 config.getMemberAttributeConfig().setAttribute(INSTANCE_GROUP_ATTRIBUTE, memberGroup);
                 theInstance = Hazelcast.newHazelcastInstance(config);
                 autoPromoteCPMembers(config);
@@ -589,7 +597,6 @@ public class HazelcastCore implements EventListener, ConfigListener {
             }
             if (env.isMicro()) {
                 if (Boolean.valueOf(configuration.getGenerateNames()) || memberName == null) {
-                    memberName = PayaraMicroNameGenerator.generateName();
                     Set<com.hazelcast.cluster.Member> clusterMembers = theInstance.getCluster().getMembers();
 
                     // If the instance name was generated, we need to compile a list of all the instance names in use within
@@ -609,7 +616,6 @@ public class HazelcastCore implements EventListener, ConfigListener {
                     if (takenNames.contains(memberName)) {
                         memberName = PayaraMicroNameGenerator.generateUniqueName(takenNames,
                                 theInstance.getCluster().getLocalMember().getUuid());
-                        setAttribute(theInstance.getCluster().getLocalMember().getUuid(), HazelcastCore.INSTANCE_ATTRIBUTE, memberName);
                     }
                 }
             }
@@ -637,7 +643,7 @@ public class HazelcastCore implements EventListener, ConfigListener {
                     if (map == null) {
                         map = new HashMap<>();
                     }
-                    map.put(HazelcastCore.INSTANCE_ATTRIBUTE, memberName);
+                    map.put(key, value);
                     return map;
                 });
     }
