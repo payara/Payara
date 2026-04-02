072e9a752d67380c6b656aff3c28a993913beed2
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2026-04-02 10:56:04 +0100

    FISH-13162: PathItemImpl sets the absolute path

--- appserver/payara-appserver-modules/microprofile/openapi/src/main/java/fish/payara/microprofile/openapi/impl/model/PathItemImpl.java
+++ appserver/payara-appserver-modules/microprofile/openapi/src/main/java/fish/payara/microprofile/openapi/impl/model/PathItemImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) [2018-2023] Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018-2026 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -86,7 +86,12 @@ public class PathItemImpl extends ExtensibleImpl<PathItem> implements PathItem {
 
     @Override
     public void setRef(String ref) {
-        this.ref = ref;
+        if (!ref.startsWith("#/")) {
+            this.ref = "#/components/pathItems/" + ref;
+        }
+        else {
+            this.ref = ref;
+        }
     }
 
     @Override
