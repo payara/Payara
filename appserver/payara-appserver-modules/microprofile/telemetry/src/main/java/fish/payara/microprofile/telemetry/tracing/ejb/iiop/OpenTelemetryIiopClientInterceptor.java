c03ed15b857434018f4b7e2976d5f1ba91d7e52d
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2026-06-19 19:53:09 -0600

    FISH-13751: re-enabling OpenTelemetry IIOP client and Server trace

---
+++ appserver/payara-appserver-modules/microprofile/telemetry/src/main/java/fish/payara/microprofile/telemetry/tracing/ejb/iiop/OpenTelemetryIiopClientInterceptor.java
@@ -0,0 +1,125 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2020-2026 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://github.com/payara/Payara/blob/main/LICENSE.txt
+ * See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at legal/OPEN-SOURCE-LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * The Payara Foundation designates this particular file as subject to the "Classpath"
+ * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package fish.payara.microprofile.telemetry.tracing.ejb.iiop;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.omg.CORBA.LocalObject;
+import org.omg.IOP.ServiceContext;
+import org.omg.PortableInterceptor.ClientRequestInfo;
+import org.omg.PortableInterceptor.ClientRequestInterceptor;
+import org.omg.PortableInterceptor.ForwardRequest;
+
+/**
+ * IIOP Client Interceptor for propagating OpenTracing SpanContext to Payara Server.
+ *
+ * @author Andrew Pielage <andrew.pielage@payara.fish>
+ */
+public class OpenTelemetryIiopClientInterceptor extends LocalObject implements ClientRequestInterceptor {
+
+    private static final Logger logger = Logger.getLogger(OpenTelemetryIiopClientInterceptor.class.getName());
+    static final int OPENTELEMETRY_IIOP_ID = 3226428;
+    
+    @Override
+    public void send_request(ClientRequestInfo clientRequestInfo) throws ForwardRequest {
+        Span currentSpan = Span.current();
+        if (!currentSpan.getSpanContext().isValid()) {
+            return;
+        }
+
+        OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
+
+        HashMap<String, String> contextMap = new HashMap<>();
+        openTelemetry.getPropagators().getTextMapPropagator()
+                .inject(Context.current(), contextMap, HashMap::put);
+
+        if (contextMap.isEmpty()) {
+            return;
+        }
+
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
+            out.writeObject(contextMap);
+            out.flush();
+            clientRequestInfo.add_request_service_context(
+                    new ServiceContext(OPENTELEMETRY_IIOP_ID, bos.toByteArray()), true);
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Exception propagating OTel span context over IIOP");
+        }
+    }
+
+    @Override
+    public void send_poll(ClientRequestInfo clientRequestInfo) {
+
+    }
+
+    @Override
+    public void receive_reply(ClientRequestInfo clientRequestInfo) {
+
+    }
+
+    @Override
+    public void receive_exception(ClientRequestInfo clientRequestInfo) throws ForwardRequest {
+
+    }
+
+    @Override
+    public void receive_other(ClientRequestInfo clientRequestInfo) throws ForwardRequest {
+
+    }
+
+    @Override
+    public String name() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
