b40864159a795d676eed6b20a71680c1a8d1d728
adrian <astoica@gmail.com>
2026-07-21 08:46:35 +0300

    FISH-14029: fixed concurrency package imports

--- appserver/tests/payara-samples/samples/concurrency/src/test/java/fish/payara/sample/concurrency/cronetrigger/CronTriggerRestIT.java
+++ appserver/tests/payara-samples/samples/concurrency/src/test/java/fish/payara/sample/concurrency/cronetrigger/CronTriggerRestIT.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) [2022] Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) [2022-2026] Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,7 +37,10 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package fish.payara.sample.concurrency.crontrigger;
+package fish.payara.sample.concurrency.cronetrigger;
+
+import fish.payara.sample.concurrency.crontrigger.CronTriggerRest;
+import fish.payara.sample.concurrency.crontrigger.CronTriggerRestApplication;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
