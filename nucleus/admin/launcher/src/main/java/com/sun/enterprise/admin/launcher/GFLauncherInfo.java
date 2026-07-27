6c259ccf97cda40817b4cdb893aac10c3254ccd4
Kalin Chan <kchan2002@outlook.com>
2026-07-27 14:09:19 +0100

    FISH-13913 Update copyright

--- nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherInfo.java
+++ nucleus/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherInfo.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// Portions Copyright 2016-2026 Payara Foundation
+// Portions Copyright 2016-2026 Payara Foundation and/or affiliates
 
 package com.sun.enterprise.admin.launcher;
 
